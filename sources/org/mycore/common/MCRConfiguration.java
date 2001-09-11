5104056fdf48a7cf095484d5db54384c67b95c8a
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2001-09-11 14:45:56 +0000

    Fixed comment pointing to the license.txt file.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@203 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/MCRConfiguration.java
+++ sources/org/mycore/common/MCRConfiguration.java
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program, in a file called gpl.txt or license.txt.
+ * along with this program, in a file called license.txt.
  * If not, write to the Free Software Foundation Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
  *
