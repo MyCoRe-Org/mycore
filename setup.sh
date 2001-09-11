e2e07b46c0d1606450d53f8b2399143774033066
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2001-09-11 11:17:19 +0000

    Change the environment to ANT 1.4.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@198 7089d063-bc14-496d-ac90-53de1872eb6c

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@
 # Configuration part
 #
 
-export ANT_HOME=/usr/local/src/jakarta-ant-1.3
+export ANT_HOME=/usr/local/src/jakarta-ant-1.4
 export CM7_HOME=/usr/lpp/cmb
 export JAVA_HOME=/usr/java_dev2/jre
 export MYCORE_HOME=/dlwww/mycore
 export XERCES_HOME=/usr/local/src/xerces-1_4_3
-#export XERCES_HOME=/usr/local/src/xerces-2_0_0_beta
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/bin:$JAVA_HOME/bin/classic
 export LIBPATH=$LIBPATH:$LD_LIBRARY_PATH
@@ -23,10 +22,9 @@ CLASSPATH=$CLASSPATH:$CM7_HOME/lib/cmbjdbc30.jar
 CLASSPATH=$CLASSPATH:$CM7_HOME/lib/cmbfed30.jar
 CLASSPATH=$CLASSPATH:$XERCES_HOME/xerces.jar
 CLASSPATH=$CLASSPATH:$XERCES_HOME/xercesSamples.jar
+CLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant.jar
+CLASSPATH=$CLASSPATH:$ANT_HOME/lib/crimson.jar
 CLASSPATH=$CLASSPATH:$ANT_HOME/lib/jaxp.jar
-CLASSPATH=$CLASSPATH:$ANT_HOME/lib/parser.jar
-CLASSPATH=$CLASSPATH:$ANT_HOME/dist/lib/ant.jar
-CLASSPATH=$CLASSPATH:$ANT_HOME/dist/lib/optional.jar
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/classes
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/demo_dublin_core/classes
 export CLASSPATH
