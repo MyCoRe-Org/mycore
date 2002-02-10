35135f13cd5e63d6e23ccc702b3c9694a6a5ef9c
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2002-02-10 15:28:18 +0000

    Moved the functionality of MCRUserMgrTest to the new MCRCommandLineInterface. The class MCRUserCommands now contains the implementation methods for the user and group management classes. I had to turn schema validation off in mycore.properties in order to get the user manager stuff work.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@304 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/cli/MCRCommandLineInterface.java
+++ sources/org/mycore/frontend/cli/MCRCommandLineInterface.java
@@ -71,6 +71,10 @@ public class MCRCommandLineInterface
     //  ( config.getInstanceOf( "MCR.parser_class_name" ) );
     // ToDo: dom = parser.parseURI( filename ); usw.
 
+    // **************************************
+    // Built-in commands 
+    // **************************************
+
     knownCommands[ numCommands++ ] = new MCRCommand( 
       "process {0}", 
       "mycore.commandline.MCRCommandLineInterface.readCommandsFile String" );
@@ -86,6 +90,53 @@ public class MCRCommandLineInterface
     knownCommands[ numCommands++ ] = new MCRCommand(
       "! {0}",
       "mycore.commandline.MCRCommandLineInterface.executeShellCommand String" );
+
+    // **************************************
+    // Commands for user and group management
+    // **************************************
+
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "load users or groups from file {0}",
+      "mycore.commandline.MCRUserCommands.loadFromFile String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "check users to groups consistency",
+      "mycore.commandline.MCRUserCommands.checkConsistency" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "delete user {0}",
+      "mycore.commandline.MCRUserCommands.deleteUser String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "delete group {0}",
+      "mycore.commandline.MCRUserCommands.deleteGroup String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "list all users",
+      "mycore.commandline.MCRUserCommands.listAllUsers" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "list all groups",
+      "mycore.commandline.MCRUserCommands.listAllGroups" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "login as user {0} with password {1}",
+      "mycore.commandline.MCRUserCommands.login String String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print group {0} as xml",
+      "mycore.commandline.MCRUserCommands.printGroupAsXML String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print group cache info",
+      "mycore.commandline.MCRUserCommands.printGroupCacheInfo" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print group {0} info",
+      "mycore.commandline.MCRUserCommands.printGroupInfo String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print user {0} as xml",
+      "mycore.commandline.MCRUserCommands.printUserAsXML String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print user cache info",
+      "mycore.commandline.MCRUserCommands.printUserCacheInfo" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "print user {0} info",
+      "mycore.commandline.MCRUserCommands.printUserInfo String" );
+    knownCommands[ numCommands++ ] = new MCRCommand(
+      "set password for user {0} to {1}",
+      "mycore.commandline.MCRUserCommands.setPassword String String" );
   }   
 
  /** 
@@ -169,7 +220,19 @@ public class MCRCommandLineInterface
       
       System.out.println( "Command not understood. Enter 'help' to get a list of commands." );
     }
-    catch( Exception ex ){ System.out.println( ex ); }
+    catch( Exception ex )
+    { 
+      if( ex instanceof InvocationTargetException )
+      {
+        Throwable t = ( (InvocationTargetException)ex ).getTargetException();
+        System.out.println( t );
+      }
+      else
+      {
+        System.out.println( ex ); 
+        System.out.println( ex.getMessage() );
+      }
+    }
   }
 
  /** 
@@ -254,3 +317,61 @@ public class MCRCommandLineInterface
     System.exit( 0 );
   }
 }
+
+/*
+   These are the original help text from Detlev's MCRUserTest class.
+   They will be included in the help system in the near future.
+
+      System.out.println("\n syntax: checkConsistency");
+      System.out.println("\n This command checks the consistency between the user and group objects.");
+      System.out.println(" MCRUserMgr.checkUserGroupConsistency(). is invoked.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: deleteGroup [groupID]");
+      System.out.println("\n This command takes a group ID as a parameter and passes this to ");
+      System.out.println(" MCRUserMgr.deleteGroup(). See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: deleteUser [userID]");
+      System.out.println("\n This command takes a user ID as a parameter and passes this to ");
+      System.out.println(" MCRUserMgr.deleteUser(). See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: listAllUsers");
+      System.out.println("\n This command invokes MCRUserMgr.getAllUserIDs() and lists all");
+      System.out.println(" user IDs of the system. See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: listAllGroups");
+      System.out.println("\n This command invokes MCRUserMgr.getAllGroupIDs() and lists all");
+      System.out.println(" group IDs of the system. See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: loadFromFile [file|directory]");
+      System.out.println("\n This command takes a filename or a directory as a parameter and passes");
+      System.out.println(" this to MCRUserMgr.loadUsersOrGroupsFromFile(). See the javadoc documentation ");
+      System.out.println(" of MCRUserMgr.");
+      System.out.println("\n syntax: login");
+      System.out.println("\n This command asks for a userID and a password and passes this to");
+      System.out.println(" MCRUserMgr.login(). It returns information whether the login attempt");
+      System.out.println(" was successful or not. See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printGroupAsXML [groupID]");
+      System.out.println("\n This command prints out all group information as xml representation");
+      System.out.println(" MCRUserMgr.getGroupAsXML() is invoked.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printGroupCacheInfo");
+      System.out.println("\n This command prints out information about the group cache.");
+      System.out.println(" MCRUserMgr.getGroupCacheInfo() is invoked.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printGroupInfo [groupID]");
+      System.out.println("\n This command takes a groupID as a parameter and passes this to");
+      System.out.println(" MCRUserMgr.getGroupInfo(). It then prints out the returned information.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printUserAsXML [userID]");
+      System.out.println("\n This command prints out all user information as xml representation");
+      System.out.println(" MCRUserMgr.getUserAsXML() is invoked.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printUserCacheInfo");
+      System.out.println("\n This command prints out information about the user cache.");
+      System.out.println(" MCRUserMgr.getUserCacheInfo() is invoked.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: printUserInfo [userID]");
+      System.out.println("\n This command takes a userID as a parameter and passes this to");
+      System.out.println(" MCRUserMgr.getUserInfo(). It then prints out the returned information.");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+      System.out.println("\n syntax: setPassword [userID]");
+      System.out.println("\n This command takes a userID as a parameter and asks for a password.");
+      System.out.println(" It then passes the information to MCRUserMgr.setPassword().");
+      System.out.println(" See the javadoc documentation of MCRUserMgr.");
+*/
---
+++ sources/org/mycore/frontend/cli/MCRUserCommands.java
@@ -0,0 +1,150 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package mycore.commandline;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import mycore.common.*;
+import mycore.user.*;
+import org.w3c.dom.DOMException;
+
+/**
+ * @author Detlev Degenhardt
+ * @author Frank Lützenkirchen
+ * @version $Revision$ $Date$
+ */
+public class MCRUserCommands
+{
+  /** This method invokes MCRUserMgr.checkUserGroupConsistency() */
+  public static void checkConsistency() throws Exception
+  {
+    MCRUserMgr.instance().checkUserGroupConsistency();
+  }
+
+  /**
+   * This method invokes MCRUserMgr.deleteGroup()
+   * @param groupID the ID of the group which will be deleted
+   */
+  public static void deleteGroup(String groupID) throws Exception
+  {
+    MCRUserMgr.instance().deleteGroup(groupID);
+    System.out.println("Group ID " + groupID + " deleted!");
+  }
+
+  /**
+   * This method invokes MCRUserMgr.deleteUser()
+   * @param userID the ID of the user which will be deleted
+   */
+  public static void deleteUser(String userID) throws Exception
+  {
+    MCRUserMgr.instance().deleteUser(userID);
+    System.out.println("User ID " + userID + " deleted!");
+  }
+
+  /** This method invokes MCRUserMgr.getAllUserIDs() */
+  public static void listAllUsers() throws Exception
+  {
+    Vector users = new Vector(MCRUserMgr.instance().getAllUserIDs());
+    System.out.println();
+    for (int i=0; i<users.size(); i++)
+      System.out.println(users.elementAt(i));
+  }
+
+  /** This method invokes MCRUserMgr.getAllGroupIDs() */
+  public static void listAllGroups() throws Exception
+  {
+    Vector groups = new Vector(MCRUserMgr.instance().getAllGroupIDs());
+    System.out.println();
+    for (int i=0; i<groups.size(); i++)
+      System.out.println(groups.elementAt(i));
+  }
+
+  /**
+   * This method invokes MCRUserMgr.loadUsersFromFile()
+   * @param filename name of a file or directory containing user or group
+   *                 information in XML files
+   */
+  public static void loadFromFile(String filename) throws Exception
+  {
+    System.out.println("Loading user data from file/directory: "+filename);
+    MCRUserMgr.instance().loadUsersOrGroupsFromFile(filename);
+  }
+
+  /** This method asks for a user ID and a password and then invokes MCRUserMgr.login() */
+  public static void login( String userID, String password ) throws Exception
+  {
+    if (MCRUserMgr.instance().login(userID, password))
+      System.out.println("User successfully logged in!");
+    else
+      System.out.println("Access denied! Wrong password?");
+  }
+
+  /**
+   * This method calls MCRUserMgr.getGroupAsXML()
+   * @param groupID the ID of the group for which the XML-representation is needed
+   */
+  public static void printGroupAsXML(String groupID) throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getGroupAsXML(groupID, "\n")); }
+
+  /**
+   * This method calls MCRUserMgr.getGroupInfo()
+   * @param groupID the ID of the group for which the group information is needed
+   */
+  public static void printGroupInfo(String groupID) throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getGroupInfo(groupID)); }
+
+  /** This method invokes MCRUserMgr.getGroupCacheInfo() */
+  public static void printGroupCacheInfo() throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getGroupCacheInfo()); }
+
+  /**
+   * This method calls MCRUserMgr.getUserAsXML()
+   * @param userID the ID of the user for which the XML-representation is needed
+   */
+  public static void printUserAsXML(String userID) throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getUserAsXML(userID, "\n")); }
+
+  /**
+   * This method calls MCRUserMgr.getUserInfo()
+   * @param userID the ID of the user for which the user information is needed
+   */
+  public static void printUserInfo(String userID) throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getUserInfo(userID)); }
+
+  /** This method invokes MCRUserMgr.getUserCacheInfo() */
+  public static void printUserCacheInfo() throws Exception
+  { System.out.println("\n"+MCRUserMgr.instance().getUserCacheInfo()); }
+
+  /**
+   * This method invokes MCRUserMgr.setPassword()
+   * @param userID the ID of the user for which the password will be set
+   */
+  public static void setPassword(String userID,String password) throws Exception
+  {
+    MCRUserMgr.instance().setPassword(userID, password);
+  }
+}
+
