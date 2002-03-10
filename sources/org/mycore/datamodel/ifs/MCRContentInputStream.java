63189f867d5d3d65c31ff31c38e195920508037e
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2002-03-10 18:42:44 +0000

    Added MCRContentInputStream to MyCoRe. This class replaces miless.datamodel.ContentInputStream. It provides functionality for counting the number of bytes read, building an MD5 checksum and pre-reading the file header for content type detection.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@358 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/datamodel/ifs/MCRContentInputStream.java
@@ -0,0 +1,150 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package mycore.ifs;
+
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import mycore.common.*;
+
+/**
+ * This input stream is used by the MyCoRe filesystem classes to
+ * read the content of a file and import it into the System. 
+ * MCRContentInputStream provides the header of the file that is read 
+ * (the first 64k) for content type detection purposes, counts the number 
+ * of bytes read and builds an MD5 checksum String while content goes 
+ * through this input stream.
+ *
+ * @author Frank Lützenkirchen
+ * @version $Revision$ $Date$
+ */
+public class MCRContentInputStream extends FilterInputStream
+{
+  /** The number of bytes that will be read for content type detection */
+  protected final static int headerSize = 65536;
+
+  /** The MD5 checksum of all bytes read through this stream */
+  protected MessageDigest digest = null;
+
+  /** The total number of bytes read so far */
+  protected int length;
+
+  /** The header of the file read */
+  protected byte[] header;
+
+  /** 
+   * Constructs a new MCRContentInputStream
+   *
+   * @param in the InputStream to read from
+   * @throws MCRConfigurationException if java classes supporting MD5 checksums are not found
+   */
+  public MCRContentInputStream( InputStream in )
+    throws IOException
+  {
+    super( null );
+
+    MCRArgumentChecker.ensureNotNull( in, "InputStream" );
+
+    try{ digest = MessageDigest.getInstance( "MD5" ); }
+    catch( NoSuchAlgorithmException exc )
+    {
+      String msg = "Could not find java classes that support MD5 checksum algorithm";
+      throw new MCRConfigurationException( msg, exc ); 
+    }
+
+    DigestInputStream      dis = new DigestInputStream( in, digest );
+    MCRBlockingInputStream bis = new MCRBlockingInputStream( dis, headerSize );
+
+    byte[] buffer = new byte[ headerSize ];
+
+    bis.mark( headerSize + 1 );
+    int num = bis.read( buffer );
+    bis.reset();    
+
+    header = new byte[ Math.max( 0, num ) ];
+    if( num > 0 ) System.arraycopy( buffer, 0, header, 0, num ); 
+
+    this.in = bis;
+  }
+
+  public int read()
+    throws IOException
+  {
+    int b = super.read();
+    if( b != -1 ) length++;
+    return b;
+  }
+
+  public int read( byte[] buf, int off, int len )
+    throws IOException
+  {
+    len = super.read( buf, off, len );
+    if( len != -1 ) length += len;
+    return len;
+  }
+
+  /**
+   * Returns the first 64 k of the underlying input stream. This is used for
+   * content type detection during file import into MyCoRe.
+   *
+   * @return the first 64 k of the input stream
+   */
+  public byte[] getHeader()
+  { return header; }
+
+  /** 
+   * Returns the number of bytes read so far 
+   *
+   * @return the number of bytes read 
+   */
+  public int getLength()
+  { return length; }
+
+  /** 
+   * Returns the MD5 message digest that has been built during reading of
+   * the underlying input stream.
+   *
+   * @return the MD5 message digest checksum of all bytes that have been read
+   */
+  public byte[] getMD5()
+  { return digest.digest(); }
+
+  /**
+   * Returns the MD5 checksum as a String
+   *
+   * @return the MD5 checksum as a String of hex digits
+   */
+  public String getMD5String()
+  {
+    byte[] bytes = digest.digest();
+    StringBuffer sb = new StringBuffer();
+    for( int i = 0; i < bytes.length; i++ )
+    {
+      String sValue = "0" + Integer.toHexString( (int) bytes[ i ] );
+      sb.append( sValue.substring( sValue.length() - 2 ) );
+    }
+    return sb.toString();
+  }
+}
