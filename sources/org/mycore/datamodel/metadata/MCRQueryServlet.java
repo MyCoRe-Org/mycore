df044069c190687ab589e9c1397139b5d90eee41
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2002-04-22 08:44:00 +0000

    Added a first version of MCRQueryServlet, the servlet that takes a query from a HTML form, executes the query and sends the result list back as XML. MCRLayoutServlet then takes the XML and renders it to HTML using a XSL stylesheet.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@398 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/datamodel/metadata/MCRQueryServlet.java
@@ -0,0 +1,80 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package mycore.datamodel;
+
+import mycore.common.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+
+/**
+ * This servlet provides a web interface to query
+ * the datastore using XQueries and deliver the result list
+ *
+ * @author Frank Lützenkirchen
+ * @version $Revision$ $Date$
+*/
+public class MCRQueryServlet extends HttpServlet 
+{
+  public void doGet( HttpServletRequest  request, 
+                     HttpServletResponse response )
+    throws IOException, ServletException
+  {  
+    String query = request.getParameter( "query" );
+    String type  = request.getParameter( "type"  );
+    String where = request.getParameter( "where" );
+
+    System.out.println( "query = " + query );
+    System.out.println( "type  = " + type  );
+    System.out.println( "where = " + where );
+
+    if( where == null ) where = "local";
+    if( query == null ) query = "";
+    if( type  == null ) return; 
+
+    try
+    {
+      MCRQueryResult result = new MCRQueryResult( type );
+      result.setFromQuery( where, query );
+
+      String xml = result.getResultArray().exportAll();
+
+      Reader in = new StringReader( xml );
+
+      org.jdom.input.SAXBuilder builder = new org.jdom.input.SAXBuilder();
+      org.jdom.Document jdom = builder.build( in );
+
+      request.setAttribute( "jdom", jdom );
+      RequestDispatcher rd = getServletContext().getNamedDispatcher( "LayoutServlet" );
+      rd.forward( request, response );
+    }
+    catch( Exception ex )
+    {
+      System.out.println( ex.getClass().getName() );
+      System.out.println( ex );
+    }
+  }
+}
+
