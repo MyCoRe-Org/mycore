2f2aaf851b334189145eb4aa840d67f61205bb6b
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2002-04-30 06:32:51 +0000

    Add a full hostlist support.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@423 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/metadata/MCRQueryServlet.java
+++ sources/org/mycore/datamodel/metadata/MCRQueryServlet.java
@@ -35,35 +35,118 @@ import mycore.common.*;
  * the datastore using XQueries and deliver the result list
  *
  * @author Frank Lützenkirchen
+ * @author Jens Kupferschmidt
  * @version $Revision$ $Date$
 */
 public class MCRQueryServlet extends HttpServlet 
 {
+  // The list of hosts from the configuration
+  private ArrayList remoteAliasList = null;
+
+  /**
+   * The methode initialized the servlet and read the host list
+   * from the mycore.property configuration.
+   *
+   * @exception ServletException for errors from the servlet engine.
+   **/
+  public void init() throws ServletException
+    {
+    // read host list from configuration
+    MCRConfiguration config = MCRConfiguration.instance();
+    String hostconf = config.getString("MCR.communication_hostaliases",
+      "local");
+    remoteAliasList = new ArrayList();
+    int i = 0;
+    int j = hostconf.length();
+    int k = 0;
+    while (k!=-1) {
+      k = hostconf.indexOf(",",i);
+      if (k==-1) {
+        remoteAliasList.add(hostconf.substring(i,j)); }
+      else {
+        remoteAliasList.add(hostconf.substring(i,k)); i = k+1; }
+      }
+    }
+
+  /**
+   * The methode get a request and resolve them to a output.
+   *
+   * @param request the HTTP request instance
+   * @param response the HTTP response instance
+   * @exception IOException for java I/O errors.
+   * @exception ServletException for errors from the servlet engine.
+   **/
   public void doGet( HttpServletRequest  request, 
                      HttpServletResponse response )
     throws IOException, ServletException
-  {  
+    {  
     String mode  = request.getParameter( "mode"  );
     String query = request.getParameter( "query" );
     String type  = request.getParameter( "type"  );
-    String where = request.getParameter( "where" );
+    String host  = request.getParameter( "hosts" );
 
-    System.out.println( "mode  = " + mode  );
-    System.out.println( "query = " + query );
-    System.out.println( "type  = " + type  );
-    System.out.println( "where = " + where );
+    //System.out.println( "mode  = " + mode  );
+    //System.out.println( "query = " + query );
+    //System.out.println( "type  = " + type  );
+    //System.out.println( "hosts = " + host  );
 
     if( mode  == null ) mode  = "ResultList";
-    if( where == null ) where = "local";
+    if( host  == null ) host  = "local";
     if( query == null ) query = "";
     if( type  == null ) return; 
 
-    MCRConfiguration config = MCRConfiguration.instance();
+    // build host list from host string
+    ArrayList hostAliasList = new ArrayList();
+    int i = 0;
+    int j = host.length();
+    int k = 0;
+    int n = 0;
+    while (n!=-1) {
+      n = host.indexOf(",",i);
+      String hostname = "";
+      if (n==-1) { hostname = host.substring(i,j); }
+      else { hostname = host.substring(i,n); i = n+1; }
+      if (hostname.equals("local")) {
+        k = -1;
+        for (int l=0;l<hostAliasList.size();l++) {
+          if (((String)hostAliasList.get(l)).equals("local")) { 
+            k = 0; break; }
+          }
+        if (k==-1) { hostAliasList.add("local"); }
+        }
+      else {
+        if (hostname.equals("remote")) {
+          for (int m=0;m<remoteAliasList.size();m++) {
+            k = -1;
+            for (int l=0;l<hostAliasList.size();l++) {
+              if (((String)hostAliasList.get(l))
+                .equals(remoteAliasList.get(m))) { k = 0; break; }
+              }
+            if (k==-1) { hostAliasList.add(remoteAliasList.get(m)); }
+            }
+          }
+        else {
+          k = -1;
+          for (int m=0;m<remoteAliasList.size();m++) {
+            if (((String)remoteAliasList.get(m)).equals(hostname)) { 
+              k = 0; break; } }
+          if (k==-1) {
+            throw new MCRException( "The host name is not in the list."); }
+          k = -1;
+          for (int l=0;l<hostAliasList.size();l++) {
+            if (((String)hostAliasList.get(l)).equals(hostname)) { 
+              k = 0; break; } }
+          if (k==-1) { hostAliasList.add(hostname); }
+          }
+        }
+      }
 
-    ArrayList hostAliasList = new ArrayList
-      ( config.getInt( "MCR.communication_max_hosts", 3 ) );
-    hostAliasList.add( where );
+    // print list for debug
+    //for (i=0;i<hostAliasList.size();i++) {
+    //  System.out.println("Host : "+hostAliasList.get(i)); }
+    //System.out.println();
 
+    // prepare the stylesheet name
     String style = mode + "-" + type;
 
     try
