ba351ec8166a15b02582fd18c54a37cff6ef3678
Werner Gresshoff <mcrwgres@duepub-1.zim.uni-duisburg-essen.de>
2003-04-08 13:20:51 +0000

    Added some methods to transform a JDOM-Document with a given Stylesheet and some optional parameters to an OutputStream.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@1335 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/xml/MCRXSLTransformation.java
+++ sources/org/mycore/common/xml/MCRXSLTransformation.java
@@ -25,26 +25,205 @@
 package org.mycore.common.xml;
 
 import java.io.File;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.Properties;
 
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.output.SAXOutputter;
 import org.jdom.transform.JDOMResult;
 import org.jdom.transform.JDOMSource;
 
+import org.mycore.common.MCRConfiguration;
+
 /**
  * This class implements XSLTransformation functions to be used in all other
- * MyCoRe packages.
+ * MyCoRe packages. The class is implemented as singleton and should be very
+ * easy to use. So here is an example:
+ * <PRE>
+ * // Get an instance of the class
+ * MCRXSLTransformation transformation = MCRXSLTransformation getInstance();
+ * 
+ * // Get the template: myStylesheet could be a String (i.e. a filename),
+ * // a File or a StreamSource
+ * Templates templates = transformation(myStylesheet);
  *
+ * // Next, you are in need of a TransformerHandler:
+ * TransformerHandler th = getTransformerHandler(templates);
+ * 
+ * // Now you are able to set some properties (if you want!):
+ * Properties parameters = new Properties();
+ * ...
+ * setParameters(th, parameters);
+ * 
+ * // Finally, you need an OutputStream and might get at work:
+ * OutputStream out = response.getOutputStream();
+ * transform(jdom, th, out);
+ * 
+ * // You might also want to transform into something different,
+ * // perhaps a ZIP-File:
+ * OutputStream out = new ZipOutputStream(response.getOutputStream());
+ * ((ZipOutputStream) out).setLevel(Deflater.BEST_COMPRESSION);
+ * ZipEntry ze = new ZipEntry("_index.htm");
+ * ((ZipOutputStream) out).putNextEntry(ze);
+ * ...
+ * 
+ * // After all this work is done, you could close the OutputStream:
+ * out.close();
+ * // This is not done by <CODE>transform</CODE>, the later example
+ * // should show, why.
+ * </PRE>
+ * 
  * @author Werner Gresshoff
  *
  * @version $Revision: 1.0 $ $Date: 2003/02/03 14:57:25 $
  **/
 public class MCRXSLTransformation {
 
+	private static Logger logger = null;
+	
+	private static SAXTransformerFactory factory = null;  
+
+	private static MCRXSLTransformation singleton = null;
+	
+	/**
+	 * Method MCRXSLTransformation.
+	 */
+	private MCRXSLTransformation() {
+		logger = Logger.getLogger(MCRXSLTransformation.class);
+    	MCRConfiguration.instance().reload(true);
+    	PropertyConfigurator.configure(MCRConfiguration.instance().getLoggingProperties());
+    	
+    	try {
+	    	TransformerFactory tf = TransformerFactory.newInstance();
+	    	if (tf.getFeature(SAXTransformerFactory.FEATURE)) {
+    	  	  	factory = (SAXTransformerFactory) tf;
+    		} else {
+    			logger.fatal("TransformerFactory could not be initialized.");
+	    	}
+    	} catch (TransformerFactoryConfigurationError tfce) {
+    		if (tfce.getMessage() != null) {
+    			logger.fatal(tfce.getMessage());
+    		} else {
+    			logger.fatal("Error in TranformerFactory configuration.");
+    		}
+    	}
+	}
+	
+	/**
+	 * Method getInstance. Creates an instance of MCRXSLTransformation, when called the first time.
+	 * @return MCRXSLTransformation
+	 */
+	public static synchronized MCRXSLTransformation getInstance() {
+		if (singleton == null) {
+			singleton = new MCRXSLTransformation();
+		}
+		
+		return singleton;
+	}
+	
+	/**
+	 * Method getStylesheet. Returns a precompiled stylesheet.
+	 * @param stylesheet Full path to the stylesheet
+	 * @return Templates The precompiled Stylesheet
+	 */
+	public Templates getStylesheet(String stylesheet) {
+	    File styleFile = new File(stylesheet);
+	    
+		if (!styleFile.exists()) {
+			logger.fatal("The Stylesheet doesn't exist.");
+			return null;
+		}
+		return getStylesheet(styleFile);
+	}
+	
+	/**
+	 * Method getStylesheet. Returns a precompiled stylesheet.
+	 * @param stylesheet A File with the stylesheet code
+	 * @return Templates The precompiled Stylesheet
+	 */
+	public Templates getStylesheet(File stylesheet) {
+		return getStylesheet(new StreamSource(stylesheet));
+	}
+	
+	/**
+	 * Method getStylesheet. Returns a precompiled stylesheet.
+	 * @param stylesheet A StreamSource
+	 * @return Templates The precompiled Stylesheet
+	 */
+	public Templates getStylesheet(StreamSource stylesheet) {
+		try {
+			Templates out = factory.newTemplates(stylesheet);
+			return out;
+		} catch (TransformerConfigurationException tcx) {
+			logger.fatal(tcx.getMessageAndLocation());
+			return null;
+		}
+	}
+	
+	/**
+	 * Method getTransformerHandler. Returns a TransformerHandler for the given Template.
+	 * @param stylesheet
+	 * @return TransformerHandler
+	 */
+	public TransformerHandler getTransformerHandler(Templates stylesheet) {
+		try {
+	        TransformerHandler handler = factory.newTransformerHandler(stylesheet);
+			return handler;
+		} catch (TransformerConfigurationException tcx) {
+			logger.fatal(tcx.getMessageAndLocation());
+			return null;
+		}
+	}
+	
+	/**
+	 * Method setParameters. Set some parameters which can be used by the Stylesheet for the transformation.
+	 * @param handler
+	 * @param parameters
+	 */
+	public void setParameters(TransformerHandler handler, Properties parameters) {
+		Transformer transformer = handler.getTransformer();
+		Enumeration names = parameters.propertyNames();
+    
+		while (names.hasMoreElements()) {
+			String name = (String) (names.nextElement());
+			String value = parameters.getProperty(name);
+
+			transformer.setParameter(name, value);
+		}
+	}
+	
+	/**
+	 * Method transform. Transforms a JDOM-Document to the given OutputStream
+	 * @param in
+	 * @param handler
+	 * @param out
+	 */
+	public void transform(org.jdom.Document in, TransformerHandler handler, OutputStream out) {
+	    handler.setResult(new StreamResult(out));
+    
+    	try {
+    		new SAXOutputter(handler).output(in); 
+    	} catch (JDOMException ex) {
+    		logger.error("Error while transforming an XML document with an XSL stylesheet.");
+	    }
+	}
+	
 	/**
 	 * Method transform. Transforms a JDOM-Document <i>in</i> with a given <i>stylesheet</i> to a new document.
 	 * @param in A JDOM-Document.
@@ -61,7 +240,7 @@ public class MCRXSLTransformation {
             return out.getDocument();
         }
         catch (TransformerException e) {
-            //logger.fatal(e.getMessage());
+            logger.fatal(e.getMessage());
             return null;
         }
     }
