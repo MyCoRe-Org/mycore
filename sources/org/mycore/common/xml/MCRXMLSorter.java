7ca6a6782c1b9bf476a33a741a3bb9b713886de7
Thomas Scheffler <github@yagee.de>
2003-06-12 13:14:17 +0000

    -added GPL preface -fixed the ugly missing xlink namespace bug
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@1602 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/xml/MCRXMLSorter.java
+++ sources/org/mycore/common/xml/MCRXMLSorter.java
@@ -1,6 +1,31 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
 package org.mycore.common.xml;
 
 import org.mycore.common.MCRException;
+import org.mycore.datamodel.metadata.MCRObject;
+
 import javax.servlet.ServletContext;
 import java.util.Hashtable;
 // JDOM imports
@@ -259,7 +284,8 @@ public class MCRXMLSorter implements MCRXMLSortInterface {
 		  new StringBuffer("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n\n")
 		      .append("<!-- This file is machine generated and can be safely removed -->\n\n")
 			  .append("<xsl:stylesheet version=\"1.0\"\n")
-			  .append("     xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n\n")
+			  .append("     xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"\n")
+			  .append("     xmlns:xlink=\"").append(MCRObject.XLINK_URL).append("\">\n\n")
 			  .append("<xsl:output\n method=\"xml\"/>\n\n")
 			  .append("<xsl:template match=\"/mcr_results\">\n\n")
 			  .append("<xsl:element name=\"mcr_results\">\n\n")
