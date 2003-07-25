6efbb4e81b41ac6ccf45ece8fe03ffb8741502b0
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2003-07-25 12:51:53 +0000

    Add the connection to MySQL and eXist.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@1750 7089d063-bc14-496d-ac90-53de1872eb6c

--- bin/setup_linux.sh
+++ bin/setup_linux.sh
@@ -4,12 +4,16 @@
 #
 
 export ANT_HOME=/opt/jakarta/ant
+export EXIST_HOME=/usr/local/src/eXist-0.9.2
 export TOMCAT_HOME=/opt/jakarta/tomcat
 
 CLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant.jar
 CLASSPATH=$CLASSPATH:$ANT_HOME/lib/optional.jar
 
 CLASSPATH=$CLASSPATH:$TOMCAT_HOME/common/lib/servlet.jar
+
+CLASSPATH=$CLASSPATH:$EXIST_HOME/exist.jar
+CLASSPATH=$CLASSPATH:$EXIST_HOME/lib/core/xmlrpc-1.1.jar
 
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/log4j-1_2_7.jar
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/xerces-xml-apis-2_2_1.jar
@@ -21,6 +25,8 @@ CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/jdom-beta8.jar
 
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/ftp.jar
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/mail.jar
+CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/mysql-connector-java-2.0.14-bin.jar
+CLASSPATH=$CLASSPATH:$MYCORE_HOME/lib/xmldb.jar
 CLASSPATH=$CLASSPATH:$MYCORE_HOME/classes
 
 export CLASSPATH
