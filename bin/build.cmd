d002c749a069c52973ac7b9704d6daaae5b4971e
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2003-08-05 15:50:17 +0000

    Removed the setup scripts, we do not need them any more. Changed build.sh and build.cmd, this is the only place where you have to modify a few lines to build MyCoRe, then call "build.sh jar"
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@1797 7089d063-bc14-496d-ac90-53de1872eb6c

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,27 +1,39 @@
 @echo off
-rem
-rem Start the ant building
-rem
+rem ######################################################################
+rem #
+rem # This script sets some basic environment variables and calls Ant to
+rem # build the MyCoRe system from sources. Use on Windows systems.
+rem # 
+rem # $Revision$ $Date$
+rem #
+rem #######################################################################
 
-rem -------------------- Configuration -----------------------------
+rem #######################################################################
+rem #
+rem # Change the following variables as needed for your local system:
+rem #
+rem #######################################################################
 
-echo.
-echo ***** build.cmd: Starting configuration tasks... *****
-echo.
-call .\setup.cmd
-echo.
-echo ***** build.cmd: Configuration done. *****
+rem Directory where JDK is installed on your machine
+set JAVA_HOME=C:\Programme\j2re1.4.0_01
 
+rem Directory where Ant is installed on your machine
+set ANT_HOME=C:\Programme\apache-ant-1.5.3-1
 
-rem -------------------- start build with ant ----------------------
+rem Persistence implementation to use [cm7 | cm8 | xmldb]
+set PERSISTENCE=xmldb
 
-echo ***** build.cmd: Starting ant... *****
-echo.
-cd "%MYCORE_HOME%"
-"%JAVA_HOME%\bin\java" -Dant.home="%ANT_HOME%" -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1
-echo.
-echo ***** build.cmd: Build done. *****
-echo.
+rem If you use cm7 or cm8, directory where IBM Content Manager is installed
+set CM_HOME=c:\cmbroot
 
+rem #######################################################################
+rem #
+rem # Do not change the rest of this script!
+rem #
+rem #######################################################################
 
+set PATH=%ANT_HOME%;%PATH%
+                                                                                                                                                                               
+cd ..
+ant -DCM_HOME=$CM_HOME -DPERSISTENCE=$PERSISTENCE $*
 
--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,42 @@
 #! /bin/sh
+
+#######################################################################
 #
-# Start the ant building
+# This script sets some basic environment variables and calls Ant to
+# build the MyCoRe system from sources. Use on UNIX systems.
 #
+# $Revision$ $Date$
+#
+#######################################################################
 
-# Configuration part
+#######################################################################
+#
+# Change the following variables as needed for your local system:
+#
+#######################################################################
 
-cd $MYCORE_HOME
-. bin/setup.sh
+# Directory where JDK is installed on your machine
+JAVA_HOME=/usr/java_dev2
 
-# Start ant
+# Directory where Ant is installed on your machine
+ANT_HOME=/opt/jakarta-ant-1.5.1
 
-$JAVA_HOME/bin/java -Dant.home=$ANT_HOME -classpath $CLASSPATH org.apache.tools.ant.Main $*
+# Persistence implementation to use [cm7 | cm8 | xmldb]
+PERSISTENCE=cm7
+
+# If you use cm7 or cm8, directory where IBM Content Manager is installed
+CM_HOME=/usr/lpp/cmb/
+
+#######################################################################
+#
+# Do not change the rest of this script!
+#
+#######################################################################
+
+export JAVA_HOME
+export ANT_HOME
+export PATH=$ANT_HOME/bin:$PATH
+
+cd ..
+ant -DCM_HOME=$CM_HOME -DPERSISTENCE=$PERSISTENCE $*
 
