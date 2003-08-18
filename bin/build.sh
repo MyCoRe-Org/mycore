aed66bd221a70256a80f5dd748e2f2c314b94c80
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2003-08-18 09:36:32 +0000

    Undid changes of ANT_HOME auto-detection, this caused problems on my system.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@1874 7089d063-bc14-496d-ac90-53de1872eb6c

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/sh
 
 #######################################################################
 #
@@ -15,11 +15,14 @@
 #
 #######################################################################
 
-WHICH=/usr/bin/which
-
 # Directory where JDK is installed on your machine, if not already set
 if [ "$JAVA_HOME" = "" ]; then
   JAVA_HOME=/usr/java_dev2
+fi
+
+# Directory where Ant is installed on your machine, if not already set
+if [ "$ANT_HOME" = "" ]; then
+  ANT_HOME=/opt/jakarta-ant-1.5.1
 fi
 
 #######################################################################
@@ -28,50 +31,8 @@ fi
 #
 #######################################################################
 
-# Look for ANT on system
-if [ -z "$ANT_BIN" ] ;  then
-  ANT_BIN=`$WHICH ant 2>/dev/null`
-fi
-if [ -x "$ANT_BIN" ] ; then
-  if [ `grep -l "org.apache.tools.ant.Main" "$ANT_BIN"` ] ; then :
-  else unset ANT_BIN
-  fi
-fi
-if [ -z "$ANT_BIN" ] ; then
-  if [ -z "$ANT_HOME" ] ; then
-    # try to find ANT
-    if [ -d /opt/ant ] ; then 
-      ANT_HOME=/opt/ant
-    fi
-  
-    if [ -z "$ANT_HOME" ] ; then
-      if [ -d "${HOME}/opt/ant" ] ; then 
-        ANT_HOME="${HOME}/opt/ant"
-      fi
-    fi
-
-    if [ -z "$ANT_HOME" ] ; then
-      echo "Need to take a deeper look in your system to find ANT, wait a minute..."
-      #some brute force may help then
-      ANT_BIN=`find / -type f -name "ant" -exec test -x {} \; -exec grep -l "org.apache.tools.ant.Main" {} \; 2>/dev/null |head -1`
-      ANT_HOME=`. $ANT_BIN >2&>/dev/null ; echo $ANT_HOME`
-    fi
-
-  fi
-  if [ -z "$ANT_BIN" ] ; then
-    if [ -x "$ANT_BIN/bin/ant" ] ; then
-      ANT_BIN=$ANT_HOME/bin/ant
-    else
-      echo "No ANT installation found!"
-      exit 1
-    fi
-  fi
-fi
-
-#when is that really needed anyway?
 export JAVA_HOME
 export ANT_HOME
-export ANT_BIN
 
-$ANT_BIN -logger org.apache.tools.ant.NoBannerLogger -find build.xml $*
+$ANT_HOME/bin/ant -logger org.apache.tools.ant.NoBannerLogger -find build.xml $*
 
