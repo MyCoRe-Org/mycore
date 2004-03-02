3fd951dad60e9817c75438c32e8c5c644676d269
Harald Richter <mcrhrich@duepub-1.zim.uni-duisburg-essen.de>
2004-03-02 13:44:05 +0000

    sql database as content store
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@2579 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/backend/sql/MCRCStoreSQLDB.java
@@ -0,0 +1,136 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package org.mycore.backend.sql;
+
+import org.mycore.common.*;
+import org.mycore.datamodel.ifs.*;
+import java.util.*;
+import java.io.*;
+import java.sql.*;
+import java.sql.*;
+
+/**
+ * This class implements the MCRContentStore interface to store the content of
+ * MCRFile objects in a sql database. 
+ *
+ * @author Harald Richter
+ * @version $Revision$ $Date$
+ */
+public class MCRCStoreSQLDB extends MCRContentStore
+{ 
+  public void init( String storeID )
+  {
+    super.init( storeID );
+
+//    System.out.println("### INIT " + storeID );
+//    MCRConfiguration config = MCRConfiguration.instance();  
+  }
+
+  private synchronized int getNextFreeID( String tableName )
+    throws Exception
+  {
+    String sql = "SELECT MAX( ID ) FROM " + tableName;
+    String id  = MCRSQLConnection.justGetSingleValue( sql );
+
+    int result = ( id == null ? 0 : Integer.parseInt( id ) );
+
+    return result + 1;
+  }
+  
+  protected String doStoreContent( MCRFileReader file, MCRContentInputStream source )
+    throws Exception
+  {
+    String tableName = file.getContentTypeID();
+    int           ID = getNextFreeID( tableName );
+    String storageID = tableName + ":" + ID;
+
+    MCRSQLConnection connection = MCRSQLConnectionPool.instance().getConnection();
+
+    try
+    {
+      connection.getJDBCConnection().setAutoCommit( false );
+
+      String insert = "INSERT INTO " + tableName + " VALUES (?, ?)";
+      PreparedStatement statement = connection.getJDBCConnection().prepareStatement( insert );
+      statement.setInt   ( 1, ID );
+      statement.setBinaryStream( 2, source , source.available());
+//      System.out.println("##### SIZE " + source.available() );
+      statement.execute();
+      statement.close();
+     
+      connection.getJDBCConnection().commit();
+      connection.getJDBCConnection().setAutoCommit( true );
+    }
+    catch( Exception ex )
+    {
+      try{ connection.getJDBCConnection().rollback(); } 
+      catch( SQLException ignored ){}
+      throw ex;
+    }
+    finally
+    { connection.release(); }
+    
+    return storageID;
+  }
+  
+  protected void doDeleteContent( String storageID )
+    throws Exception
+  {
+    int i            = storageID.indexOf( ':' );
+    String tableName = storageID.substring( 0, i);
+    String ID        = storageID.substring( i+1 );
+    String sql       = "DELETE FROM " + tableName + " WHERE ID = " + ID;
+//    System.out.println("###### DELETE " + sql );
+    MCRSQLConnection.justDoUpdate( sql );
+  }
+
+  protected void doRetrieveContent( MCRFileReader file, OutputStream target )
+    throws Exception
+  {
+    MCRSQLConnection connection = MCRSQLConnectionPool.instance().getConnection();
+
+    try
+    {
+      String storageID    = file.getStorageID();
+      int i               = storageID.indexOf( ':' );
+      String tableName    = storageID.substring( 0, i);
+      String ID           = storageID.substring( i+1 );
+      String select       = "SELECT XML FROM " + tableName + " WHERE ID = " + ID;
+      Statement statement = connection.getJDBCConnection().createStatement();
+      ResultSet rs = statement.executeQuery( select );
+
+      if( ! rs.next() )
+      {
+        String msg = ID + " is not in table  " + tableName;
+        throw new MCRUsageException( msg );
+      }
+
+      MCRUtils.copyStream( rs.getBinaryStream( 1 ), target );
+      rs.close();
+    }
+    finally{ connection.release(); }
+  }
+}
+
