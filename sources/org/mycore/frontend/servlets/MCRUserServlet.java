77c2352f60f5cf85b646ff8c2cd687e7370fafa2
Detlef Degenhard <mcrddege@duepub-1.zim.uni-duisburg-essen.de>
2004-03-08 17:19:15 +0000

    added additional funtionality for the login and first version of the user servlet
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@2616 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/frontend/servlets/MCRUserServlet.java
@@ -0,0 +1,251 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package org.mycore.frontend.servlets;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.jdom.*;
+import org.mycore.common.*;
+import org.mycore.frontend.servlets.*;
+import org.mycore.user.*;
+
+/**
+ * This servlet provides a web interface for the user management of the mycore system.
+ *
+ * @author Detlev Degenhardt
+ * @version $Revision$ $Date$
+ */
+
+public class MCRUserServlet extends MCRServlet
+{
+  // The configuration
+  private static MCRConfiguration config;
+  private static Logger logger=Logger.getLogger(MCRUserServlet.class);
+
+  // user ID and password of the guest user
+  private static String guestID  = null;
+  private static String guestPWD = null;
+
+  // The default mode for this class
+  String mode = "Select";
+
+  /** Initialisation of the servlet */
+  public void init()
+  {
+    MCRConfiguration.instance().reload(true);
+    config = MCRConfiguration.instance();
+    PropertyConfigurator.configure(config.getLoggingProperties());
+
+    guestID  = config.getString( "MCR.users_guestuser_username"   );
+    guestPWD = config.getString( "MCR.users_guestuser_userpasswd" );
+  }
+
+  /**
+   * This method overrides doGetPost of MCRServlet and handles HTTP requests. Depending
+   * on the request parameter "mode" this method delegates the request to different
+   * methods of this servlet.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   */
+  public void doGetPost(MCRServletJob job)
+    throws IOException, ServletException
+  {
+    mode  = getStringParameter(job, "mode").trim();
+    if (mode.length() == 0) mode = "Select";
+    if (mode.equals("ChangePwd")) {changePwd(job);}
+    else if (mode.equals("CreatePwdDialog")){createPwdDialog(job);}
+    else if (mode.equals("Select")){selectTask(job);}
+    else if (mode.equals("ShowUser")){showUser(job);}
+    else { // no valid mode, redirect to original URL
+      String backto_url = getStringParameter(job, "url").trim();
+      if (backto_url.length() == 0) backto_url = null;
+      job.getResponse().sendRedirect(backto_url);
+      return;
+    }
+  }
+
+  /**
+   * This method handles the "ChangePwd" (change password) mode. The change password
+   * dialog of the presentation layer must provide three passwords in the http request:
+   * The new password, an repetition of the new password and (for security reasons)
+   * the old password again. This method checks if the old password is correct and if
+   * both new passwords are equal. If so, the password is changed and the control flow
+   * is routed to the presentation of possible task for the current user. If not,
+   * error messages are displayed.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   */
+  protected void changePwd(MCRServletJob job)
+    throws IOException, ServletException
+  {
+    // Get the MCRSession object for the current thread from the session manager.
+    MCRSession mcrSession = MCRSessionMgr.getCurrentSession();
+    String currentUser = mcrSession.getCurrentUserID();
+
+    String pwd_1  = getStringParameter(job, "pwd_1").trim();
+    String pwd_2  = getStringParameter(job, "pwd_2").trim();
+    String oldpwd = getStringParameter(job, "oldpwd").trim();
+
+    org.jdom.Document jdomDoc = createJdomDocBase(job);
+    org.jdom.Element root = jdomDoc.getRootElement();
+
+    if (!pwd_1.equals(pwd_2)) {
+      root.setAttribute("new_pwd_mismatch", "true");
+    }
+    else if (!MCRUserMgr.instance().login(currentUser, oldpwd)) {
+      root.setAttribute("old_pwd_mismatch", "true");
+    }
+    else {
+      try {
+        MCRUserMgr.instance().setPassword(mcrSession, currentUser, pwd_1);
+        root.setAttribute("pwd_change_ok", "true");
+        doLayout(job, "SelectTask", jdomDoc); // use the stylesheet mcr_user-SelectTask-*.xsl
+        return;
+      }
+      catch (MCRException e) {
+        root.addContent(new org.jdom.Element("error").addContent(e.getMessage()));
+      }
+    }
+    doLayout(job, "ChangePwd", jdomDoc); // use the stylesheet mcr_user-ChangePwd-*.xsl
+  }
+
+  /**
+   * This method handles the "CreatePwdDialog" mode. It is nothing more than choosing
+   * the right stylesheet.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   */
+  protected void createPwdDialog(MCRServletJob job)
+    throws IOException, ServletException
+  {
+    org.jdom.Document jdomDoc = createJdomDocBase(job);
+    doLayout(job, "ChangePwd", jdomDoc); // use the stylesheet mcr_user-ChangePwd-*.xsl
+  }
+
+
+  /**
+   * This method handles the "Select" mode. Depending on the privileges of the current
+   * user a list of possible tasks is collected as an XML representation and forwarded
+   * to the LayoutServlet.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   */
+  protected void selectTask(MCRServletJob job)
+    throws IOException, ServletException
+  {
+    // For the moment only tasks possible for all users are presented. But this is work
+    // in progress. In the future the list of privileges for the current user will be
+    // checked here and in case he or she has additional privileges this will be
+    // forwarded to the presentation layer (i.e. XSL stylesheets).
+
+    org.jdom.Document jdomDoc = createJdomDocBase(job);
+    doLayout(job, "SelectTask", jdomDoc); // use the stylesheet mcr_user-SelectTask-*.xsl
+  }
+
+  /**
+   * This method handles the "ShowUser" mode. The current user is retrieved from the
+   * user manager and its XML representation is forwarded to the LayoutServlet.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   */
+  protected void showUser(MCRServletJob job)
+    throws IOException, ServletException
+  {
+    // Get the MCRSession object for the current thread from the session manager.
+    MCRSession mcrSession = MCRSessionMgr.getCurrentSession();
+    String currentUser = mcrSession.getCurrentUserID();
+
+    org.jdom.Document jdomDoc = createJdomDocBase(job);
+    org.jdom.Element root = jdomDoc.getRootElement();
+
+    MCRUser user = MCRUserMgr.instance().retrieveUser(mcrSession, currentUser);
+    root.addContent(user.toJDOMElement());
+
+    doLayout(job, "Metadata", jdomDoc); // use the stylesheet mcr_user-Metadata-*.xsl
+  }
+
+  /**
+   * This method creates a jdom document with elements need by all modes this
+   * servlet can run.
+   *
+   * @param  job The MCRServletJob instance
+   * @throws IOException for java I/O errors.
+   * @throws ServletException for errors from the servlet engine.
+   * @return jdom document
+   */
+  protected org.jdom.Document createJdomDocBase(MCRServletJob job)
+  {
+    // Get the MCRSession object for the current thread from the session manager.
+    MCRSession mcrSession = MCRSessionMgr.getCurrentSession();
+    String currentUser = mcrSession.getCurrentUserID();
+    String backto_url = getStringParameter(job, "url").trim();
+    if (backto_url.length() == 0) backto_url = null;
+
+    org.jdom.Element root = new org.jdom.Element("mcr_user");
+    org.jdom.Document jdomDoc = new org.jdom.Document(root);
+
+    root.addContent(new org.jdom.Element("guest_id").addContent(guestID));
+    root.addContent(new org.jdom.Element("guest_pwd").addContent(guestPWD));
+    root.addContent(new org.jdom.Element("backto_url").addContent(backto_url));
+    return jdomDoc;
+  }
+
+  /**
+   * Gather information about the XML document to be shown and the corresponding XSLT
+   * stylesheet and redirect the request to the LayoutServlet
+   *
+   * @param  job The MCRServletJob instance
+   * @param  styleBase String value to select the correct XSL stylesheet
+   * @param  jdomDoc The XML representation to be presented by the LayoutServlet
+   * @throws ServletException for errors from the servlet engine.
+   * @throws IOException for java I/O errors.
+   */
+  protected void doLayout(MCRServletJob job, String styleBase, Document jdomDoc)
+    throws ServletException, IOException
+  {
+    String language = MCRSessionMgr.getCurrentSession().getCurrentLanguage();
+    String styleSheet = styleBase + "-" + language;
+
+    job.getRequest().getSession().setAttribute("mycore.language", language);
+    job.getRequest().setAttribute("MCRLayoutServlet.Input.JDOM", jdomDoc);
+    job.getRequest().setAttribute("XSL.Style", styleSheet);
+
+    RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
+    rd.forward(job.getRequest(), job.getResponse());
+  }
+}
