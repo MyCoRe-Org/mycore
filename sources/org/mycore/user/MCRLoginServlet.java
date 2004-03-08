43cd8ebe9b19acb09433a8ada9c558b4c4adf80a
Detlef Degenhard <mcrddege@duepub-1.zim.uni-duisburg-essen.de>
2004-03-08 17:24:20 +0000

    addinional functionality of the login dialog
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@2617 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/user/MCRLoginServlet.java
+++ sources/org/mycore/user/MCRLoginServlet.java
@@ -32,7 +32,6 @@ import org.apache.log4j.PropertyConfigurator;
 import org.jdom.*;
 import org.mycore.common.*;
 import org.mycore.frontend.servlets.*;
-import org.mycore.user.*;
 
 /**
  * This servlet is used to login a user to the mycore system.
@@ -72,19 +71,19 @@ public class MCRLoginServlet extends MCRServlet
 
     String uid = getStringParameter(job, "uid").trim();
     String pwd = getStringParameter(job, "pwd").trim();
-    String url = getStringParameter(job, "url").trim();
+    String backto_url = getStringParameter(job, "url").trim();
 
     if (uid.length() == 0) uid = null;
     if (pwd.length() == 0) pwd = null;
-    if( url.length() == 0 ) url = null;
+    if (backto_url.length() == 0) backto_url = null;
 
     // Do not change login, just redirect to given url:
-    if (mcrSession.getCurrentUserID().equals(uid) && (pwd == null) && (url != null)) {
-      job.getResponse().sendRedirect(url);
+    if (mcrSession.getCurrentUserID().equals(uid) && (pwd == null) && (backto_url != null)) {
+      job.getResponse().sendRedirect(backto_url);
       return;
     }
 
-    if (url == null) url = MCRServlet.getBaseURL();
+    if (backto_url == null) backto_url = MCRServlet.getBaseURL();
 
     org.jdom.Element root = new org.jdom.Element("mcr_user");
     org.jdom.Document jdomDoc = new org.jdom.Document(root);
@@ -95,10 +94,26 @@ public class MCRLoginServlet extends MCRServlet
     try {
       loginOk = ((uid != null) && (pwd != null) && MCRUserMgr.instance().login(uid, pwd));
 
-      // If the login attempt was successfull, change the user ID and redirect to target URL
+      // If the login attempt was successfull, change the user ID and forward to the
+      // UserServlet with mode=Select. However, if the user to login is the guest user,
+      // then the request will just be redirected to the originating URL, i.e. not
+      // forwarded to the UserServlet.
+
       if (loginOk) {
         mcrSession.setCurrentUserID(uid);
-        job.getResponse().sendRedirect(url);
+        logger.info("MCRLoginServlet: user " + uid + " logged in successfully.");
+
+        if (uid.equals(guestID)) {
+          job.getResponse().sendRedirect(backto_url);
+          return;
+        }
+
+        RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRUserServlet");
+        job.getRequest().removeAttribute("lang");
+        job.getRequest().setAttribute("lang", mcrSession.getCurrentLanguage());
+        job.getRequest().removeAttribute("mode");
+        job.getRequest().setAttribute("mode", "Select");
+        rd.forward(job.getRequest(), job.getResponse());
         return;
       }
       else {
@@ -107,18 +122,21 @@ public class MCRLoginServlet extends MCRServlet
       }
     }
     catch (MCRException e) {
-      if (e.getMessage().equals("Unknown user.")) {
+      if (e.getMessage().equals("Error in UserStore.")) {
         root.setAttribute("unknown_user", "true");
-        logger.info("MCRLoginServlet: unknown user:" + uid);
+        logger.info("MCRLoginServlet: unknown user: " + uid);
       }
       else if (e.getMessage().equals("Login denied. User is disabled.")) {
         root.setAttribute("user_disabled", "true");
         logger.info("MCRLoginServlet: disabled user " + uid + " tried to login.");
       }
-      else throw e;
+      else {
+        logger.debug("MCRLoginServlet: unknown error: "+e.getMessage());
+        throw e;
+      }
     }
 
-    root.addContent(new org.jdom.Element("url").addContent(url));
+    root.addContent(new org.jdom.Element("backto_url").addContent(backto_url));
     doLayout(job, "login", jdomDoc); // use the stylesheet mcr_user-login-*.xsl
   }
 
@@ -126,24 +144,24 @@ public class MCRLoginServlet extends MCRServlet
    * Gather information about the XML document to be shown and the corresponding XSLT
    * stylesheet and redirect the request to the LayoutServlet
    *
-   * @param job
-   * @param styleBase
-   * @param jdomDoc
-   * @throws ServletException
-   * @throws IOException
+   * @param  job The MCRServletJob instance
+   * @param  styleBase String value to select the correct XSL stylesheet
+   * @param  jdomDoc The XML representation to be presented by the LayoutServlet
+   * @throws ServletException for errors from the servlet engine.
+   * @throws IOException for java I/O errors.
    */
   protected void doLayout(MCRServletJob job, String styleBase, Document jdomDoc)
-                          throws ServletException, IOException
+    throws ServletException, IOException
   {
     String language = MCRSessionMgr.getCurrentSession().getCurrentLanguage();
+    String styleSheet = styleBase + "-" + language;
 
     job.getRequest().getSession().setAttribute("mycore.language", language);
-
-    String styleSheet = styleBase + "-" + language;
     job.getRequest().setAttribute("MCRLayoutServlet.Input.JDOM", jdomDoc);
     job.getRequest().setAttribute("XSL.Style", styleSheet);
 
     RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
     rd.forward(job.getRequest(), job.getResponse());
   }
+
 }
