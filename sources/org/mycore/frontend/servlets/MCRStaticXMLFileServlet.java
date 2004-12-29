68202508176c25ee0f337a7427156678542fc5cc
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2004-12-29 11:53:45 +0000

    fix a compile bug.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4124 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/servlets/MCRStaticXMLFileServlet.java
+++ sources/org/mycore/frontend/servlets/MCRStaticXMLFileServlet.java
@@ -84,7 +84,7 @@ public class MCRStaticXMLFileServlet extends MCRServlet
     job.getRequest().setAttribute( "MCRLayoutServlet.Input.FILE", file );
 
     // Set XSL Style to current language if not XSL.Style=xml in request:
-    if( ! "xml".equals( job.getRequest().getParameter( "XSL.Style" ) )    
+    if( ! "xml".equals( job.getRequest().getParameter( "XSL.Style" ) ) )  
       job.getRequest().setAttribute( "XSL.Style", MCRSessionMgr.getCurrentSession().getCurrentLanguage() );
 
     RequestDispatcher rd = getServletContext().getNamedDispatcher( "MCRLayoutServlet" );
