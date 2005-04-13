2a6627d2ae81af65ba066415e7809202420fcca4
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2005-04-13 19:09:02 +0000

    Added event handling framework
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4576 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/common/events/MCREvent.java
@@ -0,0 +1,66 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package org.mycore.common.events;
+
+/**
+ * Represents an event that occured in the MyCoRe system. Events are of a
+ * predefined type like create, update, delete and can be handled by
+ * MCREventHandler implementations. Events are automatically created by some 
+ * MyCoRe components and are forwarded to the handlers by MCREventManager. 
+ * 
+ * @author Frank Lützenkirchen
+ **/
+public class MCREvent extends java.util.Hashtable
+{
+  /** Predefined event type metadata object created **/
+  public static final int OBJECT_CREATED = 0;
+  /** Predefined event type metadata object updated **/
+  public static final int OBJECT_UPDATED = 1;
+  /** Predefined event type metadata object deleted **/
+  public static final int OBJECT_DELETED = 2;
+
+  private String[] typeStrings = { "object created", "object updated", "object deleted" };
+  private int type;
+  
+  /**
+   * Creates a new event object of the given type
+   **/
+  public MCREvent( int type )
+  { this.type = type; }
+  
+  /**
+   * Returns the type of this event
+   * @return the type of this event
+   **/
+  public int getType()
+  { return type; }
+  
+  /**
+   * Returns the type of this event as a String for debugging
+   * @return the type of this event as a String for debugging
+   **/
+  public String getTypeString()
+  { return typeStrings[ type ]; }
+}
---
+++ sources/org/mycore/common/events/MCREventHandlerBase.java
@@ -0,0 +1,92 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package org.mycore.common.events;
+
+import org.apache.log4j.Logger;
+import org.mycore.datamodel.metadata.MCRObject;
+
+/**
+ * Abstract helper class that can be subclassed to implement event handlers
+ * more easily.
+ * 
+ * @author Frank Lützenkirchen
+ **/
+public abstract class MCREventHandlerBase implements MCREventHandler
+{
+  private static Logger logger = Logger.getLogger( MCREventHandlerBase.class );
+  
+  public void doHandleEvent( MCREvent evt )
+  {
+    MCRObject obj = (MCRObject)( evt.get( "object" ) );
+    if( obj != null )
+    {
+      logger.debug( getClass().getName() + " handling " + 
+        obj.getId().getId() + evt.getTypeString() );
+    }
+    
+    switch( evt.getType() )
+    {
+      case MCREvent.OBJECT_CREATED:
+        handleObjectCreated( evt, obj );
+        break;
+      case MCREvent.OBJECT_UPDATED:
+        handleObjectUpdated( evt, obj );
+        break;
+      case MCREvent.OBJECT_DELETED:
+        handleObjectDeleted( evt, obj );
+        break;
+    }
+  }
+  
+  /**
+   * Handles object created events. This implementation does nothing and
+   * should be overwritted by subclasses.
+   * 
+   * @param evt the event that occured
+   * @param obj the MCRObject that caused the event
+   **/
+  protected void handleObjectCreated( MCREvent evt, MCRObject obj )
+  { logger.debug( "This default handler implementation does nothing"); }
+  
+  /**
+   * Handles object updated events. This implementation does nothing and
+   * should be overwritted by subclasses.
+   * 
+   * @param evt the event that occured
+   * @param obj the MCRObject that caused the event
+   **/
+  protected void handleObjectUpdated( MCREvent evt, MCRObject obj )
+  { logger.debug( "This default handler implementation does nothing"); }
+  
+  /**
+   * Handles object deleted events. This implementation does nothing and
+   * should be overwritted by subclasses.
+   * 
+   * @param evt the event that occured
+   * @param obj the MCRObject that caused the event
+   **/
+  protected void handleObjectDeleted( MCREvent evt, MCRObject obj )
+  { logger.debug( "This default handler implementation does nothing"); }
+}
