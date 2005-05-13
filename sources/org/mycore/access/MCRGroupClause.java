b0adb02ed63561592182754704ff30872e6fa59d
Matthias Kramm <mcrkramm@duepub-1.zim.uni-duisburg-essen.de>
2005-05-13 14:04:51 +0000

    new implementation of the MyCoRe ACL concept initial revision
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4757 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/access/MCRGroupClause.java
@@ -0,0 +1,54 @@
+/**
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ **/
+
+package org.mycore.access;
+
+import java.util.Date;
+import org.mycore.user.MCRUser;
+import org.mycore.user.MCRGroup;
+
+/**
+ * Implementation of a (group xy) clause
+ * 
+ * @author   Matthias Kramm
+ **/
+
+class MCRGroupClause implements MCRAccessCtrlDefinition
+{
+    private MCRGroup group;
+    private String groupname;
+
+    MCRGroupClause(String group)
+    {
+	this.groupname = group;
+	this.group = new MCRGroup(group);
+    }
+
+    public boolean hasAccess(MCRUser user, Date date, MCRIPAddress ip)
+    {
+	return user.isMemberOf(group);
+    }
+    
+    public String toString()
+    {
+	return "group "+group+"\n";
+    }
+};
+
---
+++ sources/org/mycore/access/MCRIPAddress.java
@@ -0,0 +1,88 @@
+/**
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ **/
+
+package org.mycore.access;
+
+import java.util.Date;
+import java.net.InetAddress;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+import org.mycore.user.MCRUser;
+
+/** A class for representing an IP Address, or a range of IP addresses
+ * 
+ * @author   Matthias Kramm
+ **/
+
+public class MCRIPAddress
+{
+    byte[] address;
+    byte[] mask;
+
+    public MCRIPAddress(String ip) throws UnknownHostException {
+	int i = ip.indexOf('/');
+	if(i >= 0) {
+	    String ipstr = ip.substring(0,i);
+	    String maskstr = ip.substring(i+1);
+	    InetAddress address = InetAddress.getByName(ipstr);
+	    InetAddress mask = InetAddress.getByName(maskstr);
+	    init(address, mask);
+	} else {
+	    InetAddress address = InetAddress.getByName(ip);
+	    init(address);
+	}
+    }
+    
+    public MCRIPAddress(InetAddress address, InetAddress mask) {
+	init(address, mask);
+    }
+
+    public MCRIPAddress(InetAddress address) {
+	init(address);
+    }
+    
+    public void init(InetAddress address, InetAddress mask) {
+	int t;
+	this.address = address.getAddress();
+	this.mask = mask.getAddress();
+    }
+
+    public void init(InetAddress address) {
+	int t;
+	this.address = address.getAddress();
+	this.mask = new byte[this.address.length];
+	for(t=0;t<this.address.length;t++)
+	    this.mask[t] = (byte)255;
+    }
+
+    boolean contains(MCRIPAddress other) {
+	int t;
+	if(this.address.length != other.address.length)
+	    throw new IllegalStateException("can't map IPv6 to IPv4 and vice versa");
+	for(t=0;t<address.length;t++) {
+	    if((this.address[t] & this.mask[t]) != (other.address[t] & this.mask[t])) {
+		return false;
+	    }
+	}
+	return true;
+    }
+};
+
