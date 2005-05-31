3f39b404afbfb13a6c2048b3866682fec6767902
Matthias Kramm <mcrkramm@duepub-1.zim.uni-duisburg-essen.de>
2005-05-31 13:18:33 +0000

    class for representing class/href links
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4844 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/backend/hibernate/MCRLinkHREF.java
@@ -0,0 +1,65 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  *** 
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+
+package org.mycore.backend.sql;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import org.mycore.common.*;
+import org.mycore.datamodel.metadata.*;
+import org.mycore.datamodel.classifications.*;
+
+/** 
+ * Class for representing href/class links
+ *
+ **/
+public class MCRLinkHREF
+{
+    private String from;
+    private String to;
+    
+    MCRLinkHREF() {
+    }
+
+    MCRLinkHREF(String from, String to) {
+	this.from = from;
+	this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
+
