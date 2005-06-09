282b52bd50bab2dd0d1151fe0f425b7ca2103ecb
Matthias Kramm <mcrkramm@duepub-1.zim.uni-duisburg-essen.de>
2005-06-09 13:00:04 +0000

    table for generating unique IDs.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4895 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/backend/hibernate/tables/MCRID.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.backend.hibernate.tables;
+
+public class MCRID {
+    
+    private long id;
+    
+    public MCRID(){
+    }
+    
+    public long getId(){
+       return id;        
+    }
+
+    private void setId(long id){
+        this.id = id;
+    }
+}
