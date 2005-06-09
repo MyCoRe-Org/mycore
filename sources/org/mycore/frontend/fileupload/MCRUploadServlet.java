e0ffbbde37c6bc3011f6616b5edeefa7cd609ff2
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2005-06-09 12:24:09 +0000

    Port number and IP address for file upload are configurable now. Default port number is 22471, default IP address is the address the http request was sended to.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@4888 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/fileupload/MCRUploadServlet.java
+++ sources/org/mycore/frontend/fileupload/MCRUploadServlet.java
@@ -2,7 +2,8 @@
  * $RCSfile$
  * $Revision$ $Date$
  *
- * Copyright (C) 2000 University of Essen, Germany
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
  *
  * This program is free software; you can use it, redistribute it
  * and / or modify it under the terms of the GNU General Public License
@@ -15,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program, normally in the file documentation/license.txt.
+ * along with this program, normally in the file license.txt.
  * If not, write to the Free Software Foundation Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
  *
@@ -29,17 +30,19 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.zip.ZipInputStream;
+import java.util.zip.*;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.mycore.common.MCRException;
+import org.mycore.common.*;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
 
@@ -54,9 +57,98 @@ import org.mycore.frontend.servlets.MCRServletJob;
  * @version $Revision$ $Date$
  * @see org.mycore.frontend.fileupload.MCRMCRUploadHandlerInterface
  */
-public final class MCRUploadServlet extends MCRServlet {
+public final class MCRUploadServlet extends MCRServlet implements Runnable {
+
+    static String serverIP;
+
+    static int serverPort;
+
+    static ServerSocket server;
 
     static Logger LOGGER = Logger.getLogger(MCRUploadServlet.class);
+
+    public void init() throws ServletException {
+        super.init();
+        try {
+            String host = new java.net.URL(getBaseURL()).getHost();
+            String defIP = InetAddress.getByName(host).getHostAddress();
+            int defPort = 22471; // my birthday is the default upload port
+            serverIP = CONFIG.getString("MCR.FileUpload.IP", defIP);
+            serverPort = CONFIG.getInt("MCR.FileUpload.Port", defPort);
+
+            LOGGER.info("Opening server socket: ip=" + serverIP + " port="
+                    + serverPort);
+            server = new ServerSocket(serverPort, 1, InetAddress
+                    .getByName(serverIP));
+            LOGGER.debug("Server socket successfully created.");
+
+            // Starts separate thread that will receive and store file content
+            new Thread(this).start();
+        } catch (Exception ex) {
+            if (ex instanceof MCRException)
+                throw (MCRException) ex;
+
+            String msg = "Exception while opening file upload server port";
+            throw new MCRConfigurationException(msg, ex);
+        }
+    }
+
+    public void finalize() throws Throwable {
+        try {
+            if (server != null)
+                server.close();
+        } catch (Exception ignored) {
+        }
+        super.finalize();
+    }
+
+    public void run() {
+        LOGGER.debug("Server socket thread startet.");
+        while (true) {
+            Socket socket = null;
+
+            try {
+                LOGGER.debug("Listening on " + serverIP + ":" + serverPort
+                        + " for incoming data...");
+                socket = server.accept();
+
+                LOGGER.info("Client applet connected to socket now.");
+
+                DataOutputStream dos = new DataOutputStream(socket
+                        .getOutputStream());
+                ZipInputStream zis = new ZipInputStream(socket.getInputStream());
+
+                LOGGER
+                        .debug("Constructed ZipInputStream and DataOutputStream, receiving data soon.");
+
+                ZipEntry ze = zis.getNextEntry();
+                String path = URLDecoder.decode(ze.getName(), "UTF-8");
+                String uploadId = ze.getComment();
+
+                MCRUploadHandlerManager.instance().getHandle(uploadId)
+                        .receiveFile(path, zis);
+                LOGGER.debug("Stored incoming file content");
+
+                dos.writeUTF("OK");
+                dos.flush();
+
+                LOGGER.info("File transfer completed successfully.");
+            } catch (Exception ex) {
+                LOGGER
+                        .error(
+                                "Exception while receiving and storing file content from applet:",
+                                ex);
+            } finally {
+                try {
+                    if (socket != null)
+                        socket.close();
+                } catch (Exception ignored) {
+                }
+
+                LOGGER.debug("Socket closed.");
+            }
+        }
+    }
 
     public void doGetPost(MCRServletJob job) throws Exception {
         try {
@@ -84,8 +176,8 @@ public final class MCRUploadServlet extends MCRServlet {
         } else if (method.equals("startDerivateSession String int")) {
             String uploadId = req.getParameter("uploadId");
             int numFiles = Integer.parseInt(req.getParameter("numFiles"));
-            MCRUploadHandlerManager.instance().getHandle(uploadId)
-                    .startUpload(numFiles);
+            MCRUploadHandlerManager.instance().getHandle(uploadId).startUpload(
+                    numFiles);
             LOGGER.info("MCRUploadServlet start session " + uploadId);
             sendResponse(res, "OK");
         } else if (method.equals("createFile String")) {
@@ -104,77 +196,9 @@ public final class MCRUploadServlet extends MCRServlet {
                 return;
             }
 
-            String servername = req.getServerName();
-
             LOGGER.debug("Applet wants to send content of file " + path);
-            LOGGER
-                    .debug("Next trying to create a server socket for file transfer...");
+            sendResponse(res, serverIP + ":" + serverPort);
 
-            final ServerSocket server = new ServerSocket(0, 1, InetAddress
-                    .getByName(servername));
-            LOGGER.debug("Server socket successfully created.");
-
-            final int port = server.getLocalPort();
-            final String host = server.getInetAddress().getHostAddress();
-
-            LOGGER.debug("Informing applet that server socket is ready.");
-
-            sendResponse(res, host + ":" + port);
-
-            // Define a separate Thread that accepts the file content,
-            // otherwise Tomcat will not finish the HTTP response correctly
-            Thread thread = new Thread(new Runnable() {
-                public void run() {
-                    Socket socket = null;
-
-                    try {
-                        LOGGER.debug("Listening on " + host + ":" + port
-                                + " for incoming data...");
-                        socket = server.accept();
-
-                        LOGGER.debug("Client applet connected to socket now.");
-
-                        DataOutputStream dos = new DataOutputStream(socket
-                                .getOutputStream());
-                        ZipInputStream zis = new ZipInputStream(socket
-                                .getInputStream());
-
-                        LOGGER
-                                .debug("Constructed ZipInputStream and DataOutputStream, receiving data soon.");
-
-                        zis.getNextEntry();
-
-                        MCRUploadHandlerManager.instance().getHandle(uploadId)
-                                .receiveFile(path, zis);
-                        LOGGER.debug("Stored incoming file content");
-                        LOGGER
-                                .debug("Informing applet about location where content was stored...");
-
-                        dos.writeUTF("OK");
-
-                        LOGGER.debug("Sended acknowledgement to applet.");
-                        LOGGER.debug("File transfer completed successfully.");
-                    } catch (Exception ex) {
-                        LOGGER
-                                .error(
-                                        "Exception while receiving and storing file content from applet:",
-                                        ex);
-                    } finally {
-                        try {
-                            if (socket != null)
-                                socket.close();
-                            if (server != null)
-                                server.close();
-                        } catch (Exception ignored) {
-                        }
-
-                        LOGGER.debug("Socket closed.");
-                    }
-                }
-            });
-
-            thread.start(); // Starts separate thread that will receive and
-            // store file content
         } else if (method.equals("endDerivateSession String")) {
             String uploadId = req.getParameter("uploadId");
             MCRUploadHandlerInterface uploadHandler = MCRUploadHandlerManager
