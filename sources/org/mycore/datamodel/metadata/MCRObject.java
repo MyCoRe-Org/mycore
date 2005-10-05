6415448e6bae46a8d89c7f901c440821acf11629
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2005-10-05 11:34:49 +0000

    Add the update event to repair.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@5789 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/metadata/MCRObject.java
+++ sources/org/mycore/datamodel/metadata/MCRObject.java
@@ -867,6 +867,10 @@ final public class MCRObject extends MCRBase {
         mcr_persist.update(this);
         deleteLinksFromTable();
         addLinksToTable();
+        // handle events
+        MCREvent evt = new MCREvent(MCREvent.OBJECT_TYPE, MCREvent.UPDATE_EVENT);
+        evt.put("object", this);
+        MCREventManager.instance().handleEvent(evt);
     }
 
     /**
