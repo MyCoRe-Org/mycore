502a83c3bcc178a95f932f5fc17c6d4ca0675625
Thomas Scheffler <github@yagee.de>
2005-11-09 08:29:39 +0000

    added MCRDirectoryXML as a common used part of the FileNodeServlet
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6062 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/datamodel/ifs/MCRDirectoryXML.java
@@ -0,0 +1,345 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+package org.mycore.datamodel.ifs;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
+
+import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+
+import org.mycore.backend.remote.MCRRemoteAccessInterface;
+import org.mycore.common.MCRConfiguration;
+import org.mycore.common.xml.MCRXMLContainer;
+
+/**
+ * @author Thomas Scheffler (yagee)
+ * @author Frank Lützenkirchen
+ * 
+ * @version $Revision$ $Date$
+ */
+public class MCRDirectoryXML {
+
+    static Logger LOGGER = Logger.getLogger(MCRDirectoryXML.class);
+
+    private static final String dateFormat = "dd.MM.yyyy HH:mm:ss";
+
+    private static final DateFormat dateFormatter = new SimpleDateFormat(dateFormat);
+
+    private static final String timeFormat = "HH:mm:ss";
+
+    private static final DateFormat timeFormatter = new SimpleDateFormat(timeFormat);
+
+    private static MCRConfiguration CONFIG = MCRConfiguration.instance();
+
+    private static ArrayList remoteAliasList;
+
+    protected static MCRDirectoryXML SINGLETON;
+
+    /**
+     *  
+     */
+    private MCRDirectoryXML() {
+        super();
+        init();
+    }
+
+    public static MCRDirectoryXML getInstance() {
+        if (SINGLETON == null) {
+            SINGLETON = new MCRDirectoryXML();
+        }
+        return SINGLETON;
+    }
+
+    /**
+     * Sends the contents of an MCRDirectory as XML data to the client
+     */
+    protected Document getDirectoryXML(MCRDirectory dir){
+        LOGGER.info("MCRDirectoryXML: listing of directory " + dir.getName());
+
+        Element root = new Element("mcr_directory");
+        Document doc = new org.jdom.Document(root);
+
+        root.setAttribute("ID", dir.getID());
+
+        addString(root, "path", dir.getPath());
+        addString(root, "ownerID", dir.getOwnerID());
+        addDate(root, "lastModified", dir.getLastModified());
+        addString(root, "size", String.valueOf(dir.getSize()));
+
+        Element numChildren = new Element("numChildren");
+        root.addContent(numChildren);
+
+        Element ncHere = new Element("here");
+        numChildren.addContent(ncHere);
+
+        addString(ncHere, "directories", String.valueOf(dir.getNumChildren(MCRDirectory.DIRECTORIES, MCRDirectory.HERE)));
+        addString(ncHere, "files", String.valueOf(dir.getNumChildren(MCRDirectory.FILES, MCRDirectory.HERE)));
+
+        Element ncTotal = new Element("total");
+        numChildren.addContent(ncTotal);
+
+        addString(ncTotal, "directories", String.valueOf(dir.getNumChildren(MCRDirectory.DIRECTORIES, MCRDirectory.TOTAL)));
+        addString(ncTotal, "files", String.valueOf(dir.getNumChildren(MCRDirectory.FILES, MCRDirectory.TOTAL)));
+
+        Element nodes = new Element("children");
+        root.addContent(nodes);
+
+        MCRFilesystemNode[] children = dir.getChildren();
+
+        for (int i = 0; i < children.length; i++) {
+            Element node = new Element("child");
+            node.setAttribute("ID", children[i].getID());
+            nodes.addContent(node);
+
+            addString(node, "name", children[i].getName());
+            addString(node, "size", String.valueOf(children[i].getSize()));
+            addDate(node, "lastModified", children[i].getLastModified());
+
+            if (children[i] instanceof MCRFile) {
+                node.setAttribute("type", "file");
+
+                MCRFile file = (MCRFile) (children[i]);
+                addString(node, "contentType", file.getContentTypeID());
+                addString(node, "md5", file.getMD5());
+
+                if (file.hasAudioVideoExtender()) {
+                    MCRAudioVideoExtender ext = file.getAudioVideoExtender();
+
+                    Element xExtender = new Element("extender");
+                    node.addContent(xExtender);
+                    addExtenderData(xExtender, ext);
+                }
+            } else {
+                node.setAttribute("type", "directory");
+            }
+        }
+
+        // put it in an MCRXMLContainer
+        MCRXMLContainer resarray = new MCRXMLContainer();
+        resarray.add("local", dir.getOwnerID(), 1, doc.getRootElement());
+
+        return resarray.exportAllToDocument();
+
+    }
+
+    private void addDate(Element parent, String type, GregorianCalendar date) {
+        Element xDate = new Element("date");
+        parent.addContent(xDate);
+
+        xDate.setAttribute("type", type);
+
+        String time = dateFormatter.format(date.getTime());
+
+        xDate.setAttribute("format", dateFormat);
+        xDate.addContent(time);
+    }
+
+    private void addString(Element parent, String itemName, String content) {
+        if ((content == null) || (content.trim().length() == 0)) {
+            return;
+        }
+
+        parent.addContent(new Element(itemName).addContent(content.trim()));
+    }
+
+    private void addExtenderData(Element parent, MCRAudioVideoExtender ext) {
+        parent.setAttribute("type", ext.isVideo() ? "video" : "audio");
+
+        int hh = ext.getDurationHours();
+        int mm = ext.getDurationMinutes();
+        int ss = ext.getDurationSeconds();
+        addTime(parent, "duration", hh, mm, ss);
+
+        addString(parent, "bitRate", String.valueOf(ext.getBitRate()));
+
+        if (ext.isVideo()) {
+            addString(parent, "frameRate", String.valueOf(ext.getFrameRate()));
+        }
+
+        addString(parent, "playerURL", ext.getPlayerDownloadURL());
+    }
+
+    private void addTime(Element parent, String type, int hh, int mm, int ss) {
+        Element xTime = new Element(type);
+        parent.addContent(xTime);
+
+        GregorianCalendar date = new GregorianCalendar(2002, 01, 01, hh, mm, ss);
+        String time = timeFormatter.format(date.getTime());
+
+        xTime.setAttribute("format", timeFormat);
+        xTime.addContent(time);
+    }
+
+    /**
+     * Handles the HTTP request
+     */
+    public Document getDirectory(String requestPath, String hostAlias) {
+
+        if ((hostAlias == null) || (hostAlias.trim().length() == 0)) {
+            hostAlias = "local";
+        }
+
+        LOGGER.debug("MCRFileNodeServlet : host = " + hostAlias);
+
+        if (!remoteAliasList.contains(hostAlias)) {
+            String msg = "Error: request host is not in the alias list: " + hostAlias;
+            LOGGER.error(msg);
+
+            return getErrorDocument(msg);
+        }
+
+        LOGGER.info("MCRDirectoryResolver: request path = " + requestPath);
+
+        if (requestPath == null) {
+            String msg = "Error: request path is null";
+            LOGGER.error(msg);
+
+            return getErrorDocument(msg);
+        }
+
+        StringTokenizer st = new StringTokenizer(requestPath, "/");
+
+        if (!st.hasMoreTokens()) {
+            String msg = "Error: request path is empty";
+            LOGGER.error(msg);
+
+            return getErrorDocument(msg);
+        }
+
+        String ownerID = st.nextToken();
+
+        if (hostAlias.equals("local")) {
+            // local node to be retrieved
+            MCRFilesystemNode root;
+
+            try {
+                root = MCRFilesystemNode.getRootNode(ownerID);
+            } catch (org.mycore.common.MCRPersistenceException e) {
+                // Could not get value from JDBC result set
+                LOGGER.error("MCRFileNodeServlet: Error while getting root node!", e);
+                root = null;
+            }
+
+            if (root == null) {
+                String msg = "Error: No root node found for owner ID " + ownerID;
+                LOGGER.error(msg);
+
+                return getErrorDocument(msg);
+            }
+
+            if (root instanceof MCRFile) {
+                String msg = "Error: root node is a file: " + root.getName();
+                LOGGER.error(msg);
+
+                return getErrorDocument(msg);
+            }
+
+            // root node is a directory
+            int pos = ownerID.length() + 1;
+            String path = requestPath.substring(pos);
+
+            MCRDirectory dir = (MCRDirectory) root;
+            MCRFilesystemNode node = dir.getChildByPath(path);
+
+            if (node == null) {
+                String msg = "Error: No such file or directory " + path;
+                LOGGER.error(msg);
+
+                return getErrorDocument(msg);
+            } else if (node instanceof MCRFile) {
+                String msg = "Error: node is a file: " + root.getName();
+                LOGGER.error(msg);
+
+                return getErrorDocument(msg);
+            } else {
+                return getDirectoryXML((MCRDirectory) node);
+            }
+        }
+
+        // remote node to be retrieved
+        String prop = "MCR.remoteaccess_" + hostAlias + "_query_class";
+        MCRRemoteAccessInterface comm = (MCRRemoteAccessInterface) (CONFIG.getInstanceOf(prop));
+
+        BufferedInputStream in = comm.requestIFS(hostAlias, requestPath);
+
+        if (in == null) {
+            String msg = "Error: cannot get Inputstream from remote interface of: " + hostAlias;
+            LOGGER.error(msg);
+
+            return getErrorDocument(msg);
+        }
+
+        org.jdom.input.SAXBuilder builder = new org.jdom.input.SAXBuilder();
+
+        Document jdom;
+
+        try {
+            jdom = builder.build(in);
+        } catch (org.jdom.JDOMException ignores) {
+            jdom = getErrorDocument(ignores.getMessage());
+        } catch (IOException ignores) {
+        jdom = getErrorDocument(ignores.getMessage());
+        }
+
+        return jdom;
+
+    }
+
+    /**
+     * Initializes the servlet and reads the default language and the remote
+     * host list from the configuration.
+     */
+    public void init() {
+
+        // read host list from configuration
+        String hostconf = CONFIG.getString("MCR.remoteaccess_hostaliases", "local");
+        remoteAliasList = new ArrayList();
+
+        if (hostconf.indexOf("local") < 0) {
+            remoteAliasList.add("local");
+        }
+
+        StringTokenizer st = new StringTokenizer(hostconf, ", ");
+
+        while (st.hasMoreTokens())
+            remoteAliasList.add(st.nextToken());
+    }
+
+    /**
+     * returns a error document to display error messages
+     * TODO:should be extended to provide stacktraces etc.
+     * @return JDOM Document with root element "mcr_error"
+     */
+    private Document getErrorDocument(String msg) {
+        return new Document(new Element("mcr_error").setText(msg));
+    }
+
+}
--- sources/org/mycore/datamodel/ifs/MCRFileNodeServlet.java
+++ sources/org/mycore/datamodel/ifs/MCRFileNodeServlet.java
@@ -27,10 +27,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
@@ -41,9 +38,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.jdom.Document;
-import org.jdom.Element;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
+
 import org.mycore.backend.remote.MCRRemoteAccessInterface;
 import org.mycore.common.MCRConfigurationException;
 import org.mycore.common.MCRException;
@@ -322,71 +319,7 @@ public class MCRFileNodeServlet extends MCRServlet {
      */
     private void sendDirectory(HttpServletRequest req, HttpServletResponse res, MCRDirectory dir, String lang) throws IOException, ServletException {
         LOGGER.info("MCRFileNodeServlet: Sending listing of directory " + dir.getName());
-
-        Element root = new Element("mcr_directory");
-        Document doc = new org.jdom.Document(root);
-
-        root.setAttribute("ID", dir.getID());
-
-        addString(root, "path", dir.getPath());
-        addString(root, "ownerID", dir.getOwnerID());
-        addDate(root, "lastModified", dir.getLastModified());
-        addString(root, "size", String.valueOf(dir.getSize()));
-
-        Element numChildren = new Element("numChildren");
-        root.addContent(numChildren);
-
-        Element ncHere = new Element("here");
-        numChildren.addContent(ncHere);
-
-        addString(ncHere, "directories", String.valueOf(dir.getNumChildren(MCRDirectory.DIRECTORIES, MCRDirectory.HERE)));
-        addString(ncHere, "files", String.valueOf(dir.getNumChildren(MCRDirectory.FILES, MCRDirectory.HERE)));
-
-        Element ncTotal = new Element("total");
-        numChildren.addContent(ncTotal);
-
-        addString(ncTotal, "directories", String.valueOf(dir.getNumChildren(MCRDirectory.DIRECTORIES, MCRDirectory.TOTAL)));
-        addString(ncTotal, "files", String.valueOf(dir.getNumChildren(MCRDirectory.FILES, MCRDirectory.TOTAL)));
-
-        Element nodes = new Element("children");
-        root.addContent(nodes);
-
-        MCRFilesystemNode[] children = dir.getChildren();
-
-        for (int i = 0; i < children.length; i++) {
-            Element node = new Element("child");
-            node.setAttribute("ID", children[i].getID());
-            nodes.addContent(node);
-
-            addString(node, "name", children[i].getName());
-            addString(node, "size", String.valueOf(children[i].getSize()));
-            addDate(node, "lastModified", children[i].getLastModified());
-
-            if (children[i] instanceof MCRFile) {
-                node.setAttribute("type", "file");
-
-                MCRFile file = (MCRFile) (children[i]);
-                addString(node, "contentType", file.getContentTypeID());
-                addString(node, "md5", file.getMD5());
-
-                if (file.hasAudioVideoExtender()) {
-                    MCRAudioVideoExtender ext = file.getAudioVideoExtender();
-
-                    Element xExtender = new Element("extender");
-                    node.addContent(xExtender);
-                    addExtenderData(xExtender, ext);
-                }
-            } else {
-                node.setAttribute("type", "directory");
-            }
-        }
-
-        // put it in an MCRXMLContainer
-        MCRXMLContainer resarray = new MCRXMLContainer();
-        resarray.add("local", dir.getOwnerID(), 1, doc.getRootElement());
-
-        org.jdom.Document jdom = resarray.exportAllToDocument();
-
+        Document jdom=MCRDirectoryXML.getInstance().getDirectoryXML(dir);
         // prepare the stylesheet name
         Properties parameters = MCRLayoutServlet.buildXSLParameters(req);
         String style = parameters.getProperty("Style", "IFSMetadata-" + lang);
@@ -405,61 +338,5 @@ public class MCRFileNodeServlet extends MCRServlet {
             RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
             rd.forward(req, res);
         }
-    }
-
-    private String dateFormat = "dd.MM.yyyy HH:mm:ss";
-
-    private DateFormat dateFormatter = new SimpleDateFormat(dateFormat);
-
-    private void addDate(Element parent, String type, GregorianCalendar date) {
-        Element xDate = new Element("date");
-        parent.addContent(xDate);
-
-        xDate.setAttribute("type", type);
-
-        String time = dateFormatter.format(date.getTime());
-
-        xDate.setAttribute("format", dateFormat);
-        xDate.addContent(time);
-    }
-
-    private String timeFormat = "HH:mm:ss";
-
-    private DateFormat timeFormatter = new SimpleDateFormat(timeFormat);
-
-    private void addTime(Element parent, String type, int hh, int mm, int ss) {
-        Element xTime = new Element(type);
-        parent.addContent(xTime);
-
-        GregorianCalendar date = new GregorianCalendar(2002, 01, 01, hh, mm, ss);
-        String time = timeFormatter.format(date.getTime());
-
-        xTime.setAttribute("format", timeFormat);
-        xTime.addContent(time);
-    }
-
-    private void addString(Element parent, String itemName, String content) {
-        if ((content == null) || (content.trim().length() == 0)) {
-            return;
-        }
-
-        parent.addContent(new Element(itemName).addContent(content.trim()));
-    }
-
-    private void addExtenderData(Element parent, MCRAudioVideoExtender ext) {
-        parent.setAttribute("type", ext.isVideo() ? "video" : "audio");
-
-        int hh = ext.getDurationHours();
-        int mm = ext.getDurationMinutes();
-        int ss = ext.getDurationSeconds();
-        addTime(parent, "duration", hh, mm, ss);
-
-        addString(parent, "bitRate", String.valueOf(ext.getBitRate()));
-
-        if (ext.isVideo()) {
-            addString(parent, "frameRate", String.valueOf(ext.getFrameRate()));
-        }
-
-        addString(parent, "playerURL", ext.getPlayerDownloadURL());
     }
 }
