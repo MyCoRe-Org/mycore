617a0a70113f92771855e612298a8669c5490d2d
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2005-11-25 13:55:58 +0000

    added a default rule for the access to the mcradminservlet, so no privilege is needed, attention: this rule cannot be edited via the rule-editor, it's just a rule for the string-id "MCRAdminServlet"
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6201 7089d063-bc14-496d-ac90-53de1872eb6c

--- modules/module-accessadmin/sources/org/mycore/frontend/servlets/MCRAdminServlet.java
+++ modules/module-accessadmin/sources/org/mycore/frontend/servlets/MCRAdminServlet.java
@@ -1,16 +1,30 @@
 package org.mycore.frontend.servlets;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.mycore.access.MCRAccessRule;
+import org.mycore.access.MCRAccessStore;
+import org.mycore.access.MCRRuleMapping;
+import org.mycore.access.MCRRuleStore;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.services.query.MCRQueryCache;
 
 public class MCRAdminServlet extends MCRServlet{
+	
+	private static boolean isDefaultAccessRuleCreated ;
+	protected final static Logger LOGGER = Logger.getLogger(MCRAdminServlet.class);
+
+	public void init() throws ServletException {
+		super.init();
+		isDefaultAccessRuleCreated = createAdminDefaultRule();
+	}
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
@@ -58,4 +72,45 @@ public class MCRAdminServlet extends MCRServlet{
         }
         
     }
+    
+	/**
+	 * sets a default-rule for the use of the MCRAdminServlet
+	 * 
+	 * @param objid
+	 * @param userid
+	 * @return boolean  false if there was an Exception
+	 */
+	public static boolean createAdminDefaultRule() {
+		try {
+			MCRAccessStore accessStore = (MCRAccessStore) Class.forName(CONFIG.getString("MCR.accessstore_class_name")).newInstance();
+			MCRRuleStore ruleStore = (MCRRuleStore) Class.forName(CONFIG.getString("MCR.rulestore_class_name")).newInstance();			
+			String ruleID = "STANDARD-ACCESS-ADMIN-RULE";
+			MCRAccessRule rule = ruleStore.getRule(ruleID);
+			if (rule == null || rule.equals("")) {
+				StringBuffer ruleExpression = new StringBuffer("(user administrator)");
+				rule = new MCRAccessRule(ruleID, "administrator", 
+						new Date(),ruleExpression.toString(),
+						"PoolRight only for the user administrator'");
+				ruleStore.createRule(rule);	
+				LOGGER.info("Rule created: " + ruleID);
+			}
+
+			MCRRuleMapping ruleMapping = new MCRRuleMapping();
+			ruleMapping.setCreator("administrator");
+			ruleMapping.setCreationdate(new Date());
+			ruleMapping.setPool("modify");
+			ruleMapping.setRuleId(ruleID);
+			ruleMapping.setObjId("MCRAdminServlet");
+			
+			String anotherRuleID = accessStore.getRuleID("MCRAdminServlet","modify");
+			if(anotherRuleID == null || anotherRuleID.equals("")) {
+				accessStore.createAccessDefinition(ruleMapping);
+				LOGGER.info("Following rule was created for StringID MCRAdminServlet" 
+						+ " in the pool 'modify': " + ruleID);
+			}
+		} catch (Exception e) {
+			return false;
+		}				
+		return true;
+	}    
 }
