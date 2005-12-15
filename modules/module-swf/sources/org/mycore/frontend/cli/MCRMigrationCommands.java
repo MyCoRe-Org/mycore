5460999be775f52e2ef7daca694d0e82a9f24b1e
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2005-12-15 13:00:45 +0000

    catch the Exception
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6416 7089d063-bc14-496d-ac90-53de1872eb6c

--- modules/module-swf/sources/org/mycore/frontend/cli/MCRMigrationCommands.java
+++ modules/module-swf/sources/org/mycore/frontend/cli/MCRMigrationCommands.java
@@ -27,6 +27,8 @@ import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 import org.mycore.common.MCRException;
+import org.mycore.common.MCRPersistenceException;
+import org.mycore.datamodel.metadata.MCRActiveLinkException;
 import org.mycore.datamodel.metadata.MCRXMLTableManager;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectService;
@@ -41,124 +43,133 @@ import org.mycore.frontend.workflow.MCRSimpleWorkflowAccess;
  * @version $Revision$ $Date$
  */
 final class MCRMigrationCommands extends MCRAbstractCommands {
-    // logger
-    private static final Logger LOGGER = Logger.getLogger(MCRMigrationCommands.class);
+	// logger
+	private static final Logger LOGGER = Logger.getLogger(MCRMigrationCommands.class);
 
-    /**
-     * The constructor.
-     */
-    public MCRMigrationCommands() {
-        super();
+	/**
+	 * The constructor.
+	 */
+	public MCRMigrationCommands() {
+		super();
 
-        MCRCommand com = null;
+		MCRCommand com = null;
 
-        com = new MCRCommand("migrate xml access data of type {0}", "org.mycore.frontend.cli.MCRMigrationCommands.mirgateXMLToAccessSystem String", "The command imigrate all servflag access data to the access system of MyCoRe 1.3.");
-        command.add(com);
-    }
+		com = new MCRCommand("migrate xml access data of type {0}", "org.mycore.frontend.cli.MCRMigrationCommands.mirgateXMLToAccessSystem String", "The command imigrate all servflag access data to the access system of MyCoRe 1.3.");
+		command.add(com);
+	}
 
-    /**
-     * The method migrate the old SimpleWorkflow-servflag-data to the new
-     * MCRObject data structure of the Access System of version 1.3. It put all
-     * <ul>
-     * <li><em>User:</em> date to the <b>servusers</b>-XML-element.</li>
-     * <li><em>IP:</em> date to the <b>servnet</b>-XML-element.</li>
-     * <li>and add the group of the <em>Access:</em> to the <b>servgroup</b>-XML-element.</li>
-     * 
-     * @param type
-     *            the type of the MCRObjects
-     */
-    public static void mirgateXMLToAccessSystem(String type) throws MCRException {
-        LOGGER.info("Start the migration for access system for type " + type);
+	/**
+	 * The method migrate the old SimpleWorkflow-servflag-data to the new
+	 * MCRObject data structure of the Access System of version 1.3. It put all
+	 * <ul>
+	 * <li><em>User:</em> date to the <b>servusers</b>-XML-element.</li>
+	 * <li><em>IP:</em> date to the <b>servnet</b>-XML-element.</li>
+	 * <li>and add the group of the <em>Access:</em> to the <b>servgroup</b>-XML-element.</li>
+	 * 
+	 * @param type
+	 *            the type of the MCRObjects
+	 */
+	public static void mirgateXMLToAccessSystem(String type) throws MCRException {
+		LOGGER.info("Start the migration for access system for type " + type);
 
-        String typetest = CONFIG.getString("MCR.type_" + type, "");
+		String typetest = CONFIG.getString("MCR.type_" + type, "");
 
-        if (typetest.length() == 0) {
-            LOGGER.error("The type " + type + " was not found.");
-            LOGGER.info(" ");
+		if (typetest.length() == 0) {
+			LOGGER.error("The type " + type + " was not found.");
+			LOGGER.info(" ");
 
-            return;
-        }
+			return;
+		}
 
-        // XML table manager
-        MCRXMLTableManager mcr_xml = MCRXMLTableManager.instance();
-        ArrayList ar = mcr_xml.retrieveAllIDs(type);
-        String stid = null;
+		// XML table manager
+		MCRXMLTableManager mcr_xml = MCRXMLTableManager.instance();
+		ArrayList ar = mcr_xml.retrieveAllIDs(type);
+		String stid = null;
 
-        MCRObject obj;
-        for (int i = 0; i < ar.size(); i++) {
-            stid = (String) ar.get(i);
+		MCRObject obj;
+		for (int i = 0; i < ar.size(); i++) {
+			stid = (String) ar.get(i);
 
-            // read data
-            obj = new MCRObject();
-            obj.receiveFromDatastore(stid);
+			// read data
+			obj = new MCRObject();
+			obj.receiveFromDatastore(stid);
 
-            // migrate
-            MCRObjectService serv = obj.getService();
-            for (int j = 0; j < serv.getFlagSize(); j++) {
-                if (serv.getFlag(j).startsWith("Access:")) {
-                    String priv = serv.getFlag(j).substring(7, serv.getFlag(j).length());
-                    MCRUserMgr umgr = MCRUserMgr.instance();
-                    // read access for all groups with the 'Access:'-privileg
-                    ArrayList gar = umgr.getAllGroupIDs();
-                    for (int k = 0; k < gar.size(); k++) {
-                        if (umgr.hasGroupPrivilege((String) gar.get(k), priv)) {
-                            serv.addGroup((String) gar.get(k), MCRSimpleWorkflowAccess.READWEB_POOL);
-                        }
-                    }
-                    // serv.removeFlag(j);
-                }
-                if (serv.getFlag(j).startsWith("User:")) {
-                    String user = serv.getFlag(j).substring(5, serv.getFlag(j).length());
-                    MCRUserMgr umgr = MCRUserMgr.instance();
-                    // read access for all groups of the user
-                    // Write, delete, commit acces for the master group wit editor privilege
-                    ArrayList gg = umgr.getGroupsContainingUser(user);
-                    for (int k = 0; k < gg.size(); k++) {
-                        serv.addGroup((String) gg.get(k), MCRSimpleWorkflowAccess.READWEB_POOL);
-                        ArrayList mg = umgr.retrieveGroup((String)gg.get(k)).getMemberGroupIDs();
-                        for (int l = 0; l < mg.size(); l++) {
-                            if (umgr.hasGroupPrivilege((String)mg.get(l), "editor")) {
-                                serv.addGroup((String)mg.get(l), MCRSimpleWorkflowAccess.WRITEDB_POOL);
-                                serv.addGroup((String)mg.get(l), MCRSimpleWorkflowAccess.WRITEWF_POOL);
-                                serv.addGroup((String)mg.get(l), MCRSimpleWorkflowAccess.DELETEDB_POOL);
-                                serv.addGroup((String)mg.get(l), MCRSimpleWorkflowAccess.DELETEWF_POOL);
-                                serv.addGroup((String)mg.get(l), MCRSimpleWorkflowAccess.COMMITDB_POOL);
-                            }
-                        }
-                    }
-                    // write access for the user if he has the modify privilege
-                    if (umgr.hasUserPrivilege(user, "modify-" + type)) {
-                        serv.addUser(user, MCRSimpleWorkflowAccess.WRITEDB_POOL);
-                        serv.addUser(user, MCRSimpleWorkflowAccess.WRITEWF_POOL);
-                    }
-                    // delete access for the user if he has the delete privilege
-                    if (umgr.hasUserPrivilege(user, "delete-" + type)) {
-                        serv.addUser(user, MCRSimpleWorkflowAccess.DELETEDB_POOL);
-                        serv.addUser(user, MCRSimpleWorkflowAccess.DELETEWF_POOL);
-                    }
-                    // commit access for the user if he has the commit privilege
-                    if (umgr.hasUserPrivilege(user, "commit-" + type)) {
-                        serv.addUser(user, MCRSimpleWorkflowAccess.COMMITDB_POOL);
-                    }
-                    // serv.removeFlag(j);
-                }
-                if (serv.getFlag(j).startsWith("IP:")) {
-                    serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.READWEB_POOL,"ip");
-                    serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.WRITEDB_POOL,"ip");
-                    serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.WRITEWF_POOL,"ip");
-                    serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.DELETEDB_POOL,"ip");
-                    serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.DELETEWF_POOL,"ip");
-                    serv.removeFlag(j);
-                }
-            }
+			// migrate
+			MCRObjectService serv = obj.getService();
+			for (int j = 0; j < serv.getFlagSize(); j++) {
+				if (serv.getFlag(j).startsWith("Access:")) {
+					String priv = serv.getFlag(j).substring(7, serv.getFlag(j).length());
+					MCRUserMgr umgr = MCRUserMgr.instance();
+					// read access for all groups with the 'Access:'-privileg
+					ArrayList gar = umgr.getAllGroupIDs();
+					for (int k = 0; k < gar.size(); k++) {
+						if (umgr.hasGroupPrivilege((String) gar.get(k), priv)) {
+							serv.addGroup((String) gar.get(k), MCRSimpleWorkflowAccess.READWEB_POOL);
+						}
+					}
+					// serv.removeFlag(j);
+				}
+				if (serv.getFlag(j).startsWith("User:")) {
+					String user = serv.getFlag(j).substring(5, serv.getFlag(j).length());
+					MCRUserMgr umgr = MCRUserMgr.instance();
+					// read access for all groups of the user
+					// Write, delete, commit acces for the master group wit
+					// editor privilege
+					ArrayList gg = umgr.getGroupsContainingUser(user);
+					for (int k = 0; k < gg.size(); k++) {
+						serv.addGroup((String) gg.get(k), MCRSimpleWorkflowAccess.READWEB_POOL);
+						ArrayList mg = umgr.retrieveGroup((String) gg.get(k)).getMemberGroupIDs();
+						for (int l = 0; l < mg.size(); l++) {
+							if (umgr.hasGroupPrivilege((String) mg.get(l), "editor")) {
+								serv.addGroup((String) mg.get(l), MCRSimpleWorkflowAccess.WRITEDB_POOL);
+								serv.addGroup((String) mg.get(l), MCRSimpleWorkflowAccess.WRITEWF_POOL);
+								serv.addGroup((String) mg.get(l), MCRSimpleWorkflowAccess.DELETEDB_POOL);
+								serv.addGroup((String) mg.get(l), MCRSimpleWorkflowAccess.DELETEWF_POOL);
+								serv.addGroup((String) mg.get(l), MCRSimpleWorkflowAccess.COMMITDB_POOL);
+							}
+						}
+					}
+					// write access for the user if he has the modify privilege
+					if (umgr.hasUserPrivilege(user, "modify-" + type)) {
+						serv.addUser(user, MCRSimpleWorkflowAccess.WRITEDB_POOL);
+						serv.addUser(user, MCRSimpleWorkflowAccess.WRITEWF_POOL);
+					}
+					// delete access for the user if he has the delete privilege
+					if (umgr.hasUserPrivilege(user, "delete-" + type)) {
+						serv.addUser(user, MCRSimpleWorkflowAccess.DELETEDB_POOL);
+						serv.addUser(user, MCRSimpleWorkflowAccess.DELETEWF_POOL);
+					}
+					// commit access for the user if he has the commit privilege
+					if (umgr.hasUserPrivilege(user, "commit-" + type)) {
+						serv.addUser(user, MCRSimpleWorkflowAccess.COMMITDB_POOL);
+					}
+					// serv.removeFlag(j);
+				}
+				if (serv.getFlag(j).startsWith("IP:")) {
+					serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.READWEB_POOL, "ip");
+					serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.WRITEDB_POOL, "ip");
+					serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.WRITEWF_POOL, "ip");
+					serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.DELETEDB_POOL, "ip");
+					serv.addIP(serv.getFlag(j), MCRSimpleWorkflowAccess.DELETEWF_POOL, "ip");
+					serv.removeFlag(j);
+				}
+			}
 
-            // save data
-            obj.updateInDatastore();
-            LOGGER.info("Migrated " + (String) ar.get(i));
+			// save data
+			try {
+				obj.updateInDatastore();
+			} catch (MCRPersistenceException e) {
+				LOGGER.error("Migration error (MCRActiveLinkException) " + (String) ar.get(i));
+				continue;
+			} catch (MCRActiveLinkException e) {
+				LOGGER.error("Migration error (MCRActiveLinkException) " + (String) ar.get(i));
+				continue;
+			}
+			LOGGER.info("Migrated " + (String) ar.get(i));
 
-        }
+		}
 
-        LOGGER.info(" ");
-    }
+		LOGGER.info(" ");
+	}
 
 }
