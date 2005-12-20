0564a6e4c5e91fc5d1f9ee8b26a0d33a83709137
Harald Richter <mcrhrich@duepub-1.zim.uni-duisburg-essen.de>
2005-12-20 13:06:29 +0000

    Method MCRDoQuery added
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6466 7089d063-bc14-496d-ac90-53de1872eb6c

--- modules/module-webservices/sources/org/mycore/services/webservices/MCRWebService.java
+++ modules/module-webservices/sources/org/mycore/services/webservices/MCRWebService.java
@@ -1,8 +1,9 @@
-/**
+/*
  * $RCSfile$
  * $Revision$ $Date$
  *
- * Copyright (C) 2005 University of Essen, Germany
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
  *
  * This program is free software; you can use it, redistribute it
  * and / or modify it under the terms of the GNU General Public License
@@ -15,11 +16,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program, normally in the file documentation/license.txt.
+ * along with this program, in a file called gpl.txt or license.txt.
  * If not, write to the Free Software Foundation Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
- *
- **/
+ */
 
 package org.mycore.services.webservices;
 
@@ -28,6 +28,7 @@ import java.util.*;
 import java.io.*;
 
 import org.apache.log4j.Logger;
+import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.output.*;
 
@@ -46,6 +47,8 @@ import org.mycore.datamodel.metadata.MCRXMLTableManager;
 import org.mycore.parsers.bool.MCRCondition;
 import org.mycore.services.fieldquery.MCRQueryParser;
 import org.mycore.services.fieldquery.MCRResults;
+import org.mycore.services.fieldquery.MCRSearcher;
+import org.mycore.services.fieldquery.MCRSearcherFactory;
 
 
 /**
@@ -75,37 +78,94 @@ public class MCRWebService
   {
     try
     {
-      
+
       // check the ID and retrieve the data
       MCRXMLContainer result = new MCRXMLContainer();
       MCRObjectID mcrid = null;
       org.jdom.Document d = null;
 
-      try {
-          mcrid = new MCRObjectID(ID);
+      try
+      {
+        mcrid = new MCRObjectID(ID);
 
-          byte[] xml = TM.retrieve(mcrid);
-          result.add("local", ID, 0, xml);
-          d = result.exportAllToDocument();
-      } catch (MCRException e) {
-          logger.warn(this.getClass() + " The ID " + ID + " is not a MCRObjectID!");
-          org.jdom.Element root = new org.jdom.Element( "MCRWebServiceError" );
-          root.setAttribute("type", "MCRDoRetrieveObject");
-          d = new org.jdom.Document( root );
-        
-          root.addContent( e.getMessage() );
+        byte[] xml = TM.retrieve(mcrid);
+        result.add("local", ID, 0, xml);
+        d = result.exportAllToDocument();
+      } catch (Exception ex)
+      {
+        logger.warn(this.getClass() + " The ID " + ID + " is not a MCRObjectID!");
+        org.jdom.Element root = new org.jdom.Element("MCRWebServiceError");
+        root.setAttribute("type", "MCRDoRetrieveObject");
+        d = new org.jdom.Document(root);
+
+        root.addContent(ex.getMessage());
       }
-      
-      
+
       org.jdom.output.DOMOutputter doo = new org.jdom.output.DOMOutputter();
-      
-      if( logger.isDebugEnabled() )
+
+      if (logger.isDebugEnabled())
       {
         org.jdom.output.XMLOutputter outputter = new org.jdom.output.XMLOutputter();
-        logger.debug( outputter.outputString( d ) );
+        logger.debug(outputter.outputString(d));
       }
 
       return doo.output(d);
+    } catch (Exception ex)
+    {
+      ex.printStackTrace();
+    }
+    return null;
+  }
+  
+  /**
+   * Search for MyCoRe objects
+   * 
+   * @param query
+   *          as mycore xml query
+   * 
+   * @return resultset of search
+   * 
+   */  
+  public org.w3c.dom.Document MCRDoQuery(org.w3c.dom.Document query)
+  {
+    org.jdom.Document doc    = null;
+    org.jdom.Document result = null;
+    try
+    {
+      org.jdom.input.DOMBuilder d = new org.jdom.input.DOMBuilder();
+      doc = d.build(query);
+
+      if (logger.isDebugEnabled())
+      {
+        org.jdom.output.XMLOutputter outputter = new org.jdom.output.XMLOutputter();
+        logger.debug(outputter.outputString(doc));
+      }
+
+      org.jdom.Element root = doc.getRootElement();
+      String index = root.getAttributeValue("index");
+      MCRSearcher ls = MCRSearcherFactory.getSearcher(index);
+
+      MCRCondition cond = new MCRQueryParser().parse((Element) root.getChild("conditions")
+          .getChildren().get(0));
+      int maxResults = Integer.parseInt(root.getAttributeValue("maxResults", "100"));
+      MCRResults res = ls.search(cond, null, maxResults);
+
+      result = res.buildXML();
+
+    } catch (Exception ex)
+    {
+      org.jdom.Element root = new org.jdom.Element("MCRWebServiceError");
+      root.setAttribute("type", "MCRDoQuery");
+      result = new org.jdom.Document(root);
+
+      root.addContent(ex.toString());
+      root.addContent(doc.detachRootElement());
+    }
+
+    try
+    {
+      org.jdom.output.DOMOutputter doo = new org.jdom.output.DOMOutputter();
+      return doo.output(result);
     } catch (Exception ex)
     {
       ex.printStackTrace();
