05e50f55b129bc5b2b29ebfeb89b2617060ab104
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-01-06 19:10:07 +0000

    complicated the removing of the outer brackets in the boolean string, because of thrown exceptions with correct strings like: (user author1A ) or  ( ((group editorgroup1) or (group editorgroup2)))
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6610 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/parsers/bool/MCRBooleanClauseParser.java
+++ sources/org/mycore/parsers/bool/MCRBooleanClauseParser.java
@@ -37,6 +37,8 @@ import org.jdom.Element;
  */
 public class MCRBooleanClauseParser {
     private static Pattern bracket = Pattern.compile("\\([^)(]*\\)");
+    
+    private static Pattern singleBracket = Pattern.compile("\\(|\\)");
 
     private static Pattern and = Pattern.compile("\\b[aA][nN][dD]\\b");
 
@@ -123,9 +125,25 @@ public class MCRBooleanClauseParser {
         /* replace all bracket expressions with $n */
         while (true) {
             /* remove outer brackets () */
-            while ((s.charAt(0) == '(') && (s.charAt(s.length() - 1) == ')') && (s.substring(1, s.length() - 1).indexOf('(') < 0) && (s.substring(1, s.length() - 1).indexOf(')') < 0)) {
-                s = s.substring(1, s.length() - 1).trim();
-            }
+            while ((s.charAt(0) == '(') && (s.charAt(s.length() - 1) == ')') ) {
+         	   String tmp = s.substring(1, s.length() -1);
+         	   Matcher tmpMatcher = singleBracket.matcher(tmp);
+         	   int left=0;
+         	   int right=0;
+         	   while(tmpMatcher.find()) {
+         		   if(tmpMatcher.group().equals("("))
+         			   left++;
+         		   else
+         			   right++;
+         		   if(right > left) break;
+         	   }
+         	   if(right > left)
+         		   break;
+         	   else if (right == left)
+         		   s = tmp.trim();
+         	   else
+         		   break;
+            }        	
 
             Matcher m = bracket.matcher(s);
 
