46d34f8585e8d885288412c8de32861dd8371854
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2006-01-27 12:57:49 +0000

    Some simplifications, reformatting etc
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6954 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/services/fieldquery/MCRSearchServlet.java
+++ sources/org/mycore/services/fieldquery/MCRSearchServlet.java
@@ -23,118 +23,69 @@
 
 package org.mycore.services.fieldquery;
 
+import java.io.IOException;
+import java.util.Iterator;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.util.*;
-
+import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.output.XMLOutputter;
 import org.jdom.filter.ElementFilter;
-import org.mycore.common.MCRConfigurationException;
-import org.mycore.frontend.editor.*;
+import org.jdom.output.XMLOutputter;
+import org.mycore.frontend.editor.MCREditorSubmission;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
 import org.mycore.parsers.bool.MCRCondition;
-import org.mycore.services.fieldquery.MCRQueryParser;
-import org.mycore.services.fieldquery.MCRResults;
-import org.apache.log4j.Logger;
-
 
 /**
- * This servlet executes queries and presents result
- * pages.
- *
+ * This servlet executes queries and presents result pages.
+ * 
  * @author Harald Richter
  */
 public class MCRSearchServlet extends MCRServlet {
-  private static final long serialVersionUID = 1L;
-  private static Logger LOGGER = Logger.getLogger(MCRSearchServlet.class);
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * The initialization method for this servlet. This read the default
-   * language from the configuration.
-   */
-  public void init() throws MCRConfigurationException, ServletException {
-      super.init();
-  }
+    private static Logger LOGGER = Logger.getLogger(MCRSearchServlet.class);
 
-  /**
-   * This method handles HTTP GET/POST requests and resolves them to output.
-   * 
-   * @param job
-   *            MCRServletJob containing request and response objects
-   * @exception IOException
-   *                for java I/O errors.
-   * @exception ServletException
-   *                for errors from the servlet engine.
-   */
-  public void doGetPost(MCRServletJob job) throws IOException, ServletException
-  {
-    HttpServletRequest request = job.getRequest();
-    HttpServletResponse response = job.getResponse();
+    public void doGetPost(MCRServletJob job) throws IOException, ServletException {
+        HttpServletRequest request = job.getRequest();
+        HttpServletResponse response = job.getResponse();
 
-    MCREditorSubmission sub = (MCREditorSubmission) (request.getAttribute("MCREditorSubmission"));
-    if (null != sub)
-    {
-      org.jdom.Document input = sub.getXML();
-      XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
-      LOGGER.debug(out.outputString(input));
+        MCREditorSubmission sub = (MCREditorSubmission) (request.getAttribute("MCREditorSubmission"));
+        if (null != sub) {
+            org.jdom.Document input = sub.getXML();
+            if (LOGGER.isDebugEnabled()) {
+                XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
+                LOGGER.debug(out.outputString(input));
+            }
 
-      org.jdom.Element root = input.getRootElement();
+            org.jdom.Element root = input.getRootElement();
 
-      // Remove condition fields without values
-      ElementFilter fi = new ElementFilter("condition");
-      Iterator it = input.getDescendants(fi);
-      Vector help = new Vector();
-      int anz = 0;
-      while (it.hasNext())
-      {
-        anz++;
-        Element condition = (Element) it.next();
-        if (condition.getAttribute("value") == null)
-        {
-          LOGGER.debug("Remove condition field without value : " + condition.getAttribute("field"));
-          help.add(condition);
+            // Remove condition fields without values
+            Iterator it = root.getDescendants(new ElementFilter("condition"));
+            while (it.hasNext()) {
+                Element condition = (Element) it.next();
+                if (condition.getAttribute("value") == null) {
+                    LOGGER.debug("Remove condition field without value : " + condition.getAttribute("field"));
+                    condition.detach();
+                }
+            }
+
+            String index = root.getAttributeValue("index");
+            MCRSearcher ls = MCRSearcherFactory.getSearcher(index);
+
+            MCRCondition cond = new MCRQueryParser().parse((Element) root.getChild("conditions").getChildren().get(0));
+            int maxResults = Integer.parseInt(root.getAttributeValue("maxResults", "100"));
+            MCRResults result = ls.search(cond, null, maxResults);
+
+            // start Layout servlet
+            request.setAttribute("MCRLayoutServlet.Input.JDOM", new Document(result.buildXML()));
+            RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
+            rd.forward(request, response);
         }
-      }
-
-      //  no conditions found 
-      if (anz == help.size())
-      {
-        String url = getBaseURL() + "editor_form_search-simpledocument.xml";
-        //TODO        request.sendRedirect( url );
-        return;
-      }
-
-      //  delete conditions without values 
-      for (int i = help.size() - 1; i >= 0; i--)
-      {
-        Element condition = (Element) (help.get(i));
-        if (condition.getAttribute("value") == null)
-          condition.detach();
-      }
-
-      String index = root.getAttributeValue("index");
-      MCRSearcher ls = MCRSearcherFactory.getSearcher(index);
-
-      MCRCondition cond = new MCRQueryParser().parse((Element) root.getChild("conditions")
-          .getChildren().get(0));
-      int maxResults = Integer.parseInt(root.getAttributeValue("maxResults", "100"));
-      MCRResults result = ls.search(cond, null, maxResults);
-
-      Element resXML = result.buildXML();
-      // start Layout servlet
-      request.setAttribute("MCRLayoutServlet.Input.JDOM", new Document( resXML ) );
-
-      RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
-      rd.forward(request, response);
     }
-  }
-    
 }
-
