b20189913d00e1bb6d1cf8710d013d916f3057d7
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2006-01-27 10:35:21 +0000

    Removed setComplete/isComplete methods, changed XML representation, added convenience method to sort results by up to three fields without need to build a List object.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@6934 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/services/fieldquery/MCRResults.java
+++ sources/org/mycore/services/fieldquery/MCRResults.java
@@ -23,42 +23,39 @@
 
 package org.mycore.services.fieldquery;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
-import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
 /**
  * This class represents the results of a query performed by MCRSearcher.
- * Searchers add the hits using the addHit() method and finally call
- * setCompleted() when all hits have been added. Clients can get the hits, sort
- * the entries and do merge/and/or operations on two different result sets.
+ * Searchers add the hits using the addHit() method. Clients can get the hits,
+ * sort the entries and do merge/and/or operations on two different result sets.
  * 
- * @see MCRSearcher
+ * Searches may add the same hit (hit with the same ID) more than once. If the hit
+ * already is contained in the result set, the data of both objects is merged. 
+ * 
+ * @see MCRHit
  * 
  * @author Arne Seifert
  * @author Frank Lützenkirchen
  */
 public class MCRResults {
-    /** The number of hits * */
-    private int numHits = 0;
+    /** The list of MCRHit objects */
+    private List hits = new ArrayList();
 
-    /** The list of MCRHit objects * */
-    private List hits = new LinkedList();
-
-    /** A map containing MCRHit IDs used for and/or operations * */
+    /** A map containing MCRHit IDs used for and/or operations on two different MCRResult objects */
     private HashMap map = new HashMap();
 
-    /** If true, this results are already sorted * */
+    /** If true, this results are already sorted */
     private boolean isSorted = false;
-
-    /** If true, all hits are read from the backend * */
-    private boolean isComplete = false;
 
     /**
      * Creates a new, empty MCRResults.
@@ -75,21 +72,17 @@ public class MCRResults {
      *            the MCRHit to add
      */
     public void addHit(MCRHit hit) {
-        if (isComplete()) {
-            throw new IllegalStateException();
-        }
-
-        if (!map.containsKey(hit.getID())) {
+        String ID = hit.getID();
+        if (!map.containsKey(ID)) {
             // This is a new entry with new ID
             hits.add(hit);
-            map.put(hit.getID(), hit);
+            map.put(ID, hit);
         } else {
             // Merge data of existing hit with new one with the same ID
-            MCRHit existing = getHit(hit.getID());
-            MCRHit merged = MCRHit.buildMergedHitData(hit, existing);
-            hits.remove(existing);
-            hits.add(merged);
-            map.put(merged.getID(), merged);
+            MCRHit existing = getHit(ID);
+            MCRHit merged = MCRHit.merge(hit, existing);
+            hits.set(hits.indexOf(existing), merged);
+            map.put(ID, merged);
         }
     }
 
@@ -130,30 +123,11 @@ public class MCRResults {
      * @return the number of hits
      */
     public int getNumHits() {
-        return (isComplete ? numHits : hits.size());
+        return hits.size();
     }
 
     /**
-     * When the searcher added all hits, it MUST call this method to indicate
-     * that all hits have been added.
-     */
-    public void setComplete() {
-        isComplete = true;
-        numHits = hits.size();
-    }
-
-    /**
-     * If true, all hits have been added by the searcher and the result list is
-     * complete.
-     * 
-     * @return true, if result list is complete
-     */
-    public boolean isComplete() {
-        return isComplete;
-    }
-
-    /**
-     * The searcher should set this to true, if the hits already have been added
+     * The searcher must set this to true, if the hits already have been added
      * in sorted order.
      * 
      * @param value
@@ -173,13 +147,31 @@ public class MCRResults {
     }
 
     /**
-     * Sorts the hits using the given list of fields
+     * Convenience method to sort results by up to three differend fields.
+     * Each parameter may be null, so you can sort by only one or two fields
+     * if you like.
      * 
-     * @param fields
-     *            a list of MCRSearchField objects that are used as sort
-     *            criteria
+     * @param first the first field to sort by
+     * @param second the second field to sort by
+     * @param third the third field to sort by
      */
-    public void sort(final List fields) {
+    public void sortBy(MCRSortBy first, MCRSortBy second, MCRSortBy third) {
+        List sortByList = new ArrayList();
+        if (first != null)
+            sortByList.add(first);
+        if (second != null)
+            sortByList.add(second);
+        if (third != null)
+            sortByList.add(third);
+        sortBy(sortByList);
+    }
+
+    /**
+     * Sorts this results by the given sort criteria.
+     * 
+     * @param sortByList a List of MCRSortBy objects
+     */
+    public void sortBy(final List sortByList) {
         Collections.sort(this.hits, new Comparator() {
             public int compare(Object oa, Object ob) {
                 MCRHit a = (MCRHit) oa;
@@ -187,24 +179,10 @@ public class MCRResults {
 
                 int result = 0;
 
-                for (int i = 0; (result == 0) && (i < fields.size()); i++) {
-                    MCRSearchField field = (MCRSearchField) (fields.get(i));
-                    String va = a.getSortData().getProperty(field.getName());
-                    String vb = b.getSortData().getProperty(field.getName());
-
-                    if ((va == null) || (va.trim().length() == 0)) {
-                        result = (((vb == null) || (vb.trim().length() == 0)) ? 0 : (-1));
-                    } else if ((vb == null) || (vb.trim().length() == 0)) {
-                        result = (((va == null) || (va.trim().length() == 0)) ? 0 : 1);
-                    } else if ("decimal".equals(field.getDataType())) {
-                        result = (int) ((Double.parseDouble(va) - Double.parseDouble(vb)) * 10.0);
-                    } else if ("integer".equals(field.getDataType())) {
-                        result = (int) (Long.parseLong(va) - Long.parseLong(vb));
-                    } else {
-                        result = va.compareTo(vb);
-                    }
-
-                    if (field.getSortOrder() == MCRSearchField.DESCENDING) {
+                for (int i = 0; (result == 0) && (i < sortByList.size()); i++) {
+                    MCRSortBy sortBy = (MCRSortBy) (sortByList.get(i));
+                    result = a.compareTo(sortBy.getField(), b);
+                    if (sortBy.getSortOrder() == MCRSortBy.DESCENDING) {
                         result *= -1;
                     }
                 }
@@ -217,32 +195,31 @@ public class MCRResults {
     }
 
     /**
-     * Returns a XML document containing all hits and their data
+     * Returns a XML element containing all hits and their data
      * 
-     * @return a JDOM document of this results
+     * @return a 'results' element with attributes 'sorted' and 'numHits' and hit child elements
      */
-    public Document buildXML() {
-        Document doc = new Document();
-        Element root = new Element("mcrresults");
-        doc.setRootElement(root);
-        root.setAttribute("sorted", Boolean.toString(isSorted()));
+    public Element buildXML() {
+        Namespace mcrns = Namespace.getNamespace("mcr", "http://www.mycore.org/");
+
+        Element results = new Element("results", mcrns);
+        results.setAttribute("sorted", Boolean.toString(isSorted()));
+        results.setAttribute("numHits", String.valueOf(getNumHits()));
 
         for (int i = 0; i < getNumHits(); i++)
-            root.addContent(((MCRHit) hits.get(i)).buildXML());
+            results.addContent(((MCRHit) hits.get(i)).buildXML());
 
-        return doc;
+        return results;
     }
 
     public String toString() {
-        XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
-
-        return out.outputString(buildXML());
+        return new XMLOutputter(Format.getPrettyFormat()).outputString(buildXML());
     }
 
     /**
      * Returns a new MCRResults that only contains those hits that are members
      * of both source MCRResults objects. The compare is based on the ID of the
-     * hits.
+     * hits. The data of each single hit is merged from both results. 
      * 
      * @param a
      *            the first result list
@@ -253,16 +230,14 @@ public class MCRResults {
     public static MCRResults and(MCRResults a, MCRResults b) {
         MCRResults res = new MCRResults();
 
-        for (int i = 0; i < a.numHits; i++) {
+        for (int i = 0; i < a.getNumHits(); i++) {
             MCRHit hitA = a.getHit(i);
             MCRHit hitB = b.getHit(hitA.getID());
 
             if (hitB != null) {
-                res.addHit(MCRHit.buildMergedHitData(hitA, hitB));
+                res.addHit(MCRHit.merge(hitA, hitB));
             }
         }
-
-        res.setComplete();
 
         return res;
     }
@@ -270,7 +245,7 @@ public class MCRResults {
     /**
      * Returns a new MCRResults that contains those hits that are members of at
      * least one of the source MCRResults objects. The compare is based on the
-     * ID of the hits.
+     * ID of the hits. The data of each single hit is merged from both results.
      * 
      * @param a
      *            the first result list
@@ -281,20 +256,18 @@ public class MCRResults {
     public static MCRResults or(MCRResults a, MCRResults b) {
         MCRResults res = new MCRResults();
 
-        for (int i = 0; i < a.numHits; i++) {
+        for (int i = 0; i < a.getNumHits(); i++) {
             MCRHit hitA = a.getHit(i);
             MCRHit hitB = b.getHit(hitA.getID());
 
-            MCRHit hitC = MCRHit.buildMergedHitData(hitA, hitB);
+            MCRHit hitC = MCRHit.merge(hitA, hitB);
             res.addHit(hitC);
         }
 
-        for (int i = 0; i < b.numHits; i++)
+        for (int i = 0; i < b.getNumHits(); i++)
             if (!res.map.containsKey(b.getHit(i).getID())) {
                 res.addHit(b.getHit(i));
             }
-
-        res.setComplete();
 
         return res;
     }
@@ -313,13 +286,11 @@ public class MCRResults {
     public static MCRResults merge(MCRResults a, MCRResults b) {
         MCRResults merged = new MCRResults();
 
-        for (int i = 0; i < a.numHits; i++)
+        for (int i = 0; i < a.getNumHits(); i++)
             merged.addHit(a.getHit(i));
 
-        for (int i = 0; i < b.numHits; i++)
+        for (int i = 0; i < b.getNumHits(); i++)
             merged.addHit(b.getHit(i));
-
-        merged.setComplete();
 
         return merged;
     }
