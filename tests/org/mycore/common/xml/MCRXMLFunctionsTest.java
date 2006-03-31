5a3308852cd46ceaf6fb94abd17fac7ee6cacece
Thomas Scheffler <github@yagee.de>
2006-03-31 08:26:20 +0000

    added TestCase for MCRXMLFunctions
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@7807 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ tests/org/mycore/common/xml/MCRXMLFunctionsTest.java
@@ -0,0 +1,24 @@
+package org.mycore.common.xml;
+
+import java.text.ParseException;
+
+import org.mycore.common.MCRTestCase;
+
+public class MCRXMLFunctionsTest extends MCRTestCase {
+
+    /*
+     * Test method for 'org.mycore.common.xml.MCRXMLFunctions.formatISODate(String, String, String, String)'
+     */
+    public void testFormatISODate() throws ParseException {
+        assertEquals("24.02.1964",MCRXMLFunctions.formatISODate("1964-02-24","dd.MM.yyyy","de"));
+    }
+
+    /*
+     * Test method for 'org.mycore.common.xml.MCRXMLFunctions.getISODate(String, String)'
+     */
+    public void testGetISODate() throws ParseException {
+        assertEquals("1964-02-24",MCRXMLFunctions.getISODate("24.02.1964","dd.MM.yyyy","YYYY-MM-DD"));
+        assertEquals("Timezone was not correctly detected","1964-02-23T22:00:00Z",MCRXMLFunctions.getISODate("24.02.1964 00:00:00 +0200","dd.MM.yyyy HH:mm:ss Z","YYYY-MM-DDThh:mm:ssTZD"));
+    }
+
+}
