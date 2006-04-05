2d2184ce44cd6b620a58e8caee31ea308a27dd0c
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2006-04-05 13:54:03 +0000

    Added function to navigation in result list and to reload old query into search mask
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@7906 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/services/fieldquery/MCRSearchServlet.java
+++ sources/org/mycore/services/fieldquery/MCRSearchServlet.java
@@ -25,6 +25,7 @@ package org.mycore.services.fieldquery;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
 
 import javax.servlet.RequestDispatcher;
@@ -37,13 +38,16 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.filter.ElementFilter;
 import org.jdom.output.XMLOutputter;
+import org.mycore.common.MCRCache;
 import org.mycore.frontend.editor.MCREditorSubmission;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
+import org.mycore.parsers.bool.MCRCondition;
 
 /**
  * This servlet executes queries and presents result pages.
  * 
+ * @author Frank Lützenkirchen
  * @author Harald Richter
  */
 public class MCRSearchServlet extends MCRServlet {
@@ -51,46 +55,163 @@ public class MCRSearchServlet extends MCRServlet {
 
     private static Logger LOGGER = Logger.getLogger(MCRSearchServlet.class);
 
+    /** Cached query results */
+    private static MCRCache results = new MCRCache(50);
+
+    /** Cached queries as XML, for re-use in editor form */
+    private static MCRCache queries = new MCRCache(50);
+
+    /** Cached queries as parsed MCRCondition, for output with results */
+    private static MCRCache conds = new MCRCache(50);
+
     public void doGetPost(MCRServletJob job) throws IOException, ServletException {
-        long start = System.currentTimeMillis();
         HttpServletRequest request = job.getRequest();
         HttpServletResponse response = job.getResponse();
 
-        MCREditorSubmission sub = (MCREditorSubmission) (request.getAttribute("MCREditorSubmission"));
-        if (null != sub) {
-            org.jdom.Document input = sub.getXML();
-            if (LOGGER.isDebugEnabled()) {
-                XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
-                LOGGER.debug(out.outputString(input));
-            }
+        String mode = job.getRequest().getParameter("mode");
+        if ("results".equals(mode))
+            showResults(request, response);
+        else if ("load".equals(mode))
+            loadQuery(request, response);
+        else
+            doQuery(request, response);
+    }
 
-            org.jdom.Element root = input.getRootElement();
+    /**
+     * Returns a query that was previously submitted, 
+     * to reload it into the editor search mask. 
+     * Usage: MCRSearchServlet?mode=load&id=XXXXX
+     */
+    private void loadQuery(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        Document query = (Document) (queries.get(id));
 
-            // Find condition fields without values
-            Iterator it = root.getDescendants(new ElementFilter("condition"));
-            Vector help = new Vector();
-            while (it.hasNext()) {
-                Element condition = (Element) it.next();
-                if (condition.getAttribute("value") == null) {
-                    LOGGER.debug("Remove condition field without value : " + condition.getAttribute("field"));
-                    help.add(condition);
-                }
-            }
+        // Send query XML to editor
+        request.setAttribute("MCRLayoutServlet.Input.JDOM", query);
+        request.setAttribute("XSL.Style", "xml");
+        RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
+        rd.forward(request, response);
+    }
 
-            // Remove found conditions without values
-            for (int i = help.size() - 1; i >= 0; i--)
-                ((Element) (help.get(i))).detach();
+    /**
+     * Shows a results page.
+     * Usage: MCRSearchServlet?mode=results&numPerPage=10&page=1
+     */
+    private void showResults(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-            // Execute query
-            MCRResults result = MCRQueryManager.search(input);
+        // Get cached results
+        String id = request.getParameter("id");
+        MCRResults result = (MCRResults) (results.get(id));
+        Document query = (Document) (queries.get(id));
 
-            // start Layout servlet
-            request.setAttribute("MCRLayoutServlet.Input.JDOM", new Document(result.buildXML()));
-            RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
-            rd.forward(request, response);
-            
-            long qtime = System.currentTimeMillis() - start;
-            LOGGER.debug("total time in MCRSearchServlet:    " + qtime);
+        // Effective number of hits per page
+        String snpp = request.getParameter("numPerPage");
+        if (snpp == null)
+            snpp = "0";
+        int npp = Integer.parseInt(snpp);
+        if (npp > result.getNumHits())
+            npp = 0;
+
+        // Total number of pages
+        int numHits = Math.max(1, result.getNumHits());
+        int numPages = 1;
+        if (npp > 0)
+            numPages = (int) (Math.ceil((double) numHits / (double) npp));
+
+        // Effective current page number
+        String spage = request.getParameter("page");
+        if (spage == null)
+            spage = "1";
+        int page = Integer.parseInt(spage);
+        if (npp == 0)
+            page = 1;
+        else if (page > numPages)
+            page = numPages;
+
+        // Number of first and last hit to be shown
+        int first = (page - 1) * npp;
+        int last = result.getNumHits() - 1;
+        if (npp > 0)
+            last = Math.min(result.getNumHits(), first + npp) - 1;
+
+        // Build result hits as XML document
+        Element xml = result.buildXML(first, last);
+        
+        // Add additional data for output
+        xml.setAttribute("numPerPage", String.valueOf(npp));
+        xml.setAttribute("numPages", String.valueOf(numPages));
+        xml.setAttribute("page", String.valueOf(page));
+        
+        // The URL of the search mask that was used
+        xml.setAttribute("mask", query.getRootElement().getAttributeValue("mask"));
+
+        // The query condition, to show together with the results
+        MCRCondition cond = (MCRCondition) (conds.get(id));
+        xml.addContent(new Element("condition").setAttribute("format", "xml").setText(cond.toString()));
+        xml.addContent(new Element("condition").setAttribute("format", "text").addContent(cond.toXML()));
+
+        // Send output to LayoutServlet
+        request.setAttribute("MCRLayoutServlet.Input.JDOM", new Document(xml));
+        RequestDispatcher rd = getServletContext().getNamedDispatcher("MCRLayoutServlet");
+        rd.forward(request, response);
+    }
+
+    /**
+     * Executes a query that comes from editor search mask, and redirects the
+     * browser to the first results page
+     */
+    private void doQuery(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Document input = ((MCREditorSubmission) (request.getAttribute("MCREditorSubmission"))).getXML();
+        Object clonedQuery = input.clone(); // Keep for later re-use
+
+        // Show incoming query document
+        if (LOGGER.isDebugEnabled()) {
+            XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
+            LOGGER.debug(out.outputString(input));
         }
+
+        org.jdom.Element root = input.getRootElement();
+
+        // Rename condition elements from search mask: condition1 -> condition
+        List ch = root.getChild("conditions").getChild("boolean").getChildren();
+        for (int i = 0; i < ch.size(); i++) {
+            Element condition = (Element) (ch.get(i));
+            condition.setName("condition");
+        }
+
+        // Find condition fields without values
+        Iterator it = root.getDescendants(new ElementFilter("condition"));
+        Vector help = new Vector();
+        while (it.hasNext()) {
+            Element condition = (Element) it.next();
+            if (condition.getAttribute("value") == null) {
+                help.add(condition);
+            }
+        }
+
+        // Remove found conditions without values
+        for (int i = help.size() - 1; i >= 0; i--)
+            ((Element) (help.get(i))).detach();
+
+        // Cache parsed query for later output together with results
+        Element condElem = (Element) (root.getChild("conditions").getChildren().get(0));
+        MCRCondition cond = new MCRQueryParser().parse(condElem);
+
+        // Execute query
+        long start = System.currentTimeMillis();
+        MCRResults result = MCRQueryManager.search(input);
+        long qtime = System.currentTimeMillis() - start;
+        LOGGER.debug("MCRSearchServlet total query time: " + qtime);
+
+        String npp = root.getAttributeValue("numPerPage", "0");
+
+        // Store query and results in cache
+        results.put(result.getID(), result);
+        queries.put(result.getID(), clonedQuery);
+        conds.put(result.getID(), cond);
+
+        // Redirect browser to first results page
+        String url = "MCRSearchServlet?mode=results&id=" + result.getID() + "&numPerPage=" + npp;
+        response.sendRedirect(response.encodeRedirectURL(url));
     }
 }
