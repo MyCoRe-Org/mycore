9052b89c70bbac4da5b405aba6696db139bfb8f5
Harald Richter <mcrhrich@duepub-1.zim.uni-duisburg-essen.de>
2006-04-11 12:57:31 +0000

    interface to generate wsdl from
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@8015 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ modules/module-webservices/sources/org/mycore/services/webservices/MCRWS.java
@@ -0,0 +1,64 @@
+/*
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.services.webservices;
+
+public interface MCRWS
+{
+
+  /**
+   * Retrieves MyCoRe object   
+   *
+   * @param ID The ID of the document to retrieve   
+   * 
+   * @return data of mycore object
+   * 
+   **/
+  public abstract org.w3c.dom.Document MCRDoRetrieveObject(String ID);
+
+  /**
+   * Retrieves MyCoRe Classification   
+   *
+   * @param level   number of levels to retrievwe   
+   * @param type    parents|children   
+   * @param classID The ID of the classification   
+   * @param categID   categroryID where retrieval with level starts   
+   * 
+   * @return data of mycore classification
+   * 
+   **/
+  public abstract org.w3c.dom.Document MCRDoRetrieveClassification( String level, String type,
+      String classID, String categID);
+
+  /**
+   * Search for MyCoRe objects
+   * 
+   * @param query
+   *          as mycore xml query
+   * 
+   * @return resultset of search
+   * 
+   */
+  public abstract org.w3c.dom.Document MCRDoQuery(org.w3c.dom.Document query);
+
+}
