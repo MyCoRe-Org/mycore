dcccef86881108b7e826d5bf479f05388c7d9721
Harald Richter <mcrhrich@duepub-1.zim.uni-duisburg-essen.de>
2006-05-10 07:54:57 +0000

    change string MCR.baseurl
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@8228 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/servlets/MCRServlet.java
+++ sources/org/mycore/frontend/servlets/MCRServlet.java
@@ -107,7 +107,7 @@ public class MCRServlet extends HttpServlet {
 		String requestURL = req.getRequestURL().toString();
 		int pos = requestURL.indexOf(contextPath, 9);
         
-		BASE_URL = CONFIG.getString("MCR.basurl",requestURL.substring(0, pos) + contextPath);
+		BASE_URL = CONFIG.getString("MCR.baseurl",requestURL.substring(0, pos) + contextPath);
 		SERVLET_URL = BASE_URL + "servlets/";
 	}
 
