9e97f93f081388544806c4d540e1e8dba60ee9f5
Thomas Scheffler <github@yagee.de>
2006-05-30 15:15:09 +0000

    a bit more beautyful
    added output for the user :o)
    using Ant BuildException instead of RunTimeException
    refactored the code to make it easier to understand
    added comments
    
    
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@8616 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/MCRConfigurationTask.java
+++ sources/org/mycore/common/MCRConfigurationTask.java
@@ -37,90 +37,154 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.regex.Pattern;
 
+import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 /**
+ * Ant task that allows 'mycore.properties' manipulation via ant.
  * @author Thomas Scheffler (yagee)
- * 
- * Need to insert some things here
- *
  */
 public class MCRConfigurationTask extends Task {
-    String action;
-    String value;
-    File propertyFile;
-    Pattern INCLUDE_PATTERN= Pattern.compile("MCR.configuration.include");
-    boolean valuePresent=false;
-    ArrayList propLines;
-    int lineNumber;
-    boolean propertiesLoaded=false;
-    boolean fileChanged=false;
-    
-    public void execute(){
-        if (!action.equals("addInclude") && !action.equals("removeInclude")){
-            throw new IllegalArgumentException("action must be either 'addInclude' or 'removeInclude'");
-        }
-        if (!propertyFile.exists()){
-            throw new RuntimeException(new FileNotFoundException(propertyFile.getName()+" does not exist."));
-        }
+    // some constants
+    private static final String MCR_CONFIGURATION_INCLUDE = "MCR.configuration.include";
+
+    private static final Pattern INCLUDE_PATTERN = Pattern.compile(MCR_CONFIGURATION_INCLUDE);
+
+    // some fields settable and gettable via methods
+    private String action;
+
+    private String value;
+
+    private File propertyFile;
+
+    // some fields needed for processing
+    private boolean valuePresent = false;
+
+    private boolean propertiesLoaded = false;
+
+    private boolean fileChanged = false;
+
+    private int lineNumber = -1;
+
+    private ArrayList propLines;
+
+    /**
+     * Execute the requested operation.
+     * 
+     * @throws BuildException
+     *             if an error occurs
+     */
+    public void execute() throws BuildException {
+        checkPreConditions();
         loadLines();
-        if (!propertiesLoaded){
-            throw new RuntimeException("Could not load: "+propertyFile.getName());
+        if (!propertiesLoaded) {
+            throw new BuildException("Could not load: " + propertyFile.getName());
         }
-        if (action.equals("addInclude")){
+        if (action.equals("addInclude")) {
             addInclude();
-        } else if (action.equals("removeInclude")){
+        } else if (action.equals("removeInclude")) {
             removeInclude();
         }
-        if (fileChanged){
+        if (fileChanged) {
             writeLines();
         }
-        action=null;propertyFile=null;lineNumber=-1;propertiesLoaded=false;fileChanged=false;valuePresent=false;
-        
+        reset();
     }
-    
-    private void addInclude(){
-        if (valuePresent){
+
+    /**
+     * checks whether all preconditions are met
+     */
+    private void checkPreConditions() throws BuildException {
+        if (action == null) {
+            throw new BuildException("Must specify 'action' attribute");
+        }
+        if (propertyFile == null) {
+            throw new BuildException("Must specify 'propertyfile' attribute");
+        }
+        if (value == null) {
+            throw new BuildException("Must specify 'value' attribute");
+        }
+        if (!action.equals("addInclude") && !action.equals("removeInclude")) {
+            throw new BuildException("action must be either 'addInclude' or 'removeInclude'");
+        }
+        if (!propertyFile.exists()) {
+            throw new BuildException(new FileNotFoundException(propertyFile.getName() + " does not exist."));
+        }
+    }
+
+    /**
+     * resets all local fields
+     */
+    private void reset() {
+        action = null;
+        propertyFile = null;
+        lineNumber = -1;
+        propertiesLoaded = false;
+        fileChanged = false;
+        valuePresent = false;
+    }
+
+    /**
+     * adds an include
+     */
+    private void addInclude() {
+        if (valuePresent) {
+            handleOutput(new StringBuffer("Not changing ").append(propertyFile.getName()).append(": '").append(value).append("' allready included.").toString());
             return;
         }
-        fileChanged=true;
-        String prop=propLines.get(lineNumber).toString();
-        String newProp=prop+","+value;
+        fileChanged = true;
+        String prop = propLines.get(lineNumber).toString();
+        String newProp = prop + "," + value;
         propLines.remove(lineNumber);
-        propLines.add(lineNumber,newProp);
+        propLines.add(lineNumber, newProp);
+        handleOutput(new StringBuffer(propertyFile.getName()).append(':').append(lineNumber).append(" added '").append(value).append("' to ").append(
+                MCR_CONFIGURATION_INCLUDE).toString());
     }
-    
-    private void removeInclude(){
-        if (valuePresent){
+
+    /**
+     * removes an include
+     */
+    private void removeInclude() {
+        if (!valuePresent) {
+            handleOutput(new StringBuffer("Not changing ").append(propertyFile.getName()).append(": '").append(value).append("' not present.").toString());
             return;
         }
-        fileChanged=true;
-        String newProp=propLines.get(lineNumber).toString().replaceAll(","+value,"");
+        fileChanged = true;
+        String newProp = propLines.get(lineNumber).toString().replaceAll("," + value, "");
         propLines.remove(lineNumber);
-        propLines.add(lineNumber,newProp);
+        propLines.add(lineNumber, newProp);
+        handleOutput(new StringBuffer(propertyFile.getName()).append(':').append(lineNumber).append(" removed '").append(value).append("' from ").append(
+                MCR_CONFIGURATION_INCLUDE).toString());
     }
-    
+
+    /*
+     * writes back the property file together with changed properties
+     */
     private void writeLines() {
-        BufferedWriter writer=null;
+        BufferedWriter writer = null;
         try {
-            writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(propertyFile), Charset.forName("ISO-8859-1")));
-            for (Iterator it=propLines.iterator();it.hasNext();){
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(propertyFile), Charset.forName("ISO-8859-1")));
+            for (Iterator it = propLines.iterator(); it.hasNext();) {
                 writer.write(it.next().toString());
                 writer.newLine();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            handleErrorOutput("Error while writing '" + propertyFile.getName() + "': " + e.getMessage());
         } finally {
-            if (writer!=null){
+            if (writer != null) {
                 try {
                     writer.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    handleErrorOutput("Error while closing file '" + propertyFile.getName() + "': " + e.getMessage());
                 }
             }
         }
     }
-    
+
+    /*
+     * loads the property file and marks the occurence of
+     * MCR.configuration.include
+     */
     private void loadLines() {
         BufferedReader reader = null;
         propertiesLoaded = false;
@@ -129,11 +193,13 @@ public class MCRConfigurationTask extends Task {
             propLines = new ArrayList(1000);
             int i = 0;
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                propLines.add(line);// add each line of the property file to the
-                                    // array list
-                if (INCLUDE_PATTERN.matcher(line).find()) {
+                // add each line of the property file to the array list
+                propLines.add(line);
+                if ((lineNumber < 0) && INCLUDE_PATTERN.matcher(line).find()) {
+                    // found the MCR.configuration.include line
                     lineNumber = i;
                     if (line.indexOf(value) > 0) {
+                        // value is included
                         valuePresent = true;
                     }
                 }
@@ -141,14 +207,14 @@ public class MCRConfigurationTask extends Task {
             }
             propertiesLoaded = true;
         } catch (IOException e) {
-            e.printStackTrace();
+            handleErrorOutput("Error while reading '" + propertyFile.getName() + "': " + e.getMessage());
         } finally {
             try {
                 if (reader != null) {
                     reader.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                handleErrorOutput("Error while closing file '" + propertyFile.getName() + "': " + e.getMessage());
             }
         }
     }
@@ -156,20 +222,45 @@ public class MCRConfigurationTask extends Task {
     public String getAction() {
         return action;
     }
+
+    /**
+     * sets the action the task should perform.
+     * 
+     * @param action
+     *            either "addInclude" or "removeInclude"
+     */
     public void setAction(String action) {
         this.action = action;
     }
+
     public File getPropertyFile() {
         return propertyFile;
     }
+
+    /**
+     * sets the property file that needs to be changed.
+     * 
+     * @param action
+     *            a 'mycore.properties' file
+     */
     public void setPropertyFile(File propertyFile) {
         this.propertyFile = propertyFile;
     }
+
     public String getValue() {
         return value;
     }
+
+    /**
+     * sets the value for the action to be performed. For 'addInclude' a value
+     * of "mycore.properties.moduleXY" would result in adding
+     * ",mycore.properties.moduleXY" to the property
+     * "MCR.configuration.include".
+     * 
+     * @param action
+     *            a 'mycore.properties' file
+     */
     public void setValue(String value) {
         this.value = value;
     }
-
 }
