ddf221a239df92d63aa3be2d7f485e147cfb0ab0
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-07-27 10:30:07 +0000

    insert a cache for the linkedlist with the result of all
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@9182 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/indexbrowser/MCRIndexBrowserData.java
+++ sources/org/mycore/frontend/indexbrowser/MCRIndexBrowserData.java
@@ -1,20 +1,36 @@
 package org.mycore.frontend.indexbrowser;
 
+import java.text.CollationKey;
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.StringTokenizer;
 import java.util.Vector;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
 
 import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.Namespace;
 import org.jdom.filter.ElementFilter;
 import org.jdom.output.XMLOutputter;
+import org.jdom.transform.JDOMResult;
+import org.jdom.transform.JDOMSource;
 
 import org.mycore.access.MCRAccessManager;
+import org.mycore.common.MCRCache;
 import org.mycore.common.MCRConfiguration;
+import org.mycore.common.xml.MCRURIResolver;
+import org.mycore.datamodel.classifications.MCRCategoryItem;
+import org.mycore.datamodel.metadata.MCRMetaDate;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.parsers.bool.MCRAndCondition;
 import org.mycore.parsers.bool.MCROrCondition;
@@ -28,8 +44,9 @@ import org.mycore.services.fieldquery.MCRSortBy;
 public class MCRIndexBrowserData {
 	protected static Logger logger = Logger.getLogger(MCRIndexBrowserData.class);
 	private static final long serialVersionUID = 1L;
-	private String selectedtype = "";
-    private LinkedList ll1 = new LinkedList(); 
+    private static MCRCache ll1Cache  = new MCRCache(500);    	    
+    private static LinkedList ll1 = new LinkedList();
+    
     private MyBrowseData br = new MyBrowseData();
     private MyIndexConfiguration ic = new MyIndexConfiguration(); 
 	private XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
@@ -37,6 +54,7 @@ public class MCRIndexBrowserData {
     private Document jResult;
     private Document jQuery;
     private MCRResults mcrResult;
+
     
     class MyRangeDelim {
         int pos;
@@ -67,9 +85,6 @@ public class MCRIndexBrowserData {
             		String to = fromTo.substring(fromTo.indexOf("-")+1);
                 	this.addRange(from, to);
             	}
-                
-                
-
         }
 
         void addRange(String from, String to) {
@@ -88,7 +103,7 @@ public class MCRIndexBrowserData {
         boolean distinct;
         String browseField;
         String fields;    
-        String[] extraFields;
+        String[] outputFields;
         String fieldToSort;
         String[] sortFields;
         String order;
@@ -109,7 +124,7 @@ public class MCRIndexBrowserData {
             fields = config.getString(prefix + "ExtraOutputFields", null);
             fieldToSort = config.getString(prefix + "FieldsToSort", null);
             order = config.getString(prefix + "Order", "ascending");
-            extraFields = buildFieldList(fields);
+            outputFields = buildFieldList(fields);
             sortFields  = buildFieldList(fieldToSort);
         }
 
@@ -124,19 +139,43 @@ public class MCRIndexBrowserData {
             		ff[i] = st.nextToken();
             }
             return ff;
-        }
-                
+        }                
     }
 
     
     public MCRIndexBrowserData(String search, String mode, String path, String fromTo) {
     	
     	br.set(search, mode, path, fromTo);
-        ic.set(br.index);
-        
+        ic.set(br.index);        
         Element results = buildPageElement();
         
 		int numRows = fillmyLinkedList( );
+
+		/***
+		 * if you hav in your environment (like ibm powerpc or other) no correct sort, use this lines for a german sort
+		Collections.sort(this.ll1, 
+				new Comparator() {
+					public int compare(Object o1, Object o2) {
+						Collator germanCc = Collator.getInstance(Locale.GERMAN);
+						
+				        String[] name1 =  (String[]) o1;
+				        String[] name2 =  (String[]) o2;
+				        
+			        
+				       // int cc =  name1[0].compareTo(name2[0]);
+				       // System.out.println("NAME1:" + name2[0] + "NAME2:" + name1[0]+ "ERG: " + cc);
+
+				        CollationKey key1 = germanCc.getCollationKey( name1[0] );
+				        CollationKey key2 = germanCc.getCollationKey( name2[0] );
+				        
+				        int comp = key1.compareTo( key2 );
+				        // System.out.println("NAME1:" + name2[0] + "NAME2:" + name1[0]+ "ERG: " + comp);
+				        // System.out.println("___________________________");
+				        return comp;
+				    }
+				}
+		);
+		***/	
 		results.setAttribute("numHits", String.valueOf(numRows) );
 		
         if (br.search != null) {
@@ -148,24 +187,26 @@ public class MCRIndexBrowserData {
 
         if (numSelectedRows <= ic.maxPerPage) {
            for (int i = from; i < to; i++) {
-               String myValues[] = (String[])this.ll1.get(i);
+               String myValues[] = (String[])ll1.get(i);
                     
                Element v = new Element("value");
                v.setAttribute("pos", String.valueOf(i));
 
                Element es = new Element("sort");
+               // the first field from the lll-element must be the first outputField  
                es.addContent(myValues[0]);                       
                v.addContent(es);
 
+               // the second field from the lll-element must be se second element in outputfields 
                Element ev = new Element("idx");
                ev.addContent(myValues[1]);                   
                v.addContent(ev);
 			   
-               for (int j = 0; ic.extraFields!= null && j < ic.extraFields.length; j++) {
-                  String sField= ic.extraFields[j];
+               for (int j = 0; j < ic.outputFields.length; j++) {
+                  String sField= ic.outputFields[j];
                   Element col = new Element("col");
                   col.setAttribute("name", sField);
-                  col.addContent(myValues[2 + j]);
+                  col.addContent(myValues[2+j]);
                   v.addContent(col);
                }
                results.addContent(v);
@@ -175,17 +216,17 @@ public class MCRIndexBrowserData {
             List delims = new Vector();
             
             for (int i = from; i < to; i++) {              
-            	String myValues[] = (String[])this.ll1.get(i);
+            	String myValues[] = (String[])ll1.get(i);
             	delims.add(new MyRangeDelim(i, myValues[1]));
             	
             	i = Math.min(i + stepSize, to-1);
             	
-            	String myVal[] = (String[])this.ll1.get(i);
+            	String myVal[] = (String[])ll1.get(i);
             	String value = myVal[1];
             	           	
             	if ((i < to) && ((to - i) < 3)) {
             		i = to-1;                        
-            		value =((String[])this.ll1.get(i))[1];
+            		value =((String[])ll1.get(i))[1];
             	}
             	delims.add(new MyRangeDelim(i, value));
             }
@@ -221,45 +262,7 @@ public class MCRIndexBrowserData {
     	page.addContent(results);
     	return results;
     }
-    
-    
-    private void setListeElm(Element child, String[] Fields, int startindex, String[] listelm, boolean append){
-        if ( Fields == null) return;
-        for (int j = 0; j < Fields.length; j++) {
-        	String   value = "";
-    	    String   sField= Fields[j];
-    	    
-    	    /* mcrHit - 
-	    	 * <mcrhit id="docportal_author_000000000441">
-	    	 * ...
-			 * </mcrhit>
-	    	 */
-    	    
-    	    
-    	    //* über die ID die darzustellenden Daten holen 
-        	Element  child1 = (Element) child.clone();
-    	    
-            // Eintrag der ID
-        	if ( sField.equals("id")) {
-        		value = child1.getAttributeValue("id");
-        		
-        	} else {
-        	    MCRObject oo = new MCRObject();
-        	    Document od =  oo.receiveJDOMFromDatastore(child1.getAttributeValue("id"));
-        	    Iterator it = od.getDescendants(new ElementFilter(sField));
-        	    if ( it.hasNext() )  {
-        	      Element el = (Element) it.next();
-                  value = el.getText();
-        	    }  	
-        	}        		
-       		if ( append) {
-                if ( j==0)  listelm[startindex]  = value;
-                else    	listelm[startindex] +=", "+ value;
-       		}
-            else listelm[startindex + j] = value;
-        }
-
-	}
+       
     
     //**************************************************************************
 
@@ -326,10 +329,11 @@ public class MCRIndexBrowserData {
 
     //**************************************************************************
 
-    private void  buildQuery() {
+    private MCRQuery  buildQuery() {
         MCRAndCondition cAnd = new MCRAndCondition();
 
-        MCRFieldDef field = MCRFieldDef.getDef("objectType");
+        MCRFieldDef field = MCRFieldDef.getDef("objectType");        
+
         if (ic.table.indexOf(",") != -1) {
             MCROrCondition cOr = new MCROrCondition();
             StringTokenizer st = new StringTokenizer(ic.table,",");
@@ -345,45 +349,20 @@ public class MCRIndexBrowserData {
         String value = br.search == null ? "*" : br.search + "*";
         cAnd.addChild(new MCRQueryCondition(field,"like",value));
     	
-        logger.debug("generated query: \n" + cAnd );            
-
         boolean order = "ascending".equalsIgnoreCase(ic.order); 
         List sortCriteria = new ArrayList();
-        sortCriteria.add(new MCRSortBy(field,order));
-        
-    	for ( int i=0; ic.extraFields != null && i< ic.extraFields.length; i++){    	
-	        field = MCRFieldDef.getDef(ic.extraFields[i]);
+               
+    	for ( int i=0; ic.sortFields != null && i< ic.sortFields.length; i++){    	
+	        field = MCRFieldDef.getDef(ic.sortFields[i]);
             if (null != field )
               sortCriteria.add(new MCRSortBy(field,order));
             else
-              logger.error("MCRFieldDef not available: " + ic.extraFields[i]);
+              logger.error("MCRFieldDef not available: " + ic.sortFields[i]);
     	}
-        
-        MCRQuery query = new MCRQuery(cAnd,sortCriteria,0);
-    	mcrResult = MCRQueryManager.search(query);
-    	
-    	/**
-    	 * <mcrresults sorted="true">
-			  <mcrhit mcrid="atlibri_professorum_000000000441">
-			    <sortData>
-			      <data name="title">Aepinus</data>
-			    </sortData>
-			  </mcrhit>
-			  <mcrhit mcrid="atlibri_document_000000000001">
-			    <sortData>
-			      <data name="title">Haupttitel</data>
-			    </sortData>
-			  </mcrhit>
-			  <mcrhit mcrid="atlibri_document_000000000002">
-			    <sortData>
-			      <data name="title">Artikel Test Editor Servlet</data>
-			    </sortData>
-			  </mcrhit>
-			...  
-    	 */
-    	logger.debug("Results found hits:" + mcrResult.getNumHits());    	    	
-    	jResult = new Document(mcrResult.buildXML());
-    	
+            	
+        MCRQuery query = new MCRQuery(cAnd,sortCriteria,0);        
+    	logger.debug("Query: \n" + out.outputString(query.buildXML()));
+    	return query;
     }
     
     //**************************************************************************
@@ -400,47 +379,95 @@ public class MCRIndexBrowserData {
     
     private int fillmyLinkedList() {
 	    
-    	if (ic.table.equals(this.selectedtype))
-    	    return this.ll1.size();
-    	else this.ll1.clear();
-
-    	buildQuery();
+    	MCRQuery qq = buildQuery();
     	
+    	// look into the cache if the linkedlist with the query as key is cached
+        String cachingID = getCachingID( out.outputString(qq.buildXML()));
+        ll1 = (LinkedList) (ll1Cache.get(cachingID));
+        if (ll1 != null) {
+        	logger.debug("use the linkedList with the resuls from Cache");
+    	    return ll1.size();
+        } else {      
+        	ll1 = new LinkedList(); 
+        }        
+
+        mcrResult = MCRQueryManager.search(qq);    	
+    	logger.debug("Results found hits:" + mcrResult.getNumHits());    	    	
+    	
+    	jResult = new Document(mcrResult.buildXML());               
 	    List mcrHit = jResult.getRootElement().getChildren();
-	    /**
-    	 * <mcrresults sorted="true">
-			  <mcrhit id="atlibri_document_000000000002">
-			    <sortData>
-			      <data name="title">Artikel Test Editor Servlet</data>
-			    </sortData>
-			  </mcrhit>
-			...  
-    	 */
+	    
 	    if ( mcrHit != null ) {
 	        int numRows = mcrHit.size();
 
-	    	// List elemente[sortfield,browsefield,extrafields1, extrafield2, ... extrafieldn]
-            for (int i = 0; i< numRows; i++) {
-            	
-                String[] listelm = new String[2 + ( (ic.extraFields !=null)?ic.extraFields.length:0 ) ];                
+            for (int i = 0; i< numRows; i++) {            	           	            	            	
+                String[] listelm = new String[ ic.outputFields.length + 2];                
             	Element child = (Element)(mcrHit.get( i ));   
+                //logger.debug("\n" + out.outputString(child));
             	
-            	if ( MCRAccessManager.checkPermission( child.getAttributeValue("id"),"read") ){
-	            	String[] brFields = new String[1];
-	            	brFields[0]=  ic.sortFields[0];
-	            	           	
-	            	if ( ic.sortFields != null ) 
-	            			setListeElm(child, ic.sortFields ,0, listelm, true);
-	            	
-	           		setListeElm(child, brFields, 1, listelm, false);           		
-	           		if (ic.extraFields !=null)
-	           			setListeElm(child, ic.extraFields, 2, listelm, false);
-	           		
-	                this.ll1.add(listelm);
+	       		MCRObject oo = new MCRObject();
+	    	    Document od =  oo.receiveJDOMFromDatastore(child.getAttributeValue("id"));
+
+	    	    // List elemente[sortfield, browsefield, extraoutputfields1, extraoutputfield2, ... extrafieldn]
+            	
+	    	    // sortfield must be also a metadata field, but it's from searchfield.xml -use the same names!
+            	setListeElm(od, ic.sortFields, 0, listelm, true);
+            	
+	    	    // browsefield must be also a metadata field, but it's from searchfield.xml -use the same names!
+            	String[] brFields = new String[1];
+            	brFields[0]=  ic.browseField;
+            	setListeElm(od, brFields, 1, listelm, false);
+        	    	    	    
+            	//outputfields came only from metadataobject
+            	if ( ic.outputFields != null) {
+           			 setListeElm(od, ic.outputFields, 2, listelm, false);
             	}
+                ll1.add(listelm);
             }            
 	    }
-	    return this.ll1.size();
+	    // cache it for the next time	    
+        ll1Cache.put(cachingID, ll1);
+	    return ll1.size();
     }            
+
+    // reads the values from the metadata of the document
+    private void setListeElm(Document od, String[] Fields, int startindex, String[] listelm, boolean append){
+        if ( Fields == null) return;
+        for (int j = 0; j < Fields.length; j++) {
+        	String   value = "";
+        	String   attribute = "";        	
+    	    String   sField= Fields[j];
+    	    
+            // Eintrag der ID
+        	if ( sField.equals("id")) {
+        		value = od.getRootElement().getAttributeValue("ID");
+        		
+        	} else {
+        	    Iterator it = od.getDescendants(new ElementFilter(sField));
+        	    // only the atribute different texts are taken!!
+        	    while (it.hasNext()) {
+        	      Element el = (Element) it.next();
+        	      if ( attribute != el.getAttributeValue("type") ) {
+        	    	if ( value.length() > 0) {
+        	    		value += " - ";
+        	    	}
+        	    	value +=  el.getText();
+        	      	attribute =  el.getAttributeValue("type");
+        	      }
+        	    }  	
+        	}        		
+       		if ( append) {
+                if ( j==0)  listelm[startindex]  = value;
+                else    	listelm[startindex] +=", "+ value;
+       		}
+            else listelm[startindex + j] = value;
+        }
+
+	}
     
+    protected String getCachingID(String sQuery) {
+        int hashCode = sQuery.hashCode();
+        return String.valueOf(hashCode);
+    }
+
 }
