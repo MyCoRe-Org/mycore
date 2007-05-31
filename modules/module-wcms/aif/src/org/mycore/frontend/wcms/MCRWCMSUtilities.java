11fe436cbe226537f8bc9238e2f222a264fe474a
Andreas Trappe <mcratrap@duepub-1.zim.uni-duisburg-essen.de>
2007-05-31 15:15:55 +0000

    Migration of WCMS-User-Management to Mycore-User-Management
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@11615 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ modules/module-wcms/aif/src/org/mycore/frontend/wcms/MCRWCMSUtilities.java
@@ -0,0 +1,52 @@
+package org.mycore.frontend.wcms;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.xpath.XPath;
+import org.mycore.access.MCRAccessInterface;
+import org.mycore.access.MCRAccessManager;
+import org.mycore.common.MCRConfiguration;
+
+
+public class MCRWCMSUtilities {
+	final static String OBJIDPREFIX= "webpage:";
+	
+	public static boolean access(String webpageID, String permission) throws JDOMException, IOException {
+		return getAccessGeneral() && getAccess(webpageID, permission);
+	}
+
+	private static boolean getAccess(String webpageID, String permission) throws JDOMException, IOException {
+		// get item as JDOM-Element
+		final Document navi = getNavi();
+		String xpathExp = "//item[@href='"+webpageID+"']";
+		XPath xpath = XPath.newInstance(xpathExp); 
+		Element item = (Element)xpath.selectSingleNode(navi);
+		// check permission != false of each parent item and of current one
+		boolean access = true;
+		MCRAccessInterface am = MCRAccessManager.getAccessImpl();
+		do {
+			String itemID = item.getAttributeValue("href");
+			String objID = OBJIDPREFIX+itemID;
+			if (am.hasRule(objID, permission)) 
+				access = am.checkPermission(objID,permission);
+			item = item.getParentElement();
+		} while (item!=null && access);
+		return access;
+	}
+
+	public static boolean getAccessGeneral(){
+		return MCRAccessManager.getAccessImpl().checkPermission("wcms-access");
+	}
+	
+	private static Document getNavi() throws JDOMException, IOException {
+		final MCRConfiguration CONFIG = MCRConfiguration.instance();
+		final File navFile = new File(CONFIG.getString("MCR.WCMS.navigationFile").replace('/', File.separatorChar));
+		final Document navigation = new SAXBuilder().build(navFile);
+		return navigation;
+	}
+}
