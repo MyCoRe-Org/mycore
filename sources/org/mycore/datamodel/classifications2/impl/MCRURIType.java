381ba6050e3f4602ff77722c631306f403b25bbe
Thomas Scheffler <github@yagee.de>
2007-06-25 10:57:26 +0000

    hibernate UserType to map java.net.URI to SQL VARCHAR
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@11883 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/datamodel/classifications2/impl/MCRURIType.java
@@ -0,0 +1,166 @@
+/**
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ** M y C o R e **
+ * Visit our homepage at http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, normally in the file license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *
+ **/
+package org.mycore.datamodel.classifications2.impl;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.UserType;
+
+/**
+ * @author Thomas Scheffler (yagee)
+ * 
+ * This class maps a java.net.URI to a SQL VARCHAR.
+ * 
+ * @see URI
+ * @see Types#VARCHAR
+ */
+public class MCRURIType implements UserType {
+
+    private static final int[] SQL_TYPES = { Types.VARCHAR };
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#assemble(java.io.Serializable,
+     *      java.lang.Object)
+     */
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#deepCopy(java.lang.Object)
+     */
+    public Object deepCopy(Object value) throws HibernateException {
+        // arg0 is not mutable
+        return value;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#disassemble(java.lang.Object)
+     */
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable) value;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#equals(java.lang.Object,
+     *      java.lang.Object)
+     */
+    public boolean equals(Object x, Object y) throws HibernateException {
+        if (x == y) {
+            return true;
+        } else if (x != null) {
+            return x.equals(y);
+        }
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#hashCode(java.lang.Object)
+     */
+    public int hashCode(Object x) throws HibernateException {
+        if (x == null) {
+            return 0;
+        }
+        return x.hashCode();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#isMutable()
+     */
+    public boolean isMutable() {
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#nullSafeGet(java.sql.ResultSet,
+     *      java.lang.String[], java.lang.Object)
+     */
+    public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
+        String name = rs.getString(names[0]);
+        return rs.wasNull() ? null : URI.create(name);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#nullSafeSet(java.sql.PreparedStatement,
+     *      java.lang.Object, int)
+     */
+    public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
+        if (value == null) {
+            st.setNull(index, Types.VARCHAR);
+        } else {
+            st.setString(index, value.toString());
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#replace(java.lang.Object,
+     *      java.lang.Object, java.lang.Object)
+     */
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#returnedClass()
+     */
+    public Class returnedClass() {
+        return URI.class;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hibernate.usertype.UserType#sqlTypes()
+     */
+    public int[] sqlTypes() {
+        return SQL_TYPES;
+    }
+
+}
