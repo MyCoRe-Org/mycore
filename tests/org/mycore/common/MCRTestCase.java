90d9446bf6fe992b38570448d9af1c51e7f4c20f
Thomas Scheffler <github@yagee.de>
2007-06-29 08:12:28 +0000

    dont log with debug Level by default
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@11999 7089d063-bc14-496d-ac90-53de1872eb6c

--- tests/org/mycore/common/MCRTestCase.java
+++ tests/org/mycore/common/MCRTestCase.java
@@ -5,45 +5,52 @@ import java.io.File;
 import junit.framework.TestCase;
 
 public class MCRTestCase extends TestCase {
-    File properties=null;
-    static MCRConfiguration CONFIG; 
+    File properties = null;
+
+    static MCRConfiguration CONFIG;
+
+    protected boolean debug = false;
 
     /**
      * initializes MCRConfiguration with an empty property file.
      * 
-     * This can be used to test MyCoRe classes without any propties set, using default.
-     * You may want to set Properties per TestCase with the getProperties() method of <code>MCRConfiguration</code>
+     * This can be used to test MyCoRe classes without any propties set, using
+     * default. You may want to set Properties per TestCase with the
+     * getProperties() method of <code>MCRConfiguration</code>
      * 
      * @see MCRConfiguration#getProperties()
      */
     protected void setUp() throws Exception {
         super.setUp();
-        if (System.getProperties().getProperty("MCR.Configuration.File")==null){
-            properties=File.createTempFile("test",".properties");
-            System.getProperties().setProperty("MCR.Configuration.File",properties.getAbsolutePath());
+        if (System.getProperties().getProperty("MCR.Configuration.File") == null) {
+            properties = File.createTempFile("test", ".properties");
+            System.getProperties().setProperty("MCR.Configuration.File", properties.getAbsolutePath());
         }
-        CONFIG=MCRConfiguration.instance();
-        boolean setPropertie=false;
-        setPropertie=setProperty("log4j.rootLogger","DEBUG, stdout",false)?true:setPropertie;
-        setPropertie=setProperty("log4j.appender.stdout","org.apache.log4j.ConsoleAppender",false)?true:setPropertie;
-        setPropertie=setProperty("log4j.appender.stdout.layout","org.apache.log4j.PatternLayout",false)?true:setPropertie;
-        setPropertie=setProperty("log4j.appender.stdout.layout.ConversionPattern","%-5p %m%n",false)?true:setPropertie;
-        if (setPropertie){
+        CONFIG = MCRConfiguration.instance();
+        boolean setPropertie = false;
+        if (debug)
+            setPropertie = setProperty("log4j.rootLogger", "DEBUG, stdout", false) ? true : setPropertie;
+        else
+            setPropertie = setProperty("log4j.rootLogger", "INFO, stdout", false) ? true : setPropertie;
+        setPropertie = setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender", false) ? true : setPropertie;
+        setPropertie = setProperty("log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout", false) ? true : setPropertie;
+        setPropertie = setProperty("log4j.appender.stdout.layout.ConversionPattern", "%-5p %m%n", false) ? true : setPropertie;
+        if (setPropertie) {
             CONFIG.configureLogging();
         }
-   }
-    
+    }
+
     protected void tearDown() throws Exception {
         super.tearDown();
-        if (properties!=null){
+        if (properties != null) {
             properties.delete();
         }
     }
-    
-    protected boolean setProperty(String key, String value, boolean overwrite){
-        String propValue=CONFIG.getProperties().getProperty(key);
-        if ((propValue==null) || (overwrite==true)){
-            CONFIG.getProperties().setProperty(key,value);
+
+    protected boolean setProperty(String key, String value, boolean overwrite) {
+        String propValue = CONFIG.getProperties().getProperty(key);
+        if ((propValue == null) || (overwrite == true)) {
+            CONFIG.getProperties().setProperty(key, value);
             return true;
         }
         return false;
