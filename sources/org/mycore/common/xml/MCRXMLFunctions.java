7ada95a74a56ee9691b3834ab2ddf16a6d784669
Thomas Scheffler <github@yagee.de>
2007-08-07 08:52:57 +0000

    Incomplete - task 1759545: Formatierung des Datums in einem Styleshheet http://sourceforge.net/tracker/?group_id=92005&func=detail&atid=599192&aid=1759545
    should not return "null" anymore.
    
    
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@12181 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/xml/MCRXMLFunctions.java
+++ sources/org/mycore/common/xml/MCRXMLFunctions.java
@@ -46,107 +46,109 @@ import org.mycore.services.fieldquery.MCRResults;
  */
 public class MCRXMLFunctions {
 
-	static MCRConfiguration CONFIG = MCRConfiguration.instance();
+    static MCRConfiguration CONFIG = MCRConfiguration.instance();
 
-	private static final String HOST_PREFIX = "MCR.remoteaccess_";
+    private static final String HOST_PREFIX = "MCR.remoteaccess_";
 
-	private static final String QUERY_SUFFIX = "_query_servlet";
+    private static final String QUERY_SUFFIX = "_query_servlet";
 
-	private static final String IFS_SUFFIX = "_ifs_servlet";
+    private static final String IFS_SUFFIX = "_ifs_servlet";
 
-	private static final String HOST_SUFFIX = "_host";
+    private static final String HOST_SUFFIX = "_host";
 
-	private static final String PORT_SUFFIX = "_port";
+    private static final String PORT_SUFFIX = "_port";
 
-	private static final String PROTOCOLL_SUFFIX = "_protocol";
+    private static final String PROTOCOLL_SUFFIX = "_protocol";
 
-	private static final String DEFAULT_PORT = "80";
-    
-    private static final Logger LOGGER=Logger.getLogger(MCRXMLFunctions.class);
+    private static final String DEFAULT_PORT = "80";
 
-	/**
-	 * returns the given String trimmed
-	 * 
-	 * @param arg0
-	 *            String to be trimmed
-	 * @return trimmed copy of arg0
-	 * @see java.lang.String#trim()
-	 */
-	public static String trim(String arg0) {
-		return arg0.trim();
-	}
+    private static final Logger LOGGER = Logger.getLogger(MCRXMLFunctions.class);
 
-	/**
-	 * returns the QueryServlet-Link of the given hostAlias
-	 * 
-	 * @param hostAlias
-	 *            remote alias
-	 * @return QueryServlet-Link
-	 */
-	public static String getQueryServlet(String hostAlias) {
-		return getBaseLink(hostAlias).append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(QUERY_SUFFIX).toString())).toString();
-	}
-
-	/**
-	 * returns the FileNodeServlet-Link of the given hostAlias
-	 * 
-	 * @param hostAlias
-	 *            remote alias
-	 * @return FileNodeServlet-Link
-	 */
-	public static String getIFSServlet(String hostAlias) {
-		return getBaseLink(hostAlias).append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(IFS_SUFFIX).toString())).toString();
-	}
-
-	public static StringBuffer getBaseLink(String hostAlias) {
-		StringBuffer returns = new StringBuffer();
-		returns.append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(PROTOCOLL_SUFFIX).toString(), "http")).append("://").append(
-				CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(HOST_SUFFIX).toString()));
-		String port = CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(PORT_SUFFIX).toString(), DEFAULT_PORT);
-		if (!port.equals(DEFAULT_PORT)) {
-			returns.append(":").append(port);
-		}
-		return returns;
-	}
-    
-    public static String formatISODate(String isoDate, String simpleFormat, String iso639Language) throws ParseException{
-        return formatISODate(isoDate,null, simpleFormat,iso639Language);
+    /**
+     * returns the given String trimmed
+     * 
+     * @param arg0
+     *            String to be trimmed
+     * @return trimmed copy of arg0
+     * @see java.lang.String#trim()
+     */
+    public static String trim(String arg0) {
+        return arg0.trim();
     }
 
-    public static String formatISODate(String isoDate, String isoFormat, String simpleFormat, String iso639Language) throws ParseException{
-        if (LOGGER.isDebugEnabled()){
-            StringBuffer sb=new StringBuffer("isoDate=");
-            sb.append(isoDate).append(", simpleFormat=").append(simpleFormat)
-            .append(", isoFormat=").append(isoFormat).append(", iso649Language=").append(iso639Language);
+    /**
+     * returns the QueryServlet-Link of the given hostAlias
+     * 
+     * @param hostAlias
+     *            remote alias
+     * @return QueryServlet-Link
+     */
+    public static String getQueryServlet(String hostAlias) {
+        return getBaseLink(hostAlias).append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(QUERY_SUFFIX).toString())).toString();
+    }
+
+    /**
+     * returns the FileNodeServlet-Link of the given hostAlias
+     * 
+     * @param hostAlias
+     *            remote alias
+     * @return FileNodeServlet-Link
+     */
+    public static String getIFSServlet(String hostAlias) {
+        return getBaseLink(hostAlias).append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(IFS_SUFFIX).toString())).toString();
+    }
+
+    public static StringBuffer getBaseLink(String hostAlias) {
+        StringBuffer returns = new StringBuffer();
+        returns.append(CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(PROTOCOLL_SUFFIX).toString(), "http")).append("://").append(
+                CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(HOST_SUFFIX).toString()));
+        String port = CONFIG.getString(new StringBuffer(HOST_PREFIX).append(hostAlias).append(PORT_SUFFIX).toString(), DEFAULT_PORT);
+        if (!port.equals(DEFAULT_PORT)) {
+            returns.append(":").append(port);
+        }
+        return returns;
+    }
+
+    public static String formatISODate(String isoDate, String simpleFormat, String iso639Language) throws ParseException {
+        return formatISODate(isoDate, null, simpleFormat, iso639Language);
+    }
+
+    public static String formatISODate(String isoDate, String isoFormat, String simpleFormat, String iso639Language) throws ParseException {
+        if (LOGGER.isDebugEnabled()) {
+            StringBuffer sb = new StringBuffer("isoDate=");
+            sb.append(isoDate).append(", simpleFormat=").append(simpleFormat).append(", isoFormat=").append(isoFormat).append(", iso649Language=").append(
+                    iso639Language);
             LOGGER.debug(sb.toString());
         }
-        Locale locale=new Locale(iso639Language);
-        SimpleDateFormat df=new SimpleDateFormat(simpleFormat,locale);
-        MCRMetaISO8601Date mcrdate=new MCRMetaISO8601Date();
+        Locale locale = new Locale(iso639Language);
+        SimpleDateFormat df = new SimpleDateFormat(simpleFormat, locale);
+        MCRMetaISO8601Date mcrdate = new MCRMetaISO8601Date();
         mcrdate.setFormat(isoFormat);
         mcrdate.setDate(isoDate);
-        Date date=mcrdate.getDate();
-        return df.format(date);
+        Date date = mcrdate.getDate();
+        return (date == null) ? "?"+isoDate+"?" : df.format(date);
     }
 
-    public static String getISODate(String simpleDate,String simpleFormat, String isoFormat) throws ParseException{
-        SimpleDateFormat df=new SimpleDateFormat(simpleFormat);
-        df.setTimeZone(TimeZone.getTimeZone("UTC")); // or else testcase "1964-02-24" would result "1964-02-23"
-        Date date=df.parse(simpleDate);
-        MCRMetaISO8601Date mcrdate=new MCRMetaISO8601Date();
+    public static String getISODate(String simpleDate, String simpleFormat, String isoFormat) throws ParseException {
+        SimpleDateFormat df = new SimpleDateFormat(simpleFormat);
+        df.setTimeZone(TimeZone.getTimeZone("UTC")); // or else testcase
+                                                        // "1964-02-24" would
+                                                        // result "1964-02-23"
+        Date date = df.parse(simpleDate);
+        MCRMetaISO8601Date mcrdate = new MCRMetaISO8601Date();
         mcrdate.setDate(date);
         mcrdate.setFormat(isoFormat);
         return mcrdate.getISOString();
     }
 
-    public static String getISODate(String simpleDate,String simpleFormat) throws ParseException{
-        return getISODate(simpleDate,simpleFormat,null);
+    public static String getISODate(String simpleDate, String simpleFormat) throws ParseException {
+        return getISODate(simpleDate, simpleFormat, null);
     }
-    
-    public static String regexp(String orig, String match, String replace){
+
+    public static String regexp(String orig, String match, String replace) {
         return orig.replaceAll(match, replace);
     }
-    
+
     public static int getQueryHitCount(String query) {
         Element queryElement = new Element("query");
         queryElement.setAttribute("maxResults", "0");
