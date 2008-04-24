ff6a23858e5438efb8a2de71a0818aae8620e6bd
Andreas Trappe <mcratrap@duepub-1.zim.uni-duisburg-essen.de>
2008-04-24 12:55:40 +0000

    New feature:
    All active sessions can be watched by GUI.
    
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@13438 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/MCRSessionMgr.java
+++ sources/org/mycore/common/MCRSessionMgr.java
@@ -202,4 +202,8 @@ public class MCRSessionMgr {
         listenersLock.writeLock().unlock();
     }
 
+    public static Map<String, MCRSession> getAllSessions() {
+        return sessions;
+    }
+
 }
