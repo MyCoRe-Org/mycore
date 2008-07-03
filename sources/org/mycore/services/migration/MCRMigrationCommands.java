73d49026a6cc3f52030db5a326a1a6958c2d51eb
Andreas Trappe <mcratrap@duepub-1.zim.uni-duisburg-essen.de>
2008-07-03 16:12:39 +0000

    bug fix: orphaned links are skipped
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@13704 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/services/migration/MCRMigrationCommands.java
+++ sources/org/mycore/services/migration/MCRMigrationCommands.java
@@ -6,16 +6,12 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import org.apache.log4j.Logger;
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Projection;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.impl.CriteriaImpl;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -73,6 +69,7 @@ public class MCRMigrationCommands extends MCRAbstractCommands {
             dao.addCategory(null, cat);
             MCRXMLTableManager.instance().delete(objID);
         }
+
         LOGGER.info("Migrating object links....");
         // init
         LOGGER.debug("retrieving all relevant objects id's");
@@ -81,6 +78,7 @@ public class MCRMigrationCommands extends MCRAbstractCommands {
         MCRXMLTableManager xmlTable = MCRXMLTableManager.instance();
         MCRCategLinkService clsf = MCRCategLinkServiceFactory.getInstance();
         LOGGER.debug(lengthAbs + "object id's retrieved");
+
         // pass through found objects
         int pos = 0;
         long startTime = System.currentTimeMillis();
@@ -89,32 +87,37 @@ public class MCRMigrationCommands extends MCRAbstractCommands {
             LOGGER.debug("Processing object " + id);
             Collection<MCRCategoryID> categories = new HashSet<MCRCategoryID>();
             LOGGER.debug("parsing " + id);
-            Document obj = xmlTable.readDocument(new MCRObjectID(id));
-            LOGGER.debug("executing XPATH " + xpathExpr);
-            List<Element> classElements = classSelector.selectNodes(obj);
-            int length = classElements.size();
-            LOGGER.debug("passing through " + length + " found category ids");
-            for (Element el : classElements) {
-                String clid = el.getAttributeValue("classid");
-                String catid = el.getAttributeValue("categid");
-                categories.add(new MCRCategoryID(clid, catid));
-            }
-            if (categories.size() > 0) {
-                LOGGER.debug("updating references");
-                String type = id.substring(id.indexOf("_") + 1, id.lastIndexOf("_") - 1);
-                MCRObjectReference objectReference = new MCRObjectReference(id, type);
-                try {
-                    clsf.setLinks(objectReference, categories);
-                } catch (Exception e) {
-                    LOGGER.error("Error occured while creating category links for object " + id, e);
+            if (xmlTable.exist(new MCRObjectID(id))) {
+                Document obj = xmlTable.readDocument(new MCRObjectID(id));
+                LOGGER.debug("executing XPATH " + xpathExpr);
+                List<Element> classElements = classSelector.selectNodes(obj);
+                int length = classElements.size();
+                LOGGER.debug("passing through " + length + " found category ids");
+                for (Element el : classElements) {
+                    String clid = el.getAttributeValue("classid");
+                    String catid = el.getAttributeValue("categid");
+                    categories.add(new MCRCategoryID(clid, catid));
                 }
-            }
-            if (pos % 100 == 0 || pos == lengthAbs) {
-                long currTime = System.currentTimeMillis();
-                long finishTime = currTime + ((currTime - startTime) * (lengthAbs - pos) / pos);
-                LOGGER.info(((float) (pos / lengthAbs) * 100) + " %, estimated finish time is " + new Date(finishTime));
-            }
+                if (categories.size() > 0) {
+                    LOGGER.debug("updating references");
+                    String type = id.substring(id.indexOf("_") + 1, id.lastIndexOf("_") - 1);
+                    MCRObjectReference objectReference = new MCRObjectReference(id, type);
+                    try {
+                        clsf.setLinks(objectReference, categories);
+                    } catch (Exception e) {
+                        LOGGER.error("Error occured while creating category links for object " + id, e);
+                    }
+                }
+                if (pos % 100 == 0 || pos == lengthAbs) {
+                    long currTime = System.currentTimeMillis();
+                    long finishTime = currTime + ((currTime - startTime) * (lengthAbs - pos) / pos);
+                    LOGGER.info(((float) (pos / lengthAbs) * 100) + " %, estimated finish time is " + new Date(finishTime));
+                }
+            } else
+                LOGGER.warn("stored link from " + id + " found, that is not in database");
         }
+
+        // }
         LOGGER.info("Deleting old object links...");
         final Session session = MCRHIBConnection.instance().getSession();
         Criteria c = session.createCriteria(MCRLINKHREF.class).add(Restrictions.eq("key.mcrtype", "classid"));
@@ -129,6 +132,11 @@ public class MCRMigrationCommands extends MCRAbstractCommands {
         Criteria c = session.createCriteria(MCRLINKHREF.class);
         c.setProjection(Projections.distinct(Projections.property("key.mcrfrom")));
         c.add(Restrictions.eq("key.mcrtype", "classid"));
+        // String query = "select DISTINCT MCRFROM from MCRLINKHREF where
+        // MCRTYPE = 'classid'";
+        // String query = "select key.mcrfrom from MCRLINKHREF where key.mcrtype
+        // = 'classid'";
+        // List<String> l = session.createQuery(query).list();
         return c.list();
     }
 
