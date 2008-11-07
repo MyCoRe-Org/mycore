257de4c3336e614b8005f43e7e24ff15c4570f37
Thomas Scheffler <github@yagee.de>
2008-11-07 14:38:53 +0000

    disable saving gnuplot data per default
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14356 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/cli/MCRCommandLineInterface.java
+++ sources/org/mycore/frontend/cli/MCRCommandLineInterface.java
@@ -30,7 +30,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -198,7 +197,7 @@ public class MCRCommandLineInterface {
                 if (interactiveMode) {
                     command = readCommandFromPrompt();
                 } else {
-                    if (firstCommand != null)
+                    if (firstCommand != null && config.getBoolean("MCR.CLI.SaveRuntimeStatistics", false));
                         try {
                             saveMillis(firstCommand);
                         } catch (IOException e) {
