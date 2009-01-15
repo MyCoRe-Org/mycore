155e0f1878b38457c190b6a42a1458a907de6909
Thomas Scheffler <github@yagee.de>
2009-01-15 13:41:19 +0000

    a new metadata type to link files of (possible foreign) derivates
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14590 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/datamodel/metadata/MCRMetaDerivateLink.java
@@ -0,0 +1,23 @@
+package org.mycore.datamodel.metadata;
+
+import org.mycore.datamodel.ifs.MCRDirectory;
+import org.mycore.datamodel.ifs.MCRFile;
+
+public class MCRMetaDerivateLink extends MCRMetaLink {
+
+    public void setLinkToFile(MCRFile file) {
+        String owner = file.getOwnerID();
+        String path = file.getAbsolutePath();
+        super.href = owner + path;
+    }
+
+    public MCRFile getLinkedFile() {
+        int index = super.href.indexOf('/');
+        if (index < 0)
+            return null;
+        String owner = super.href.substring(0, index);
+        String path = super.href.substring(index);
+        return (MCRFile) ((MCRDirectory) MCRFile.getRootNode(owner)).getChildByPath(path);
+    }
+
+}
