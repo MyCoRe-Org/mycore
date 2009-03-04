372a2a0fced972d268eb3339e9992da8e3e53876
Thomas Scheffler <github@yagee.de>
2009-03-04 08:54:23 +0000

    prove that bug #2383207 (Default Language in new Classbrowser ignored?) is invalid
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14797 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ tests/org/mycore/datamodel/classifications2/impl/MCRAbstractCategoryImplTest.java
@@ -0,0 +1,45 @@
+package org.mycore.datamodel.classifications2.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mycore.common.MCRSession;
+import org.mycore.common.MCRSessionMgr;
+import org.mycore.common.MCRTestCase;
+import org.mycore.datamodel.classifications2.MCRCategory;
+import org.mycore.datamodel.classifications2.MCRLabel;
+
+public class MCRAbstractCategoryImplTest extends MCRTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setProperty("MCR.Metadata.DefaultLang", "de", true);
+    }
+    
+    public void testGetCurrentLabel(){
+        MCRCategory cat=new MCRAbstractCategoryImpl(){
+            {
+                labels=new ArrayList<MCRLabel>();
+            }
+            @Override
+            protected void setChildren(List<MCRCategory> children) {}
+            public int getLevel() {return 0;}
+        };
+        MCRLabel label1=new MCRLabel("de", "german", null);
+        MCRLabel label2=new MCRLabel("fr", "french", null);
+        MCRLabel label3=new MCRLabel("at", "austrian", null);
+        cat.getLabels().add(label1);
+        cat.getLabels().add(label2);
+        cat.getLabels().add(label3);
+        MCRSession session=MCRSessionMgr.getCurrentSession();
+        session.setCurrentLanguage("en");
+        assertEquals("German label expected", cat.getCurrentLabel(), label1);
+        cat.getLabels().clear();
+        cat.getLabels().add(label2);
+        cat.getLabels().add(label3);
+        cat.getLabels().add(label1);
+        assertEquals("German label expected", cat.getCurrentLabel(), label1);
+    }
+
+}
