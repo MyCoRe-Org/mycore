8dfc0c7b3c83ff074dc8084f6730b15f0a301999
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2009-03-06 12:53:48 +0000

    Added functionality for multi-language labels
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14811 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/ifs2/MCRStoredNode.java
+++ sources/org/mycore/datamodel/ifs2/MCRStoredNode.java
@@ -1,15 +1,24 @@
 package org.mycore.datamodel.ifs2;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.VFS;
 import org.jdom.Element;
+import org.mycore.common.MCRSessionMgr;
 
 public abstract class MCRStoredNode extends MCRNode
 {
-  protected String label;
+  protected SortedMap<String,String> labels;
   
   protected MCRStoredNode( MCRDirectory parent, FileObject fo ) throws Exception
-  { super( parent, fo ); }
+  { 
+    super( parent, fo );
+    labels = new TreeMap<String,String>();
+  }
   
   public void delete() throws Exception
   { 
@@ -26,12 +35,25 @@ public abstract class MCRStoredNode extends MCRNode
   protected void writeChildData( Element entry ) throws Exception
   {
     entry.setAttribute( "name", this.getName() );
-    if( label != null )
-      entry.setAttribute( "label", this.getLabel() );
+    entry.removeChildren( "label" );
+    if( ! labels.isEmpty() )
+    {
+      Iterator<String> it = labels.keySet().iterator();
+      while( it.hasNext() )
+      {
+        String lang = it.next();  
+        String label = labels.get( lang );
+        entry.addContent( new Element( "label" ).setAttribute( "lang", lang ).setText( label ) );
+      }
+    }
   }
   
   protected void readChildData( Element entry ) throws Exception
-  { label = entry.getAttributeValue( "label", (String)null ); }
+  { 
+    labels.clear();
+    for( Element label : (List<Element>)( entry.getChildren( "label" ) ) )
+      labels.put( label.getAttributeValue( "lang" ), label.getTextTrim() );  
+  }
   
   public void renameTo( String name ) throws Exception
   {
@@ -44,12 +66,34 @@ public abstract class MCRStoredNode extends MCRNode
       ((MCRDirectory)parent).updateMetadata( oldName, this );
   }
   
-  public void setLabel( String label ) throws Exception
+  public void setLabel( String lang, String label ) throws Exception
   { 
-    this.label = label; 
+    labels.put( lang, label ); 
     updateMetadata();
   }
   
-  public String getLabel()
-  { return label; }
+  public void clearLabels() throws Exception
+  {
+    labels.clear();
+    updateMetadata();
+  }
+  
+  public SortedMap<String,String> getLabels()
+  { return labels; }
+  
+  public String getLabel( String lang )
+  { return labels.get( lang ); }
+  
+  private final static String defaultLang = "de";
+  
+  public String getCurrentLabel()
+  {
+    if( labels.isEmpty() ) return null;
+    String lang = MCRSessionMgr.getCurrentSession().getCurrentLanguage();
+    String label = labels.get( lang );
+    if( label != null ) return label;
+    label = labels.get( defaultLang );
+    if( label != null ) return label;
+    return labels.get( labels.firstKey() );
+  }
 }
