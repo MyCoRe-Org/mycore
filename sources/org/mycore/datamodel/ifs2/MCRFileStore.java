f7bd7f83bdf73aedea11016c46a20bdeaa788fe9
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2009-03-08 19:19:05 +0000

    Added complete JavaDocs, removed getLastModified(id), this can be invoked on the retrieved MCRFileCollection itself.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14829 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/ifs2/MCRFileStore.java
+++ sources/org/mycore/datamodel/ifs2/MCRFileStore.java
@@ -9,10 +9,25 @@ import org.apache.commons.vfs.FileObject;
 import org.mycore.common.MCRConfiguration;
 import org.mycore.common.MCRException;
 
+/**
+ * Stores file collections containing files and directories.
+ *
+ * For each store, properties must be defined, for example
+ * 
+ * MCR.IFS2.FileStore.STOREID.BaseDir=c:\\store
+ * MCR.IFS2.FileStore.STOREID.SlotLayout=3-3-2-8
+ */
 public class MCRFileStore extends MCRStore 
 {
+  /**
+   * Map of defined file stores. Key is store ID, value is
+   * the store with that ID.
+   */
   private static HashMap<String,MCRFileStore> stores;
     
+  /**
+   * Reads configuration and initializes stores.
+   */
   static
   {
     stores = new HashMap<String,MCRFileStore>();
@@ -34,21 +49,48 @@ public class MCRFileStore extends MCRStore
     }
   }
       
+  /**
+   * Returns the store with the given ID
+   *
+   * @param id the store ID
+   * @return the store with the given ID
+   */
   public static MCRFileStore getStore( String id )
   { return stores.get( id ); }
-      
+
+  /**
+   * Creates a new file store instance
+   * 
+   * @param id the store ID
+   * @param baseDir the base directory storing data
+   * @param slotLayout the layout of slot subdirectories
+   */
   protected MCRFileStore( String id, String baseDir, String slotLayout )
   { 
     super( id, baseDir, slotLayout, "", "" );
     stores.put( id, this );
   }
   
+  /**
+   * Creates and stores a new, empty file collection using the 
+   * next free ID in the store.
+   * 
+   * @return a newly created file collection
+   */
   public MCRFileCollection create() throws Exception
   {
     int id = getNextFreeID();
     return create( id );
   }
-  
+
+  /**
+   * Creates and stores a new, empty file collection with the given
+   * ID
+   * 
+   * @param id the ID of the file collection
+   * @return a newly created file collection
+   * @throws Exception when a file collection with the given ID already exists
+   */
   public MCRFileCollection create( int id ) throws Exception
   {
     FileObject fo = getSlot( id );
@@ -59,7 +101,14 @@ public class MCRFileStore extends MCRStore
     }
     return new MCRFileCollection( this, id, getSlot( id ), true ); 
   }
-  
+
+  /**
+   * Returns the file collection stored under the given ID.
+   * 
+   * @param id the file collection's ID
+   * @return the file collection with the given ID
+   * @throws Exception when a collection with that ID does not exist
+   */
   public MCRFileCollection retrieve( int id ) throws Exception
   {
     FileObject fo = getSlot( id );
@@ -71,14 +120,11 @@ public class MCRFileStore extends MCRStore
     return new MCRFileCollection( this, id, getSlot( id ), false ); 
   }
   
-  public Date getLastModified( int id ) throws Exception
-  {
-    FileObject fo = getSlot( id );
-    if( ! fo.exists() ) return null;
-    long time = fo.getContent().getLastModifiedTime();
-    return new Date( time );
-  }
-  
+  /**
+   * Repairs metadata of all file collections stored here
+   * 
+   * @throws Exception
+   */
   public void repairAllMetadata() throws Exception
   {
     for( Enumeration<Integer> e = listIDs( MCRStore.ASCENDING ); e.hasMoreElements(); )
