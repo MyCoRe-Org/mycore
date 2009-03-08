02b97fac1eec36caaf69d2fb3a559a174cffd982
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2009-03-08 20:07:10 +0000

    Added complete JavaDocs
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@14832 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/ifs2/MCRNode.java
+++ sources/org/mycore/datamodel/ifs2/MCRNode.java
@@ -10,35 +10,93 @@ import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileType;
 import org.apache.commons.vfs.VFS;
 
+/**
+ * Represents a file, directory or file collection within a file store.
+ * Files and directories can be either really stored, or virtually existing
+ * as a child node contained within a stored container file like zip or tar.
+ * 
+ * @author Frank Lützenkirchen
+ */
 public abstract class MCRNode
 {
+  /**
+   * The file object representing this node in the underlying filesystem.
+   */
   protected FileObject fo;
+  
+  /**
+   * The parent node owning this file, a directory or container file
+   */
   protected MCRNode parent;
   
+  /**
+   * Creates a new node representing a child of the given parent
+   *  
+   * @param parent the parent node
+   * @param fo the file object representing this node in the underlying filesystem
+   */
   protected MCRNode( MCRNode parent, FileObject fo )
   {
     this.fo = fo;
     this.parent = parent;
   }
   
+  /**
+   * Returns the file or directory name
+   * 
+   * @return the node's filename
+   */
   public String getName()
   { return fo.getName().getBaseName(); }
   
+  /**
+   * Returns the complete path of this node up to the root file collection.
+   * Path always start with a slash, slash is used as directory delimiter.
+   * 
+   * @return the absolute path of this node
+   * @throws Exception
+   */
   public String getPath() throws Exception
   { return parent.getPath() + "/" + getName(); }
   
+  /**
+   * Returns the parent node containing this node
+   * 
+   * @return the parent directory or container file
+   */
   public MCRNode getParent()
   { return parent; }
   
-  public MCRNode getRoot()
+  /**
+   * Returns the root file collection this node belongs to
+   * 
+   * @return the root file collection
+   */
+  public MCRFileCollection getRoot()
   { return parent.getRoot(); }
   
+  /**
+   * Returns true if this node is a file
+   * 
+   * @return true if this node is a file
+   */
   public boolean isFile() throws Exception
   { return fo.getType().equals( FileType.FILE ); }
-  
+
+  /**
+   * Returns true if this node is a directory
+   * 
+   * @return true if this node is a directory
+   */
   public boolean isDirectory() throws Exception
   { return fo.getType().equals( FileType.FOLDER ); }
-  
+
+  /**
+   * For file nodes, returns the file content size in bytes,
+   * otherwise returns 0.
+   * 
+   * @return the file size in bytes
+   */
   public long getSize() throws Exception
   {
     if( isFile() )
@@ -47,6 +105,12 @@ public abstract class MCRNode
       return 0;
   }
 
+  /**
+   * Returns the time this node was last modified, or null if no such time
+   * is defined in the underlying filesystem
+   * 
+   * @return the time this node was last modified
+   */
   public Date getLastModified() throws Exception
   {
     FileContent content = fo.getContent();
@@ -56,9 +120,23 @@ public abstract class MCRNode
       return null;
   }
   
+  /**
+   * Returns true if this node has child nodes. Directories and container 
+   * files like zip or tar may have child nodes.
+   * 
+   * @return true if children exist
+   */
   public boolean hasChildren() throws Exception
   { return getNumChildren() > 0; }
 
+  /**
+   * Returns the FileObject that is the father of all logical 
+   * children of this FileObject. This may not be the current node itself,
+   * in case the node is a container file, because then intermediate 
+   * FileObject instances are created by Apache VFS.
+   * 
+   * @return the father of this node's children in VFS
+   */
   private FileObject getFather() throws Exception
   {
     if( isDirectory() ) 
@@ -74,7 +152,12 @@ public abstract class MCRNode
     }
     else return null;
   }
-  
+
+  /**
+   * Returns the number of child nodes of this node.
+   * 
+   * @return the number of child nodes of this node.
+   */
   public int getNumChildren() throws Exception
   {
     FileObject father = getFather();
@@ -86,6 +169,12 @@ public abstract class MCRNode
       return father.getChildren().length;
   }
 
+  /**
+   * Returns the children of this node. Directories and container
+   * files like zip or tar may have child nodes.
+   * 
+   * @return a List of child nodes, which may be empty, in undefined order
+   */
   public List<MCRNode> getChildren() throws Exception
   {
     List<MCRNode> children = new ArrayList<MCRNode>();
@@ -103,14 +192,36 @@ public abstract class MCRNode
     return children;
   }
   
+  /**
+   * Creates a node instance for the given FileObject, which represents the child
+   *
+   * @param fo the FileObject representing the child in the underlying filesystem 
+   * @return the child node
+   */
   protected abstract MCRNode buildChildNode( FileObject fo ) throws Exception;
   
+  /**
+   * Returns the child node with the given filename, or null
+   * 
+   * @param name the name of the child node
+   * @return the child node with that name, or null when no such file exists
+   */
   public MCRNode getChild( String name ) throws Exception
   { 
     FileObject father = getFather();
     return ( father == null ? null : buildChildNode( getFather().getChild( name ) ) ); 
   }
-  
+
+  /**
+   * Returns the node with the given relative or absolute path in the file collection this node
+   * belongs to. Slash is used as directory delimiter. When the path starts with a slash, it is 
+   * an absolute path and resolving is startet at the root file collection. When the path is
+   * relative, resolving starts with the current node. One dot represents the current node,
+   * Two dots represent the parent node, like in paths used by typical real filesystems.
+   * 
+   * @param path the absolute or relative path of the node to find, may contain . or ..
+   * @return the node at the given path, or null 
+   */
   public MCRNode getNodeByPath( String path ) throws Exception
   {
     MCRNode current = path.startsWith( "/" ) ? getRoot() : this;
