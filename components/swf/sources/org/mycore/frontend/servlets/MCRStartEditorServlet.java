d54422115fbafc0027b6904d75f3302d990e6ee5
Thomas Scheffler <github@yagee.de>
2009-08-07 08:33:14 +0000

    bugfix: something went wrong merging c13ed331f1a6d4418bcb1af84ca498ea9e695441
     from master
    
    fixed position of bracket
    
    git-svn-id: https://server.mycore.de/svn/mycore/branches/release_2_0_fixes@15699 7089d063-bc14-496d-ac90-53de1872eb6c

--- components/swf/sources/org/mycore/frontend/servlets/MCRStartEditorServlet.java
+++ components/swf/sources/org/mycore/frontend/servlets/MCRStartEditorServlet.java
@@ -324,10 +324,10 @@ public class MCRStartEditorServlet extends MCRServlet {
             if (file.compareTo(max) > 0)
                 max = file;
         }
-        int maxIDinWorkflow = Integer.parseInt( max.substring( max.lastIndexOf( "_" ) + 1 ), max.length() - 4 );  
-            
+        int maxIDinWorkflow = Integer.parseInt(max.substring(max.lastIndexOf("_") + 1, max.length() - 4));
+
         MCRObjectID mcridnext = new MCRObjectID();
-        mcridnext.setNextFreeId(myproject + "_" + mytype, maxIDinWorkflow );
+        mcridnext.setNextFreeId(myproject + "_" + mytype, maxIDinWorkflow);
         return mcridnext.getId();
     }
 
