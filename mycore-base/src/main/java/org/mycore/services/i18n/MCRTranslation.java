1d28527a057cf20ced5a3a1e45a4b5f7bf50f744
Thomas Scheffler <github@yagee.de>
2009-10-02 10:56:16 +0000

    undo b76da35324a7927a17c295eaa28553c05a69d566
    : readded support for deprecated-messages.properties
    
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@15874 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/services/i18n/MCRTranslation.java
+++ mycore-base/src/main/java/org/mycore/services/i18n/MCRTranslation.java
@@ -23,10 +23,13 @@
  **/
 package org.mycore.services.i18n;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -46,9 +49,15 @@ import org.mycore.common.MCRSessionMgr;
  */
 public class MCRTranslation {
 
+    private static final String DEPRECATED_MESSAGES_PROPERTIES = "/deprecated-messages.properties";
+
     private static final Logger LOGGER = Logger.getLogger(MCRTranslation.class);
 
     private static final Pattern ARRAY_DETECTOR = Pattern.compile(";");
+
+    private static boolean DEPRECATED_MESSAGES_PRESENT = false;
+
+    private static Properties DEPRECATED_MAPPING = loadProperties();
 
     /**
      * provides translation for the given label (property key).
@@ -77,6 +86,20 @@ public class MCRTranslation {
             result = message.getString(label);
             LOGGER.debug("Translation for " + label + "=" + result);
         } catch (java.util.MissingResourceException mre) {
+            // try to get new key if 'label' is deprecated
+            if (!DEPRECATED_MESSAGES_PRESENT) {
+                LOGGER.warn("Could not load resource '" + DEPRECATED_MESSAGES_PROPERTIES + "' to check for depreacted I18N keys.");
+            } else if (DEPRECATED_MAPPING.keySet().contains(label)) {
+                String newLabel = DEPRECATED_MAPPING.getProperty(label);
+                try {
+                    result = message.getString(newLabel);
+                } catch (java.util.MissingResourceException e) {
+                }
+                if (result != null) {
+                    LOGGER.warn("Usage of deprected I18N key '" + label + "'. Please use '" + newLabel + "' instead.");
+                    return result;
+                }
+            }
             result = "???" + label + "???";
             LOGGER.debug(mre.getMessage());
         }
@@ -183,6 +206,22 @@ public class MCRTranslation {
             }
         }
         return false;
+    }
+
+    static Properties loadProperties() {
+        Properties deprecatedMapping = new Properties();
+        try {
+            final InputStream propertiesStream = MCRTranslation.class.getResourceAsStream(DEPRECATED_MESSAGES_PROPERTIES);
+            if (propertiesStream == null) {
+                LOGGER.warn("Could not find resource '" + DEPRECATED_MESSAGES_PROPERTIES + "'.");
+                return deprecatedMapping;
+            }
+            deprecatedMapping.load(propertiesStream);
+            DEPRECATED_MESSAGES_PRESENT = true;
+        } catch (IOException e) {
+            LOGGER.warn("Could not load resource '" + DEPRECATED_MESSAGES_PROPERTIES + "'.", e);
+        }
+        return deprecatedMapping;
     }
 
 }
