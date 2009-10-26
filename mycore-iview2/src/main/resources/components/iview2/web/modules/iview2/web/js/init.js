86040b8ab0f5cfa05db65b4a860bb629b9bab0ac
Thomas Scheffler <github@yagee.de>
2009-10-26 13:04:11 +0000

    fixed exception with missing blank.gif (loadTile was undefined)
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@16653 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/web/js/init.js
+++ mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/web/js/init.js
@@ -19,6 +19,7 @@ function initializeGraphic(viewID) {
 			maxZoom: Iview[viewID].zoomMax,
 			initialZoom: Iview[viewID].zoomInit,//Anfangs-Zoomlevel
 			blankTile: "../modules/iview2/web/" + styleFolderUri + 'blank.gif',
+			loadingTile: "../modules/iview2/web/" + styleFolderUri + 'blank.gif',
 			viewID: viewID//Add Viewer ID mit übergeben damit der Viewer darauf arbeiten kann					
 		});
 		Iview[viewID].viewerBean.init();
