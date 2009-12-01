8f31055e89127047a2530c6dca7993e5b5ecebf2
Robert Stephan <rsteph-de@users.noreply.github.com>
2009-12-01 10:01:42 +0000

    merged r16240 from release_2_0_fixes branch - allow longer texts in MCRMetaHistoryDate
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@16241 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaHistoryDate.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaHistoryDate.java
@@ -51,7 +51,7 @@ public class MCRMetaHistoryDate extends MCRMetaDefault {
     protected static Logger LOGGER = Logger.getLogger(MCRMetaHistoryDate.class.getName());
 
     /** The maximal length of 'text' */
-    public static final int MCRHISTORYDATE_MAX_TEXT = 256;
+    public static final int MCRHISTORYDATE_MAX_TEXT = 512;
 
     // Data of this class
     private ArrayList<MCRMetaHistoryDateText> texts;
