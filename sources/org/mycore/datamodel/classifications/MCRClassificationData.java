5d056abb656865121ff58b81bc30fbc08be2640c
Huu Chi Vu <chi.vuhuu@gmail.com>
2009-12-09 16:16:29 +0000

    clean up classification browser create useful isValid method for MCRObjectID
    git-svn-id: https://server.mycore.de/svn/mycore/branches/release_2_0_fixes@16296 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/datamodel/classifications/MCRClassificationData.java
+++ sources/org/mycore/datamodel/classifications/MCRClassificationData.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
