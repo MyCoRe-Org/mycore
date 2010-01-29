1b1983e28c7f06d24363f9b15de0c3c5c5ded89d
Nicky Kuhnt <mcrkuhnt@duepub-1.zim.uni-duisburg-essen.de>
2010-01-29 14:20:14 +0000

    tiles out of viewer are not loaded
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@16831 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/web/js/init.js
+++ mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/web/js/init.js
@@ -147,6 +147,8 @@ function initializeGraphic(viewID) {
 				this.y = 0;
 				motion.y = 0;
 			}
+			this.bildHoehe = Iview[viewID].bildHoehe;
+			this.bildBreite = Iview[viewID].bildBreite;
 			this.positionTilesOrig(motion, reset);
 			/*verschieben des Preload bildes damit man eine grobe Vorschau sieht von dem was kommt
 			  wird nur ausgeführt wenn Seite geladen ist, da ansonsten die Eigenschaften noch nicht vorhanden sind*/
@@ -176,91 +178,92 @@ function initializeGraphic(viewID) {
 		};
 		//TODO gehts auch besser ohne komplettes Überschreiben
 		Iview[viewID].viewerBean.assignTileImage =  function(tile, forceBlankImage) {
-		var tileImgId, src;
-		var useBlankImage = (forceBlankImage ? true : false);
-
-		// check if image has been scrolled too far in any particular direction
-		// and if so, use the null tile image
-		if (!useBlankImage) {
-			var left = tile.xIndex < 0;
-			var high = tile.yIndex < 0;
-			var right = tile.xIndex >= Math.pow(2, this.zoomLevel);
-			var low = tile.yIndex >= Math.pow(2, this.zoomLevel);
-			if (high || left || low || right) {
+			var tileImgId, src;
+			var useBlankImage = (forceBlankImage ? true : false);
+	
+			// check if image has been scrolled too far in any particular direction
+			// and if so, use the null tile image
+			if (!useBlankImage) {
+				var left = tile.xIndex < 0;
+				var high = tile.yIndex < 0;
+				var right = tile.xIndex >= Math.pow(2, this.zoomLevel);
+				var low = tile.yIndex >= Math.pow(2, this.zoomLevel);
+				if (high || left || low || right) {
+					useBlankImage = true;
+				}
+			}
+	
+			if(tile.posy >= this.height || tile.posx >= this.width){
 				useBlankImage = true;
 			}
-		}
-
-		if (useBlankImage) {
-			tileImgId = 'blank:' + tile.qx + ':' + tile.qy;
-			src = this.cache['blank'].src;
-		}
-		else {
-			tileImgId = src = this.tileUrlProvider.assembleUrl(tile.xIndex, tile.yIndex, this.zoomLevel);
-		}
-
-		// only remove tile if identity is changing
-		if (tile.element != null &&
-			tile.element.parentNode != null &&
-			tile.element.relativeSrc != src) {
-			this.well.removeChild(tile.element);
-		}
-
-		var tileImg = this.cache[tileImgId];
-		// create cache if not exist
-		if (tileImg == null) {
-			tileImg = this.cache[tileImgId] = this.createPrototype(src);
-		}
-
-		if (useBlankImage || !PanoJS.USE_LOADER_IMAGE || tileImg.complete || (tileImg.image && tileImg.image.complete)) {
-			tileImg.onload = function() {};
-			if (tileImg.image) {
-				tileImg.image.onload = function() {};
+	
+			if (useBlankImage) {
+				tileImgId = 'blank:' + tile.qx + ':' + tile.qy;
+				src = this.cache['blank'].src;
 			}
-
-			if (tileImg.parentNode == null) {
-				tile.element = this.well.appendChild(tileImg);
+			else {
+				tileImgId = src = this.tileUrlProvider.assembleUrl(tile.xIndex, tile.yIndex, this.zoomLevel);
 			}
-		}
-		else {
-			var loadingImgId = 'loading:' + tile.qx + ':' + tile.qy;
-			var loadingImg = this.cache[loadingImgId];
-			if (loadingImg == null) {
-				loadingImg = this.cache[loadingImgId] = this.createPrototype(this.cache['loading'].src);
+			// only remove tile if identity is changing
+			if (tile.element != null &&
+				tile.element.parentNode != null &&
+				tile.element.relativeSrc != src) {
+				this.well.removeChild(tile.element);
 			}
-
-			loadingImg.targetSrc = tileImgId;
-
-			var well = this.well;
-			tile.element = well.appendChild(loadingImg);
-			tileImg.onload = function() {
-				// make sure our destination is still present
-				if (loadingImg.parentNode && loadingImg.targetSrc == tileImgId) {
-					tileImg.style.top = loadingImg.style.top;
-					tileImg.style.left = loadingImg.style.left;
-					well.replaceChild(tileImg, loadingImg);
-					tile.element = tileImg;
-				}
-
+	
+			var tileImg = this.cache[tileImgId];
+			// create cache if not exist
+			if (tileImg == null) {
+				tileImg = this.cache[tileImgId] = this.createPrototype(src);
+			}
+			if (useBlankImage || !PanoJS.USE_LOADER_IMAGE || tileImg.complete || (tileImg.image && tileImg.image.complete)) {
 				tileImg.onload = function() {};
-				return false;
+				if (tileImg.image) {
+					tileImg.image.onload = function() {};
+				}
+				if (tileImg.parentNode == null) {
+					tile.element = this.well.appendChild(tileImg);
+				}
 			}
-
-			// konqueror only recognizes the onload event on an Image
-			// javascript object, so we must handle that case here
-			if (!PanoJS.DOM_ONLOAD) {
-				tileImg.image = new Image();
-				tileImg.image.onload = tileImg.onload;
-				tileImg.image.src = tileImg.src;
+			else {
+				var loadingImgId = 'loading:' + tile.qx + ':' + tile.qy;
+				var loadingImg = this.cache[loadingImgId];
+				if (loadingImg == null) {
+					loadingImg = this.cache[loadingImgId] = this.createPrototype(this.cache['loading'].src);
+				}
+	
+				loadingImg.targetSrc = tileImgId;
+	
+				var well = this.well;
+				tile.element = well.appendChild(loadingImg);
+				tileImg.onload = function() {
+					// make sure our destination is still present
+					if (loadingImg.parentNode && loadingImg.targetSrc == tileImgId) {
+						tileImg.style.top = loadingImg.style.top;
+						tileImg.style.left = loadingImg.style.left;
+						well.replaceChild(tileImg, loadingImg);
+						tile.element = tileImg;
+					}
+	
+					tileImg.onload = function() {};
+					return false;
+				}
+	
+				// konqueror only recognizes the onload event on an Image
+				// javascript object, so we must handle that case here
+				if (!PanoJS.DOM_ONLOAD) {
+					tileImg.image = new Image();
+					tileImg.image.onload = tileImg.onload;
+					tileImg.image.src = tileImg.src;
+				}
 			}
-		}
 //additions	
-		isloaded(tileImg, this.viewID);
-		var viewID = this.viewID;
-		//changes all not available Tiles to the blank one, so that no ugly Image not Found Pics popup.
-		tileImg.onerror = function () {this.src = Iview[viewID].viewerBean.cache['blank'].src; return true;};
+			isloaded(tileImg, this.viewID);
+			var viewID = this.viewID;
+			//changes all not available Tiles to the blank one, so that no ugly Image not Found Pics popup.
+			tileImg.onerror = function () {this.src = Iview[viewID].viewerBean.cache['blank'].src; return true;};
 //endadd
-	}
+		}
 		Iview[viewID].viewerBean.init();
 	}
 }
