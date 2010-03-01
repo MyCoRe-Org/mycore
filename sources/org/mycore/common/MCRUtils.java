c9a99139ff64af17c3a1b08c73f6118815b7a947
Matthias Eichner <Mewel@users.noreply.github.com>
2010-03-01 16:13:04 +0000

    added delete directoy method to mcrutils
    git-svn-id: https://server.mycore.de/svn/mycore/branches/release_2_0_fixes@16564 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/common/MCRUtils.java
+++ sources/org/mycore/common/MCRUtils.java
@@ -839,6 +839,27 @@ public class MCRUtils {
         return out;
     }
 
+    /**
+     * This method deletes a directory and all its content.
+     *
+     * @param dir
+     *            the File instance of the directory to delete
+     * @return
+     *            true if the directory was deleted successfully, otherwise false
+     */
+    public static boolean deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDirectory(new File(dir, children[i]));
+                if (!success)
+                    return false;
+            }
+        }
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
+
     public static String arrayToString(Object[] objArray, String seperator) {
         StringBuffer buf = new StringBuffer();
 
