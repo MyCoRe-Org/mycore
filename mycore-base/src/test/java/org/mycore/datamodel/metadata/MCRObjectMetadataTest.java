8a93efcbf9b2b2330b155c2774d0a618845a4a95
Thomas Scheffler <github@yagee.de>
2010-03-20 00:04:39 +0000

    added MCRObjectMetadataTest stub and datamodel2 resources
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@17073 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/datamodel/metadata/MCRObjectMetadataTest.java
@@ -0,0 +1,138 @@
+/*
+ * 
+ * $Revision: 13085 $ $Date: 19.03.2010 23:42:07 $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package org.mycore.datamodel.metadata;
+
+import org.mycore.common.MCRTestCase;
+
+/**
+ * @author Thomas Scheffler
+ *
+ */
+public class MCRObjectMetadataTest extends MCRTestCase {
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.MCRTestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.MCRTestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#size()}.
+     */
+    public void testSize() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#getMetadataTagName(int)}.
+     */
+    public void testGetMetadataTagName() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#getHeritableMetadata()}.
+     */
+    public void testGetHeritableMetadata() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#removeInheritedMetadata()}.
+     */
+    public void testRemoveInheritedMetadata() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#appendMetadata(org.mycore.datamodel.metadata.MCRObjectMetadata)}.
+     */
+    public void testAppendMetadata() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#getMetadataElement(java.lang.String)}.
+     */
+    public void testGetMetadataElementString() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#getMetadataElement(int)}.
+     */
+    public void testGetMetadataElementInt() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#setMetadataElement(org.mycore.datamodel.metadata.MCRMetaElement, java.lang.String)}.
+     */
+    public void testSetMetadataElement() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#removeMetadataElement(java.lang.String)}.
+     */
+    public void testRemoveMetadataElementString() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#removeMetadataElement(int)}.
+     */
+    public void testRemoveMetadataElementInt() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#setFromDOM(org.jdom.Element)}.
+     */
+    public void testSetFromDOM() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#createXML()}.
+     */
+    public void testCreateXML() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.mycore.datamodel.metadata.MCRObjectMetadata#isValid()}.
+     */
+    public void testIsValid() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
