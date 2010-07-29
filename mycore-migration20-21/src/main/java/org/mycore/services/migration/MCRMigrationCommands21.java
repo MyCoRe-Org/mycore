df1bea026fd806bed5f8b6bfce03b92ed8c832f7
Thomas Scheffler <github@yagee.de>
2010-07-29 13:14:52 +0000

    should fix issues with mysql
    "ERROR gration.MCRMigrationCommands21 - Could not migrate...
    java.lang.ClassCastException: org.hibernate.impl.StatelessSessionImpl cannot be cast to org.hibernate.engine.jdbc.LobCreationContext
    	at org.hibernate.Hibernate.getLobCreator(Hibernate.java:420)
    	at org.hibernate.type.BlobType.nullSafeGet(BlobType.java:125)
    	at org.hibernate.type.AbstractType.hydrate(AbstractType.java:105)
    	at org.hibernate.persister.entity.AbstractEntityPersister.hydrate(AbstractEntityPersister.java:2267)
    "
    
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@18365 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-migration20-21/src/main/java/org/mycore/services/migration/MCRMigrationCommands21.java
+++ mycore-migration20-21/src/main/java/org/mycore/services/migration/MCRMigrationCommands21.java
@@ -5,8 +5,7 @@ import java.util.Date;
 import org.apache.log4j.Logger;
 import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
-import org.hibernate.StatelessSession;
-import org.hibernate.Transaction;
+import org.hibernate.Session;
 import org.mycore.backend.hibernate.MCRHIBConnection;
 import org.mycore.backend.hibernate.tables.MCRXMLTABLE;
 import org.mycore.datamodel.common.MCRXMLTableManager;
@@ -21,40 +20,31 @@ public class MCRMigrationCommands21 extends MCRAbstractCommands {
         MCRCommand com = null;
 
         com = new MCRCommand("migrate xmltable", "org.mycore.services.migration.MCRMigrationCommands21.migrateXMLTable",
-                "The command migrates all entries from MCRXMLTable to IFS2.");
+            "The command migrates all entries from MCRXMLTable to IFS2.");
         command.add(com);
         com = new MCRCommand("convert datamodel1 to datamodel2 from file {0}",
-            "org.mycore.frontend.cli.command.MCRDatamodelToDatamodel2Command.convert String",
-            "converts a datamodel 1 file to a new datamodel 2 one");
+            "org.mycore.frontend.cli.command.MCRDatamodelToDatamodel2Command.convert String", "converts a datamodel 1 file to a new datamodel 2 one");
         command.add(com);
     }
 
     public static void migrateXMLTable() {
-        StatelessSession session = MCRHIBConnection.instance().getSessionFactory().openStatelessSession();
-        Transaction tx = session.beginTransaction();
+        Session session = MCRHIBConnection.instance().getSession();
         MCRXMLTableManager manager = MCRXMLTableManager.instance();
-        try {
-            ScrollableResults xmlentries = session.createCriteria(MCRXMLTABLE.class).scroll(ScrollMode.FORWARD_ONLY);
-            while (xmlentries.next()) {
-                MCRXMLTABLE xmlentry = (MCRXMLTABLE) xmlentries.get(0);
-                MCRObjectID mcrId = new MCRObjectID(xmlentry.getId());
-                Date lastModified = xmlentry.getLastModified();
-                byte[] xmlByteArray = xmlentry.getXmlByteArray();
-                if (manager.exists(mcrId)){
-                    LOGGER.warn(xmlentry.getId()+ " allready exists in IFS2 - skipping.");
-                    continue;
-                }
-                LOGGER.info("Migrating " + xmlentry.getId() + " to IFS2.");
-                manager.create(mcrId, xmlByteArray, lastModified);
+        ScrollableResults xmlentries = session.createCriteria(MCRXMLTABLE.class).scroll(ScrollMode.FORWARD_ONLY);
+        while (xmlentries.next()) {
+            MCRXMLTABLE xmlentry = (MCRXMLTABLE) xmlentries.get(0);
+            MCRObjectID mcrId = new MCRObjectID(xmlentry.getId());
+            Date lastModified = xmlentry.getLastModified();
+            byte[] xmlByteArray = xmlentry.getXmlByteArray();
+            session.evict(xmlentry);
+            if (manager.exists(mcrId)) {
+                LOGGER.warn(xmlentry.getId() + " allready exists in IFS2 - skipping.");
+                continue;
             }
-            tx.commit();
-        } catch (Exception e) {
-            LOGGER.error("Could not migrate...",e);
-            if (tx.isActive())
-                tx.rollback();
-        } finally {
-            session.close();
+            LOGGER.info("Migrating " + mcrId + " to IFS2.");
+            manager.create(mcrId, xmlByteArray, lastModified);
         }
+        session.clear();
 
     }
 }
