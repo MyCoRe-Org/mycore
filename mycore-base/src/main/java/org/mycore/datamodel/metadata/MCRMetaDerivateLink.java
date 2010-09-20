d28a5390c17236067106fc3be2086229757ae9ce
Silvio Hermann <Possommi@users.noreply.github.com>
2010-09-20 10:44:06 +0000

    add annotation support
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@18717 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaDerivateLink.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaDerivateLink.java
@@ -1,10 +1,27 @@
 package org.mycore.datamodel.metadata;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.mycore.common.MCRException;
 import org.mycore.datamodel.ifs.MCRDirectory;
 import org.mycore.datamodel.ifs.MCRFile;
-import org.mycore.datamodel.ifs.MCRFilesystemNode;
 
 public class MCRMetaDerivateLink extends MCRMetaLink {
+
+    private static final String ANNOTATION = "annotation";
+    private static final String ATTRIBUTE = "lang";
+
+    private HashMap<String, String> map;
+
+    /** Constructor initializes the HashMap */
+    public MCRMetaDerivateLink() {
+        super();
+        map = new HashMap<String, String>();
+    }
 
     public void setLinkToFile(MCRFile file) {
         String owner = file.getOwnerID();
@@ -12,14 +29,49 @@ public class MCRMetaDerivateLink extends MCRMetaLink {
         super.href = owner + path;
     }
 
+    public void setFromDOM(org.jdom.Element element) throws MCRException {
+        super.setFromDOM(element);
+        @SuppressWarnings("unchecked")
+        List<Element> childrenList = element.getChildren(MCRMetaDerivateLink.ANNOTATION);
+        if (childrenList == null)
+            return;
+        Iterator<Element> annotationsIter = childrenList.iterator();
+
+        while (annotationsIter.hasNext()) {
+            org.jdom.Element anAnnotation = annotationsIter.next();
+            String key = anAnnotation.getAttributeValue(MCRMetaDerivateLink.ATTRIBUTE,
+                    Namespace.XML_NAMESPACE);
+            String annotationText = anAnnotation.getText();
+            this.map.put(key, annotationText);
+        }
+    }
+
+    public org.jdom.Element createXML() throws MCRException {
+        org.jdom.Element elm = super.createXML();
+
+        Iterator<String> keys = map.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            org.jdom.Element annotationElem = new Element(MCRMetaDerivateLink.ANNOTATION);
+            annotationElem
+                    .setAttribute(MCRMetaDerivateLink.ATTRIBUTE, key, Namespace.XML_NAMESPACE);
+            String content = map.get(key);
+            if (content == null || content.length() == 0)
+                continue;
+            annotationElem.addContent(content);
+            elm.addContent(annotationElem);
+        }
+
+        return elm;
+    }
+
     public MCRFile getLinkedFile() {
         int index = super.href.indexOf('/');
-        if (index < 0) {
+        if (index < 0)
             return null;
-        }
         String owner = super.href.substring(0, index);
         String path = super.href.substring(index);
-        return (MCRFile) ((MCRDirectory) MCRFilesystemNode.getRootNode(owner)).getChildByPath(path);
+        return (MCRFile) ((MCRDirectory) MCRFile.getRootNode(owner)).getChildByPath(path);
     }
 
     @Override
@@ -30,5 +82,4 @@ public class MCRMetaDerivateLink extends MCRMetaLink {
         }
         return super.isValid();
     }
-
 }
