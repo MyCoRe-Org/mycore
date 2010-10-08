597b9c70d9b8d986e3b99305a3a90f815a11d0e7
Thomas Scheffler <github@yagee.de>
2010-10-08 08:33:37 +0000

    fixed resource folder structure and added package-info.java files
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@18961 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-migration20-21/src/main/java/org/mycore/migration20_21/datamodel/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Store for migrating online from database to IFS2 
+ */
+package org.mycore.migration20_21.datamodel;
---
+++ mycore-migration20-21/src/main/java/org/mycore/migration20_21/hibernate/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Hibernate classes to access old MyCoRe 2.0 tables
+ */
+package org.mycore.migration20_21.hibernate;
