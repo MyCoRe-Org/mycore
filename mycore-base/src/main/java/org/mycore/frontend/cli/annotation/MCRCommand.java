c4864ee3c146186c4a657a0b05890f0aa4a4cfa9
Thomas Scheffler <github@yagee.de>
2010-11-04 11:30:01 +0000

    added @MCRCommand annotation
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19180 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/frontend/cli/annotation/MCRCommand.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.mycore.frontend.cli.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.text.MessageFormat;
+
+import org.mycore.frontend.cli.MCRCommandLineInterface;
+
+/**
+ * Annotates a public static method as a command that could be executed via {@link MCRCommandLineInterface}.
+ * @author Thomas Scheffler (yagee)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target(ElementType.METHOD)
+public @interface MCRCommand {
+    /**
+     * The syntax of the command in {@link MessageFormat} syntax
+     */
+    String syntax();
+    /**
+     * Help text that should be returned by <code>help</code> command.
+     */
+    String help() default "";
+    /**
+     * I18N key for the help text that should be returned by <code>help</code> command.
+     */
+    String helpKey() default "";
+}
