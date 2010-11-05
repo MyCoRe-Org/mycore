460d7987e2f0edc23cbefb954e6232d06672a9cd
Silvio Hermann <Possommi@users.noreply.github.com>
2010-11-05 10:13:58 +0000

    enhanced MCRDerivateWithURNSearcher
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19195 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/services/fieldquery/MCRDerivateWithURNSearcher.java
+++ mycore-base/src/main/java/org/mycore/services/fieldquery/MCRDerivateWithURNSearcher.java
@@ -5,18 +5,10 @@ package org.mycore.services.fieldquery;
 
 import java.util.List;
 
-import org.mycore.backend.hibernate.MCRHIBConnection;
-import org.mycore.common.events.MCREvent;
-import org.mycore.datamodel.ifs.MCRFile;
-import org.mycore.datamodel.metadata.MCRDerivate;
-import org.mycore.datamodel.metadata.MCRMetadataManager;
-import org.mycore.datamodel.metadata.MCRObject;
-import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.parsers.bool.MCRCondition;
 
 /**
  * @author shermann
- * 
  */
 public class MCRDerivateWithURNSearcher extends MCRSearcher {
 
@@ -38,162 +30,66 @@ public class MCRDerivateWithURNSearcher extends MCRSearcher {
         String fieldName = ((MCRQueryCondition) condition).getField().getName();
 
         if (fieldName.equals("objectURN")) {
-            return handleObjectsURN(condition);
+            return getObjectsForURN(condition);
         }
 
         if (fieldName.equals("objectsWithURN")) {
-            return handleObjectsWithURN(condition);
+            return getObjectsWithURN(condition);
         }
         return new MCRResults();
     }
 
-    @SuppressWarnings("unchecked")
-    private MCRResults handleObjectsURN(MCRCondition condition) {
+    /** Retrieves parent objects matching a given urn */
+    private MCRResults getObjectsForURN(MCRCondition condition) {
         String value = null;
         if (condition instanceof MCRQueryCondition) {
             value = ((MCRQueryCondition) condition).getValue();
         }
-        List<String> resultList = null;
-
-        MCRHIBConnection conn = MCRHIBConnection.instance();
-
         if (value == null) {
             return new MCRResults();
         }
-        /*
-         * get objects matching the given urn (only non derivates)
-         */
-        resultList = conn.getSession().createSQLQuery("SELECT DISTINCT U.mcrid FROM MCRURN U WHERE U.MCRURN LIKE '%" + value + "%'").list();
+
+        MCRCondition tFCond = new MCRQueryParser().parse("derivateURN like \"" + value + "\"");
+        MCRQuery q = new MCRQuery(tFCond);
+        MCRResults results = MCRQueryManager.search(q);
 
         MCRResults toReturn = new MCRResults();
-        for (String entry : resultList) {
-            MCRHit aHit = new MCRHit(entry);
+        for (MCRHit aHit : results) {
             if (aHit.getID().indexOf("_derivate_") == -1) {
                 toReturn.addHit(aHit);
             } else {
-                MCRDerivate der = MCRMetadataManager.retrieveMCRDerivate(MCRObjectID.getInstance(aHit.getID()));
-                String parentId = der.getDerivate().getMetaLink().getXLinkHref();
-                MCRHit hit = new MCRHit(parentId);
-                toReturn.addHit(hit);
+                MCRFieldValue derivateOwner = aHit.getMetaData("derivateOwner");
+                if (derivateOwner != null) {
+                    MCRHit newHit = new MCRHit(derivateOwner.getValue());
+                    toReturn.addHit(newHit);
+                }
             }
         }
 
         return toReturn;
     }
 
-    @SuppressWarnings("unchecked")
-    private MCRResults handleObjectsWithURN(MCRCondition condition) {
+    /**
+     * Depending on the condition provided this methods returns either all
+     * objects with urn or all objects without urn
+     * 
+     * @param condition
+     */
+    private MCRResults getObjectsWithURN(MCRCondition condition) {
         String value = null;
         if (condition instanceof MCRQueryCondition) {
             value = ((MCRQueryCondition) condition).getValue();
         }
-        List<String> resultList = null;
 
-        MCRHIBConnection conn = MCRHIBConnection.instance();
-
+        MCRCondition tFCond = null;
         if (value != null && value.equalsIgnoreCase("false")) {
-            /* all objects without urn */
-            resultList = conn.getSession().createSQLQuery(
-                    "SELECT MCRID FROM MCRXMLTABLE WHERE MCRID NOT IN (SELECT DISTINCT urn.MCRID FROM MCRURN urn)").list();
+            LOGGER.info("The condition " + condition + " is not supported");
+            return new MCRResults();
         } else {
             /* all objects with urn */
-            resultList = conn.getSession().createSQLQuery("SELECT DISTINCT mcrid FROM MCRURN ORDER BY 1 ASC").list();
+            tFCond = new MCRQueryParser().parse("derivateURN like \"urn\"");
         }
-
-        MCRResults toReturn = new MCRResults();
-        for (String entry : resultList) {
-            MCRHit aHit = new MCRHit(entry);
-            toReturn.addHit(aHit);
-        }
-
-        return toReturn;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleFileCreated(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.ifs.MCRFile)
-     */
-    @Override
-    protected void handleFileCreated(MCREvent evt, MCRFile file) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleFileDeleted(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.ifs.MCRFile)
-     */
-    @Override
-    protected void handleFileDeleted(MCREvent evt, MCRFile file) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleFileRepaired(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.ifs.MCRFile)
-     */
-    @Override
-    protected void handleFileRepaired(MCREvent evt, MCRFile file) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleFileUpdated(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.ifs.MCRFile)
-     */
-    @Override
-    protected void handleFileUpdated(MCREvent evt, MCRFile file) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleObjectCreated(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.metadata.MCRObject)
-     */
-    @Override
-    protected void handleObjectCreated(MCREvent evt, MCRObject obj) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleObjectDeleted(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.metadata.MCRObject)
-     */
-    @Override
-    protected void handleObjectDeleted(MCREvent evt, MCRObject obj) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleObjectRepaired(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.metadata.MCRObject)
-     */
-    @Override
-    protected void handleObjectRepaired(MCREvent evt, MCRObject obj) {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.mycore.services.fieldquery.MCRSearcher#handleObjectUpdated(org.mycore
-     * .common.events.MCREvent, org.mycore.datamodel.metadata.MCRObject)
-     */
-    @Override
-    protected void handleObjectUpdated(MCREvent evt, MCRObject obj) {
+        MCRQuery q = new MCRQuery(tFCond);
+        return MCRQueryManager.search(q);
     }
 }
