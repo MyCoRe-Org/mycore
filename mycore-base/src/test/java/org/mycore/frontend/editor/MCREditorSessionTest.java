fcdba80e1bbdb5030dba6b05f17e2198e9c9edec
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-08 16:24:24 +0000

    Improved test
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19229 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/test/java/org/mycore/frontend/editor/MCREditorSessionTest.java
+++ mycore-base/src/test/java/org/mycore/frontend/editor/MCREditorSessionTest.java
@@ -5,23 +5,41 @@ import static org.junit.Assert.*;
 
 import org.jdom.Element;
 import org.junit.Test;
+import org.junit.Before;
 
 public class MCREditorSessionTest {
-    
-    @Test
-    public void test() {
-        String defaultURL = "http://www.test.de";
-        
-        Element xml = new Element("editor");
+
+    private static String defaultURL = "http://www.test.de";
+
+    private Element xml;
+
+    private MCRParameters parameters;
+
+    @Before
+    public void setup() {
+        xml = new Element("editor");
         xml.addContent(new Element("source").setAttribute("uri", "mcrobject:{id}"));
-        xml.addContent(new Element("cancel").setAttribute("url", defaultURL ));
+        xml.addContent(new Element("cancel").setAttribute("url", defaultURL));
 
-        MCRParameters parameters = new MCRParameters();
+        parameters = new MCRParameters();
         parameters.addParameterValue("id", "4711");
+    }
 
+    @Test
+    public void testAttributeTokenSubstitution() {
         MCREditorSession session = new MCREditorSession(xml, parameters);
-        assertThat( session.getID(), notNullValue() );
-        assertThat( session.getCancelURL(), equalTo( defaultURL ) );
-        assertThat( session.getSourceURI(), equalTo( "mcrobject:4711" ) );
+        assertThat(session.getCancelURL(), equalTo(defaultURL));
+        assertThat(session.getSourceURI(), equalTo("mcrobject:4711"));
+    }
+
+    @Test
+    public void testUniqueID() {
+        MCREditorSession session1 = new MCREditorSession(xml, parameters);
+        assertThat(session1.getID(), notNullValue());
+
+        MCREditorSession session2 = new MCREditorSession(xml, parameters);
+        assertThat(session2.getID(), notNullValue());
+
+        assertThat(session1.getID(), not(equalTo(session2.getID())));
     }
 }
