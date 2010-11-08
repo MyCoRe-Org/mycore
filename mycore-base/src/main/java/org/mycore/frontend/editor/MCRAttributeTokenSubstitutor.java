5025d0487e934e8b6322a87ed07bcc37254be05d
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-08 14:17:27 +0000

    Substitutes tokens in attribute values, e.g. <source uri="mcrobject:{id}" />
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19215 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/frontend/editor/MCRAttributeTokenSubstitutor.java
@@ -0,0 +1,34 @@
+package org.mycore.frontend.editor;
+
+import java.util.List;
+
+import org.jdom.Element;
+
+public class MCRAttributeTokenSubstitutor {
+
+    private Element xml;
+
+    private MCRTokenSubstitutor tokenSubstitutor;
+
+    public MCRAttributeTokenSubstitutor(Element xml, MCRParameters parameters) {
+        this.tokenSubstitutor = new MCRTokenSubstitutor(parameters);
+        this.xml = xml;
+    }
+
+    public String substituteTokens(String elementName, String attributeName, String defaultValue) {
+        for (Element child : (List<Element>) (xml.getChildren(elementName))) {
+            String value = child.getAttributeValue("uri", "");
+            if (value.isEmpty())
+                continue;
+
+            value = tokenSubstitutor.substituteTokens(value);
+            if (containsNoTokens(value))
+                return value;
+        }
+        return defaultValue;
+    }
+
+    private static boolean containsNoTokens(String text) {
+        return !text.contains("{");
+    }
+}
