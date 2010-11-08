dbac3f10dfd2945417af9763757aaa67b55264c5
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-08 16:31:39 +0000

    Added tests for MCREditorSessionCache
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19230 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/MCREditorSessionCacheTest.java
@@ -0,0 +1,39 @@
+package org.mycore.frontend.editor;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.jdom.Element;
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCREditorSessionCacheTest {
+
+    private Element xml;
+
+    private MCRParameters parameters;
+
+    private MCREditorSessionCache cache = MCREditorSessionCache.instance();
+
+    @Before
+    public void setup() {
+        xml = new Element("editor");
+        parameters = new MCRParameters();
+        parameters.addParameterValue("id", "4711");
+    }
+
+    @Test
+    public void testEmptyCache() {
+        assertThat(cache.getEditorSession("foo"), nullValue());
+    }
+
+    @Test
+    public void testGetSession() {
+        MCREditorSession session = new MCREditorSession( xml, parameters);
+        String sessionID = session.getID();
+        cache.storeEditorSession(session);
+        
+        assertThat(cache.getEditorSession(sessionID), notNullValue());
+        assertThat(cache.getEditorSession(sessionID).getID(), equalTo(sessionID));
+    }
+}
