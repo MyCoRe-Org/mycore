dd2d228c1b1aba7ba9987ed4022527f7f41b29f9
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-08 14:19:43 +0000

    Represents a MCREditor session
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19216 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/frontend/editor/MCREditorSession.java
@@ -0,0 +1,54 @@
+package org.mycore.frontend.editor;
+
+import org.jdom.Element;
+import org.mycore.frontend.servlets.MCRServlet;
+
+public class MCREditorSession {
+
+    private String id;
+
+    private Element xml;
+
+    private String cancelURL;
+
+    private String sourceURI;
+
+    private MCRParameters parameters;
+
+    public MCREditorSession(Element xml, MCRParameters parameters) {
+        this.xml = xml;
+        this.parameters = parameters;
+
+        buildAndSetID();
+        buildSourceURIandCancelURL();
+    }
+
+    private void buildSourceURIandCancelURL() {
+        MCRAttributeTokenSubstitutor pts = new MCRAttributeTokenSubstitutor(xml, parameters);
+        String defaultCancelURL = MCRServlet.getBaseURL();
+        String defaultSourceURI = "buildxml:_rootName_=emptySource";
+        this.sourceURI = pts.substituteTokens("source", "uri", defaultSourceURI);
+        this.cancelURL = pts.substituteTokens("cancel", "url", defaultCancelURL);
+    }
+
+    private void buildAndSetID() {
+        this.id = MCRUniqueID.buildID();
+        xml.setAttribute("session", id);
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public Element getXML() {
+        return xml;
+    }
+
+    public String getCancelURL() {
+        return cancelURL;
+    }
+
+    public String getSourceURI() {
+        return sourceURI;
+    }
+}
