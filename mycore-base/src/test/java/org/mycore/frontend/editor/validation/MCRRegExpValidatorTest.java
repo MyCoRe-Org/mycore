df616cc8d2596e04d0bfec32ff8d36d231d368c8
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-09 15:18:19 +0000

    Added more tests
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19254 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRRegExpValidatorTest.java
@@ -0,0 +1,39 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRRegExpValidatorTest {
+
+    MCRRegExpValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRRegExpValidator();
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("regexp", "a*");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testValidPattern() {
+        validator.setProperty("regexp", "a*");
+        assertTrue(validator.isValidExceptionsCatched("aaa"));
+    }
+
+    @Test
+    public void testInvalidPattern() {
+        validator.setProperty("regexp", "a*");
+        assertFalse(validator.isValidExceptionsCatched("aba"));
+    }
+}
---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRRequiredValidatorTest.java
@@ -0,0 +1,48 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRRequiredValidatorTest {
+
+    MCRRequiredValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRRequiredValidator();
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("required", "true");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testRequiredAndValid() {
+        validator.setProperty("required", "true");
+        assertTrue(validator.isValidExceptionsCatched("input"));
+    }
+
+    @Test
+    public void testRequiredAndMissing() {
+        validator.setProperty("required", "true");
+        assertFalse(validator.isValidExceptionsCatched(""));
+        assertFalse(validator.isValidExceptionsCatched(" "));
+    }
+
+    @Test
+    public void testNotRequired() {
+        validator.setProperty("required", "false");
+        assertTrue(validator.isValidExceptionsCatched(""));
+        assertTrue(validator.isValidExceptionsCatched(" "));
+        assertTrue(validator.isValidExceptionsCatched("input"));
+    }
+}
