162971902c06ea68348e027936538a9bb70a3da0
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-09 15:30:09 +0000

    Added more tests
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19256 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRExternalValidatorTest.java
@@ -0,0 +1,42 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRExternalValidatorTest {
+
+    MCRExternalValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRExternalValidator();
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("class", "org.mycore.frontend.editor.validation.MCRExternalValidatorTest");
+        validator.setProperty("method", "externalTestForValue");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testExternal() {
+        validator.setProperty("class", "org.mycore.frontend.editor.validation.MCRExternalValidatorTest");
+        validator.setProperty("method", "externalTestForValue");
+        assertTrue(validator.isValidExceptionsCatched("valid"));
+        assertFalse(validator.isValidExceptionsCatched("invalid"));
+    }
+
+    @Ignore
+    public static boolean externalTestForValue(String value) {
+        return "valid".equals(value);
+    }
+}
