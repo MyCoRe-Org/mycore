cc3b383e3931c2e6db2f76486d0af92a01f47256
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-09 15:41:47 +0000

    Added more tests
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19259 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRDateTimeValidatorTest.java
@@ -0,0 +1,52 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRDateTimeValidatorTest {
+
+    MCRDateTimeValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRDateTimeValidator();
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("type", "datetime");
+        validator.setProperty("format", "dd.MM.yyyy");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testValidDates() {
+        validator.setProperty("type", "datetime");
+        validator.setProperty("format", "dd.MM.yyyy");
+        assertTrue(validator.isValidExceptionsCatched("22.04.1971"));
+        assertTrue(validator.isValidExceptionsCatched("29.02.2000"));
+    }
+
+    @Test
+    public void testInvalidDates() {
+        validator.setProperty("type", "datetime");
+        validator.setProperty("format", "dd.MM.yyyy");
+        assertFalse(validator.isValidExceptionsCatched("text"));
+        assertFalse(validator.isValidExceptionsCatched("29.02.2001"));
+    }
+
+    @Test
+    public void testMultipleFormats() {
+        validator.setProperty("type", "datetime");
+        validator.setProperty("format", "dd.MM.yyyy ; yyyy-MM-dd");
+        assertTrue(validator.isValidExceptionsCatched("22.04.1971"));
+        assertTrue(validator.isValidExceptionsCatched("1971-04-22"));
+    }
+}
