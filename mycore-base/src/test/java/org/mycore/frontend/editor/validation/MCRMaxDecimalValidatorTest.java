b76768e6d6c44f24cad64b2042fe2424cbe5a16c
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-09 14:59:08 +0000

    Added more tests
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19253 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRMaxDecimalValidatorTest.java
@@ -0,0 +1,41 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRMaxDecimalValidatorTest {
+
+    MCRMaxDecimalValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRMaxDecimalValidator();
+        validator.setProperty("type", "decimal");
+        validator.setProperty("format", "de");
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("max", "3,14");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testMaxValid() {
+        validator.setProperty("max", "3,14");
+        assertTrue(validator.isValidExceptionsCatched("3,13"));
+    }
+
+    @Test
+    public void testMaxInvalid() {
+        validator.setProperty("max", "3,14");
+        assertFalse(validator.isValidExceptionsCatched("4"));
+    }
+}
---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/MCRMinDecimalValidatorTest.java
@@ -0,0 +1,41 @@
+package org.mycore.frontend.editor.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MCRMinDecimalValidatorTest {
+
+    MCRMinDecimalValidator validator;
+
+    @Before
+    public void setup() {
+        validator = new MCRMinDecimalValidator();
+        validator.setProperty("type", "decimal");
+        validator.setProperty("format", "de");
+    }
+
+    @Test
+    public void testPropertiesMissing() {
+        assertFalse(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testHasRequiredProperties() {
+        validator.setProperty("min", "3,14");
+        assertTrue(validator.hasRequiredPropertiesForValidation());
+    }
+
+    @Test
+    public void testMinValid() {
+        validator.setProperty("min", "3,14");
+        assertTrue(validator.isValidExceptionsCatched("3,14"));
+    }
+
+    @Test
+    public void testMinInvalid() {
+        validator.setProperty("min", "3,14");
+        assertFalse(validator.isValidExceptionsCatched("2"));
+    }
+}
