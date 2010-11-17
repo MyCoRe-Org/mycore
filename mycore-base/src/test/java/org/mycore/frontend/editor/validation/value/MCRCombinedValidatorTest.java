b3ca42ac62d1e89cebfabcfa9cef4a0b15465171
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-17 15:33:04 +0000

    Fixed imported package
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19368 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/test/java/org/mycore/frontend/editor/validation/value/MCRCombinedValidatorTest.java
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/value/MCRCombinedValidatorTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mycore.frontend.editor.validation.MCRCombinedValidator;
 import org.mycore.frontend.editor.validation.MCRValidatorBuilder;
 import org.mycore.frontend.editor.validation.value.MCRMaxLengthValidator;
 import org.mycore.frontend.editor.validation.value.MCRMinLengthValidator;
