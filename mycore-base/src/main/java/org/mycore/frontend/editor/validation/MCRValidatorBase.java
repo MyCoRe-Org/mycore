3f4999b1c303137d49e8721abf7652e1267f0cbd
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-18 14:08:35 +0000

    Added exception logging
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19381 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/frontend/editor/validation/MCRValidatorBase.java
+++ mycore-base/src/main/java/org/mycore/frontend/editor/validation/MCRValidatorBase.java
@@ -28,19 +28,28 @@ public abstract class MCRValidatorBase implements MCRValidator {
         boolean isValid;
 
         try {
+            System.out.println(getClass().getSimpleName() + " hello!");
             isValid = isValidOrDie(input);
         } catch (Exception ex) {
+            logException(ex);
             isValid = false;
         }
 
         if (LOGGER.isDebugEnabled())
             LOGGER.debug(buildLogInfo(isValid, input));
-        
+
         return isValid;
     }
 
     protected abstract boolean isValidOrDie(Object... input) throws Exception;
-    
+
+    private void logException(Throwable ex) {
+        LOGGER.debug(getClass().getSimpleName() + " caught " + ex.toString());
+        ex = ex.getCause();
+        if (ex != null)
+            logException(ex);
+    }
+
     private String buildLogInfo(boolean isValid, Object... input) {
         StringBuffer buffer = new StringBuffer();
 
