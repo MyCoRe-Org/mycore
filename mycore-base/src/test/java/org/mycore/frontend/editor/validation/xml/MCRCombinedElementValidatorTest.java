3217d893f8abd59db7fbd121c199d7b1834869c4
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2010-11-18 16:00:41 +0000

    Added test for combined element validator
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19390 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/frontend/editor/validation/xml/MCRCombinedElementValidatorTest.java
@@ -0,0 +1,52 @@
+package org.mycore.frontend.editor.validation.xml;
+
+import static org.junit.Assert.*;
+
+import org.jdom.Element;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mycore.frontend.editor.validation.MCRCombinedValidator;
+import org.mycore.frontend.editor.validation.MCRValidatorBuilder;
+import org.mycore.frontend.editor.validation.MCRValidatorTest;
+import org.mycore.frontend.editor.validation.value.MCRMaxLengthValidator;
+import org.mycore.frontend.editor.validation.value.MCRMinLengthValidator;
+
+public class MCRCombinedElementValidatorTest extends MCRValidatorTest {
+
+    @Before
+    public void setup() {
+        validator = MCRValidatorBuilder.buildPredefinedCombinedElementValidator();
+    }
+
+    @Test
+    public void testEmptyCombinedValidator() {
+        assertFalse(validator.hasRequiredProperties());
+        assertTrue(validator.isValid(new Element("foo")));
+    }
+
+    @Test
+    public void testSingleValidator() {
+        validator.setProperty("xsl", "string-length(name())=3");
+        assertTrue(validator.isValid(null));
+        assertTrue(validator.isValid(new Element("foo")));
+        assertFalse(validator.isValid(new Element("foobar")));
+    }
+
+    @Test
+    public void testMultipleValidators() {
+        validator.setProperty("xsl", "string-length(name())=3");
+        validator.setProperty("class", this.getClass().getName());
+        validator.setProperty("method", "firstCharEqualsLastChar");
+        assertFalse(validator.isValid(new Element("abcd")));
+        assertFalse(validator.isValid(new Element("ab")));
+        assertTrue(validator.isValid(new Element("aba")));
+        assertFalse(validator.isValid(new Element("abc")));
+    }
+
+    @Ignore
+    public static boolean firstCharEqualsLastChar(Element xml) {
+        String name = xml.getName();
+        return name.charAt(0) == name.charAt(name.length() - 1);
+    }
+}
