a686b5f55c621b06cb5529183d11fbd52c03fc55
Thomas Scheffler <github@yagee.de>
2010-11-19 11:09:00 +0000

    simplified code and removed many bugs
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19397 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/frontend/servlets/MCRDerivateServlet.java
+++ mycore-base/src/main/java/org/mycore/frontend/servlets/MCRDerivateServlet.java
@@ -4,97 +4,75 @@
 package org.mycore.frontend.servlets;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.mycore.access.MCRAccessManager;
-import org.mycore.common.MCRException;
 import org.mycore.datamodel.ifs.MCRDirectory;
+import org.mycore.datamodel.ifs.MCRFilesystemNode;
 import org.mycore.datamodel.metadata.MCRDerivate;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 
 /**
- * @author basti, shermann
+ * @author Sebastian Hofmann; Silvio Hermann; Thomas Scheffler (yagee)
  */
 public class MCRDerivateServlet extends MCRServlet {
 
     private static final long serialVersionUID = 1L;
 
-    static Logger LOGGER = Logger.getLogger(MCRDerivateServlet.class);
-
     @Override
     protected void doGetPost(MCRServletJob job) throws Exception {
-        // check what to do
-        String task = job.getRequest().getParameter("todo");
-        if (task == null) {
-            LOGGER.error("Parameter \"todo\" is not provided");
-            job.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST);
+        HttpServletRequest request = job.getRequest();
+        HttpServletResponse response = job.getResponse();
+        checkPreConditions(request, response);
+        if (response.isCommitted()) {
+            return;
         }
-        // derivateid
-        String myCoreDerivateId = job.getRequest().getParameter("derivateid");
-        if (myCoreDerivateId == null) {
-            LOGGER.error("Parameter \"derivateid\" is not provided");
-            job.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST);
+        String derivateId = getProperty(request, "derivateid");
+        if (performTask(job, getProperty(request, "todo"), derivateId, getProperty(request, "file"))) {
+            response.sendRedirect(response.encodeRedirectURL(getServletBaseURL() + "MCRFileNodeServlet/" + derivateId));
         }
-        // owner id
-        String myCoreObjectId = job.getRequest().getParameter("objectid");
-        if (myCoreObjectId == null) {
-            LOGGER.error("Parameter \"objectid\" is not provided");
-            job.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST);
-        }
-        // file to delete or to set
-        String file = job.getRequest().getParameter("file");
-        if (file == null) {
-            LOGGER.error("Parameter \"file\" is not provided");
-            job.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST);
-        }
-
-        performTask(job, task, myCoreDerivateId, myCoreObjectId, file);
-        job.getResponse().sendRedirect(
-                job.getResponse().encodeRedirectURL(getBaseURL() + "servlets/MCRFileNodeServlet/" + myCoreDerivateId));
     }
 
-    private void performTask(MCRServletJob job, String task, String myCoreDerivateId, String myCoreObjectId, String file)
-            throws IOException {
-        if (task.equals("ssetfile")) {
-            if (MCRAccessManager.checkPermission(myCoreDerivateId, "writedb")) {
-                setMainFile(myCoreDerivateId, myCoreObjectId, file);
-            } else {
-                LOGGER.error("User has not the \"writedb\" permission on object " + myCoreDerivateId);
-                job.getResponse().sendError(HttpServletResponse.SC_FORBIDDEN);
-            }
-        } else if (task.equals("sdelfile")) {
-            if (MCRAccessManager.checkPermission(myCoreDerivateId, "deletedb")) {
-                deleteFile(myCoreDerivateId, myCoreObjectId, file);
-            } else {
-                LOGGER.error("User has not the \"deletedb\" permission on object " + myCoreDerivateId);
-                job.getResponse().sendError(HttpServletResponse.SC_FORBIDDEN);
-            }
-        } else {
-            LOGGER.warn("The task \"" + task + "\" is not supported");
+    protected void checkPreConditions(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (getProperty(request, "todo") == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter \"todo\" is not provided");
+        } else if (getProperty(request, "derivateid") == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter \"derivateid\" is not provided");
+        } else if (getProperty(request, "file") == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter \"file\" is not provided");
         }
+    }
+
+    private boolean performTask(MCRServletJob job, String task, String myCoreDerivateId, String file) throws IOException {
+        if (task.equals("ssetfile")) {
+            setMainFile(myCoreDerivateId, file, job.getResponse());
+        } else if (task.equals("sdelfile")) {
+            deleteFile(myCoreDerivateId, file, job.getResponse());
+        } else {
+            job.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST, MessageFormat.format("The task \"{0}\" is not supported.", task));
+        }
+        return !job.getResponse().isCommitted();
     }
 
     /**
      * The method set the main file of a derivate object that is stored in the
      * server. The method use the input parameter: <b>type</b>,<b>step</b>
      * <b>se_mcrid</b> and <b>re_mcrid</b>. Access rights must be 'writedb'.
-     * 
      * @param job
      *            the MCRServletJob instance
      */
-    private boolean setMainFile(String derivateId, String ObjectId, String file) throws IOException {
-        try {
+    private void setMainFile(String derivateId, String file, HttpServletResponse response) throws IOException {
+        if (MCRAccessManager.checkPermission(derivateId, "writedb")) {
             MCRObjectID mcrid = MCRObjectID.getInstance(derivateId);
             MCRDerivate der = MCRMetadataManager.retrieveMCRDerivate(mcrid);
             der.getDerivate().getInternals().setMainDoc(file);
             MCRMetadataManager.updateMCRDerivateXML(der);
-            return true;
-        } catch (MCRException ex) {
-            LOGGER.error("Cannot set main file in derivate " + derivateId, ex);
-            return false;
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, MessageFormat.format("User has not the \"writedb\" permission on object {0}.", derivateId));
         }
     }
 
@@ -102,18 +80,16 @@ public class MCRDerivateServlet extends MCRServlet {
      * The method delete a file from a derivate object that is stored in the
      * server. The method use the input parameter: <b>type</b>,<b>step</b>
      * <b>se_mcrid</b> and <b>re_mcrid</b>. Access rights must be 'deletedb'.
-     * 
      * @param job
      *            the MCRServletJob instance
      */
-    private boolean deleteFile(String derivateId, String ObjectId, String file) throws IOException {
-        MCRDirectory rootdir = MCRDirectory.getRootDirectory(derivateId);
-        try {
-            rootdir.getChildByPath(file).delete();
-            return true;
-        } catch (Exception ex) {
-            LOGGER.error("Cannot delete file " + file + " in derivate " + derivateId, ex);
-            return false;
+    private void deleteFile(String derivateId, String file, HttpServletResponse response) throws IOException {
+        if (MCRAccessManager.checkPermission(derivateId, "deletedb")) {
+            MCRDirectory rootdir = MCRDirectory.getRootDirectory(derivateId);
+            MCRFilesystemNode filesystemNode = rootdir.getChildByPath(file);
+            filesystemNode.delete();
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, MessageFormat.format("User has not the \"deletedb\" permission on object {0}.", derivateId));
         }
     }
 }
