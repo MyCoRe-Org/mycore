adc334a5be26349a10ea453d76e98dec5cf403e7
Thomas Scheffler <github@yagee.de>
2010-12-06 08:49:27 +0000

    added servlet to logout from a session
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19495 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/frontend/servlets/MCRLogoutServlet.java
@@ -0,0 +1,60 @@
+/*
+ * $Id$
+ * $Revision: 5697 $ $Date: 06.12.2010 $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.frontend.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Thomas Scheffler (yagee)
+ *
+ */
+public class MCRLogoutServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.invalidate();
+        String returnURL = req.getHeader("Referer");
+        log("Hello");
+        if (returnURL == null) {
+            returnURL = req.getContextPath() + "/"; //redirect to servlets container root
+        }
+        resp.sendRedirect(returnURL);
+    }
+
+}
