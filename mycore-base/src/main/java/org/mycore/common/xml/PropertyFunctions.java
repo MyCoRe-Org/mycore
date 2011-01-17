e3c69d09185e35167c3837e91a36053bcdd4daf7
Silvio Hermann <Possommi@users.noreply.github.com>
2011-01-17 11:16:57 +0000

    moved PropertyFunctions to mycore-base
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19767 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/xml/PropertyFunctions.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package org.mycore.common.xml;
+
+import org.apache.log4j.Logger;
+import org.mycore.common.MCRConfiguration;
+import org.mycore.common.MCRConfigurationException;
+
+/**
+ * @author shermann
+ */
+public class PropertyFunctions {
+
+    private static MCRConfiguration CONFIG = MCRConfiguration.instance();
+
+    /**
+     * @param property
+     * @return
+     */
+    public static String getProperty(String property) {
+
+        String value = "";
+        try {
+            value = CONFIG.getString(property);
+        } catch (MCRConfigurationException ex) {
+            Logger.getLogger(PropertyFunctions.class).warn("The requested property '" + property + "' could not be found.");
+            return "";
+        }
+        return value;
+    }
+
+    /**
+     * @param property
+     * @return
+     */
+    public static String getProperty(String property, String defaultValue) {
+
+        String value = "";
+        try {
+            value = CONFIG.getString(property, defaultValue);
+        } catch (MCRConfigurationException ex) {
+            Logger.getLogger(PropertyFunctions.class).warn("The requested property '" + property + "' could not be found.");
+            return "";
+        }
+        return value;
+    }
+
+    /**
+     * @param url
+     * @param lookup
+     * @return
+     */
+    public static String getParameterValue(String url, String lookup) {
+        String parameters = url.substring(url.indexOf("?") + 1);
+        String[] params = parameters.split("&");
+        for (String val : params) {
+            if (val.startsWith(lookup + "=")) {
+                String toReturn = val.substring(val.indexOf("=") + 1);
+                return toReturn;
+            }
+        }
+        return null;
+    }
+}
