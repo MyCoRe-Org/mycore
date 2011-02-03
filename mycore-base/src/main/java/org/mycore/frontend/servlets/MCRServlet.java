5bcea7861cf91e0b71444fef7a46a4139a15a03c
Silvio Hermann <Possommi@users.noreply.github.com>
2011-02-03 09:51:29 +0000

    fixed url encoding
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@19845 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/frontend/servlets/MCRServlet.java
+++ mycore-base/src/main/java/org/mycore/frontend/servlets/MCRServlet.java
@@ -1,24 +1,24 @@
 /*
  * 
  * $Revision$ $Date$
- *
- * This file is part of ***  M y C o R e  ***
- * See http://www.mycore.de/ for details.
- *
- * This program is free software; you can use it, redistribute it
- * and / or modify it under the terms of the GNU General Public License
- * (GPL) as published by the Free Software Foundation; either version 2
- * of the License or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program, in a file called gpl.txt or license.txt.
- * If not, write to the Free Software Foundation Inc.,
- * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ * 
+ * This file is part of *** M y C o R e *** See http://www.mycore.de/ for
+ * details.
+ * 
+ * This program is free software; you can use it, redistribute it and / or
+ * modify it under the terms of the GNU General Public License (GPL) as
+ * published by the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program, in a file called gpl.txt or license.txt. If not, write to the
+ * Free Software Foundation Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307 USA
  */
 
 package org.mycore.frontend.servlets;
@@ -26,6 +26,8 @@ package org.mycore.frontend.servlets;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.text.MessageFormat;
 import java.util.Collection;
@@ -65,7 +67,6 @@ import org.mycore.services.i18n.MCRTranslation;
  * @author Detlev Degenhardt
  * @author Frank Lützenkirchen
  * @author Thomas Scheffler (yagee)
- * 
  * @version $Revision$ $Date: 2008-02-06 17:27:24 +0000 (Mi, 06 Feb
  *          2008) $
  */
@@ -245,9 +246,9 @@ public class MCRServlet extends HttpServlet {
         MCRSession session = getSession(job.getRequest());
         bindSessionToRequest(req, getServletName(), session);
         try {
-            //transaction around 1st phase of request
+            // transaction around 1st phase of request
             Exception thinkException = processThinkPhase(job);
-            //first phase completed, start rendering phase
+            // first phase completed, start rendering phase
             processRenderingPhase(job, thinkException);
         } catch (Exception ex) {
             if (getProperty(req, INITIAL_SERVLET_NAME_KEY).equals(getServletName())) {
@@ -333,15 +334,15 @@ public class MCRServlet extends HttpServlet {
     }
 
     /**
-     * 1st phase of doGetPost.
+     * 1st phase of doGetPost. This method has a seperate transaction. Per
+     * default id does nothing as a fallback to the old behaviour.
      * 
-     * This method has a seperate transaction. Per default id does nothing as a fallback to the old behaviour.
      * @param job
      * @throws Exception
      * @see #render(MCRServletJob, Exception)
      */
     protected void think(MCRServletJob job) throws Exception {
-        //not implemented by default
+        // not implemented by default
     }
 
     private void processRenderingPhase(MCRServletJob job, Exception thinkException) throws Exception {
@@ -358,19 +359,22 @@ public class MCRServlet extends HttpServlet {
     }
 
     /**
-     * 2nd phase of doGetPost
+     * 2nd phase of doGetPost This method has a seperate transaction and gets
+     * the same MCRServletJob from the first phase (think) and any exception
+     * that occurs at the first phase. By default this method calls
+     * doGetPost(MCRServletJob) as a fallback to the old behaviour.
      * 
-     * This method has a seperate transaction and gets the same MCRServletJob from the first phase (think)
-     * and any exception that occurs at the first phase.
-     * 
-     * By default this method calls doGetPost(MCRServletJob) as a fallback to the old behaviour.
-     * 
-     * @param job same instance as of think(MCRServlet job)
-     * @param ex any exception thrown by think(MCRServletJob) or transaction commit
-     * @throws Exception if render could not handle <code>ex</code> to produce a nice user page
+     * @param job
+     *            same instance as of think(MCRServlet job)
+     * @param ex
+     *            any exception thrown by think(MCRServletJob) or transaction
+     *            commit
+     * @throws Exception
+     *             if render could not handle <code>ex</code> to produce a nice
+     *             user page
      */
     protected void render(MCRServletJob job, Exception ex) throws Exception {
-        //no info here how to handle
+        // no info here how to handle
         if (ex != null)
             throw ex;
         if (job.getRequest().getMethod().equals("POST")) {
@@ -405,11 +409,12 @@ public class MCRServlet extends HttpServlet {
     }
 
     /**
-     * @deprecated use {@link HttpServletResponse#sendError(int, String)} instead or throw Exception
+     * @deprecated use {@link HttpServletResponse#sendError(int, String)}
+     *             instead or throw Exception
      */
     @Deprecated()
-    protected void generateErrorPage(HttpServletRequest request, HttpServletResponse response, int error, String msg, Exception ex, boolean xmlstyle)
-        throws IOException {
+    protected void generateErrorPage(HttpServletRequest request, HttpServletResponse response, int error, String msg, Exception ex,
+            boolean xmlstyle) throws IOException {
         LOGGER.error(getClass().getName() + ": Error " + error + " occured. The following message was given: " + msg, ex);
 
         String rootname = "mcr_error";
@@ -449,7 +454,8 @@ public class MCRServlet extends HttpServlet {
             return;
         } else {
             if (request.getAttribute(requestAttr) != null) {
-                LOGGER.warn("Could not send error page. Generating error page failed. The original message:\n" + request.getAttribute(requestAttr));
+                LOGGER.warn("Could not send error page. Generating error page failed. The original message:\n"
+                        + request.getAttribute(requestAttr));
             } else {
                 LOGGER.warn("Could not send error page. Response allready commited. The following message was given:\n" + msg);
             }
@@ -492,8 +498,8 @@ public class MCRServlet extends HttpServlet {
         return redirectURL.toString();
     }
 
-    protected void generateActiveLinkErrorpage(HttpServletRequest request, HttpServletResponse response, String msg, MCRActiveLinkException activeLinks)
-        throws IOException {
+    protected void generateActiveLinkErrorpage(HttpServletRequest request, HttpServletResponse response, String msg,
+            MCRActiveLinkException activeLinks) throws IOException {
         StringBuilder msgBuf = new StringBuilder(msg);
         msgBuf.append("\nThere are links active preventing the commit of work, see error message for details. The following links where affected:");
         Map<String, Collection<String>> links = activeLinks.getActiveLinks();
@@ -508,22 +514,16 @@ public class MCRServlet extends HttpServlet {
     }
 
     /**
-     * allows browser to cache requests.
-     * 
-     * This method is usefull as it allows browsers to cache content that is not
-     * changed.
-     * 
-     * Please overwrite this method in every Servlet that depends on "remote"
-     * data.
-     * 
+     * allows browser to cache requests. This method is usefull as it allows
+     * browsers to cache content that is not changed. Please overwrite this
+     * method in every Servlet that depends on "remote" data.
      */
     @Override
     protected long getLastModified(HttpServletRequest request) {
         if (ENABLE_BROWSER_CACHE) {
             // we can cache every (local) request
             long lastModified = MCRSessionMgr.getCurrentSession().getLoginTime() > MCRConfiguration.instance().getSystemLastModified() ? MCRSessionMgr
-                .getCurrentSession()
-                .getLoginTime() : MCRConfiguration.instance().getSystemLastModified();
+                    .getCurrentSession().getLoginTime() : MCRConfiguration.instance().getSystemLastModified();
             LOGGER.info("LastModified: " + lastModified);
             return lastModified;
         }
@@ -625,8 +625,8 @@ public class MCRServlet extends HttpServlet {
                 // parameter is not empty -> store
                 if (!request.getParameter(name).trim().equals("")) {
                     mcrSession.put(key, request.getParameter(name));
-                    LOGGER.debug("Found HTTP-Req.-Parameter " + name + "=" + request.getParameter(name) + " that should be saved in session, safed " + key
-                        + "=" + request.getParameter(name));
+                    LOGGER.debug("Found HTTP-Req.-Parameter " + name + "=" + request.getParameter(name)
+                            + " that should be saved in session, safed " + key + "=" + request.getParameter(name));
                 }
                 // paramter is empty -> do not store and if contained in
                 // session, remove from it
@@ -644,8 +644,8 @@ public class MCRServlet extends HttpServlet {
                 // attribute is not empty -> store
                 if (!request.getAttribute(name).toString().trim().equals("")) {
                     mcrSession.put(key, request.getAttribute(name));
-                    LOGGER.debug("Found HTTP-Req.-Attribute " + name + "=" + request.getParameter(name) + " that should be saved in session, safed " + key
-                        + "=" + request.getParameter(name));
+                    LOGGER.debug("Found HTTP-Req.-Attribute " + name + "=" + request.getParameter(name)
+                            + " that should be saved in session, safed " + key + "=" + request.getParameter(name));
                 }
                 // attribute is empty -> do not store and if contained in
                 // session, remove from it
@@ -659,14 +659,41 @@ public class MCRServlet extends HttpServlet {
     }
 
     /**
-     * returns a translated error message for the current Servlet.
+     * returns a translated error message for the current Servlet. I18N keys are
+     * of form 'error.'{SimpleServletClassName}'.'{subIdentifier}
      * 
-     * I18N keys are of form 'error.'{SimpleServletClassName}'.'{subIdentifier}
-     * @param subIdentifier last part of I18n key
-     * @param args any arguments that should be passed to {@link MCRTranslation#translate(String, Object...)}
+     * @param subIdentifier
+     *            last part of I18n key
+     * @param args
+     *            any arguments that should be passed to
+     *            {@link MCRTranslation#translate(String, Object...)}
      */
     protected String getErrorI18N(String subIdentifier, Object... args) {
         String key = MessageFormat.format("error.{0}.{1}", getClass().getSimpleName(), subIdentifier);
         return MCRTranslation.translate(key, args);
+    }
+
+    /**
+     * This method encodes the url. After the encoding the url is redirectable.
+     * 
+     * @param url
+     *            the source URL
+     */
+    public static String encodeURL(String url) throws URISyntaxException {
+        url = url.replace("%", "%25");
+        url = url.replace(" ", "%20");
+
+        URI uri = new URI(url);
+
+        String scheme = uri.getScheme();
+        String userInfo = uri.getUserInfo();
+        String host = uri.getHost();
+        int port = uri.getPort();
+        String path = uri.getPath();
+        String query = uri.getQuery();
+        String fragment = uri.getFragment();
+
+        uri = new URI(scheme, userInfo, host, port, path, query, fragment);
+        return uri.toASCIIString();
     }
 }
