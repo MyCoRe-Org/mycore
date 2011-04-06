b9786e9f2a43148266ed3fb9f20693b22a72fddb
Thomas Scheffler <github@yagee.de>
2011-04-06 15:56:45 +0000

    added MCRMODSWrapper to wrap a mods document into a MCRObject instance
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@20602 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-mods/src/main/java/org/mycore/mods/MCRMODSWrapper.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.mycore.mods;
+
+import java.text.MessageFormat;
+import java.util.Collections;
+
+import org.jdom.Element;
+import org.mycore.datamodel.metadata.MCRMetaElement;
+import org.mycore.datamodel.metadata.MCRMetaXML;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.metadata.MCRObjectID;
+
+/**
+ * @author Thomas Scheffler
+ *
+ */
+public class MCRMODSWrapper {
+
+    public static final String MODS_OBJECT_TYPE = "mods";
+
+    public static MCRObject wrapMODSDocument(Element modsDefinition, String projectID) {
+        MCRObjectID objID = MCRObjectID.getInstance(MessageFormat.format("{0}_{1}_0", projectID, MODS_OBJECT_TYPE));
+        MCRObject obj = new MCRObject();
+        obj.setId(objID);
+        MCRMetaXML modsContainer = new MCRMetaXML("modsContainer", null, 0);
+        modsContainer.addContent(modsDefinition);
+        MCRMetaElement defModsContainer = new MCRMetaElement(MCRMetaXML.class, "def.modsContainer", false, true, Collections.nCopies(1,
+                modsContainer));
+        obj.getMetadata().setMetadataElement(defModsContainer);
+        return obj;
+    }
+}
