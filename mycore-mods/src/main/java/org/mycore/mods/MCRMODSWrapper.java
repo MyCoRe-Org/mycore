41cb707d4b4ebc6958acf64a701cce96c353244c
Thomas Scheffler <github@yagee.de>
2011-04-07 06:29:24 +0000

    added junit test for MCRMODSWrapper
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@20608 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-mods/src/main/java/org/mycore/mods/MCRMODSWrapper.java
+++ mycore-mods/src/main/java/org/mycore/mods/MCRMODSWrapper.java
@@ -7,6 +7,7 @@ import java.text.MessageFormat;
 import java.util.Collections;
 
 import org.jdom.Element;
+import org.jdom.Namespace;
 import org.mycore.datamodel.metadata.MCRMetaElement;
 import org.mycore.datamodel.metadata.MCRMetaXML;
 import org.mycore.datamodel.metadata.MCRObject;
@@ -20,14 +21,16 @@ public class MCRMODSWrapper {
 
     public static final String MODS_OBJECT_TYPE = "mods";
 
+    public static final Namespace MODS_NS = Namespace.getNamespace("mods", "http://www.loc.gov/mods/v3");
+
     public static MCRObject wrapMODSDocument(Element modsDefinition, String projectID) {
         MCRObjectID objID = MCRObjectID.getInstance(MessageFormat.format("{0}_{1}_0", projectID, MODS_OBJECT_TYPE));
         MCRObject obj = new MCRObject();
         obj.setId(objID);
+        obj.setSchema("datamodel-mods.xsd");
         MCRMetaXML modsContainer = new MCRMetaXML("modsContainer", null, 0);
         modsContainer.addContent(modsDefinition);
-        MCRMetaElement defModsContainer = new MCRMetaElement(MCRMetaXML.class, "def.modsContainer", false, true, Collections.nCopies(1,
-                modsContainer));
+        MCRMetaElement defModsContainer = new MCRMetaElement(MCRMetaXML.class, "def.modsContainer", false, true, Collections.nCopies(1, modsContainer));
         obj.getMetadata().setMetadataElement(defModsContainer);
         return obj;
     }
---
+++ mycore-mods/src/test/java/org/mycore/mods/MCRMODSWrapperTest.java
@@ -0,0 +1,87 @@
+/*
+ * $Id$
+ * $Revision: 5697 $ $Date: 07.04.2011 $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.mods;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
+import org.junit.Before;
+import org.junit.Test;
+import org.mycore.common.MCRException;
+import org.mycore.common.MCRTestCase;
+import org.mycore.common.xml.MCRXMLHelper;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.xml.sax.SAXParseException;
+
+/**
+ * @author Thomas Scheffler (yagee)
+ *
+ */
+public class MCRMODSWrapperTest extends MCRTestCase {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        setProperty("MCR.Metadata.Type.mods", "true", false);
+    }
+
+    /**
+     * Test method for {@link org.mycore.mods.MCRMODSWrapper#wrapMODSDocument(org.jdom.Element, java.lang.String)}.
+     * @throws URISyntaxException 
+     * @throws SAXParseException 
+     * @throws MCRException 
+     * @throws JDOMException 
+     * @throws MalformedURLException 
+     */
+    @Test
+    public void testWrapMODSDocument() throws MCRException, SAXParseException, URISyntaxException, JDOMException, MalformedURLException {
+        Document modsDoc = loadMODSDocument();
+        MCRObject mcrObj = MCRMODSWrapper.wrapMODSDocument(modsDoc.getRootElement(), "JUnit");
+        assertTrue("Generated MCRObject is not valid.", mcrObj.isValid());
+        Document mcrObjXml = mcrObj.createXML();
+        //check load from XML throws no exception
+        MCRObject mcrObj2 = new MCRObject(mcrObjXml);
+        mcrObjXml = mcrObj2.createXML();
+        XPath xpathCheck = XPath.newInstance("//mods:mods");
+        xpathCheck.addNamespace(MCRMODSWrapper.MODS_NS);
+        assertEquals("Did not find mods data", 1, xpathCheck.selectNodes(mcrObjXml).size());
+    }
+
+    private Document loadMODSDocument() throws MCRException, SAXParseException, URISyntaxException, MalformedURLException {
+        URL worlClassUrl = this.getClass().getResource("/mods80700998.xml");
+        Document xml = MCRXMLHelper.parseURI(worlClassUrl.toURI());
+        return xml;
+    }
+
+}
