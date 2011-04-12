eafc9d242a710bdae6994e838e49aa27b9a4b3f2
Thomas Scheffler <github@yagee.de>
2011-04-12 08:59:43 +0000

    bugfix: fixed shutdown process
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@20650 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/backend/lucene/MCRLuceneSearcher.java
+++ mycore-base/src/main/java/org/mycore/backend/lucene/MCRLuceneSearcher.java
@@ -615,10 +615,10 @@ public class MCRLuceneSearcher extends MCRSearcher implements MCRShutdownHandler
         modifyExecutor.shutdown();
         long taskCount = modifyExecutor.getTaskCount();
         try {
-            for (int tSec = 0; !modifyExecutor.isShutdown() || tSec < 60 * 6; tSec++) {
+            for (int tSec = 0; !modifyExecutor.isShutdown() && tSec < 60 * 6; tSec++) {
                 long numProcessed = modifyExecutor.getCompletedTaskCount();
                 LOGGER.info(MessageFormat.format("Processed {0} of {1} modification requests, still working...",numProcessed,taskCount,toString()));
-                modifyExecutor.awaitTermination(tSec * 10, TimeUnit.SECONDS);
+                modifyExecutor.awaitTermination(10, TimeUnit.SECONDS);
             }
         } catch (InterruptedException e) {
             LOGGER.warn("Error while closing " + toString(), e);
