f4b8d0b85494958fcab421f7338984f0f0bd17d5
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2011-07-15 15:26:04 +0000

    New class to handle XML parsing errors
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@21516 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/xml/MCRXMLParserErrorHandler.java
@@ -0,0 +1,89 @@
+/*
+ * 
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.xml;
+
+import org.apache.log4j.Logger;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+
+/**
+ * Handles errors during XML parsing.
+ * 
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRXMLParserErrorHandler implements ErrorHandler {
+
+    private final static Logger LOGGER = Logger.getLogger(MCRXMLParserErrorHandler.class);
+
+    /**
+     * Handles parser warnings
+     */
+    public void warning(SAXParseException ex) {
+        LOGGER.warn(getSAXErrorMessage(ex), ex);
+    }
+
+    /**
+     * Handles parse errors
+     */
+    public void error(SAXParseException ex) {
+        LOGGER.error(getSAXErrorMessage(ex), ex);
+        throw new RuntimeException(ex);
+    }
+
+    /**
+     * Handles fatal parse errors
+     */
+    public void fatalError(SAXParseException ex) {
+        LOGGER.fatal(getSAXErrorMessage(ex));
+        throw new RuntimeException(ex);
+    }
+
+    /**
+     * Returns a text indicating at which line and column the error occured.
+     * 
+     * @param ex the SAXParseException exception
+     * @return the location string
+     */
+    public static String getSAXErrorMessage(SAXParseException ex) {
+        StringBuffer str = new StringBuffer();
+
+        String systemId = ex.getSystemId();
+        if (systemId != null) {
+            int index = systemId.lastIndexOf('/');
+
+            if (index != -1) {
+                systemId = systemId.substring(index + 1);
+            }
+
+            str.append(systemId).append(": ");
+        }
+
+        str.append("line ").append(ex.getLineNumber());
+        str.append(", column ").append(ex.getColumnNumber());
+        str.append(", ");
+        str.append(ex.getLocalizedMessage());
+
+        return str.toString();
+    }
+}
