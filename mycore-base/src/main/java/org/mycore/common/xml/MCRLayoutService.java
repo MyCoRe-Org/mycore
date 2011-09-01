6800d435dcd623159001a3b062cc1c03808b18e4
Thomas Scheffler <github@yagee.de>
2011-09-01 08:40:43 +0000

    organized import
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@21932 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/common/xml/MCRLayoutService.java
+++ mycore-base/src/main/java/org/mycore/common/xml/MCRLayoutService.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Writer;
-import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
