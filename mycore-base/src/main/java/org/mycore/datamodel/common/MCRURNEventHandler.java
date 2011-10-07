d84502c3f9f72a1f8da8bb2a41b0301dcc55ad0e
Sebastian Hofmann <hofmann.s@uni-jena.de>
2011-10-07 10:07:12 +0000

    insert urn into the database if a derivate imported.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@22326 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/datamodel/common/MCRURNEventHandler.java
+++ mycore-base/src/main/java/org/mycore/datamodel/common/MCRURNEventHandler.java
@@ -3,7 +3,15 @@
  */
 package org.mycore.datamodel.common;
 
+import java.io.File;
+import java.util.Formatter;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
 import org.mycore.common.events.MCREvent;
 import org.mycore.common.events.MCREventHandlerBase;
 import org.mycore.datamodel.metadata.MCRDerivate;
@@ -17,7 +25,7 @@ import org.mycore.services.urn.MCRURNManager;
  * @author shermann
  */
 public class MCRURNEventHandler extends MCREventHandlerBase {
-    private Logger logger = Logger.getLogger(MCRURNEventHandler.class);
+    private static final Logger LOGGER = Logger.getLogger(MCRURNEventHandler.class);
 
     /**
      * This method handle all calls for EventHandler for the event types
@@ -32,7 +40,7 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
         if (evt.getObjectType().equals(MCREvent.OBJECT_TYPE)) {
             MCRObject obj = (MCRObject) (evt.get("object"));
             if (obj != null) {
-                logger.debug(getClass().getName() + " handling " + obj.getId().toString() + " " + evt.getEventType());
+                LOGGER.debug(getClass().getName() + " handling " + obj.getId().toString() + " " + evt.getEventType());
                 if (evt.getEventType().equals(MCREvent.CREATE_EVENT)) {
                     handleObjectCreated(evt, obj);
                 } else if (evt.getEventType().equals(MCREvent.UPDATE_EVENT)) {
@@ -42,11 +50,11 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
                 } else if (evt.getEventType().equals(MCREvent.REPAIR_EVENT)) {
                     handleObjectRepaired(evt, obj);
                 } else {
-                    logger.warn("Can't find method for an object data handler for event type " + evt.getEventType());
+                    LOGGER.warn("Can't find method for an object data handler for event type " + evt.getEventType());
                 }
                 return;
             }
-            logger.warn("Can't find method for " + MCREvent.OBJECT_TYPE + " for event type " + evt.getEventType());
+            LOGGER.warn("Can't find method for " + MCREvent.OBJECT_TYPE + " for event type " + evt.getEventType());
             return;
         }
 
@@ -54,7 +62,7 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
         if (evt.getObjectType().equals(MCREvent.DERIVATE_TYPE)) {
             MCRDerivate der = (MCRDerivate) (evt.get("derivate"));
             if (der != null) {
-                logger.debug(getClass().getName() + " handling " + der.getId().toString() + " " + evt.getEventType());
+                LOGGER.debug(getClass().getName() + " handling " + der.getId().toString() + " " + evt.getEventType());
                 if (evt.getEventType().equals(MCREvent.CREATE_EVENT)) {
                     handleDerivateCreated(evt, der);
                 } else if (evt.getEventType().equals(MCREvent.UPDATE_EVENT)) {
@@ -64,11 +72,11 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
                 } else if (evt.getEventType().equals(MCREvent.REPAIR_EVENT)) {
                     handleDerivateRepaired(evt, der);
                 } else {
-                    logger.warn("Can't find method for a derivate data handler for event type " + evt.getEventType());
+                    LOGGER.warn("Can't find method for a derivate data handler for event type " + evt.getEventType());
                 }
                 return;
             }
-            logger.warn("Can't find method for " + MCREvent.DERIVATE_TYPE + " for event type " + evt.getEventType());
+            LOGGER.warn("Can't find method for " + MCREvent.DERIVATE_TYPE + " for event type " + evt.getEventType());
             return;
         }
     }
@@ -86,12 +94,12 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
     protected void handleObjectDeleted(MCREvent evt, MCRObject obj) {
         try {
             if (MCRURNManager.hasURNAssigned(obj.getId().toString())) {
-                logger.info("Deleting urn from database for object belonging to " + obj.getId().toString());
+                LOGGER.info("Deleting urn from database for object belonging to " + obj.getId().toString());
                 MCRURNManager.removeURNByObjectID(obj.getId().toString());
             }
 
         } catch (Exception ex) {
-            logger.error("Could not delete the urn from the database for object with id " + obj.getId().toString(), ex);
+            LOGGER.error("Could not delete the urn from the database for object with id " + obj.getId().toString(), ex);
         }
     }
 
@@ -108,9 +116,70 @@ public class MCRURNEventHandler extends MCREventHandlerBase {
     protected void handleDerivateDeleted(MCREvent evt, MCRDerivate der) {
         try {
             MCRURNManager.removeURNByObjectID(der.getId().toString());
-            logger.info("Deleting urn from database for derivates belonging to " + der.getId().toString());
+            LOGGER.info("Deleting urn from database for derivates belonging to " + der.getId().toString());
         } catch (Exception ex) {
-            logger.error("Could not delete the urn from the database for object with id " + der.getId().toString(), ex);
+            LOGGER.error("Could not delete the urn from the database for object with id " + der.getId().toString(), ex);
         }
+    }
+
+    /**
+     * Handles derivate created events. This implementation adds the urn records in the MCRURN table
+     * 
+     * @param evt
+     *            the event that occured
+     * @param der
+     *            the MCRDerivate that caused the event
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void handleDerivateCreated(MCREvent evt, MCRDerivate der) {
+        try {
+            Document derivateXml = MCRXMLMetadataManager.instance().retrieveXML(der.getId());
+
+            // get all filesets from Derivate
+            XPath filesetPath = XPath.newInstance("./mycorederivate/derivate/fileset");
+
+            Object obj = filesetPath.selectSingleNode(derivateXml);
+            if (!(obj instanceof Element)) {
+                return;
+            }
+
+            // add the urn of the fileset
+            Element result = (Element) obj;
+            String urn = result.getAttributeValue("urn");
+
+            if (urn != null && MCRURNManager.isValid(urn)) {
+                MCRURNManager.assignURN(urn, der.getId().toString());
+                LOGGER.info("Loading fileset urn: " + urn + ", " + der.getId().toString());
+                // get all files in the fileset
+                XPath filePath = XPath.newInstance("./mycorederivate/derivate/fileset[@urn='" + urn + "']/file");
+                List<Element> files = filePath.selectNodes(derivateXml);
+
+                for (Element fileResult : files) {
+                    // add the urn of each file
+                    Element file = fileResult;
+                    String fileName, fileUrn, fileDirectory;
+
+                    if ((fileName = file.getAttributeValue("name")) != null && (fileUrn = file.getChildText("urn")) != null) {
+
+                        File derivateFile = new File(fileName);
+
+                        fileName = derivateFile.getName();
+                        fileDirectory = derivateFile.getParent();
+                        LOGGER.info(new Formatter().format("load file urn : %s, %s, %s, %s ", fileUrn, der.getId().toString(),
+                                fileDirectory, fileName).toString());
+                        MCRURNManager.assignURN(fileUrn, der.getId().toString(), fileDirectory, fileName);
+                    }
+                }
+            }
+        } catch (JDOMException je) {
+            LOGGER.error("error while reading filesets or files from derivate " + der.getId().toString(), je);
+        }
+    }
+
+    @Override
+    protected void handleDerivateUpdated(MCREvent evt, MCRDerivate der) {
+        this.handleDerivateDeleted(evt, der);
+        this.handleDerivateCreated(evt, der);
     }
 }
