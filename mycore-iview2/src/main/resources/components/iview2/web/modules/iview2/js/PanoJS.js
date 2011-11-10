0baed7e0c837789d5ee261168f8aaeb04cce5852
Philipp Heinze <mcrheinz@duepub-1.zim.uni-duisburg-essen.de>
2011-11-10 09:21:48 +0000

    PanoJS with images was using wrong cache
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@22661 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/js/PanoJS.js
+++ mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/js/PanoJS.js
@@ -109,7 +109,7 @@ function PanoJS(viewer, options) {
 	//TODO maybe its possible to store all data which is kept in images in tiles
 	this.images = [];
 	//create Cache Object where the size is the amount of tiles which can be displayed at once plus one row/column on each site
-	this.cache = new Cache(Math.ceil(screen.availWidth / this.tileSize) * Math.ceil(screen.availHeight / this.tileSize) * 4);
+	this.cache = new Cache((Math.ceil(screen.availWidth / this.tileSize) + 2) *  (Math.ceil(screen.availHeight / this.tileSize) + 2));
 	this.cche = {};
 	var blankTile = options.blankTile ? options.blankTile : PanoJS.BLANK_TILE_IMAGE;
 	var loadingTile = options.loadingTile ? options.loadingTile : PanoJS.LOADING_TILE_IMAGE;
