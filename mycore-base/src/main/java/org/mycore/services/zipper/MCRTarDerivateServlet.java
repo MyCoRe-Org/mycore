d2aa655ae11b7d3093945ccaf43670f57e889d5e
Silvio Hermann <Possommi@users.noreply.github.com>
2011-12-01 12:39:22 +0000

    check if id parameter is set
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@22833 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/services/zipper/MCRTarDerivateServlet.java
+++ mycore-base/src/main/java/org/mycore/services/zipper/MCRTarDerivateServlet.java
@@ -20,101 +20,101 @@ import org.mycore.frontend.servlets.MCRServletJob;
  * This servlet delivers all files of a derivate packed in a tar File
  * 
  * @author sebastian
- * 
  */
 public class MCRTarDerivateServlet extends MCRServlet {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private static final Logger LOGGER = Logger
-			.getLogger(MCRTarDerivateServlet.class);
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGetPost(MCRServletJob job) throws Exception {
-		HttpServletResponse res = job.getResponse();
-		String derivateId = getProperty(job.getRequest(), "id");
-		LOGGER.info("TarDerivate :" + derivateId);
-		if (!MCRAccessManager.checkPermission(derivateId, "writedb")) {
-			res.sendError(HttpServletResponse.SC_FORBIDDEN,
-					"FileNodeServlet: access forbidden to " + derivateId);
-			return;
-		}
+    private static final Logger LOGGER = Logger.getLogger(MCRTarDerivateServlet.class);
 
-		TarOutputStream tos = buildTarOutputStream(res, derivateId);
-		putAllFiles(derivateId, tos);
-		tos.close();
-	}
+    @Override
+    protected void doGetPost(MCRServletJob job) throws Exception {
+        HttpServletResponse res = job.getResponse();
+        String derivateId = getProperty(job.getRequest(), "id");
 
-	/**
-	 * Puts all files of a derivate to a TarOutputStream.
-	 * 
-	 * @param id
-	 *            the id of the derivate
-	 * @param tos
-	 *            the taroutputstream
-	 * @throws IOException
-	 */
-	private void putAllFiles(String id, TarOutputStream tos) throws IOException {
-		MCRFilesystemNode node = MCRFilesystemNode.getRootNode(id);
-		if (node instanceof MCRDirectory) {
-			putNode(node, tos);
-		}
-	}
+        if (derivateId == null || derivateId.length() == 0) {
+            job.getResponse().sendError(HttpServletResponse.SC_PRECONDITION_FAILED, "You must provide the id of the derivate");
+            return;
+        }
 
-	/**
-	 * Puts all childs of a MCRFilesystemNode to a TarOutputStream (works
-	 * recursive)
-	 * 
-	 * @param node
-	 *            the MCRFilesystemNode that has the childs.
-	 * @param tos
-	 *            the TarOutputStream were the childs should be written to.
-	 * @throws IOException
-	 */
-	private void putNode(MCRFilesystemNode node, TarOutputStream tos)
-			throws IOException {
-		MCRDirectory dir = (MCRDirectory) node;
-		MCRFilesystemNode[] children = dir.getChildren();
-		for (MCRFilesystemNode child : children) {
-			if (child instanceof MCRDirectory) {
-				putNode(child, tos);
+        LOGGER.info("Creating tar archive for derivate " + derivateId);
+        if (!MCRAccessManager.checkPermission(derivateId, "writedb")) {
+            res.sendError(HttpServletResponse.SC_FORBIDDEN, "Access forbidden to " + derivateId);
+            return;
+        }
 
-			}
-			if (child instanceof MCRFile) {
-				MCRFile mcrFile = (MCRFile) child;
-				LOGGER.info("Put File : " + mcrFile.getPath());
-				TarEntry te = new TarEntry(mcrFile.getPath());
-				te.setModTime(new Date().getTime());
-				te.setSize(mcrFile.getSize());
+        TarOutputStream tos = buildTarOutputStream(res, derivateId);
+        putAllFiles(derivateId, tos);
+        tos.close();
+    }
 
-				tos.putNextEntry(te);
+    /**
+     * Puts all files of a derivate to a TarOutputStream.
+     * 
+     * @param id
+     *            the id of the derivate
+     * @param tos
+     *            the taroutputstream
+     * @throws IOException
+     */
+    private void putAllFiles(String id, TarOutputStream tos) throws IOException {
+        MCRFilesystemNode node = MCRFilesystemNode.getRootNode(id);
+        if (node instanceof MCRDirectory) {
+            putNode(node, tos);
+        }
+    }
 
-				mcrFile.getContentTo(tos);
+    /**
+     * Puts all childs of a MCRFilesystemNode to a TarOutputStream (works
+     * recursive)
+     * 
+     * @param node
+     *            the MCRFilesystemNode that has the childs.
+     * @param tos
+     *            the TarOutputStream were the childs should be written to.
+     * @throws IOException
+     */
+    private void putNode(MCRFilesystemNode node, TarOutputStream tos) throws IOException {
+        MCRDirectory dir = (MCRDirectory) node;
+        MCRFilesystemNode[] children = dir.getChildren();
+        for (MCRFilesystemNode child : children) {
+            if (child instanceof MCRDirectory) {
+                putNode(child, tos);
 
-				tos.closeEntry();
-			}
-		}
-	}
+            }
+            if (child instanceof MCRFile) {
+                MCRFile mcrFile = (MCRFile) child;
+                LOGGER.info("Adding file : " + mcrFile.getPath());
+                TarEntry te = new TarEntry(mcrFile.getPath());
+                te.setModTime(new Date().getTime());
+                te.setSize(mcrFile.getSize());
 
-	/**
-	 * Builds a TarOutputstream from a HttpServletResponse
-	 * 
-	 * @param res
-	 * @param filename
-	 *            the Name of the downloaded file
-	 * @return
-	 * @throws IOException
-	 */
-	private TarOutputStream buildTarOutputStream(HttpServletResponse res,
-			String filename) throws IOException {
-		BufferedOutputStream bos = new BufferedOutputStream(
-				res.getOutputStream());
-		res.setContentType("application/x-tar");
-		res.addHeader("Content-Disposition", "atachment; filename=\""
-				+ filename + ".tar\"");
+                tos.putNextEntry(te);
 
-		return new TarOutputStream(bos);
-	}
+                mcrFile.getContentTo(tos);
+
+                tos.closeEntry();
+            }
+        }
+    }
+
+    /**
+     * Builds a TarOutputstream from a HttpServletResponse
+     * 
+     * @param res
+     * @param filename
+     *            the Name of the downloaded file
+     * @return
+     * @throws IOException
+     */
+    private TarOutputStream buildTarOutputStream(HttpServletResponse res, String filename) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(res.getOutputStream());
+        res.setContentType("application/x-tar");
+        res.addHeader("Content-Disposition", "atachment; filename=\"" + filename + ".tar\"");
+
+        return new TarOutputStream(bos);
+    }
 }
