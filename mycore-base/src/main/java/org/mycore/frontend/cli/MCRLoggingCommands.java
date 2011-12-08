390f41347d68dabc8ab666485970a6bd483662dd
Silvio Hermann <Possommi@users.noreply.github.com>
2011-12-08 14:29:54 +0000

    allow to change log levels for classes and packages through web cli
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@22901 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/frontend/cli/MCRLoggingCommands.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.mycore.frontend.cli;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * Use this class to change log levels of java packages and classes.
+ * 
+ * @author shermann
+ */
+public class MCRLoggingCommands extends MCRAbstractCommands {
+
+    private static final Logger LOGGER = Logger.getLogger(MCRLoggingCommands.class);
+
+    public MCRLoggingCommands() {
+        super("Logging");
+
+        MCRCommand com = new MCRCommand(
+                "change log level of {0} to {1}",
+                "org.mycore.frontend.cli.MCRLoggingCommands.changeLogLevel String String",
+                "{0} the package or class name for which to change the log level, {1} the log level to set. If the log level cannot be read it is set to DEBUG by default.");
+
+        command.add(com);
+    }
+
+    /**
+     * @param name
+     *            the name of the java class or java package to set the log
+     *            level for
+     * @param logLevelToSet
+     *            the log level to set e.g. TRACE, DEBUG, INFO, WARN, ERROR and
+     *            FATAL, providing any other value will lead to DEBUG as new log
+     *            level
+     */
+    synchronized public static void changeLogLevel(String name, String logLevelToSet) {
+        LOGGER.info("Setting log level for \"" + name + "\" to \"" + logLevelToSet + "\"");
+
+        Level newLevel = Level.toLevel(logLevelToSet.toUpperCase());
+        if (newLevel == null) {
+            LOGGER.error("Unknown log level \"" + logLevelToSet + "\"");
+            return;
+        }
+
+        Logger log = Logger.getLogger(name);
+        if (log == null) {
+            LOGGER.error("Could not get logger for \"" + name + "\"");
+            return;
+        }
+
+        LOGGER.info("Change log level from " + log.getLevel() + " to " + newLevel);
+        log.setLevel(newLevel);
+    }
+}
