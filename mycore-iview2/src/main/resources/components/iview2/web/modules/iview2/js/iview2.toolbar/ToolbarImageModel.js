5c7aea05927268370ae845297ad6e3cf8d5c98a0
Philipp Heinze <mcrheinz@duepub-1.zim.uni-duisburg-essen.de>
2011-12-15 13:13:09 +0000

    removed reserved word, which caused an error
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@22948 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/js/iview2.toolbar/ToolbarImageModel.js
+++ mycore-iview2/src/main/resources/components/iview2/web/modules/iview2/js/iview2.toolbar/ToolbarImageModel.js
@@ -8,15 +8,13 @@
  * 			String:		elementName,		//name of the text element
  * 			String:		type				//type text, to differ between buttonsets, dividers and text
  * 			integer:	src					//src of the picture which shall be displayed
- * 			String:		float				//telling if the object will be floated left or right
  * 			Object:		relatedToolbar		//related toolbar model to navigate from the text to its toolbar
  * 		}
  */
-var ToolbarImageModel = function (elementName, src, float) {
+var ToolbarImageModel = function (elementName, src) {
     this.elementName = elementName;
     this.type = "image";
     this.src = src;
-    this.float = float || "left";
     // will set indirectly while adding
     this.relatedToolbar = null;
 };
