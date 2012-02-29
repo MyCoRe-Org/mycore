aa86dea24757bb35c687f7476122a267d40c918b
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2012-02-29 09:30:43 +0000

    Added new transformer package that provides some transformation methods for MCRContent. These can be used to export and import content by invoking external transformation commands, do XSL etc.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@23753 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/content/transformer/MCRBibUtilsTransformer.java
@@ -0,0 +1,99 @@
+/*
+ * $Revision$ 
+ * $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.content.transformer;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.mycore.common.MCRConfiguration;
+import org.mycore.common.MCRException;
+import org.mycore.common.content.MCRContent;
+import org.mycore.frontend.cli.MCRExternalProcess;
+
+/**
+ * Transforms MCRContent by invoking an external BibUtils command.
+ * See http://www.scripps.edu/~cdputnam/software/bibutils/
+ * The BibUtils commands provide functionality to convert between
+ * RIS, Endnote, BibTeX, ISI web of science, Word 2007 bibliography format
+ * and MODS. For each transformer instance, the command must be specified,
+ * for example
+ * 
+ * MCR.ContentTransformer.{ID}.Command=cmd.exe /c C:\\Java\\bibutils_4.12\\xml2bib.exe -b -w 
+ *
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRBibUtilsTransformer extends MCRContentTransformer {
+
+    private final static Logger LOGGER = Logger.getLogger(MCRBibUtilsTransformer.class);
+
+    /** The external Bibutils command to invoke */
+    private String command;
+
+    @Override
+    public void init(String id) {
+        command = MCRConfiguration.instance().getString("MCR.ContentTransformer." + id + ".Command");
+    }
+
+    @Override
+    public MCRContent transform(MCRContent source) throws Exception {
+        File modsFile = File.createTempFile("mods", ".xml");
+        source.sendTo(modsFile);
+        try {
+            return export(modsFile);
+        } finally {
+            modsFile.delete();
+        }
+    }
+
+    private MCRContent export(File modsFile) {
+        String[] arguments = buildCommandArguments(modsFile);
+        MCRExternalProcess ep = null;
+        try {
+            ep = new MCRExternalProcess(arguments);
+            ep.run();
+            String errors = ep.getErrors();
+            if (!errors.isEmpty())
+                LOGGER.warn(errors);
+            return ep.getOutput();
+        } catch (Exception ex) {
+            String msg = "Exception invoking external command " + command;
+            throw new MCRException(msg, ex);
+        }
+    }
+
+    /**
+     * Builds the command arguments to invoke the external BibUtils command 
+     */
+    private String[] buildCommandArguments(File modsFile) {
+        String path = modsFile.getAbsolutePath();
+
+        String[] args1 = command.split(" ");
+        String[] args2 = new String[args1.length + 1];
+        System.arraycopy(args1, 0, args2, 0, args1.length);
+        args2[args1.length] = path;
+
+        LOGGER.info(command + " " + path);
+        return args2;
+    }
+}
---
+++ mycore-base/src/main/java/org/mycore/common/content/transformer/MCRTransformerPipe.java
@@ -0,0 +1,68 @@
+/*
+ * $Revision$ 
+ * $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.content.transformer;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.mycore.common.MCRConfiguration;
+import org.mycore.common.content.MCRContent;
+
+/**
+ * Transforms MCRContent by using a pipe of multiple transformers.
+ * The transformers to execute are configured by giving a list of their IDs, for example
+ * 
+ * MCR.ContentTransformer.{ID}.Steps=ID2 ID3
+ * 
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRTransformerPipe extends MCRContentTransformer {
+
+    /** List of transformers to execute */
+    private List<MCRContentTransformer> transformers;
+
+    @Override
+    public void init(String id) {
+        String steps = MCRConfiguration.instance().getString("MCR.ContentTransformer." + id + ".Steps");
+        StringTokenizer tokens = new StringTokenizer(steps, " ;,");
+        while (tokens.hasMoreTokens()) {
+            String transformerID = tokens.nextToken();
+            MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer(transformerID);
+            transformers.add(transformer);
+        }
+    }
+
+    @Override
+    public MCRContent transform(MCRContent content) throws Exception {
+        for (MCRContentTransformer transformer : transformers)
+            content = transformer.transform(content);
+
+        return content;
+    }
+
+    @Override
+    public String getMimeType() {
+        return transformers.get(transformers.size() - 1).getMimeType();
+    }
+}
