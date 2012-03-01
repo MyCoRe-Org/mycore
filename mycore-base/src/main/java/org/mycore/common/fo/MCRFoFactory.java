6cf28cb01a638de6bd5da4f3e5c63239ec911dbc
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2012-03-01 14:06:45 +0000

    Returns the XSL-FO formatter instance to use (extracted from MCRLayoutService)
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@23782 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/fo/MCRFoFactory.java
@@ -0,0 +1,57 @@
+/*
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.fo;
+
+import org.mycore.common.MCRConfiguration;
+
+/**
+ * Returns the XSL-FO formatter instance configured via property, for example
+ * 
+ * MCR.LayoutService.FoFormatter.class=org.mycore.common.fo.MCRFoFormatterFOP
+ * 
+ * (which is the default using Apache FOP)
+ * 
+ * @see MCRFoFormatterInterface
+ * 
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRFoFactory {
+
+    /** The default class implementing the MCRFoFormatterInterface */
+    private final static String defaultFo = "org.mycore.common.fo.MCRFoFormatterFOP";
+
+    /** The configuration property */
+    private final static String property = "MCR.LayoutService.FoFormatter.class";
+
+    /** The singleton */
+    private static MCRFoFormatterInterface formatter;
+
+    /** Returns the XSL-FO formatter instance configured */
+    public static synchronized MCRFoFormatterInterface getFoFormatter() {
+        if (formatter == null) {
+            Object instance = MCRConfiguration.instance().getInstanceOf(property, defaultFo);
+            formatter = (MCRFoFormatterInterface) instance;
+        }
+        return formatter;
+    }
+}
