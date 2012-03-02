a5ffd81b8c1906ea2f3c4917d52c19b5d78e1f9b
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2012-03-02 09:02:25 +0000

    New transformer for debugging transformation steps, may be inserted into transformation pipes at any step.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@23790 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/content/transformer/MCRDebuggingTransformer.java
@@ -0,0 +1,51 @@
+/*
+ * $Revision$ 
+ * $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.content.transformer;
+
+import org.apache.log4j.Logger;
+import org.mycore.common.content.MCRContent;
+
+/**
+ * Does not really transform content, instead logs the current contents.
+ * This can be useful when multiple transformers are combined in a pipe. 
+ *  
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRDebuggingTransformer extends MCRContentTransformer {
+
+    private final static Logger LOGGER = Logger.getLogger(MCRDebuggingTransformer.class);
+
+    @Override
+    public MCRContent transform(MCRContent source) throws Exception {
+        if (LOGGER.isDebugEnabled()) {
+            if (!source.isReusable())
+                source = source.getReusableCopy();
+
+            LOGGER.debug(">>>>>>>>>>>>>>>>>>>>");
+            LOGGER.debug(source.toString());
+            LOGGER.debug("<<<<<<<<<<<<<<<<<<<<");
+        }
+        return source;
+    }
+}
