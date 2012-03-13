a67891f65ec212f2dcd6f4f1ab5cd7b6c236133c
Thomas Scheffler <github@yagee.de>
2012-03-13 14:55:42 +0000

    added class MCRJAXBContent to handle JAXB objects
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@23876 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/main/java/org/mycore/common/content/MCRJAXBContent.java
@@ -0,0 +1,142 @@
+/*
+ * $Id$
+ * $Revision: 5697 $ $Date: 13.03.2012 $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.common.content;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.transform.Source;
+
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.transform.JDOMResult;
+import org.xml.sax.SAXParseException;
+
+/**
+ * @author Thomas Scheffler (yagee)
+ *
+ */
+public class MCRJAXBContent<T> extends MCRXMLContent {
+
+    JAXBContext ctx;
+
+    T jaxbObject;
+
+    private String docType;
+
+    /**
+     * @param ctx
+     * @param jaxbObject
+     */
+    public MCRJAXBContent(JAXBContext ctx, T jaxbObject) {
+        this.ctx = ctx;
+        this.jaxbObject = jaxbObject;
+        Class<? extends Object> clazz = jaxbObject.getClass();
+        if (!clazz.isAnnotationPresent(XmlRootElement.class)) {
+            throw new IllegalArgumentException("Class " + clazz.getName() + " is not a JAXB annotated.");
+        }
+        this.docType = getRootTag(jaxbObject);
+        setSystemId(jaxbObject.toString());
+    }
+
+    private String getRootTag(T jaxbObject) {
+        return jaxbObject.getClass().getAnnotation(XmlRootElement.class).name();
+    }
+
+    private Marshaller getMarshaller() throws JAXBException {
+        Marshaller marshaller = ctx.createMarshaller();
+        return marshaller;
+    }
+
+    public T getObject() {
+        return jaxbObject;
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.content.MCRContent#getSource()
+     */
+    @Override
+    public Source getSource() throws IOException {
+        try {
+            return new JAXBSource(ctx, jaxbObject);
+        } catch (JAXBException e) {
+            throw new IOException(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.content.MCRContent#sendTo(java.io.OutputStream)
+     */
+    @Override
+    public void sendTo(OutputStream out) throws IOException {
+        try {
+            Marshaller marshaller = getMarshaller();
+            marshaller.marshal(jaxbObject, out);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.content.MCRContent#sendTo(java.io.File)
+     */
+    @Override
+    public void sendTo(File target) throws IOException {
+        try {
+            Marshaller marshaller = getMarshaller();
+            marshaller.marshal(jaxbObject, target);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.content.MCRContent#getDocType()
+     */
+    @Override
+    public String getDocType() throws IOException {
+        return docType;
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.common.content.MCRContent#asXML()
+     */
+    @Override
+    public Document asXML() throws JDOMException, IOException, SAXParseException {
+        JDOMResult result = new JDOMResult();
+        try {
+            Marshaller marshaller = getMarshaller();
+            marshaller.marshal(jaxbObject, result);
+        } catch (JAXBException e) {
+            throw new IOException(e);
+        }
+        return result.getDocument();
+    }
+}
