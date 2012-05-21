7d8f2016e79c2f3b37c039537ee74d4d1bafea19
Silvio Hermann <Possommi@users.noreply.github.com>
2012-05-21 12:15:47 +0000

    allow getting ids of parent categories for a given category id
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@24304 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-classhandler/src/main/java/org/mycore/frontend/servlets/MCRClassExportServlet.java
+++ mycore-classhandler/src/main/java/org/mycore/frontend/servlets/MCRClassExportServlet.java
@@ -3,9 +3,14 @@
  */
 package org.mycore.frontend.servlets;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.jdom.Attribute;
 import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.xpath.XPath;
 import org.mycore.common.MCRException;
 import org.mycore.common.content.MCRJDOMContent;
 import org.mycore.datamodel.classifications2.MCRCategory;
@@ -27,17 +32,56 @@ public class MCRClassExportServlet extends MCRServlet {
 
     public void doGetPost(MCRServletJob job) throws Exception, MCRException {
         try {
+            String categid = getProperty(job.getRequest(), "categid");
             String id = getProperty(job.getRequest(), "id");
-            if (id == null)
-                throw new MCRException("Classification ID is null!");
-            MCRCategoryID catId = MCRCategoryID.rootID(id);
-            MCRCategory category = DAO.getCategory(catId, -1);
-            if (category == null)
-                throw new MCRException("Cannot find classification with id: " + id);
-            Document jdom = MCRCategoryTransformer.getMetaDataDocument(category, true);
-            getLayoutService().sendXML(job.getRequest(), job.getResponse(), new MCRJDOMContent(jdom));
+
+            if (categid == null || categid.length() == 0) {
+
+                if (id == null)
+                    throw new MCRException("Classification ID is null!");
+                MCRCategoryID catId = MCRCategoryID.rootID(id);
+                MCRCategory category = DAO.getCategory(catId, -1);
+                if (category == null)
+                    throw new MCRException("Cannot find classification with id: " + id);
+                Document jdom = MCRCategoryTransformer.getMetaDataDocument(category, true);
+                getLayoutService().sendXML(job.getRequest(), job.getResponse(), new MCRJDOMContent(jdom));
+            } else {
+                Document snippet = getParentIdentifiers(id, categid);
+                getLayoutService().sendXML(job.getRequest(), job.getResponse(), new MCRJDOMContent(snippet));
+            }
         } catch (Exception e) {
             generateErrorPage(job.getRequest(), job.getResponse(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), e, false);
         }
+    }
+
+    /**
+     * Returns the identifiers of all parent categories.
+     * 
+     * @param classification the identifier of the classification
+     * @param categid the category identifier for getting the parent ids for
+     * @return
+     * @throws Exception
+     * 
+     * TODO cache generated snippets
+     */
+    @SuppressWarnings("unchecked")
+    private Document getParentIdentifiers(String classification, String categid) throws Exception {
+        MCRCategoryID catId = MCRCategoryID.rootID(classification);
+        MCRCategory category = DAO.getCategory(catId, -1);
+        Document jdom = MCRCategoryTransformer.getMetaDataDocument(category, true);
+        XPath xp = XPath.newInstance("/mycoreclass/categories//category[@ID='" + categid + "']/ancestor::category/@ID");
+        List<Attribute> nodes = xp.selectNodes(jdom);
+
+        Element identifiers = new Element("identifiers");
+        identifiers.setAttribute("classification", classification);
+        identifiers.setAttribute("parents-for-categid", categid);
+
+        Document parents = new Document(identifiers);
+
+        for (Attribute attr : nodes) {
+            identifiers.addContent(new Element("id").setText(attr.getValue()));
+        }
+
+        return parents;
     }
 }
