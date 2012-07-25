07f4a9316782aee492edf594a0be115f90a59276
Thomas Scheffler <github@yagee.de>
2012-07-25 12:36:24 +0000

    added simple test for MCRGroupClause
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@24766 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-base/src/test/java/org/mycore/access/mcrimpl/MCRGroupClauseTest.java
@@ -0,0 +1,51 @@
+package org.mycore.access.mcrimpl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mycore.common.MCRSessionMgr;
+import org.mycore.common.MCRUserInformation;
+
+public class MCRGroupClauseTest {
+
+    private static final String INGROUP_NAME = "ingroup";
+
+    @Before
+    public void setUp() throws Exception {
+        MCRUserInformation userInfo = new MCRUserInformation() {
+
+            @Override
+            public boolean isUserInRole(String role) {
+                return INGROUP_NAME.equals(role);
+            }
+
+            @Override
+            public String getUserID() {
+                return "junit";
+            }
+
+            @Override
+            public String getUserAttribute(String attribute) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+        };
+        MCRSessionMgr.getCurrentSession().setUserInformation(userInfo);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public final void testEvaluate() {
+        String outGroup="outgroup";
+        MCRGroupClause groupClause = new MCRGroupClause(INGROUP_NAME, false);
+        assertTrue("Did not accept " + INGROUP_NAME, groupClause.evaluate(null));
+        groupClause = new MCRGroupClause(outGroup, true);
+        assertTrue("Did not accept " + outGroup, groupClause.evaluate(null));
+    }
+
+}
