c7e6869d9dbe454f2ea6947c78876b3f320bc7c6
Silvio Hermann <Possommi@users.noreply.github.com>
2013-01-17 11:06:33 +0000

    commit after all docs of the current bunch have been indexed
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@25796 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/index/cs/MCRXMLSolrIndexer.java
+++ mycore-solr/src/main/java/org/mycore/solr/index/cs/MCRXMLSolrIndexer.java
@@ -13,26 +13,33 @@ import org.mycore.solr.logging.MCRSolrLogLevels;
  */
 public class MCRXMLSolrIndexer {
 
-    private static final String UPDATE_PATH = MCRConfiguration.instance().getString("MCR.Module-solr.UpdatePath", "/update");
+    final private static String UPDATE_PATH = MCRConfiguration.instance().getString("MCR.Module-solr.UpdatePath", "/update");
 
     final static Logger LOGGER = Logger.getLogger(MCRXMLSolrIndexer.class);
 
-    static String TRANSFORM = MCRConfiguration.instance().getString("MCR.Module-solr.transform", "object2fields.xsl");
+    final static String TRANSFORM = MCRConfiguration.instance().getString("MCR.Module-solr.transform", "object2fields.xsl");
     static {
         LOGGER.log(MCRSolrLogLevels.SOLR_INFO, MCRXMLSolrIndexer.class.getName() + " will use " + TRANSFORM);
     }
 
     public void index(ContentStreamBase stream) {
         try {
-            LOGGER.trace("Solr: indexing data of\"" + stream.getName() + "\"");
+            MCRSolrServerFactory.getConcurrentSolrServer().blockUntilFinished();
+            LOGGER.trace("Indexing data of\"" + stream.getName() + "\"");
             long tStart = System.currentTimeMillis();
             ContentStreamUpdateRequest updateRequest = new ContentStreamUpdateRequest(UPDATE_PATH);
             updateRequest.addContentStream(stream);
             updateRequest.setParam("tr", TRANSFORM);
             MCRSolrServerFactory.getConcurrentSolrServer().request(updateRequest);
-            LOGGER.trace("Solr: indexing data of\"" + stream.getName() + "\" (" + (System.currentTimeMillis() - tStart) + "ms)");
+            try {
+                MCRSolrServerFactory.getConcurrentSolrServer().commit();
+            } catch (Exception e) {
+                LOGGER.log(MCRSolrLogLevels.SOLR_ERROR, "Could not commit changes", e);
+            }
+            LOGGER.trace("Indexing data of\"" + stream.getName() + "\" (" + (System.currentTimeMillis() - tStart) + "ms)");
         } catch (Exception ex) {
-            LOGGER.error("Error sending content to solr through content stream " + this, ex);
+            LOGGER.log(MCRSolrLogLevels.SOLR_ERROR, "Error sending content to solr through content stream " + getClass().getSimpleName(),
+                    ex);
         }
     }
 }
