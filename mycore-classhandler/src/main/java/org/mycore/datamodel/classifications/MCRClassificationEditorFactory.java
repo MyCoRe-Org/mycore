e478bd4778e4587776f53142b6ded25e8431808c
Thomas Scheffler <github@yagee.de>
2013-01-23 15:10:15 +0000

    removed mycore-classhandler
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@25899 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-classhandler/src/main/java/org/mycore/datamodel/classifications/MCRClassificationEditorFactory.java
+++
@@ -1,9 +0,0 @@
-package org.mycore.datamodel.classifications;
-
-public class MCRClassificationEditorFactory {
-    private static MCRClassificationEditor classificationEditor = new MCRClassificationEditor();
-    public static MCRClassificationEditor getInstance() {
-        return classificationEditor;
-    }
-
-}
