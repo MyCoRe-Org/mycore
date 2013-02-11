a55d33fc639e5f8e60c6a99e921b39f05be46c6b
Thomas Scheffler <github@yagee.de>
2013-02-11 06:42:44 +0000

    if resolvedURL is null return null and do not throw exception
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26157 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/common/xml/MCRXMLResource.java
+++ mycore-base/src/main/java/org/mycore/common/xml/MCRXMLResource.java
@@ -55,8 +55,7 @@ public class MCRXMLResource {
     private static Logger LOGGER = Logger.getLogger(MCRXMLResource.class);
 
     private MCRXMLResource() {
-        resourceCache = new MCRCache<String, CacheEntry>(MCRConfiguration.instance().getInt("MCR.MCRXMLResource.Cache.Size", 100),
-                "XML resources");
+        resourceCache = new MCRCache<String, CacheEntry>(MCRConfiguration.instance().getInt("MCR.MCRXMLResource.Cache.Size", 100), "XML resources");
     }
 
     /**
@@ -129,6 +128,10 @@ public class MCRXMLResource {
             LOGGER.debug("Using cached resource " + name);
             return entry.content;
         }
+        if (resolvedURL == null) {
+            LOGGER.warn("Could not resolve resource: " + name);
+            return null;
+        }
         entry = new CacheEntry();
         resourceCache.put(name, entry);
         entry.resourceURL = resolvedURL;
@@ -158,8 +161,7 @@ public class MCRXMLResource {
         if (con == null) {
             return null;
         }
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(64 * 1024);
-                InputStream in = new BufferedInputStream(con.getInputStream())) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(64 * 1024); InputStream in = new BufferedInputStream(con.getInputStream())) {
             IOUtils.copy(in, baos);
             return baos.toByteArray();
         } finally {
