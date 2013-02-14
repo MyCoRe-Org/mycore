226bc4b55c65c52289b6e0a7ee8584c3afb3ae8b
Thomas Scheffler <github@yagee.de>
2013-02-14 06:11:31 +0000

    added log statements
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26213 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/datamodel/ifs2/MCRStoreManager.java
+++ mycore-base/src/main/java/org/mycore/datamodel/ifs2/MCRStoreManager.java
@@ -1,26 +1,29 @@
 package org.mycore.datamodel.ifs2;
 
+import org.apache.log4j.Logger;
 import org.mycore.common.MCRException;
 import org.mycore.datamodel.ifs2.MCRStore.MCRStoreConfig;
 
 public class MCRStoreManager {
-    
+    private static final Logger LOGGER = Logger.getLogger(MCRStoreManager.class);
+
     public static <T extends MCRStore> T createStore(String ID, Class<T> storeClass) throws InstantiationException, IllegalAccessException {
         return createStore(new MCRStoreDefaultConfig(ID), storeClass);
     }
-    
+
     public static <T extends MCRStore> T createStore(MCRStoreConfig config, Class<T> storeClass) throws InstantiationException, IllegalAccessException {
         T store = storeClass.newInstance();
         store.init(config);
         try {
+            LOGGER.info("Adding instance of " + storeClass.getSimpleName() + " as MCRStore '" + store.getID() + "'.");
             MCRStoreCenter.instance().addStore(store.getID(), store);
         } catch (Exception e) {
             throw new MCRException("Could not create store with ID " + config.getID() + ", store allready exists");
         }
-        
+
         return store;
     }
-    
+
     /**
      * Returns the store with the given ID
      * 
@@ -34,8 +37,9 @@ public class MCRStoreManager {
     public static <T extends MCRStore> T getStore(String ID, Class<T> storeClass) {
         return MCRStoreCenter.instance().getStore(ID, storeClass);
     }
-    
+
     public static void removeStore(String id) {
+        LOGGER.info("Remove MCRStore '" + id + "'.");
         MCRStoreCenter.instance().removeStore(id);
     }
 }
