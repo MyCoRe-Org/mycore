3ac87d38f96fb95fbcf976c8908d9802d3b0f750
Thomas Scheffler <github@yagee.de>
2013-04-11 14:44:31 +0000

    added more debug messages on finish
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26692 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/index/cs/MCRSolrDefaultIndexHandler.java
+++ mycore-solr/src/main/java/org/mycore/solr/index/cs/MCRSolrDefaultIndexHandler.java
@@ -1,11 +1,13 @@
 package org.mycore.solr.index.cs;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.common.util.NamedList;
 import org.mycore.common.MCRConfiguration;
 
 /**
@@ -15,7 +17,7 @@ import org.mycore.common.MCRConfiguration;
  */
 public class MCRSolrDefaultIndexHandler extends MCRSolrAbstractIndexHandler {
 
-    final static Logger LOGGER = Logger.getLogger(MCRSolrDefaultIndexHandler.class);
+    private final static Logger LOGGER = Logger.getLogger(MCRSolrDefaultIndexHandler.class);
 
     final static String STYLESHEET = MCRConfiguration.instance().getString("MCR.Module-solr.transform", "object2fields.xsl");
 
@@ -33,19 +35,32 @@ public class MCRSolrDefaultIndexHandler extends MCRSolrAbstractIndexHandler {
      * Invokes an index request for the current content stream.
      */
     public void index() throws IOException, SolrServerException {
-        if(LOGGER.isTraceEnabled()) {
+        if (LOGGER.isTraceEnabled()) {
             LOGGER.trace("Solr: indexing data of\"" + this.getStream().getName() + "\"");
         }
         long tStart = System.currentTimeMillis();
+        SolrServer solrServer = getSolrServer();
         ContentStreamUpdateRequest updateRequest = new ContentStreamUpdateRequest(UPDATE_PATH);
         updateRequest.addContentStream(getStream());
         updateRequest.setParam("tr", STYLESHEET);
-        if(getCommitWithin() != null) {
+        if (getCommitWithin() != null) {
             updateRequest.setCommitWithin(getCommitWithin());
         }
-        getSolrServer().request(updateRequest);
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("Solr: indexing data of\"" + this.getStream().getName() + "\" (" + (System.currentTimeMillis() - tStart) + "ms)");
+        NamedList<Object> request = solrServer.request(updateRequest);
+        if (LOGGER.isTraceEnabled()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Solr: indexing data of \"");
+            stringBuilder.append(this.getStream().getName());
+            stringBuilder.append("\" (");
+            stringBuilder.append((System.currentTimeMillis() - tStart));
+            stringBuilder.append("ms)");
+            for (Map.Entry<String, Object> entry : request) {
+                stringBuilder.append('\n');
+                stringBuilder.append(entry.getKey());
+                stringBuilder.append('=');
+                stringBuilder.append(entry.getValue().toString());
+            }
+            LOGGER.trace(stringBuilder.toString());
         }
     }
 
