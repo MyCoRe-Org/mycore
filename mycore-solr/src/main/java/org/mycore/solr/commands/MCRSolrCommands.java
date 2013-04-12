5c86411ebd66de609fab529389147b4d32ee9129
Thomas Scheffler <github@yagee.de>
2013-04-12 08:07:40 +0000

    fixed command invocation
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26703 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/commands/MCRSolrCommands.java
+++ mycore-solr/src/main/java/org/mycore/solr/commands/MCRSolrCommands.java
@@ -30,55 +30,59 @@ public class MCRSolrCommands extends MCRAbstractCommands {
         super("Solr Commands");
         MCRCommand com = null;
 
-        com = new MCRCommand("rebuild solr metadata and content index", "org.mycore.solr.index.cs.MCRSolrIndexer.rebuildMetadataAndContentIndex",
-                "rebuilds solr's metadata and content index");
+        String solrIndexerClassName = MCRSolrIndexer.class.getCanonicalName();
+        String commandClassName = getClass().getCanonicalName();
+
+        com = new MCRCommand("rebuild solr metadata and content index", solrIndexerClassName + ".rebuildMetadataAndContentIndex",
+            "rebuilds solr's metadata and content index");
         addCommand(com);
 
-        com = new MCRCommand("rebuild solr metadata index", "org.mycore.solr.index.cs.MCRSolrIndexer.rebuildMetadataIndex", "rebuilds solr's metadata index");
+        com = new MCRCommand("rebuild solr metadata index", solrIndexerClassName + ".rebuildMetadataIndex",
+            "rebuilds solr's metadata index");
         addCommand(com);
 
-        com = new MCRCommand("rebuild solr content index", "org.mycore.solr.index.cs.MCRSolrIndexer.rebuildContentIndex", "rebuilds solr's content index");
+        com = new MCRCommand("rebuild solr content index", solrIndexerClassName + ".rebuildContentIndex", "rebuilds solr's content index");
         addCommand(com);
 
-        com = new MCRCommand("restricted rebuild solr metadata index for objecttype {0}",
-                "org.mycore.solr.index.cs.MCRSolrIndexer.rebuildMetadataIndex String", "rebuilds solr's metadata index for the given type in {0}");
+        com = new MCRCommand("restricted rebuild solr metadata index for objecttype {0}", solrIndexerClassName
+            + ".rebuildMetadataIndex String", "rebuilds solr's metadata index for the given type in {0}");
         addCommand(com);
 
-        com = new MCRCommand("restricted rebuild solr metadata index for selected", "org.mycore.solr.commands.MCRSolrCommands.rebuildMetadataIndexForSelected",
-                "rebuilds solr's metadata index for selected objects");
+        com = new MCRCommand("restricted rebuild solr metadata index for selected", commandClassName + ".rebuildMetadataIndexForSelected",
+            "rebuilds solr's metadata index for selected objects");
         addCommand(com);
 
-        com = new MCRCommand("restricted rebuild solr content index for selected", "org.mycore.solr.commands.MCRSolrCommands.rebuildContentIndexForSelected",
-                "rebuilds solr's content index for selected objects and or derivates");
+        com = new MCRCommand("restricted rebuild solr content index for selected", commandClassName + ".rebuildContentIndexForSelected",
+            "rebuilds solr's content index for selected objects and or derivates");
         addCommand(com);
 
-        com = new MCRCommand("restricted rebuild solr metadata index for object {0}",
-                "org.mycore.solr.commands.MCRSolrCommands.rebuildMetadataIndexForObject String",
-                "rebuilds solr's metadata index for object and all its children");
+        com = new MCRCommand("restricted rebuild solr metadata index for object {0}", commandClassName
+            + ".rebuildMetadataIndexForObject String", "rebuilds solr's metadata index for object and all its children");
         addCommand(com);
 
-        com = new MCRCommand("optimize solr index", "org.mycore.solr.index.cs.MCRSolrIndexer.optimize",
-                "An optimize is like a hard commit except that it forces all of the index segments to be merged into a single segment first. "
-                        + "Depending on the use cases, this operation should be performed infrequently (like nightly), "
-                        + "if at all, since it is very expensive and involves reading and re-writing the entire index");
+        com = new MCRCommand("optimize solr index", solrIndexerClassName + ".optimize",
+            "An optimize is like a hard commit except that it forces all of the index segments to be merged into a single segment first. "
+                + "Depending on the use cases, this operation should be performed infrequently (like nightly), "
+                + "if at all, since it is very expensive and involves reading and re-writing the entire index");
         addCommand(com);
 
-        com = new MCRCommand("drop solr index", "org.mycore.solr.index.cs.MCRSolrIndexer.dropIndex", "Deletes an existing index from solr");
+        com = new MCRCommand("drop solr index", solrIndexerClassName + ".dropIndex", "Deletes an existing index from solr");
         addCommand(com);
 
-        com = new MCRCommand("delete index part for type {0}", "org.mycore.solr.index.cs.MCRSolrIndexer.dropIndexByType String",
-                "Deletes an existing index from solr but only for the given object type.");
+        com = new MCRCommand("delete index part for type {0}", solrIndexerClassName + ".dropIndexByType String",
+            "Deletes an existing index from solr but only for the given object type.");
         addCommand(com);
 
-        com = new MCRCommand("delete from solr index by id {0}", "org.mycore.solr.index.cs.MCRSolrIndexer.deleteByIdFromSolr String",
-                "Deletes an document from the index by id");
+        com = new MCRCommand("delete from solr index by id {0}", solrIndexerClassName + ".deleteByIdFromSolr String",
+            "Deletes an document from the index by id");
         addCommand(com);
 
-        com = new MCRCommand("create solr metadata and content index at {0}", "org.mycore.solr.commands.MCRSolrCommands.createIndex String",
-                "create solr's metadata and content index on specific solr server");
+        com = new MCRCommand("create solr metadata and content index at {0}", solrIndexerClassName + ".createIndex String",
+            "create solr's metadata and content index on specific solr server");
         addCommand(com);
 
-        com = new MCRCommand("set solr server {0}", "org.mycore.solr.MCRSolrServerFactory.setSolrServer String", "Sets the solr new server");
+        com = new MCRCommand("set solr server {0}", MCRSolrServerFactory.class.getCanonicalName() + ".setSolrServer String",
+            "Sets the solr new server");
         addCommand(com);
     }
 
