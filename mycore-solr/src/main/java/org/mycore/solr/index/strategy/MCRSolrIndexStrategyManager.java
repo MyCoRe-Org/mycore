789fdf30637bce678180d6ae52579eff572b0201
Thomas Scheffler <github@yagee.de>
2013-04-25 08:16:14 +0000

    moved another default value to mycore.properties
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26789 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/index/strategy/MCRSolrIndexStrategyManager.java
+++ mycore-solr/src/main/java/org/mycore/solr/index/strategy/MCRSolrIndexStrategyManager.java
@@ -13,8 +13,7 @@ public class MCRSolrIndexStrategyManager {
     private static final MCRSolrFileStrategy FILE_STRATEGY;
 
     static {
-        FILE_STRATEGY = MCRConfiguration.instance().getInstanceOf(CONFIG_PREFIX + "FileIndexStrategy",
-            MCRSolrFileSizeStrategy.class.getCanonicalName(), MCRSolrFileStrategy.class);
+        FILE_STRATEGY = MCRConfiguration.instance().getInstanceOf(CONFIG_PREFIX + "FileIndexStrategy", MCRSolrFileStrategy.class);
     }
 
     public static boolean checkFile(MCRFile file) {
