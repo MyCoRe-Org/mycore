a5ec99843f30c4a704daaf16581e61848fcb6c2d
Thomas Scheffler <github@yagee.de>
2013-05-15 12:11:17 +0000

    update dependencies
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@26911 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/search/MCRSolrSelectProxyServlet.java
+++ mycore-solr/src/main/java/org/mycore/solr/search/MCRSolrSelectProxyServlet.java
@@ -3,6 +3,7 @@ package org.mycore.solr.search;
 import static org.mycore.solr.MCRSolrConstants.QUERY_PATH;
 import static org.mycore.solr.MCRSolrConstants.QUERY_XML_PROTOCOL_VERSION;
 import static org.mycore.solr.MCRSolrConstants.SERVER_URL;
+import static org.mycore.solr.MCRSolrConstants.CONFIG_PREFIX;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -11,19 +12,22 @@ import java.net.URLEncoder;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.mycore.common.MCRConfiguration;
@@ -32,7 +36,6 @@ import org.mycore.common.content.MCRStreamContent;
 import org.mycore.common.xml.MCRLayoutService;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
-import org.mycore.solr.MCRSolrConstants;
 
 public class MCRSolrSelectProxyServlet extends MCRServlet {
 
@@ -54,22 +57,21 @@ public class MCRSolrSelectProxyServlet extends MCRServlet {
      */
     public static final String QUERY_KEY = MCRSolrSelectProxyServlet.class.getName() + ".query";
 
-    private static int MAX_CONNECTIONS = MCRConfiguration.instance().getInt(MCRSolrConstants.CONFIG_PREFIX + "SelectProxy.MaxConnections", 20);
+    private static int MAX_CONNECTIONS = MCRConfiguration.instance().getInt(
+        CONFIG_PREFIX + "SelectProxy.MaxConnections", 20);
 
     private HttpClient httpClient;
-
-    private MultiThreadedHttpConnectionManager connectionManager;
 
     @Override
     protected void doGetPost(MCRServletJob job) throws Exception {
         HttpServletRequest request = job.getRequest();
         Map<String, String[]> solrParameter = getSolrQueryParameter(request);
 
-        HttpMethod solrHttpMethod = MCRSolrSelectProxyServlet.getSolrHttpMethod(solrParameter);
+        HttpGet solrHttpMethod = MCRSolrSelectProxyServlet.getSolrHttpMethod(solrParameter);
         try {
             LOGGER.info("Sending Request: " + solrHttpMethod.getURI());
-            int statusCode = httpClient.executeMethod(solrHttpMethod);
-            InputStream solrResponseStream = solrHttpMethod.getResponseBodyAsStream();
+            HttpResponse response = httpClient.execute(solrHttpMethod);
+            int statusCode = response.getStatusLine().getStatusCode();
 
             HttpServletResponse resp = job.getResponse();
 
@@ -77,18 +79,21 @@ public class MCRSolrSelectProxyServlet extends MCRServlet {
             resp.setStatus(statusCode);
 
             // set all header
-            for (Header header : solrHttpMethod.getResponseHeaders()) {
+            for (Header header : response.getAllHeaders()) {
                 if (!("Transfer-Encoding".equals(header.getName()) && statusCode == HttpStatus.SC_OK)) {
                     resp.setHeader(header.getName(), header.getValue());
                 }
             }
 
-            boolean isXML = solrHttpMethod.getResponseHeader("Content-Type").getValue().contains("/xml");
+            boolean isXML = response.getFirstHeader("Content-Type").getValue().contains("/xml");
+
+            HttpEntity solrResponseEntity = response.getEntity();
+            InputStream solrResponseStream = solrResponseEntity.getContent();
 
             if (statusCode == HttpStatus.SC_OK && isXML) {
                 String[] xslt = solrParameter.get("xslt");
-                MCRStreamContent solrResponse = new MCRStreamContent(solrResponseStream, solrHttpMethod.getURI().toString(), xslt != null ? xslt[0]
-                        : "response");
+                MCRStreamContent solrResponse = new MCRStreamContent(solrResponseStream, solrHttpMethod.getURI()
+                    .toString(), xslt != null ? xslt[0] : "response");
                 MCRLayoutService.instance().doLayout(request, resp, solrResponse);
                 return;
             }
@@ -129,18 +134,18 @@ public class MCRSolrSelectProxyServlet extends MCRServlet {
     public void init() throws ServletException {
         super.init();
 
-        connectionManager = new MultiThreadedHttpConnectionManager();
-        HttpConnectionManagerParams connectionManagerParams = connectionManager.getParams();
-        connectionManagerParams.setDefaultMaxConnectionsPerHost(MAX_CONNECTIONS);
-        connectionManagerParams.setMaxTotalConnections(MAX_CONNECTIONS);
+        PoolingClientConnectionManager connectionManager = new PoolingClientConnectionManager();
+        connectionManager.setDefaultMaxPerRoute(MAX_CONNECTIONS);
+        connectionManager.setMaxTotal(MAX_CONNECTIONS);
 
-        httpClient = new HttpClient(connectionManager);
+        httpClient = new DefaultHttpClient(connectionManager);
     }
 
     @Override
     public void destroy() {
-        connectionManager.closeIdleConnections(0);
-        connectionManager.deleteClosedConnections();
+        ClientConnectionManager clientConnectionManager = httpClient.getConnectionManager();
+        clientConnectionManager.closeIdleConnections(0, TimeUnit.SECONDS);
+        clientConnectionManager.closeExpiredConnections();
         super.destroy();
     }
 
@@ -151,14 +156,14 @@ public class MCRSolrSelectProxyServlet extends MCRServlet {
      *            Parameters to use with the Request
      * @return a method to make the request
      */
-    public static HttpMethod getSolrHttpMethod(Map<String, String[]> parameterMap) {
+    public static HttpGet getSolrHttpMethod(Map<String, String[]> parameterMap) {
         String queryString = getQueryString(parameterMap);
         if (!parameterMap.containsKey("version")) {
             queryString += "&version=" + QUERY_XML_PROTOCOL_VERSION;
         }
-        GetMethod getMethod = new GetMethod(MessageFormat.format("{0}{1}?{2}", SERVER_URL, QUERY_PATH, queryString));
+        HttpGet httpGet = new HttpGet(MessageFormat.format("{0}{1}?{2}", SERVER_URL, QUERY_PATH, queryString));
 
-        return getMethod;
+        return httpGet;
     }
 
     private static String getQueryString(Map<String, String[]> parameters) {
