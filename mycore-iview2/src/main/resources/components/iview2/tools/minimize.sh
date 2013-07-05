fa088ff49d7afbf9316c29e97524189f8d1c830b
Sebastian Hofmann <hofmann.s@uni-jena.de>
2013-07-05 08:59:48 +0000

    Adapted script to new location of the javascript source files.
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@27339 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/resources/components/iview2/tools/minimize.sh
+++ mycore-iview2/src/main/resources/components/iview2/tools/minimize.sh
@@ -4,27 +4,29 @@
 
 declare -a sources
 sources=( "Utils.js" "cache.js" "jquery.mousewheel.min.js" "jquery.tree.min.js" "jquery.simplemodal.js" "jquery.mcri18n.js" "ecma5compat.js" "xdr.js" "i18n.js" "PanoJS.js" "preload.js" "iviewInstance.js" "properties.js" "context.js" "currentImage.js" "chapter.js" "overview.js" "METS.js" "XML.js" "../lib/fg-menu/fg.menu.js" "iview2.toolbar/ToolbarManager.js" "iview2.toolbar/ToolbarModel.js" "iview2.toolbar/ToolbarButtonsetModel.js" "iview2.toolbar/ToolbarDividerModel.js" "iview2.toolbar/ToolbarSpringModel.js" "iview2.toolbar/ToolbarTextModel.js" "iview2.toolbar/ToolbarButtonModel.js" "iview2.toolbar/ToolbarImageModel.js" "iview2.toolbar/ToolbarController.js" "iview2.toolbar/ToolbarView.js" "iview2.toolbar/StandardToolbarModelProvider.js" "iview2.toolbar/PreviewToolbarModelProvider.js" "Permalink.js" "createPdf.js" "canvas.js" "canvas.rotate.js" "urnOverlay.js" "storage.js" "iview2.METS/ChapterModel.js" "iview2.METS/ChapterModelProvider.js" "iview2.METS/PhysicalEntry.js" "iview2.METS/PhysicalModel.js" "iview2.METS/PhysicalModelProvider.js" "iview2.thumbnailPanel/thumbnailPanelController.js" "iview2.thumbnailPanel/thumbnailPanelView.js" "iview2.scrollbar/scrollbarController.js" "iview2.scrollbar/scrollbarEvent.js" "iview2.scrollbar/scrollbarView.js" "jgestures.js")
-destpath=$(readlink -f ${1})
-
+destpath=${1%/*}
+destfile=$1
 path=`dirname $0`
-srcpath=${path}"/../web/modules/iview2/js/"
+
+srcpath=${path}"/../../../META-INF/resources/modules/iview2/js/"
+mkdir -p $destpath
 
 #initial Creation, we dont know what happened before the start of the script
-rm -f $destpath
-touch $destpath
+rm -f $destfile
+touch $destfile
 for file in ${sources[@]}
 do
-	cat ${srcpath}$file >> $destpath
+	cat ${srcpath}$file >> $destfile
 done
 
 while { inotifywait -r -e modify -e create -e move -e delete $srcpath; }; do
-	echo -n "building new version of ${destpath} "
-	rm -f $destpath
-	touch $destpath
+	echo -n "building new version of ${destfile} "
+	rm -f $destfile
+	touch $destfile
 	for file in ${sources[@]}
 	do
 		echo -n "."
-		cat ${srcpath}$file >> $destpath
+		cat ${srcpath}$file >> $destfile
 	done
 	echo "done."
 done
