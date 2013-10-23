c536799b4c1b813f003d8c083133e1f88e802284
Jens Kupferschmidt <bt1cu@users.noreply.github.com>
2013-10-23 13:49:00 +0000

    remove obsolete lines
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@28246 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/backend/hibernate/MCRHIBConnection.java
+++ mycore-base/src/main/java/org/mycore/backend/hibernate/MCRHIBConnection.java
@@ -27,7 +27,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
 import java.text.MessageFormat;
 import java.util.Iterator;
 
@@ -36,7 +35,6 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.jmx.StatisticsService;
 import org.hibernate.mapping.Table;
 import org.hibernate.service.ServiceRegistry;
@@ -225,18 +223,6 @@ public class MCRHIBConnection implements Closeable {
             return;
         }
         LOGGER.debug("Closing hibernate sessions.");
-//        Statistics stats = sessionFactory.getStatistics();
-//        if (stats.isStatisticsEnabled()) {
-//            try {
-//                handleStatistics(stats);
-//            } catch (FileNotFoundException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//        }
         sessionFactory.close();
         sessionFactory = null;
         SINGLETON = null;
