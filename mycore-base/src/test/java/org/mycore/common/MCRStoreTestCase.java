8c75e17ee237d4b5b523192610a4e2efaa2cdc34
Thomas Scheffler <github@yagee.de>
2013-12-11 11:07:07 +0000

    use TemporaryFolder of JUnit to store test files
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@28613 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/test/java/org/mycore/common/MCRStoreTestCase.java
+++ mycore-base/src/test/java/org/mycore/common/MCRStoreTestCase.java
@@ -1,47 +1,39 @@
 package org.mycore.common;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
 
 public abstract class MCRStoreTestCase extends MCRHibTestCase {
 
     private static MCRXMLMetadataManager store;
 
-    private static Path storeBaseDir;
+    @Rule
+    public TemporaryFolder storeBaseDir = new TemporaryFolder();
 
-    private static Path svnBaseDir;
+    @Rule
+    public TemporaryFolder svnBaseDir = new TemporaryFolder();
 
-    @BeforeClass
-    public static void init() throws Exception {
-        storeBaseDir = Files.createTempDirectory(MCRStoreTestCase.class.getSimpleName());
-        setProperty("MCR.Metadata.Store.BaseDir", storeBaseDir.toString(), true);
-        svnBaseDir = Files.createTempDirectory(MCRStoreTestCase.class.getSimpleName() + "_svn");
-        setProperty("MCR.Metadata.Store.SVNBase", svnBaseDir.toUri().toString(), true);
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        setProperty("MCR.Metadata.Store.BaseDir", storeBaseDir.getRoot().getAbsolutePath(), true);
+        Logger.getLogger(getClass()).info("SVN URI:" + svnBaseDir.getRoot().toURI().toString());
+        setProperty("MCR.Metadata.Store.SVNBase", svnBaseDir.getRoot().toURI().toString(), true);
         store = MCRXMLMetadataManager.instance();
         store.reload();
     }
 
-    @AfterClass
-    public static void destroy() throws Exception {
-        if (storeBaseDir != null) {
-            FileUtils.deleteDirectory(storeBaseDir.toFile());
-        }
-        if (svnBaseDir != null) {
-            FileUtils.deleteDirectory(svnBaseDir.toFile());
-        }
+    public Path getStoreBaseDir() {
+        return storeBaseDir.getRoot().toPath();
     }
 
-    public static Path getStoreBaseDir() {
-        return storeBaseDir;
-    }
-
-    public static Path getSvnBaseDir() {
-        return svnBaseDir;
+    public Path getSvnBaseDir() {
+        return svnBaseDir.getRoot().toPath();
     }
 
     public static MCRXMLMetadataManager getStore() {
