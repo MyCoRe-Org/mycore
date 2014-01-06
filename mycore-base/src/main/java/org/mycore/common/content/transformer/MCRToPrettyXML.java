da4341af32ba6555c605daf9ff577140fa176bd3
Thomas Scheffler <github@yagee.de>
2014-01-06 09:54:35 +0000

    set mime type to "text/xml"
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@28728 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/common/content/transformer/MCRToPrettyXML.java
+++ mycore-base/src/main/java/org/mycore/common/content/transformer/MCRToPrettyXML.java
@@ -61,4 +61,9 @@ public class MCRToPrettyXML extends MCRContentTransformer {
     protected String getDefaultExtension() {
         return "xml";
     }
+
+    @Override
+    public String getMimeType() throws Exception {
+        return "text/xml";
+    }
 }
