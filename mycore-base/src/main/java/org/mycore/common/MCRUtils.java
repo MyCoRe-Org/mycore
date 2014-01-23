0d6b29b15a7550738bf9aa9d3ef419f828db5839
Silvio Hermann <Possommi@users.noreply.github.com>
2014-01-23 09:25:09 +0000

    log error message
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@28898 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/common/MCRUtils.java
+++ mycore-base/src/main/java/org/mycore/common/MCRUtils.java
@@ -440,46 +440,46 @@ public class MCRUtils {
         T id;
 
         switch (operation) {
-            case COMMAND_OR:
-                merged.addAll(set1);
-                merged.addAll(set2);
+        case COMMAND_OR:
+            merged.addAll(set1);
+            merged.addAll(set2);
 
-                break;
+            break;
 
-            case COMMAND_AND:
+        case COMMAND_AND:
 
-                for (T aSet11 : set1) {
-                    id = aSet11;
+            for (T aSet11 : set1) {
+                id = aSet11;
 
-                    if (set2.contains(id)) {
-                        merged.add(id);
-                    }
+                if (set2.contains(id)) {
+                    merged.add(id);
                 }
+            }
 
-                break;
+            break;
 
-            case COMMAND_XOR:
+        case COMMAND_XOR:
 
-                for (T aSet1 : set1) {
-                    id = aSet1;
+            for (T aSet1 : set1) {
+                id = aSet1;
 
-                    if (!set2.contains(id)) {
-                        merged.add(id);
-                    }
+                if (!set2.contains(id)) {
+                    merged.add(id);
                 }
+            }
 
-                for (T aSet2 : set2) {
-                    id = aSet2;
+            for (T aSet2 : set2) {
+                id = aSet2;
 
-                    if (!set1.contains(id) && !merged.contains(id)) {
-                        merged.add(id);
-                    }
+                if (!set1.contains(id) && !merged.contains(id)) {
+                    merged.add(id);
                 }
+            }
 
-                break;
+            break;
 
-            default:
-                throw new IllegalArgumentException("operation not permited: " + operation);
+        default:
+            throw new IllegalArgumentException("operation not permited: " + operation);
         }
 
         return merged;
@@ -876,7 +876,7 @@ public class MCRUtils {
             value = new String(os.toByteArray());
             os.close();
         } catch (Exception e) {
-            return null;
+            LOGGER.error("Could not convert Document to String", e);
         }
         return value;
     }
@@ -906,8 +906,7 @@ public class MCRUtils {
         return MCRCrypt.crypt(salt, text);
     }
 
-    private static String getHash(int iterations, byte[] salt, String text, String algorithm)
-        throws NoSuchAlgorithmException {
+    private static String getHash(int iterations, byte[] salt, String text, String algorithm) throws NoSuchAlgorithmException {
         MessageDigest digest;
         if (--iterations < 0) {
             iterations = 0;
@@ -970,8 +969,7 @@ public class MCRUtils {
      */
     public static List<MCRFile> getFiles(String derivate) {
         List<MCRFile> fList = new Vector<MCRFile>();
-        if (derivate == null || derivate.length() == 0
-            || !(MCRMetadataManager.exists(MCRObjectID.getInstance(derivate)))) {
+        if (derivate == null || derivate.length() == 0 || !(MCRMetadataManager.exists(MCRObjectID.getInstance(derivate)))) {
             return fList;
         }
 
@@ -1056,8 +1054,7 @@ public class MCRUtils {
             }
         }
         @SuppressWarnings("unchecked")
-        Constructor<? extends Exception>[] constructors = (Constructor<? extends Exception>[]) mainExceptionClass
-            .getConstructors();
+        Constructor<? extends Exception>[] constructors = (Constructor<? extends Exception>[]) mainExceptionClass.getConstructors();
         for (Constructor<? extends Exception> c : constructors) {
             Class<?>[] parameterTypes = c.getParameterTypes();
             try {
@@ -1069,8 +1066,7 @@ public class MCRUtils {
                 if (parameterTypes.length == 1 && parameterTypes[0].isAssignableFrom(mainExceptionClass)) {
                     return c.newInstance(e);
                 }
-            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-                | InvocationTargetException ex) {
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
                 LOGGER.warn("Exception while initializing exception " + mainExceptionClass.getCanonicalName(), ex);
                 return e;
             }
