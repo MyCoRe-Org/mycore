4a39f13369ee506f1f66a69caea472acb4013eee
Thomas Scheffler <github@yagee.de>
2014-01-29 18:25:00 +0000

    delegate to org.mycore.common.config.MCRConfiguration
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@28976 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/common/MCRConfiguration.java
+++ mycore-base/src/main/java/org/mycore/common/MCRConfiguration.java
@@ -24,42 +24,29 @@
 package org.mycore.common;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import org.mycore.common.config.MCRConfigurationException;
 
 /**
+ * Forwards every method to {@link org.mycore.common.config.MCRConfiguration}
  * @author Thomas Scheffler (yagee)
  * @deprecated use {@link org.mycore.common.config.MCRConfiguration} instead
  */
 @Deprecated
-public class MCRConfiguration extends org.mycore.common.config.MCRConfiguration {
+public class MCRConfiguration {
 
-    private static MCRConfiguration singleton;
+    private static MCRConfiguration singleton = new MCRConfiguration();
 
-    static {
-        createSingleton();
-    }
+    org.mycore.common.config.MCRConfiguration backend;
 
-    protected MCRConfiguration() throws IOException {
-        super();
-    }
-
-    public static MCRConfiguration instance() {
-        return singleton;
-    }
-
-    /**
-     * Instantiates the singleton by calling the protected constructor.
-     */
-    protected static void createSingleton() {
-        try {
-            singleton = new MCRConfiguration();
-        } catch (IOException e) {
-            throw new MCRConfigurationException("Could not instantiate MCRConfiguration.", e);
-        }
-        singleton.systemModified();
+    private MCRConfiguration() {
+        backend = org.mycore.common.config.MCRConfiguration.instance();
     }
 
     public Properties getProperties() {
@@ -74,5 +61,167 @@ public class MCRConfiguration extends org.mycore.common.config.MCRConfiguration 
 
     public Properties getProperties(String startsWith) {
         return toProperties(getPropertiesMap(startsWith));
+    }
+
+    /**
+     * Returns the single instance of this class that can be used to read and
+     * manage the configuration properties.
+     * 
+     * @return the single instance of <CODE>MCRConfiguration</CODE> to be used
+     */
+    public static MCRConfiguration instance() {
+        return singleton;
+    }
+
+    public final long getSystemLastModified() {
+        return backend.getSystemLastModified();
+    }
+
+    public final void systemModified() {
+        backend.systemModified();
+    }
+
+    public Map<String, String> getPropertiesMap() {
+        return backend.getPropertiesMap();
+    }
+
+    public Map<String, String> getPropertiesMap(String startsWith) {
+        return backend.getPropertiesMap(startsWith);
+    }
+
+    public void configureLogging() {
+        backend.configureLogging();
+    }
+
+    public <T> T getInstanceOf(String name, String defaultname, Class<T> type) {
+        return backend.getInstanceOf(name, defaultname, type);
+    }
+
+    public <T> T getInstanceOf(String name, String defaultname) throws MCRConfigurationException {
+        return backend.getInstanceOf(name, defaultname);
+    }
+
+    public <T> T getInstanceOf(String name, Class<T> type) throws MCRConfigurationException {
+        return backend.getInstanceOf(name, type);
+    }
+
+    public <T> T getInstanceOf(String name) throws MCRConfigurationException {
+        return backend.getInstanceOf(name);
+    }
+
+    public <T> T getSingleInstanceOf(String name, String defaultname) throws MCRConfigurationException {
+        return backend.getSingleInstanceOf(name, defaultname);
+    }
+
+    public <T> T getSingleInstanceOf(String name, String defaultname, Class<T> type) throws MCRConfigurationException {
+        return backend.getSingleInstanceOf(name, defaultname, type);
+    }
+
+    public <T> T getSingleInstanceOf(String name) {
+        return backend.getSingleInstanceOf(name);
+    }
+
+    public <T> T getSingleInstanceOf(String name, Class<T> type) {
+        return backend.getSingleInstanceOf(name, type);
+    }
+
+    public String getString(String name) {
+        return backend.getString(name);
+    }
+
+    public List<String> getStrings(String name) {
+        return backend.getStrings(name);
+    }
+
+    public List<String> getStrings(String name, List<String> defaultValue) {
+        return backend.getStrings(name, defaultValue);
+    }
+
+    public String getString(String name, String defaultValue) {
+        return backend.getString(name, defaultValue);
+    }
+
+    public int getInt(String name) throws NumberFormatException {
+        return backend.getInt(name);
+    }
+
+    public int getInt(String name, int defaultValue) throws NumberFormatException {
+        return backend.getInt(name, defaultValue);
+    }
+
+    public long getLong(String name) throws NumberFormatException {
+        return backend.getLong(name);
+    }
+
+    public long getLong(String name, long defaultValue) throws NumberFormatException {
+        return backend.getLong(name, defaultValue);
+    }
+
+    public float getFloat(String name) throws NumberFormatException {
+        return backend.getFloat(name);
+    }
+
+    public float getFloat(String name, float defaultValue) throws NumberFormatException {
+        return backend.getFloat(name, defaultValue);
+    }
+
+    public double getDouble(String name) throws NumberFormatException {
+        return backend.getDouble(name);
+    }
+
+    public double getDouble(String name, double defaultValue) throws NumberFormatException {
+        return backend.getDouble(name, defaultValue);
+    }
+
+    public boolean getBoolean(String name) {
+        return backend.getBoolean(name);
+    }
+
+    public boolean getBoolean(String name, boolean defaultValue) {
+        return backend.getBoolean(name, defaultValue);
+    }
+
+    public void set(String name, String value) {
+        backend.set(name, value);
+    }
+
+    public void initialize(Map<String, String> props, boolean clear) {
+        backend.initialize(props, clear);
+    }
+
+    public void set(String name, int value) {
+        backend.set(name, value);
+    }
+
+    public void set(String name, long value) {
+        backend.set(name, value);
+    }
+
+    public void set(String name, float value) {
+        backend.set(name, value);
+    }
+
+    public void set(String name, double value) {
+        backend.set(name, value);
+    }
+
+    public void set(String name, boolean value) {
+        backend.set(name, value);
+    }
+
+    public void list(PrintStream out) {
+        backend.list(out);
+    }
+
+    public void list(PrintWriter out) {
+        backend.list(out);
+    }
+
+    public void store(OutputStream out, String header) throws IOException {
+        backend.store(out, header);
+    }
+
+    public String toString() {
+        return backend.toString();
     }
 }
