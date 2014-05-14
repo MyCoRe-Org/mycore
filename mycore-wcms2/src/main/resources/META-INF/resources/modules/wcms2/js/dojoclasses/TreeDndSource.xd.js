3a926e5939cfbb2657da82e37bcbfb8c40b196c0
Kathleen Neumann <neumann@gbv.de>
2014-05-14 10:48:55 +0000

    merged f113a13031b3cbc99cd1a713dc882b3e6711ef5e
     (fix cross domain loading when using https)
    
    git-svn-id: https://server.mycore.de/svn/mycore/branches/mycore-2014.03.x@29814 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-wcms2/src/main/resources/META-INF/resources/modules/wcms2/js/dojoclasses/TreeDndSource.xd.js
@@ -0,0 +1,23 @@
+/**
+ * TODO: delete this class in future wcms releases when upgrading
+ * to a newer dojo version. this is just a hack to handle cross-domain
+ * loading.
+ */
+dojo.provide("dojoclasses.TreeDndSource");
+dojo.require("dijit.tree.dndSource");
+
+dojo.declare("dojoclasses.TreeDndSource", dijit.tree.dndSource, {
+
+	onMouseDown: function(e) {
+		// this is a workaround to fix dnd support in tree
+		// the id is only set if the scrollbar is hit, this allows us
+		// to abort the event and don't do an invalid drag
+		var element = document.elementFromPoint(e.clientX, e.clientY);
+		var id = element.id;
+		if(id != null && id != "") {
+			return;
+		}
+		this.inherited("onMouseDown", arguments);		
+	},
+
+});
