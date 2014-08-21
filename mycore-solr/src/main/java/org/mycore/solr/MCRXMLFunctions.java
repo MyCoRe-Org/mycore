9611a8d830b580e3bd13824d01233752a6c86410
Silvio Hermann <Possommi@users.noreply.github.com>
2014-08-21 13:54:23 +0000

    added MCRXMLFunctions#getNumFound(String q)
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@30522 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mycore-solr/src/main/java/org/mycore/solr/MCRXMLFunctions.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.mycore.solr;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+
+/**
+ * @author shermann
+ *
+ */
+public class MCRXMLFunctions {
+
+    /**
+     * Convenience method for retrieving the result count for a given solr query.
+     * 
+     * @param q the query to execute (in solr syntax)
+     * 
+     * @return the amount of documents matching the given query
+     *  
+     * @throws SolrServerException
+     */
+    public static long getNumFound(String q) throws SolrServerException {
+        if (q == null || q.length() == 0) {
+            throw new IllegalArgumentException("The query string must not be null");
+        }
+        SolrQuery solrQuery = new SolrQuery(q);
+        solrQuery.set("rows", 0);
+        QueryResponse queryResponse;
+        queryResponse = MCRSolrServerFactory.getSolrServer().query(solrQuery);
+        return queryResponse.getResults().getNumFound();
+    }
+}
