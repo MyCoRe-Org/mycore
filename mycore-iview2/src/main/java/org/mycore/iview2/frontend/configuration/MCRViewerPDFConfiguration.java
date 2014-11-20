2cefa0bece89093d9cdcc331564daad878c4728d
Sebastian Hofmann <hofmann.s@uni-jena.de>
2014-11-20 08:28:19 +0000

    Added mycore-viewer param to pdf.worker.js Added text-layer for alto Added search implementation for alto Fixed some performance issues Refactoring...
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@31211 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-iview2/src/main/java/org/mycore/iview2/frontend/configuration/MCRViewerPDFConfiguration.java
+++ mycore-iview2/src/main/java/org/mycore/iview2/frontend/configuration/MCRViewerPDFConfiguration.java
@@ -1,9 +1,10 @@
 package org.mycore.iview2.frontend.configuration;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.mycore.frontend.MCRFrontendUtil;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.iview2.services.MCRIView2Tools;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class MCRViewerPDFConfiguration extends MCRViewerBaseConfiguration {
 
@@ -12,9 +13,10 @@ public class MCRViewerPDFConfiguration extends MCRViewerBaseConfiguration {
         super.setup(request);
 
         String pdfProviderURL = MCRIView2Tools.getIView2Property("pdfProviderURL", MCRServlet.getServletBaseURL() + "MCRFileNodeServlet/{derivate}/{filePath}");
-        
+        String pdfWorkerLocation = MCRFrontendUtil.getBaseURL() + "modules/iview2/js/lib/pdf.min.worker.js";
+
         setProperty("pdfProviderURL", pdfProviderURL);
-        
+        setProperty("pdfWorkerURL", pdfWorkerLocation);
         // script
         addLocalScript("lib/pdf.js", false);
         addLocalScript("iview-client-pdf.js");
