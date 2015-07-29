2832711077c835d9bbd344fcc5a62a6ea528dfef
René Adler <adlerre@users.noreply.github.com>
2015-07-29 08:12:06 +0000

    MCR-1000 #comment save only valid user
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@32956 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-user2/src/main/java/org/mycore/user2/events/MCRPersistTransientUserEventHandler.java
+++ mycore-user2/src/main/java/org/mycore/user2/events/MCRPersistTransientUserEventHandler.java
@@ -46,7 +46,7 @@ public class MCRPersistTransientUserEventHandler extends MCREventHandlerBase {
     @Override
     protected void handleObjectCreated(MCREvent evt, MCRObject obj) {
         MCRUser currentUser = MCRUserManager.getCurrentUser();
-        if (MCRUserManager.getUser(currentUser.getUserID()) == null) {
+        if (!MCRUserManager.isInvalidUser(currentUser) && MCRUserManager.getUser(currentUser.getUserID()) == null) {
             LOGGER.info("create new user \"" + currentUser.getUserID() + "\"");
             MCRUserManager.createUser((MCRTransientUser) currentUser);
         }
