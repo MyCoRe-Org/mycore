b83872269f27b5a9ab246763bd306191453e0733
Kathleen Neumann <neumann@gbv.de>
2015-08-11 14:15:07 +0000

    MCR-1018 #comment merged aaf1f97f248cf3f6d94d7e8e640068bfd082ec4a
     to 2014.06 and to mycore-base/editor XSL postprocessing
    
    git-svn-id: https://server.mycore.de/svn/mycore/branches/mycore-2014.06.0.x@33147 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-xeditor/src/main/java/org/mycore/frontend/xeditor/MCRXEditorPostProcessor.java
+++ mycore-xeditor/src/main/java/org/mycore/frontend/xeditor/MCRXEditorPostProcessor.java
@@ -1,12 +1,18 @@
 package org.mycore.frontend.xeditor;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.Text;
+import org.jdom2.filter.Filters;
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.MCRJDOMContent;
+import org.mycore.common.content.transformer.MCRContentTransformer;
 import org.mycore.common.content.transformer.MCRXSL2XMLTransformer;
+import org.mycore.common.xml.MCRXMLFunctions;
 import org.xml.sax.SAXException;
 
 public class MCRXEditorPostProcessor {
@@ -23,6 +29,25 @@ public class MCRXEditorPostProcessor {
 
         MCRContent source = new MCRJDOMContent(xml);
         MCRContent transformed = MCRXSL2XMLTransformer.getInstance("xsl/" + stylesheet).transform(source);
-        return transformed.asXML();
+        MCRContent normalized = new MCRNormalizeUnicodeTransformer().transform(transformed);
+        return normalized.asXML();
+    }
+}
+
+class MCRNormalizeUnicodeTransformer extends MCRContentTransformer {
+
+    public MCRJDOMContent transform(MCRContent source) throws IOException {
+        try {
+            Element root = source.asXML().getRootElement().clone();
+            for (Iterator<Text> iter = root.getDescendants(Filters.text()).iterator(); iter.hasNext();) {
+                Text text = iter.next();
+                text.setText(MCRXMLFunctions.normalizeUnicode(text.getText()));
+            }
+            return new MCRJDOMContent(root);
+        } catch (JDOMException ex) {
+            throw new IOException(ex);
+        } catch (SAXException ex) {
+            throw new IOException(ex);
+        }
     }
 }
