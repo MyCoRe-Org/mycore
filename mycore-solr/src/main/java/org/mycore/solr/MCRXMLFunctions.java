663ef1051cc4f0aca58cfa686dad1a45b8b98364
Silvio Hermann <Possommi@users.noreply.github.com>
2015-08-18 09:40:52 +0000

    added getIdentifierOfFirst(String q)
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@33246 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-solr/src/main/java/org/mycore/solr/MCRXMLFunctions.java
+++ mycore-solr/src/main/java/org/mycore/solr/MCRXMLFunctions.java
@@ -32,4 +32,26 @@ public class MCRXMLFunctions {
         queryResponse = MCRSolrClientFactory.getSolrClient().query(solrQuery);
         return queryResponse.getResults().getNumFound();
     }
+
+    /**
+     * @param q the query to execute (in solr syntax)
+     * @return the identifier of the first document matching the query
+     * @throws SolrServerException
+     * @throws IOException
+     */
+    public static String getIdentifierOfFirst(String q) throws SolrServerException, IOException {
+        if (q == null || q.length() == 0) {
+            throw new IllegalArgumentException("The query string must not be null");
+        }
+        SolrQuery solrQuery = new SolrQuery(q);
+        solrQuery.set("rows", 1);
+        QueryResponse queryResponse;
+        queryResponse = MCRSolrClientFactory.getSolrClient().query(solrQuery);
+
+        if (queryResponse.getResults().getNumFound() == 0) {
+            return null;
+        }
+
+        return queryResponse.getResults().get(0).get("id").toString();
+    }
 }
