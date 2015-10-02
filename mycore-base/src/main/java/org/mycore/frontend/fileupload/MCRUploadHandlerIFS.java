16118287cb248fa736ae0f425d337567edb3c5a0
Kathleen Neumann <neumann@gbv.de>
2015-10-02 09:46:59 +0000

    MCR-1071 #comment use projectID of containing documentID for derivateID
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@33601 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/frontend/fileupload/MCRUploadHandlerIFS.java
+++ mycore-base/src/main/java/org/mycore/frontend/fileupload/MCRUploadHandlerIFS.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * $Revision$ $Date$
  *
  * This file is part of ***  M y C o R e  ***
@@ -51,7 +51,7 @@ import org.mycore.datamodel.niofs.MCRPath;
 
 /**
  * handles uploads via the UploadApplet and store files directly into the IFS.
- * 
+ *
  * @author Thomas Scheffler (yagee)
  * @author Frank L\u00FCtzenkirchen
  * @version $Revision$ $Date$
@@ -64,8 +64,6 @@ public class MCRUploadHandlerIFS extends MCRUploadHandler {
     private static final MCRConfiguration CONFIG = MCRConfiguration.instance();
 
     private static final String ID_TYPE = "derivate";
-
-    private static final String ID_PROJECT = MCRConfiguration.instance().getString("MCR.SWF.Project.ID", "MCR");
 
     protected String documentID;
 
@@ -100,7 +98,8 @@ public class MCRUploadHandlerIFS extends MCRUploadHandler {
 
     private MCRObjectID getOrCreateDerivateID() {
         if (derivateID == null) {
-            MCRObjectID oid = MCRObjectID.getNextFreeId(ID_PROJECT + '_' + ID_TYPE);
+            String projectID = MCRObjectID.getInstance(this.documentID).getProjectId();
+            MCRObjectID oid = MCRObjectID.getNextFreeId(projectID + '_' + ID_TYPE);
             this.derivateID = oid.toString();
             return oid;
         } else
