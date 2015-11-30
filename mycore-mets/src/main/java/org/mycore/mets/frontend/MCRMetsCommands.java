c1953abbf218f92e0cd025083f1bd9dcdf1bc2d9
Sebastian Hofmann <hofmann.s@uni-jena.de>
2015-11-30 09:44:00 +0000

    MCR-1128 #comment added command try fix invalid mets. First you need to run "validate selected mets" and if a validation error occurs, than the mets will be noticed. Now run "try fix invalid mets".
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@34096 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-mets/src/main/java/org/mycore/mets/frontend/MCRMetsCommands.java
+++ mycore-mets/src/main/java/org/mycore/mets/frontend/MCRMetsCommands.java
@@ -2,13 +2,17 @@ package org.mycore.mets.frontend;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.MCRPathContent;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
@@ -19,6 +23,9 @@ import org.mycore.frontend.cli.MCRObjectCommands;
 import org.mycore.frontend.cli.annotation.MCRCommand;
 import org.mycore.frontend.cli.annotation.MCRCommandGroup;
 import org.mycore.mets.model.MCRMETSGenerator;
+import org.mycore.mets.model.converter.MCRSimpleModelXMLConverter;
+import org.mycore.mets.model.converter.MCRXMLSimpleModelConverter;
+import org.mycore.mets.model.simple.MCRMetsSimpleModel;
 import org.mycore.mets.tools.MCRMetsSave;
 import org.mycore.mets.validator.METSValidator;
 import org.mycore.mets.validator.validators.ValidationException;
@@ -28,7 +35,9 @@ public class MCRMetsCommands extends MCRAbstractCommands {
 
     private static final Logger LOGGER = Logger.getLogger(MCRMetsCommands.class);
 
-    @MCRCommand(syntax = "validate selected mets", help = "validates all mets.xml of selected derivates", order = 10)
+	public static ConcurrentLinkedQueue<String> invalidMetsQueue = new ConcurrentLinkedQueue<>();
+
+	@MCRCommand(syntax = "validate selected mets", help = "validates all mets.xml of selected derivates", order = 10)
     public static void validateSelectedMets() {
         List<String> selectedObjectIDs = MCRObjectCommands.getSelectedObjectIDs();
 
@@ -41,7 +50,10 @@ public class MCRMetsCommands extends MCRAbstractCommands {
                     InputStream metsIS = content.getInputStream();
                     METSValidator mv = new METSValidator(metsIS);
                     List<ValidationException> validationExceptionList = mv.validate();
-                    for (ValidationException validationException : validationExceptionList) {
+					if (validationExceptionList.size() > 0) {
+						invalidMetsQueue.add(objectID);
+					}
+					for (ValidationException validationException : validationExceptionList) {
                         LOGGER.error(validationException.getMessage());
                     }
                 } catch (IOException e) {
@@ -53,6 +65,38 @@ public class MCRMetsCommands extends MCRAbstractCommands {
 
         }
     }
+
+	@MCRCommand(syntax = "try fix invalid mets", help = "This Command can be used to fix invalid mets files that was found in any validate selected mets runs.", order = 15)
+	public static void fixInvalidMets() {
+		invalidMetsQueue.stream().forEach(selectedObjectID -> {
+			LOGGER.info("Try to fix METS of " + selectedObjectID);
+			MCRPath metsFile = MCRPath.getPath(selectedObjectID, "/mets.xml");
+			SAXBuilder saxBuilder = new SAXBuilder();
+			Document metsDocument;
+			try (InputStream metsInputStream = Files.newInputStream(metsFile)) {
+				metsDocument = saxBuilder.build(metsInputStream);
+			} catch (IOException | JDOMException e) {
+				LOGGER.error("Cannot fix METS of " + selectedObjectID + ". Can not parse mets.xml!", e);
+				return;
+			}
+
+			MCRMetsSimpleModel mcrMetsSimpleModel;
+			try {
+				mcrMetsSimpleModel = MCRXMLSimpleModelConverter.fromXML(metsDocument);
+			} catch (Exception e) {
+				LOGGER.error("Cannot fix METS of " + selectedObjectID + ". Can not convert to SimpleModel!", e);
+				return;
+			}
+
+			Document newMets = MCRSimpleModelXMLConverter.toXML(mcrMetsSimpleModel);
+			XMLOutputter xmlOutputter = new XMLOutputter();
+			try (OutputStream os = Files.newOutputStream(metsFile)) {
+				xmlOutputter.output(newMets, os);
+			} catch (IOException e) {
+				LOGGER.error("Cannot fix METS of " + selectedObjectID + ". Can not write mets to derivate.");
+			}
+		});
+	}
 
 	@MCRCommand(syntax = "add mets files for derivate {0}", help = "", order = 20)
 	public static void addMetsFileForDerivate(String derivateID) {
