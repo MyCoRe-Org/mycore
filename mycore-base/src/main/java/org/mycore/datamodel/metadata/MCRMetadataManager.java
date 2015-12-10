7fdb10482007521c2ec91eec37addbdb5fba050c
Robert Stephan <rsteph-de@users.noreply.github.com>
2015-12-10 21:52:49 +0000

    MCR-1134 #comment handle xlink:label attribute and check if xlink:title attribute exists
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@34212 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetadataManager.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetadataManager.java
@@ -687,9 +687,15 @@ public final class MCRMetadataManager {
         for (MCRMetaLinkID derID : mcrObject.getStructure().getDerivates()) {
             derOrder.add(derID.getXLinkHref());
         }
-        HashMap<String, String> newlinkIDs = new HashMap<>();
+        HashMap<String, String> newlinkTitles = new HashMap<String, String>();
+        HashMap<String, String> newlinkLabels = new HashMap<String, String>();
         for (MCRMetaLinkID newlinkID : mcrObject.getStructure().getDerivates()) {
-            newlinkIDs.put(newlinkID.getXLinkHref(), newlinkID.getXLinkTitle());
+            if(newlinkID.getXLinkTitle()!=null){
+                newlinkTitles.put(newlinkID.getXLinkHref(), newlinkID.getXLinkTitle());
+            }
+            if(newlinkID.getXLinkLabel()!=null){
+                newlinkLabels.put(newlinkID.getXLinkHref(), newlinkID.getXLinkLabel());
+            }
         }
         mcrObject.getStructure().clearDerivates();
 
@@ -697,8 +703,11 @@ public final class MCRMetadataManager {
         List <MCRMetaLinkID> linkIDs = mcrObject.getStructure().getDerivates();
         List <MCRMetaLinkID> oldlinkIDs = old.getStructure().getDerivates();
         for (MCRMetaLinkID oldlinkID : oldlinkIDs) {
-            if (newlinkIDs.containsKey(oldlinkID.getXLinkHref())) {
-                oldlinkID.setXLinkTitle(newlinkIDs.get(oldlinkID.getXLinkHref()));
+            if (newlinkTitles.containsKey(oldlinkID.getXLinkHref())) {
+                oldlinkID.setXLinkTitle(newlinkTitles.get(oldlinkID.getXLinkHref()));
+            }
+            if (newlinkLabels.containsKey(oldlinkID.getXLinkHref())) {
+                oldlinkID.setXLinkLabel(newlinkLabels.get(oldlinkID.getXLinkHref()));
             }
             linkIDs.add(oldlinkID);
         }
