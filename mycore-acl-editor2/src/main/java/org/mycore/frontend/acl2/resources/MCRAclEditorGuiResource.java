54e89f9e324b744a902782b19712cd0bf4620c41
Michel Büchner <michel.buechner@uni-jena.de>
2016-01-08 14:22:07 +0000

    MCR-1152 #comment fixed wrong Content-Type, now files get sent with proper Content-Type
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@34295 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-acl-editor2/src/main/java/org/mycore/frontend/acl2/resources/MCRAclEditorGuiResource.java
+++ mycore-acl-editor2/src/main/java/org/mycore/frontend/acl2/resources/MCRAclEditorGuiResource.java
@@ -1,16 +1,36 @@
 package org.mycore.frontend.acl2.resources;
 
-import java.io.InputStream;
-
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @Path("ACLE/gui")
 public class MCRAclEditorGuiResource {
+    
+    @Context
+    HttpServletResponse response;
+    
     @GET
     @Path("{filename:.*}")
-    public InputStream getResources(@PathParam("filename") String filename){
-        return getClass().getResourceAsStream("/META-INF/resources/modules/acl-editor2/gui/" + filename);
+    public Response getResources(@PathParam("filename") String filename){
+        if (filename.endsWith(".js")) {
+            return Response.ok(getClass()
+                .getResourceAsStream("/META-INF/resources/modules/acl-editor2/gui/" + filename))
+                .header("Content-Type", "application/javascript")
+                .build();
+        }
+        
+        if (filename.endsWith(".css")) {
+            return Response.ok(getClass()
+                .getResourceAsStream("/META-INF/resources/modules/acl-editor2/gui/" + filename))
+                .header("Content-Type", "text/css")
+                .build();
+        }
+        return Response.ok(getClass()
+            .getResourceAsStream("/META-INF/resources/modules/acl-editor2/gui/" + filename))
+            .build();
     }
 }
