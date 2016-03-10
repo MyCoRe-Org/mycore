46b0b9c83410270bd042c7ba723ce79953005ee7
René Adler <adlerre@users.noreply.github.com>
2016-03-10 10:13:20 +0000

    MCR-1214 #comment add missing tests
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@34787 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/test/java/org/mycore/common/xml/MCRXMLFunctionsTest.java
+++ mycore-base/src/test/java/org/mycore/common/xml/MCRXMLFunctionsTest.java
@@ -15,7 +15,8 @@ public class MCRXMLFunctionsTest extends MCRTestCase {
     private final static String[] HTML_STRINGS = new String[] { "<h1>Hello World!</h1>",
             "<h1>Hell<i>o</i> World!<br /></h1>", "<h1>Hell<i>o</i> World!<br></h1>",
             "<h1>Hell<i>o</i> World!&lt;br&gt;</h1>", "<h1>Hell<i>&ouml;</i> World!&lt;br&gt;</h1>",
-            "<h1>Hello</h1> <h2>World!</h2><br/>" };
+            "<h1>Hello</h1> <h2>World!</h2><br/>", "Hello <a href=\"http://www.mycore.de\">MyCoRe</a>!",
+            "Hello <a href='http://www.mycore.de'>MyCoRe</a>!" };
 
     /*
      * Test method for 'org.mycore.common.xml.MCRXMLFunctions.formatISODate(String, String, String, String)'
