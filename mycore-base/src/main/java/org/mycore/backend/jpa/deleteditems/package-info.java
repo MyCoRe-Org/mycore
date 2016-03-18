a604197717af65376bc12cbf5c39f26dc51c93e9
Kathleen Neumann <neumann@gbv.de>
2016-03-18 08:07:04 +0000

    fixed javadoc
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@34880 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/backend/jpa/deleteditems/package-info.java
+++ mycore-base/src/main/java/org/mycore/backend/jpa/deleteditems/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Classes required for persistence of {@link org.mycore.backend.hibernate.MCRDeletedItemManager}.
+ * Classes required for persistence of {@link org.mycore.backend.jpa.deleteditems.MCRDeletedItemManager}.
  * @author Thomas Scheffler (yagee)
  */
 package org.mycore.backend.jpa.deleteditems;
