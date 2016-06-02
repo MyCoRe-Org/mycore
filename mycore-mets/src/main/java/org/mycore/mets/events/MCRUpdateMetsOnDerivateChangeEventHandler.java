d379d8dce9949924c7206d75fe6ea79fb962e01f
Matthias Eichner <Mewel@users.noreply.github.com>
2016-06-02 16:45:55 +0000

    MCR-1297 #comment use MCRMarkManager on mets.xml UpdateHandler -> don't update mets.xml if the derivate or the corresponding mycore object is marked for deletion
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@35553 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-mets/src/main/java/org/mycore/mets/events/MCRUpdateMetsOnDerivateChangeEventHandler.java
+++ mycore-mets/src/main/java/org/mycore/mets/events/MCRUpdateMetsOnDerivateChangeEventHandler.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.mycore.common.events.MCREvent;
 import org.mycore.common.events.MCREventHandlerBase;
+import org.mycore.datamodel.common.MCRMarkManager;
 import org.mycore.datamodel.metadata.MCRDerivate;
+import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.niofs.MCRPath;
 import org.mycore.mets.tools.MCRMetsSave;
 
@@ -23,23 +26,62 @@ public class MCRUpdateMetsOnDerivateChangeEventHandler extends MCREventHandlerBa
 
     private String mets = MCRMetsSave.getMetsFileName();
 
+    /**
+     * Checks if the mets.xml should be updated.
+     * 
+     * @param evt the mcr event
+     * @param file the file which was changed
+     * @param attrs the file attributes
+     * @return true if the mets shoud be updated, otherwise false
+     */
+    protected boolean checkUpdateMets(MCREvent evt, Path file, BasicFileAttributes attrs) {
+        // don't update if no MCRPath
+        if (!(file instanceof MCRPath)) {
+            return false;
+        }
+        // don't update if mets.xml is deleted
+        Path fileName = file.getFileName();
+        if (fileName != null && fileName.toString().equals(mets)) {
+            return false;
+        }
+        MCRPath mcrPath = MCRPath.toMCRPath(file);
+        String derivateId = mcrPath.getOwner();
+        // don't update if mets.xml does not exist
+        if (Files.notExists(MCRPath.getPath(derivateId, '/' + mets))) {
+            return false;
+        }
+        // don't update if derivate or mycore object is marked for deletion
+        MCRObjectID mcrDerivateId = MCRObjectID.getInstance(derivateId);
+        if (isMarkedForDeletion(MCRMetadataManager.retrieveMCRDerivate(mcrDerivateId))) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the derivate or the corresponding mycore object is
+     * marked for deletion.
+     * 
+     * @return true if one of them is marked for deletion
+     */
+    protected boolean isMarkedForDeletion(MCRDerivate derivate) {
+        MCRMarkManager markManager = MCRMarkManager.instance();
+        if (markManager.isMarkedForDeletion(derivate.getId()) ||
+            markManager.isMarkedForDeletion(derivate.getOwnerID())) {
+            return true;
+        }
+        return false;
+    }
+
     /* (non-Javadoc)
      * @see org.mycore.common.events.MCREventHandlerBase#handleFileDeleted(org.mycore.common.events.MCREvent, org.mycore.datamodel.ifs.MCRFile)
      */
     @Override
     protected void handlePathDeleted(MCREvent evt, Path file, BasicFileAttributes attrs) {
-        if (!(file instanceof MCRPath)) {
-            return;
-        }
-        // do nothing if mets.xml itself is deleted
-        Path fileName = file.getFileName();
-        if (fileName != null && fileName.toString().equals(mets)) {
+        if (!checkUpdateMets(evt, file, attrs)) {
             return;
         }
         MCRPath mcrPath = MCRPath.toMCRPath(file);
-        if (Files.notExists(MCRPath.getPath(mcrPath.getOwner(), '/' + mets))) {
-            return;
-        }
         try {
             MCRMetsSave.updateMetsOnFileDelete(mcrPath);
         } catch (Exception e) {
@@ -49,18 +91,10 @@ public class MCRUpdateMetsOnDerivateChangeEventHandler extends MCREventHandlerBa
 
     @Override
     protected void handlePathCreated(MCREvent evt, Path file, BasicFileAttributes attrs) {
-        if (!(file instanceof MCRPath)) {
-            return;
-        }
-        // do nothing if mets.xml itself is deleted
-        if (file.getFileName().toString().equals(mets)) {
+        if (!checkUpdateMets(evt, file, attrs)) {
             return;
         }
         MCRPath mcrPath = MCRPath.toMCRPath(file);
-        if (Files.notExists(MCRPath.getPath(mcrPath.getOwner(), '/' + mets))) {
-            return;
-        }
-
         try {
             MCRMetsSave.updateMetsOnFileAdd(mcrPath);
         } catch (Exception e) {
@@ -70,6 +104,9 @@ public class MCRUpdateMetsOnDerivateChangeEventHandler extends MCREventHandlerBa
 
     @Override
     protected void handleDerivateUpdated(MCREvent evt, MCRDerivate der) {
+        if (isMarkedForDeletion(der)) {
+            return;
+        }
         try {
             Map<String, String> urnFileMap = der.getUrnMap();
             if (urnFileMap.size() > 0) {
