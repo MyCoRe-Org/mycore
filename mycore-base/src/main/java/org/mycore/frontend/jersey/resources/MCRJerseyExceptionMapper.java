59455646d9b3e3c043d823b630d20087939f3e3b
Kathleen Neumann <neumann@gbv.de>
2016-06-27 08:29:32 +0000

    MCR-1263 #comment merged 9aec77e0d3fb82c858bc1e0a435fc51ed82194c2
     to LTS2015.05
    
    git-svn-id: https://server.mycore.de/svn/mycore/branches/mycore-2015.05.0.x@35730 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-base/src/main/java/org/mycore/frontend/jersey/resources/MCRJerseyExceptionMapper.java
+++ mycore-base/src/main/java/org/mycore/frontend/jersey/resources/MCRJerseyExceptionMapper.java
@@ -1,5 +1,11 @@
 package org.mycore.frontend.jersey.resources;
 
+import org.jdom2.Document;
+import org.mycore.common.MCRSessionMgr;
+import org.mycore.common.content.MCRContent;
+import org.mycore.frontend.jersey.MCRJerseyUtil;
+import org.mycore.frontend.servlets.MCRErrorServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
@@ -10,21 +16,15 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.jdom2.Document;
-import org.mycore.common.MCRSessionMgr;
-import org.mycore.common.content.MCRContent;
-import org.mycore.frontend.jersey.MCRJerseyUtil;
-import org.mycore.frontend.servlets.MCRErrorServlet;
-
 /**
  * Handles jersey web application exceptions. By default this class
  * just forwards the exception. Only if the request accepts HTML a
  * custom error page is generated.
- * 
+ *
  * @author Matthias Eichner
  */
 @Provider
-public class MCRJerseyExceptionMapper implements ExceptionMapper<WebApplicationException> {
+public class MCRJerseyExceptionMapper implements ExceptionMapper<Exception> {
 
     @Context
     HttpHeaders headers;
@@ -33,27 +33,36 @@ public class MCRJerseyExceptionMapper implements ExceptionMapper<WebApplicationE
     HttpServletRequest request;
 
     @Override
-    public Response toResponse(WebApplicationException exc) {
+    public Response toResponse(Exception exc) {
+        Response response = getResponse(exc);
         if (headers.getAcceptableMediaTypes().contains(MediaType.TEXT_HTML_TYPE)) {
             // try to return a html error page
-            if(!MCRSessionMgr.getCurrentSession().isTransactionActive()) {
+            if (!MCRSessionMgr.getCurrentSession().isTransactionActive()) {
                 MCRSessionMgr.getCurrentSession().beginTransaction();
             }
             try {
-                int status = exc.getResponse().getStatus();
+                int status = response.getStatus();
                 String source = exc.getStackTrace().length > 0 ? exc.getStackTrace()[0].toString() : null;
                 Document errorPageDocument = MCRErrorServlet.buildErrorPage(exc.getMessage(), status,
-                    request.getRequestURI(), null, source, exc);
+                                                                            request.getRequestURI(), null, source, exc);
                 MCRContent result = MCRJerseyUtil.transform(errorPageDocument, request);
                 return Response.serverError().entity(result.getInputStream())
-                    .type(MediaType.valueOf(result.getMimeType())).status(status).build();
+                               .type(MediaType.valueOf(result.getMimeType())).status(status).build();
             } catch (Exception transformException) {
                 return Response.status(Status.INTERNAL_SERVER_ERROR).entity(transformException).build();
             } finally {
                 MCRSessionMgr.getCurrentSession().commitTransaction();
             }
         }
-        return exc.getResponse();
+        return response;
+    }
+
+    private Response getResponse(Exception exc) {
+        if (exc instanceof WebApplicationException) {
+            return ((WebApplicationException) exc).getResponse();
+        }
+
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(exc).build();
     }
 
 }
