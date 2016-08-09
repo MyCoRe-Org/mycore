0f072cc09ccbd06f6cec0727c82b234e4a263a26
René Adler <adlerre@users.noreply.github.com>
2016-08-09 08:23:41 +0000

    remove unused imports
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@36094 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-media/src/main/java/org/mycore/media/video/MCRMediaSourceProvider.java
+++ mycore-media/src/main/java/org/mycore/media/video/MCRMediaSourceProvider.java
@@ -12,7 +12,6 @@ import java.util.function.Supplier;
 import org.apache.logging.log4j.LogManager;
 import org.mycore.common.MCRSessionMgr;
 import org.mycore.common.config.MCRConfiguration2;
-import org.mycore.common.xml.MCRXMLFunctions;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.niofs.MCRAbstractFileStore;
 import org.mycore.datamodel.niofs.MCRPath;
