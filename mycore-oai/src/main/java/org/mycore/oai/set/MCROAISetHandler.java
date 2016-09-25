a4ab644fc42aec3364dad24ac82c9faae55d3c42
Kathleen Neumann <neumann@gbv.de>
2016-09-25 20:14:54 +0000

    try to fix javadoc error, seems to have problems with parameter <T>
    git-svn-id: https://server.mycore.de/svn/mycore/trunk@36330 7089d063-bc14-496d-ac90-53de1872eb6c

--- mycore-oai/src/main/java/org/mycore/oai/set/MCROAISetHandler.java
+++ mycore-oai/src/main/java/org/mycore/oai/set/MCROAISetHandler.java
@@ -4,10 +4,10 @@ import org.mycore.oai.pmh.Set;
 
 /**
  * Base interface to handle mycore oai sets.
- * 
+ *
  * @author Matthias Eichner
  *
- * @param <T> 
+ * @param <T>
  */
 public interface MCROAISetHandler<T> {
 
@@ -15,16 +15,16 @@ public interface MCROAISetHandler<T> {
      * Initializes the set handler with the configPrefix
      * (MCR.OAIDataProvider.MY_PROVIDER) and a setId
      * (MCR.OAIDataProvider.MY_PROVIDER.Sets.SET_ID).
-     * 
+     *
      * @param configPrefix the config prefix
      * @param setId the set id without any prefix
      */
     public void init(String configPrefix, String setId);
 
     /**
-     * Called before {@link #apply(Set, T)} to check if the
+     * Called before {@link #apply(Set, Object)} to check if the
      * given set should be added to the ListSets view.
-     * 
+     *
      * @return false if the given set should be added (the
      *           set is not filtered)
      */
