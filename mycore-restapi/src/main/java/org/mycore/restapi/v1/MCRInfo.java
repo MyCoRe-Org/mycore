4c8b0b70cc0b11f0176a56ff06c47bb07b0dca8d
Thomas Scheffler <github@yagee.de>
2016-12-06 12:29:49 +0100

    MCR-1465 Git Improvements for MCRCoreVersion
    Use git-commit-id-plugin to get some Git properties useful for MCRCoreVersion.
    This commit changes output of main() and return value of getCompleteVersion()
    
    Also added "/api/v1/mycore/version" resource to get all git properties.

---
+++ mycore-restapi/src/main/java/org/mycore/restapi/v1/MCRInfo.java
@@ -0,0 +1,23 @@
+package org.mycore.restapi.v1;
+
+import java.util.Properties;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.mycore.common.MCRCoreVersion;
+
+@Path("/v1/mycore")
+public class MCRInfo {
+
+    @GET
+    @Path("version")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Properties getGitInfos(){
+        Properties properties = new Properties();
+        properties.putAll(MCRCoreVersion.getVersionProperties());
+        return properties;
+    }
+}
