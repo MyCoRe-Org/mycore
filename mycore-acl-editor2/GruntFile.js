6e15bd8feec06d6bf34f499cc203a2b81e6de328
Kathleen Neumann <neumann@gbv.de>
2017-03-03 11:55:23 +0100

    Strings must use doublequote

--- mycore-acl-editor2/GruntFile.js
+++ mycore-acl-editor2/GruntFile.js
@@ -1,27 +1,27 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-maven');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks("grunt-maven");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
     grunt
         .initConfig({
-            pkg: grunt.file.readJSON('package.json'),
+            pkg: grunt.file.readJSON("package.json"),
             copy: {
                 main: {
                     files: [
                         {
                             expand: true,
-                            cwd: './bower_components',
-                            dest: './target/classes/META-INF/resources/modules/acl-editor2/gui/bower_components',
+                            cwd: "./bower_components",
+                            dest: "./target/classes/META-INF/resources/modules/acl-editor2/gui/bower_components",
                             flatten: true,
                             src: [
-                                './jquery-stupid-table/stupidtable.min.js',
-                                './bootstrap3-typeahead/bootstrap3-typeahead.min.js'
+                                "./jquery-stupid-table/stupidtable.min.js",
+                                "./bootstrap3-typeahead/bootstrap3-typeahead.min.js"
                             ]
                         },
                         {
                             expand: true,
-                            cwd: './bower_components/select2',
-                            dest: './target/classes/META-INF/resources/modules/acl-editor2/gui/bower_components/select2',
+                            cwd: "./bower_components/select2",
+                            dest: "./target/classes/META-INF/resources/modules/acl-editor2/gui/bower_components/select2",
                             src: [
                                 '**'
                             ]
@@ -31,6 +31,6 @@ module.exports = function (grunt) {
             }
         });
 
-    grunt.registerTask('default', ['copy']);
+    grunt.registerTask("default", ["copy"]);
 
 };
