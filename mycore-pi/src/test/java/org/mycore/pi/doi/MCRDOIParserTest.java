cbb13086cba3a3b1c33a3910aa54c0c074e5c6b1
Sebastian Hofmann <hofmann.s@uni-jena.de>
2017-03-17 11:05:19 +0100

    MCR-1562 doi-registrant-codes added test for registrant dois

--- mycore-pi/src/test/java/org/mycore/pi/doi/MCRDOIParserTest.java
+++ mycore-pi/src/test/java/org/mycore/pi/doi/MCRDOIParserTest.java
@@ -9,11 +9,17 @@ import org.mycore.pi.MCRPersistentIdentifier;
 
 public class MCRDOIParserTest {
 
+    public static final String EXAMPLE_DOI2_PREFIX = "10.22032.0";
+
+    public static final String EXAMPLE_DOI2_SUFFIX = "hj34";
+
     private static final String EXAMPLE_DOI1_PREFIX = "10.1000";
 
     private static final String EXAMPLE_DOI1_SUFFIX = "123456";
 
     private static final String EXAMPLE_DOI1 = EXAMPLE_DOI1_PREFIX + "/" + EXAMPLE_DOI1_SUFFIX;
+
+    private static final String EXAMPLE_DOI2 = EXAMPLE_DOI2_PREFIX + "/" + EXAMPLE_DOI2_SUFFIX;
 
     private MCRDOIParser parser;
 
@@ -24,18 +30,30 @@ public class MCRDOIParserTest {
 
     @Test
     public void parseRegularDOITest() {
-        Optional<MCRPersistentIdentifier> parsedDOIOptional = parser.parse(EXAMPLE_DOI1);
+        testDOI(EXAMPLE_DOI1, EXAMPLE_DOI1_PREFIX, EXAMPLE_DOI1_SUFFIX);
+
+    }
+
+    private void testDOI(String doi, String expectedPrefix, String expectedSuffix) {
+        Optional<MCRPersistentIdentifier> parsedDOIOptional = parser.parse(doi);
 
         Assert.assertTrue("DOI should be parsable!", parsedDOIOptional.isPresent());
 
         MCRDigitalObjectIdentifier parsedDOI = (MCRDigitalObjectIdentifier) parsedDOIOptional.get();
 
-        Assert.assertEquals("DOI Prefix should match!", EXAMPLE_DOI1_PREFIX,
+        Assert.assertEquals("DOI Prefix should match!", expectedPrefix,
             parsedDOI.getPrefix());
 
-        Assert.assertEquals("DOU Suffix should match", EXAMPLE_DOI1_SUFFIX,
+        Assert.assertEquals("DOU Suffix should match", expectedSuffix,
             parsedDOI.getSuffix());
+    }
 
+    @Test
+    /**
+     * JUNIT Test for MCR-1562
+     */
+    public void parseRegistrantCodeDOI() {
+        testDOI(EXAMPLE_DOI2, EXAMPLE_DOI2_PREFIX, EXAMPLE_DOI2_SUFFIX);
     }
 
 }
