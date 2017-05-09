2227ec3394e0d583d814c8cd15148903d813f3a8
Mewel <Mewel@users.noreply.github.com>
2017-05-09 20:05:29 +0200

    MCR-1482 improve ALTO text alot (#242)

--- mycore-viewer/src/main/typescript/modules/base/widgets/canvas/PageView.ts
+++ mycore-viewer/src/main/typescript/modules/base/widgets/canvas/PageView.ts
@@ -1,10 +1,19 @@
 namespace mycore.viewer.widgets.canvas {
+
     export class PageView {
 
-        constructor(private _drawImage:boolean = true, private _drawHTML:boolean = true) {
-            jQuery(this._drawCanvas).appendTo(this._container);
-            jQuery(this._markCanvas).appendTo(this._container);
-            this._container.css({
+        public container:JQuery = jQuery("<div></div>");
+        public drawCanvas:HTMLCanvasElement = null;
+        public markCanvas:HTMLCanvasElement = null;
+
+        constructor(public drawImage:boolean = true, public drawHTML:boolean = true) {
+            let drawFilter = drawImage && drawHTML ? "grayscale() contrast(1000%)" : null;
+            this.drawCanvas = PageView.createCanvas(1, drawFilter);
+            this.markCanvas = PageView.createCanvas(4);
+
+            jQuery(this.drawCanvas).appendTo(this.container);
+            jQuery(this.markCanvas).appendTo(this.container);
+            this.container.css({
                 "position": "absolute",
                 "top": "0px",
                 "left": "0px",
@@ -13,9 +22,8 @@ namespace mycore.viewer.widgets.canvas {
                 "overflow":"hidden"
             });
 
-            var ctx1 = <CanvasRenderingContext2D>this.drawCanvas.getContext("2d");
-            var ctx2 = <CanvasRenderingContext2D>this.markCanvas.getContext("2d");
-
+            let ctx1 = <CanvasRenderingContext2D>this.drawCanvas.getContext("2d");
+            let ctx2 = <CanvasRenderingContext2D>this.markCanvas.getContext("2d");
             if ("imageSmoothingEnabled" in ctx1) {
                 (<any>ctx1).imageSmoothingEnabled = false;
                 (<any>ctx2).imageSmoothingEnabled = false;
@@ -23,45 +31,16 @@ namespace mycore.viewer.widgets.canvas {
 
         }
 
-        private _container:JQuery = jQuery("<div></div>");
-        private _drawCanvas:HTMLCanvasElement = PageView.createCanvas(1);
-        private _markCanvas:HTMLCanvasElement = PageView.createCanvas(4);
-
-        private _ctx:CanvasRenderingContext2D;
-
-        public get drawCanvas() {
-            return this._drawCanvas;
-        }
-
-        public get markCanvas() {
-            return this._markCanvas;
-        }
-
-        public get container() {
-            return this._container;
-        }
-
-        public set container(container:JQuery) {
-            this._container = container;
-        }
-
-        private static createCanvas(zIndex:number = 1) {
-            var canvas:HTMLCanvasElement = document.createElement("canvas");
+        private static createCanvas(zIndex:number = 1, filter?: string) {
+            let canvas:HTMLCanvasElement = document.createElement("canvas");
             canvas.style.transform = "scale(1.0)";
             canvas.style.position = "absolute";
             canvas.style.zIndex = zIndex+"";
+            if(filter) {
+                canvas.style.filter = filter;
+            }
             return canvas;
         }
-
-
-        public get drawImage():boolean {
-            return this._drawImage;
-        }
-
-        public get drawHTML():boolean {
-            return this._drawHTML;
-        }
-
 
     }
 }
