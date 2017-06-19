d3cac9e8d6e60cb06cbd04d06ce5e1e07b70a4bb
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2017-06-19 08:50:52 +0200

    MCR-1629 Implemented MODS Sorter (#270)
    * MCR-1629 Implemented MODS Sorter
    
    * MCR-1629 Follow Codacy recommendations

---
+++ mycore-mods/src/main/java/org/mycore/mods/MCRMODSSorter.java
@@ -0,0 +1,83 @@
+/*
+ * $Revision: 30923 $ $Date: 2014-10-22 10:54:47 +0200 (Mi, 22 Okt 2014) $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.mycore.mods;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+
+import org.jdom2.Element;
+import org.jdom2.transform.JDOMSource;
+import org.mycore.common.xml.MCRURIResolver;
+
+/**
+ * Provides functionality to sort MODS elements to a predefined order.
+ * The MODSSorter can be either used as URIResolver via
+ * "sort:[...URI returning MODS...]"
+ * or by invoking
+ * MCRMODSSorter.sort( [JDOM Element with mods:mods] );
+ *
+ * @author Frank L\u00FCtzenkirchen
+ */
+public class MCRMODSSorter implements URIResolver {
+
+    private final static String[] ORDER = { "genre", "typeofResource", "titleInfo", "nonSort", "subTitle", "title",
+            "partNumber", "partName", "name", "namePart", "displayForm", "role", "affiliation", "originInfo", "place",
+            "publisher", "dateIssued", "dateCreated", "dateModified", "dateValid", "dateOther", "edition", "issuance",
+            "frequency", "relatedItem", "language", "physicalDescription", "abstract", "note", "subject",
+            "classification", "location", "shelfLocator", "url", "accessCondition", "part", "extension",
+            "recordInfo", };
+
+    private final static List<String> ORDER_LIST = Arrays.asList(ORDER);
+
+    @Override
+    public Source resolve(String href, String base) throws TransformerException {
+        href = href.substring(href.indexOf(":") + 1);
+        Element mods = MCRURIResolver.instance().resolve(href);
+        MCRMODSSorter.sort(mods);
+        return new JDOMSource(mods);
+    }
+
+    public static void sort(Element mods) {
+        mods.sortChildren((Element e1, Element e2) -> compare(e1, e2));
+    }
+
+    private static int compare(Element e1, Element e2) {
+        int pos1 = getPos(e1);
+        int pos2 = getPos(e2);
+
+        if (pos1 == pos2) {
+            return e1.getName().compareTo(e2.getName());
+        } else {
+            return pos1 - pos2;
+        }
+    }
+
+    private static int getPos(Element e) {
+        String name = e.getName();
+        return ORDER_LIST.contains(name) ? ORDER_LIST.indexOf(name) : ORDER_LIST.size();
+    }
+}
