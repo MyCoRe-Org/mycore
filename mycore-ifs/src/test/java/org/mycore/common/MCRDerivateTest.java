01ed9ab0287e2bed77467b8a50649fbd3cf646c4
Mewel <Mewel@users.noreply.github.com>
2017-09-20 15:49:22 +0200

    MCR-1715 add files to derivate test (#392)

--- mycore-ifs/src/test/java/org/mycore/common/MCRDerivateTest.java
+++ mycore-ifs/src/test/java/org/mycore/common/MCRDerivateTest.java
@@ -1,20 +1,11 @@
 package org.mycore.common;
 
 import org.junit.Test;
-import org.mycore.access.MCRAccessBaseImpl;
-import org.mycore.access.strategies.MCRAccessCheckStrategy;
-import org.mycore.common.events.MCREventManager;
-import org.mycore.datamodel.common.MCRXMLMetadataEventHandler;
 import org.mycore.datamodel.metadata.MCRDerivate;
-import org.mycore.datamodel.metadata.MCRMetaIFS;
-import org.mycore.datamodel.metadata.MCRMetaLinkID;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
-import org.mycore.datamodel.metadata.MCRObjectID;
 
-import java.util.Map;
-
-public class MCRDerivateTest extends MCRStoreTestCase {
+public class MCRDerivateTest extends MCRIFSTest {
 
     MCRObject root;
 
@@ -22,39 +13,10 @@ public class MCRDerivateTest extends MCRStoreTestCase {
 
     @Test
     public void create() throws Exception {
-        MCREventManager.instance().clear().addEventHandler("MCRObject", new MCRXMLMetadataEventHandler());
-        root = new MCRObject();
-        root.setId(MCRObjectID.getInstance("mycore_object_00000001"));
-        root.setSchema("noSchema");
-        derivate = createDerivate();
+        root = createObject();
+        derivate = createDerivate(root.getId());
         MCRMetadataManager.create(root);
         MCRMetadataManager.create(derivate);
-    }
-
-    @Override
-    protected Map<String, String> getTestProperties() {
-        Map<String, String> testProperties = super.getTestProperties();
-        testProperties
-                .put("MCR.Persistence.LinkTable.Store.Class", "org.mycore.backend.hibernate.MCRHIBLinkTableStore");
-        testProperties.put("MCR.Access.Class", MCRAccessBaseImpl.class.getName());
-        testProperties.put("MCR.Access.Strategy.Class", AlwaysTrueStrategy.class.getName());
-        testProperties.put("MCR.Metadata.Type.object", "true");
-        testProperties.put("MCR.Metadata.Type.derivate", "true");
-        return testProperties;
-    }
-
-    private MCRDerivate createDerivate() {
-        MCRDerivate derivate = new MCRDerivate();
-        derivate.setId(MCRObjectID.getNextFreeId("mycore_derivate"));
-        derivate.setSchema("datamodel-derivate.xsd");
-        MCRMetaIFS ifs = new MCRMetaIFS();
-        ifs.setSubTag("internal");
-        ifs.setSourcePath(null);
-        derivate.getDerivate().setInternals(ifs);
-        MCRMetaLinkID mcrMetaLinkID = new MCRMetaLinkID();
-        mcrMetaLinkID.setReference("mycore_object_00000001", null, null);
-        derivate.getDerivate().setLinkMeta(mcrMetaLinkID);
-        return derivate;
     }
 
     @Override
@@ -62,15 +24,6 @@ public class MCRDerivateTest extends MCRStoreTestCase {
         MCRMetadataManager.delete(derivate);
         MCRMetadataManager.delete(root);
         super.tearDown();
-    }
-
-    public static class AlwaysTrueStrategy implements MCRAccessCheckStrategy {
-
-        @Override
-        public boolean checkPermission(String id, String permission) {
-            return true;
-        }
-
     }
 
 }
---
+++ mycore-ifs/src/test/java/org/mycore/common/MCRIFSCopyTest.java
@@ -0,0 +1,107 @@
+package org.mycore.common;
+
+import org.junit.Test;
+import org.mycore.datamodel.metadata.MCRDerivate;
+import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.niofs.MCRPath;
+import org.mycore.util.concurrent.MCRFixedUserCallable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class MCRIFSCopyTest extends MCRIFSTest {
+
+    private MCRObject root;
+
+    private MCRDerivate derivate;
+
+    @Test
+    public void sync() throws Exception {
+        create();
+        copy("/anpassbar.jpg", "anpassbar.jpg", derivate);
+        copy("/nachhaltig.jpg", "nachhaltig.jpg", derivate);
+        copy("/vielseitig.jpg", "vielseitig.jpg", derivate);
+        assertEquals("the derivate should contain three files", 3,
+                Files.list(MCRPath.getPath(derivate.getId().toString(), "/")).count());
+    }
+
+    @Test
+    public void async() throws Exception {
+        // create derivate
+        create();
+        MCRSessionMgr.getCurrentSession().commitTransaction();
+
+        // execute threads
+        MCRSystemUserInformation systemUser = MCRSystemUserInformation.getSystemUserInstance();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Future<Exception> future1 = executorService
+                .submit(new MCRFixedUserCallable<>(new CopyTask("anpassbar.jpg", derivate), systemUser));
+        Future<Exception> future2 = executorService
+                .submit(new MCRFixedUserCallable<>(new CopyTask("nachhaltig.jpg", derivate), systemUser));
+        Future<Exception> future3 = executorService
+                .submit(new MCRFixedUserCallable<>(new CopyTask("vielseitig.jpg", derivate), systemUser));
+        assertNull(future1.get(5, TimeUnit.SECONDS));
+        assertNull(future2.get(5, TimeUnit.SECONDS));
+        assertNull(future3.get(5, TimeUnit.SECONDS));
+        assertEquals("the derivate should contain three files", 3,
+                Files.list(MCRPath.getPath(derivate.getId().toString(), "/")).count());
+
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+    }
+
+    public void create() throws Exception {
+        root = createObject();
+        derivate = createDerivate(root.getId());
+        MCRMetadataManager.create(root);
+        MCRMetadataManager.create(derivate);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        MCRMetadataManager.delete(derivate);
+        MCRMetadataManager.delete(root);
+        super.tearDown();
+    }
+
+    public static void copy(String from, String to, MCRDerivate derivate) throws IOException {
+        try (InputStream fileInputStream = MCRIFSTest.class.getResourceAsStream(from)) {
+            assertNotNull("cannot find file " + from, fileInputStream);
+            Files.copy(fileInputStream, MCRPath.getPath(derivate.toString(), to));
+        }
+    }
+
+    private static class CopyTask implements Callable<Exception> {
+
+        private String fileName;
+
+        private MCRDerivate derivate;
+
+        CopyTask(String fileName, MCRDerivate derivate) {
+            this.fileName = fileName;
+            this.derivate = derivate;
+        }
+
+        @Override
+        public Exception call() throws Exception {
+            try {
+                MCRIFSCopyTest.copy("/" + fileName, fileName, derivate);
+                return null;
+            } catch (Exception exc) {
+                return exc;
+            }
+        }
+
+    }
+
+}
