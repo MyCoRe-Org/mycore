MyCoRe-Org/mycore

commit 6a9471c0a517b27a16125b782f913cdd45e7ef76

MCR-1657 use IntelliJ Code Cleanup to fix a bunch of codacy issues (#444)
fixed Java 5, 7 & 8 issues
fixed Log4J issues 
organized imports
reformatted code

mycore-oai/src/main/java/org/mycore/oai/MCROAISetManager.java
@@ -74,11 +74,11 @@ import org.mycore.util.concurrent.MCRFixedUserCallable;
  */
 public class MCROAISetManager {
 
-    protected final static Logger LOGGER = LogManager.getLogger(MCROAISetManager.class);
+    protected static final Logger LOGGER = LogManager.getLogger(MCROAISetManager.class);
 
     protected String configPrefix;
 
-    protected Map<String, MCROAISetConfiguration<?, ?, ?>> setConfigurationMap;
+    protected final Map<String, MCROAISetConfiguration<?, ?, ?>> setConfigurationMap;
 
     /**
      * Time in milliseconds when the classification changed.
@@ -138,11 +138,12 @@ public class MCROAISetManager {
     }
 
     protected void updateURIs() {
-        this.setConfigurationMap = Collections.synchronizedMap(new HashMap<>());
-        getDefinedSetIds().stream()
+        Map<String, MCROAISolrSetConfiguration> newVersion = getDefinedSetIds().stream()
             .map(String::trim)
             .map(setId -> new MCROAISolrSetConfiguration(this.configPrefix, setId))
-            .forEach(c -> setConfigurationMap.put(c.getId(), c));
+            .collect(Collectors.toMap(c -> c.getId(), c -> c));
+        setConfigurationMap.entrySet().removeIf(c -> !newVersion.containsKey(c.getKey()));
+        setConfigurationMap.replaceAll((k, v) -> newVersion.get(k));
     }
 
     public List<String> getDefinedSetIds() {
@@ -253,7 +254,7 @@ public class MCROAISetManager {
 
     private String getSetSpec(String elementText) {
         if (elementText.contains(":")) {
-            StringBuffer setSpec = new StringBuffer();
+            StringBuilder setSpec = new StringBuilder();
             String classID = elementText.substring(0, elementText.indexOf(':')).trim();
             classID = MCRClassificationAndSetMapper.mapClassificationToSet(this.configPrefix, classID);
             setSpec.append(classID).append(elementText.substring(elementText.indexOf(':')));

commit 93301123c56cfa82acf2e17eb04d1bef12a19b65

MCR-1657 fixed bug introduced in 6a9471c0a517b27a16125b782f913cdd45e7ef76

mycore-oai/src/main/java/org/mycore/oai/MCROAISetManager.java
@@ -143,7 +143,7 @@ public class MCROAISetManager {
             .map(setId -> new MCROAISolrSetConfiguration(this.configPrefix, setId))
             .collect(Collectors.toMap(c -> c.getId(), c -> c));
         setConfigurationMap.entrySet().removeIf(c -> !newVersion.containsKey(c.getKey()));
-        setConfigurationMap.replaceAll((k, v) -> newVersion.get(k));
+        setConfigurationMap.putAll(newVersion);
     }
 
     public List<String> getDefinedSetIds() {