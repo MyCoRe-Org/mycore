90fb82534391eaee758bcd727c869db2a492514b
Sebastian Hofmann <hofmann.s@uni-jena.de>
2017-12-18 13:46:43 +0100

    MCR-1786 Add PURL to MyCoRe-PI

---
+++ mycore-pi/src/main/java/org/mycore/pi/purl/MCRPURLManager.java
@@ -0,0 +1,361 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.pi.purl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * PURL Manager to register Persistent URLs on a PURL server
+ *
+ * for further documentation see PURLZ Wiki:
+ * https://code.google.com/archive/p/persistenturls/wikis
+ *
+ * Hint:
+ * -----
+ * Please check in your code that you do not register / override regular PURLs in test / development
+ * by checking: 
+ *    if (resolvingURL.contains("localhost")) {
+ *          purl = "/test" + purl;
+ *    }
+ *
+ * @author Robert Stephan
+ *
+ */
+public class MCRPURLManager {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private String purlServerBaseURL;
+
+    private String cookie = null;
+
+    private final Charset UTF_8 = Charset.forName("UTF-8");
+
+    public static void main(String[] args) {
+        MCRPURLManager app = new MCRPURLManager();
+        // app.run();
+        System.out.println(app.registerNewPURL("/test/vzg/mycore", "http://www.mycore.org", "302", "test"));
+        System.out.println(app.updateExistingPURL("/test/vzg/mycore", "http://www.mycore.de", "302", "test"));
+        // --> try your newly registered PURL:
+        // http://purl.uni-rostock.de/test/vzg/google/mycore
+        app.logout();
+    }
+
+    /**
+     * sets the session cookie, if the login was successful
+     *
+     * @param purlServerURL
+     *            - the base URL of the PURL server
+     * @param user
+     *            - the PURL server user
+     * @param password
+     *            - the user's password
+     */
+    public void login(String purlServerURL, String user, String password) {
+        HttpURLConnection conn = null;
+        try {
+            purlServerBaseURL = purlServerURL;
+            // Get Cookie
+            URL url = new URL(purlServerBaseURL + "/admin/login/login.bsh?referrer=/docs/index.html");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+
+            String headerName = null;
+            for (int i = 1; (headerName = conn.getHeaderFieldKey(i)) != null; i++) {
+                if (headerName.equals("Set-Cookie")) {
+                    cookie = conn.getHeaderField(i);
+                    LOGGER.debug("Cookie: " + cookie);
+                }
+            }
+            conn.disconnect();
+
+            // Login
+            String data = "id=" + URLEncoder.encode(user, "UTF-8");
+            data += "&passwd=" + URLEncoder.encode(password, "UTF-8");
+
+            url = new URL(purlServerBaseURL + "/admin/login/login-submit.bsh");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Cookie", cookie);
+
+            conn.setDoOutput(true);
+            try (OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream(), UTF_8)) {
+                wr.write(data);
+                wr.flush();
+                LOGGER.debug(url.toString() + " -> " + conn.getResponseCode());
+
+                // Get the response
+                try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), UTF_8))) {
+
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        if (line.trim().equals("PURL User Login Failure")) {
+                            cookie = null;
+                            break;
+                        }
+
+                    }
+                }
+            }
+            conn.disconnect();
+
+        } catch (IOException e) {
+            if (!e.getMessage().contains(
+                "Server returned HTTP response code: 403 for URL: http://purl.uni-rostock.de/docs/index.html")) {
+                LOGGER.error(e);
+            }
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+
+    /**
+     * logout from PURL server
+     */
+    public void logout() {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(purlServerBaseURL + "/admin/logout?referrer=/docs/index.html");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Cookie", cookie);
+
+            conn.setDoOutput(true);
+            try (OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream(), UTF_8)) {
+                wr.flush();
+            }
+            LOGGER.debug(url.toString() + " -> " + conn.getResponseCode());
+        } catch (IOException e) {
+            if (!e.getMessage().contains(
+                "Server returned HTTP response code: 403 for URL: http://purl.uni-rostock.de/docs/index.html")) {
+                LOGGER.error(e);
+            }
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+
+    // returns the response code
+
+    /**
+     * register a new PURL
+     * @param purl - the PURL
+     * @param target the target URL
+     * @param type - the PURL type
+     * @param maintainers - the maintainers
+     * @return the HTTP Status Code of the request
+     */
+    public int registerNewPURL(String purl, String target, String type, String maintainers) {
+        int response = 0;
+        HttpURLConnection conn = null;
+        try {
+            // opener.open("http://localhost:8080/admin/purl/net/test2",
+            // urllib.urlencode(dict(type="410", maintainers="admin"))).read().close() #
+            // Create a 410 purl
+
+            URL url = new URL(purlServerBaseURL + "/admin/purl" + purl);
+            LOGGER.debug(url.toString());
+
+            String data = "target=" + URLEncoder.encode(target, "UTF-8");
+            data += "&maintainers=" + maintainers;
+            data += "&type=" + type;
+
+            LOGGER.debug(data);
+
+            // Send data
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Cookie", cookie);
+            conn.setRequestMethod("POST");
+
+            conn.setDoOutput(true);
+
+            try (OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream(), UTF_8)) {
+                wr.write(data);
+                wr.flush();
+            }
+            response = conn.getResponseCode();
+
+            if (response != 200) {
+                if (conn.getErrorStream() != null) {
+                    try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), UTF_8))) {
+                        String line = null;
+                        while ((line = rd.readLine()) != null) {
+                            LOGGER.debug(line);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return response;
+    }
+
+    /**
+     * updates an existing PURL
+     *
+     * @param purl
+     *            - the PURL (relative URL)
+     * @param target
+     *            - the target URL
+     * @param type
+     *            - the PURL type
+     * @param maintainers
+     *            list of maintainers (PURL server users or groups)
+     * @return the HTTP Status Code of the request
+     */
+    public int updateExistingPURL(String purl, String target, String type, String maintainers) {
+        int response = 0;
+        HttpURLConnection conn = null;
+        try {
+            // opener.open("http://localhost:8080/admin/purl/net/test2",
+            // urllib.urlencode(dict(type="410", maintainers="admin"))).read().close() #
+            // Create a 410 purl
+
+            String strURL = purlServerBaseURL + "/admin/purl" + purl;
+            strURL += "?target=" + URLEncoder.encode(target, "UTF-8") + "&maintainers=" + maintainers + "&type=" + type;
+
+            URL url = new URL(strURL);
+            LOGGER.debug(url.toString());
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Cookie", cookie);
+            conn.setRequestMethod("PUT");
+            response = conn.getResponseCode();
+
+            if (response != 200) {
+                if (conn.getErrorStream() != null) {
+                    try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), UTF_8))) {
+                        String line = null;
+                        while ((line = rd.readLine()) != null) {
+                            LOGGER.debug(line);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return response;
+    }
+
+    /**
+     * deletes an existing PURL
+     *
+     * @param purl
+     * @return the HTTP Status Code of the request
+     */
+    public int deletePURL(String purl) {
+        int response = 0;
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(purlServerBaseURL + "/admin/purl" + purl);
+            LOGGER.debug(url.toString());
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Cookie", cookie);
+            conn.setRequestMethod("DELETE");
+            response = conn.getResponseCode();
+
+            if (response != 200) {
+                if (conn.getErrorStream() != null) {
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), UTF_8));
+                    String line = null;
+                    while ((line = rd.readLine()) != null) {
+                        LOGGER.debug(line);
+                    }
+                    rd.close();
+                }
+            }
+
+            conn.disconnect();
+        } catch (Exception e) {
+            LOGGER.error(e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return response;
+    }
+
+    /**
+     * check if a purl has the given target url
+     * @param purl - the purl
+     * @param targetURL - the target URL
+     * @return true, if the target URL is registered at the given PURL
+     */
+    public boolean isPURLTargetURLUnchanged(String purl, String targetURL) {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(purlServerBaseURL + "/admin/purl" + purl);
+            conn = (HttpURLConnection) url.openConnection();
+            int response = conn.getResponseCode();
+
+            if (response == 200) {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(conn.getInputStream());
+                /*
+                 * <purl status="1"> <id>/test/rosdok/ppn750527188</id> <type>302</type>
+                 * <maintainers><uid>rosdok</uid><uid>test</uid></maintainers>
+                 * <target><url>http://localhost:8080/rosdok/resolve/id/
+                 * rosdok_document_0000000259</url></target> </purl>
+                 */
+                Element eTarget = (Element) doc.getDocumentElement().getElementsByTagName("target").item(0);
+                Element eTargetUrl = (Element) eTarget.getElementsByTagName("url").item(0);
+                return targetURL.equals(eTargetUrl.getTextContent().trim());
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return false;
+    }
+}
