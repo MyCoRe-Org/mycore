88f6b95a145b280122bb49869f8e094617d86f43
Thomas Scheffler <github@yagee.de>
2018-05-08 15:45:46 +0200

    MCR-1824 WIP MCRJerseyExceptionMapper

--- mycore-base/src/main/java/org/mycore/frontend/jersey/resources/MCRJerseyExceptionMapper.java
+++ mycore-base/src/main/java/org/mycore/frontend/jersey/resources/MCRJerseyExceptionMapper.java
@@ -18,6 +18,9 @@
 
 package org.mycore.frontend.jersey.resources;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.WebApplicationException;
@@ -26,8 +29,14 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -53,15 +62,19 @@ public class MCRJerseyExceptionMapper implements ExceptionMapper<Exception> {
     HttpHeaders headers;
 
     @Context
-    HttpServletRequest request;
+    HttpServletRequest request; //required for MCRParamCollector
+
+    @Context
+    UriInfo uriInfo;
 
     @Override
     public Response toResponse(Exception exc) {
         Response response = getResponse(exc);
         if (exc instanceof WebApplicationException && !(exc instanceof InternalServerErrorException)) {
-            LOGGER.warn("{}: {}, {}", request.getRequestURI(), response.getStatus(), exc.getMessage());
+            LOGGER.warn("{}: {}, {}", uriInfo.getRequestUri(), response.getStatus(), exc.getMessage());
+            LOGGER.debug("Exception: ", exc);
         } else {
-            LOGGER.warn(() -> "Error while processing request " + request.getRequestURI(), exc);
+            LOGGER.warn(() -> "Error while processing request " + uriInfo.getRequestUri(), exc);
         }
         if (headers.getAcceptableMediaTypes().contains(MediaType.TEXT_HTML_TYPE)) {
             // try to return a html error page
@@ -72,11 +85,11 @@ public class MCRJerseyExceptionMapper implements ExceptionMapper<Exception> {
                 int status = response.getStatus();
                 String source = exc.getStackTrace().length > 0 ? exc.getStackTrace()[0].toString() : null;
                 Document errorPageDocument = MCRErrorServlet.buildErrorPage(exc.getMessage(), status,
-                    request.getRequestURI(), null, source, exc);
+                    uriInfo.getRequestUri().toASCIIString(), null, source, exc);
                 MCRContent result = MCRJerseyUtil.transform(errorPageDocument, request);
                 return Response.serverError()
                     .entity(result.getInputStream())
-                    .type(MediaType.valueOf(result.getMimeType()))
+                    .type(result.getMimeType())
                     .status(status)
                     .build();
             } catch (Exception transformException) {
@@ -99,29 +112,94 @@ public class MCRJerseyExceptionMapper implements ExceptionMapper<Exception> {
                 .type(MediaType.TEXT_PLAIN_TYPE)
                 .build();
         }
-        LOGGER.warn("Content-Type: {}", request.getContentType());
-        MediaType requestType = MediaType.valueOf(request.getContentType());
-        if (requestType.isCompatible(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
-            || requestType.isCompatible(MediaType.MULTIPART_FORM_DATA_TYPE)) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(exc.getMessage()).build();
-        }
-        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new MCRExceptionContainer(exc)).build();
+        Object entity = Optional.ofNullable(request.getContentType())
+            .map(MediaType::valueOf)
+            .filter(t -> t.isCompatible(MediaType.APPLICATION_FORM_URLENCODED_TYPE) || t
+                .isCompatible(MediaType.MULTIPART_FORM_DATA_TYPE))
+            .map(t -> (Object) exc.getMessage()) //do not container form requests responses
+            .orElseGet(() -> new MCRExceptionContainer(exc));
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(entity).build();
     }
 
+    @XmlRootElement(name = "error")
     private static class MCRExceptionContainer{
         private Exception exception;
+
+        private MCRExceptionContainer() {
+            //required for JAXB
+        }
+
         public MCRExceptionContainer(Exception e){
             this.exception=e;
         }
 
+        @XmlElement
+        @XmlJavaTypeAdapter(ExceptionTypeAdapter.class)
         public Exception getException(){
             return exception;
         }
 
+        @XmlAttribute
         public Class<? extends Exception> getType() {
             return exception.getClass();
         }
 
+    }
+
+    private static class ExceptionTypeAdapter extends XmlAdapter<RException, Exception> {
+        @Override
+        public Exception unmarshal(RException v) throws Exception {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public RException marshal(Exception v) {
+            RException ep = new RException();
+            ep.message = v.getMessage();
+            ep.stackTrace = Stream.of(v.getStackTrace()).map(RStackTraceElement::getInstance)
+                .toArray(RStackTraceElement[]::new);
+            Optional.ofNullable(v.getCause())
+                .filter(Exception.class::isInstance)
+                .map(Exception.class::cast)
+                .map(this::marshal)
+                .ifPresent(c -> ep.cause = c);
+            return ep;
+        }
+    }
+
+    private static class RException {
+        @XmlElement
+        private String message;
+
+        @XmlElement
+        private RStackTraceElement[] stackTrace;
+
+        @XmlElement
+        private RException cause;
+
+    }
+
+    private static class RStackTraceElement {
+        @XmlAttribute
+        private String className;
+
+        @XmlAttribute
+        private String method;
+
+        @XmlAttribute
+        private String file;
+
+        @XmlAttribute
+        private int line;
+
+        private static RStackTraceElement getInstance(StackTraceElement ste) {
+            RStackTraceElement rste = new RStackTraceElement();
+            rste.className = ste.getClassName();
+            rste.method = ste.getMethodName();
+            rste.file = ste.getFileName();
+            rste.line = ste.getLineNumber();
+            return rste;
+        }
     }
 
 }
