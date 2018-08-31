5b6b4f20c1629502817cfb4abeb1b07ccc0def81
Jens Kupferschmidt <kupferschmidt@rz.uni-leipzig.de>
2018-08-31 10:59:31 +0200

    MCR-1937 fix clone and refactoring constructor

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaSpatial.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRMetaSpatial.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.datamodel.metadata;
 
 import java.math.BigDecimal;
@@ -17,18 +35,36 @@ import org.mycore.common.MCRException;
  * where two BigDecimal build a point.
  *
  * @author Matthias Eichner
+ * @author Jens Kupferschmidt
  */
 public class MCRMetaSpatial extends MCRMetaDefault {
 
     private List<BigDecimal> data;
 
+    /**
+     * initializes with empty values.
+     */
     public MCRMetaSpatial() {
         super();
         this.data = new ArrayList<>();
     }
 
+    @Deprecated
     public MCRMetaSpatial(String subtag, String defaultLanguage, String type, Integer inherited) throws MCRException {
         super(subtag, defaultLanguage, type, inherited);
+        this.data = new ArrayList<>();
+    }
+
+    /**
+     * The constructor for a MCRMetaSpatial instance with an empty data container.
+     * @param subtag the name of the subtag
+     * @param type an optional type or an empty string
+     * @param inherited a value &gt;= 0
+     * 
+     * @throws MCRException if the set_subtag value is null or empty
+     */
+    public MCRMetaSpatial(String subtag, String type, Integer inherited) throws MCRException {
+        super(subtag, null, type, inherited);
         this.data = new ArrayList<>();
     }
 
@@ -112,11 +148,17 @@ public class MCRMetaSpatial extends MCRMetaDefault {
         }
     }
 
-    @Override
-    public MCRMetaSpatial clone() {
-        MCRMetaSpatial newSpatial = new MCRMetaSpatial(getSubTag(), getLang(), getType(), getInherited());
-        newSpatial.setData(new ArrayList<>(this.data));
-        return newSpatial;
+    /**
+     * clone of this instance
+     * 
+     * you will get a (deep) clone of this element
+     * 
+     * @see java.lang.Object#clone()
+     */
+    public final MCRMetaSpatial clone() {
+        MCRMetaSpatial out = new MCRMetaSpatial();
+        out.setFromDOM(createXML().clone());
+        return out;
     }
 
 }
