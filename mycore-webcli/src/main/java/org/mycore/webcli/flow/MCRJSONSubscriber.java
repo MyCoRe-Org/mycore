7a3808b89cc40f963f6babde1809c767b0e83310
Thomas Scheffler <github@yagee.de>
2018-09-28 08:17:35 +0200

    MCR-1739 update to Java 11
    use Flow API for WebCLI

---
+++ mycore-webcli/src/main/java/org/mycore/webcli/flow/MCRJSONSubscriber.java
@@ -0,0 +1,89 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.webcli.flow;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.concurrent.Flow;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.websocket.CloseReason;
+import javax.websocket.Session;
+
+import org.apache.logging.log4j.LogManager;
+
+import com.google.gson.JsonObject;
+
+public class MCRJSONSubscriber implements Flow.Subscriber<JsonObject> {
+
+    private final Session session;
+
+    private final ReentrantLock lock;
+
+    private Flow.Subscription subscription;
+
+    public MCRJSONSubscriber(Session session, ReentrantLock lock) {
+        this.session = session;
+        this.lock = lock;
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(JsonObject item) {
+        if (!this.session.isOpen()) {
+            subscription.cancel();
+        }
+        LogManager.getLogger().info("Sending json: " + item.toString());
+        lock.lock();
+        try {
+            session.getBasicRemote().sendText(item.toString());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } finally {
+            lock.unlock();
+            subscription.request(1);
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        if (this.session.isOpen()) {
+            try {
+                LogManager.getLogger().error("Close session " + session.getId(), throwable);
+                this.session.close(new CloseReason(CloseReason.CloseCodes.CLOSED_ABNORMALLY, throwable.getMessage()));
+            } catch (IOException e) {
+                LogManager.getLogger().warn("Error in Publisher or Subscriber.", throwable);
+            }
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        //nothing to do, session handling is outside of this scope
+    }
+
+    public void cancel() {
+        this.subscription.cancel();
+    }
+}
