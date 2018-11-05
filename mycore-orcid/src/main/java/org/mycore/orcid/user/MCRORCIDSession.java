445d41470119de37636f54f8fb481f3d329047ff
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2018-11-05 14:21:30 +0100

    MCR-1972 Fixed bug: MCRORCIDSession may not hold the currently active ORCID user e.g. after login

--- mycore-orcid/src/main/java/org/mycore/orcid/user/MCRORCIDSession.java
+++ mycore-orcid/src/main/java/org/mycore/orcid/user/MCRORCIDSession.java
@@ -3,6 +3,7 @@ package org.mycore.orcid.user;
 import java.io.IOException;
 
 import org.jdom2.JDOMException;
+import org.mycore.common.MCRSession;
 import org.mycore.common.MCRSessionMgr;
 import org.mycore.user2.MCRUser;
 import org.mycore.user2.MCRUserManager;
@@ -12,16 +13,16 @@ public class MCRORCIDSession {
 
     private static final String KEY_ORCID_USER = "ORCID_USER";
 
-    private static MCRORCIDUser setCurrentUser() {
-        MCRUser user = MCRUserManager.getCurrentUser();
-        MCRORCIDUser orcidUser = new MCRORCIDUser(user);
-        MCRSessionMgr.getCurrentSession().put(KEY_ORCID_USER, orcidUser);
-        return orcidUser;
-    }
-
     public static MCRORCIDUser getCurrentUser() {
-        MCRORCIDUser orcidUser = (MCRORCIDUser) MCRSessionMgr.getCurrentSession().get(KEY_ORCID_USER);
-        return (orcidUser == null ? setCurrentUser() : orcidUser);
+        MCRSession session = MCRSessionMgr.getCurrentSession();
+        MCRUser user = MCRUserManager.getCurrentUser();
+
+        MCRORCIDUser orcidUser = (MCRORCIDUser) session.get(KEY_ORCID_USER);
+        if ((orcidUser == null) || !orcidUser.getUser().equals(user)) {
+            orcidUser = new MCRORCIDUser(user);
+            session.put(KEY_ORCID_USER, orcidUser);
+        }
+        return orcidUser;
     }
 
     public static int getNumWorks() throws JDOMException, IOException, SAXException {
