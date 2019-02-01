83328d416a51131e9f871777dd02886fef353f95
Thomas Scheffler <github@yagee.de>
2019-02-01 11:21:30 +0100

    MCR-2016 fixed MODS classification mapping

--- mycore-mods/src/main/java/org/mycore/mods/classification/MCRMODSClassificationSupport.java
+++ mycore-mods/src/main/java/org/mycore/mods/classification/MCRMODSClassificationSupport.java
@@ -139,7 +139,7 @@ public final class MCRMODSClassificationSupport {
             throw new MCRException(e);
         }
 
-        return String.format(Locale.ROOT, "classification:metadata:0:parents:{0}:{1}", category.getRootID(), id);
+        return String.format(Locale.ROOT, "classification:metadata:0:parents:%s:%s", category.getRootID(), id);
     }
 
     static String getText(final Element element) {
