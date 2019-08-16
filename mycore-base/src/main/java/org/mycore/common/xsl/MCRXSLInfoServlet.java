f76806361edd69d9ae97c212a099b87c55149f3f
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2019-08-16 13:40:50 +0200

    MCR-2088 Fixed NPE in case WEB-INF/classes/xsl/ is not used

--- mycore-base/src/main/java/org/mycore/common/xsl/MCRXSLInfoServlet.java
+++ mycore-base/src/main/java/org/mycore/common/xsl/MCRXSLInfoServlet.java
@@ -104,9 +104,11 @@ public final class MCRXSLInfoServlet extends MCRServlet {
     }
 
     private void findXSLinLibJars() throws IOException {
-        for (String path : diveInto("/WEB-INF/lib/"))
-            if (path.endsWith(".jar"))
+        for (String path : diveInto("/WEB-INF/lib/")) {
+            if (path.endsWith(".jar")) {
                 findXSLinJar(path);
+            }
+        }
     }
 
     private Set<String> diveInto(String base) {
@@ -114,11 +116,15 @@ public final class MCRXSLInfoServlet extends MCRServlet {
         Set<String> paths = getServletContext().getResourcePaths(base);
 
         Set<String> more = new HashSet<>();
-        more.addAll(paths);
+        if (paths != null) {
+            more.addAll(paths);
 
-        for (String path : paths)
-            if (path.endsWith("/"))
-                more.addAll(diveInto(path));
+            for (String path : paths) {
+                if (path.endsWith("/")) {
+                    more.addAll(diveInto(path));
+                }
+            }
+        }
 
         return more;
     }
@@ -131,8 +137,9 @@ public final class MCRXSLInfoServlet extends MCRServlet {
 
         for (ZipEntry ze = null; (ze = zis.getNextEntry()) != null;) {
             String name = ze.getName();
-            if (name.startsWith("xsl/") && name.endsWith(".xsl"))
+            if (name.startsWith("xsl/") && name.endsWith(".xsl")) {
                 foundStylesheet(name, pathOfJarFile);
+            }
             zis.closeEntry();
         }
         zis.close();
@@ -141,8 +148,9 @@ public final class MCRXSLInfoServlet extends MCRServlet {
     private void findXSLinClassesDir() {
         String base = "/WEB-INF/classes/xsl/";
         for (String path : diveInto(base))
-            if (path.endsWith(".xsl"))
+            if (path.endsWith(".xsl")) {
                 foundStylesheet(path, base);
+            }
     }
 
     private void foundStylesheet(String path, String source) {
