44f74a4744f8787f7d96a7d6b9c5c4ffb095d4ef
Sebastian Hofmann <hofmann.s@uni-jena.de>
2019-09-23 10:14:26 +0200

    MCR-2107 big pdf files load slow in viewer
    - prevent download of all rendering data on startup

--- mycore-viewer/src/main/typescript/modules/base/widgets/thumbnail/ThumbnailOverviewThumbnail.ts
+++ mycore-viewer/src/main/typescript/modules/base/widgets/thumbnail/ThumbnailOverviewThumbnail.ts
@@ -20,7 +20,6 @@ namespace mycore.viewer.widgets.thumbnail {
     export interface ThumbnailOverviewThumbnail {
         id: string;
         label: string;
-        href: string;
         requestImgdataUrl:(callback:(imgdata:string)=>void)=>void;
     }
 
