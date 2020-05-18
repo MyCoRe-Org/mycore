efbeba1b49394b1e5d89a5abf93e937c668a5a74
Robert Stephan <rsteph-de@users.noreply.github.com>
2020-05-18 13:58:17 +0200

    MCR-2213 RestAPIv2 simple search and filter endpoint (/explore)

---
+++ mycore-restapi/src/main/java/org/mycore/restapi/v2/explore/MCRLinkListJSONSerializer.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.restapi.v2.explore;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.mycore.restapi.v2.explore.model.MCRRestExploreResponseLink;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+/**
+ * a JSON serializer which converts a list of links to a JSON object with properties for each link.
+ *
+ * @author Robert Stephan
+ *
+ */
+public class MCRLinkListJSONSerializer extends StdSerializer<List<MCRRestExploreResponseLink>> {
+
+    private static final long serialVersionUID = 1L;
+
+    @SuppressWarnings("unchecked")
+    public MCRLinkListJSONSerializer() {
+        super((Class<List<MCRRestExploreResponseLink>>) ((Class<?>) List.class));
+    }
+
+    @Override
+    public void serialize(List<MCRRestExploreResponseLink> value, JsonGenerator generator, SerializerProvider provider)
+            throws IOException {
+        generator.writeStartObject();
+        for (MCRRestExploreResponseLink link : value) {
+            generator.writeStringField(link.getRel(), link.getUrl());
+        }
+        generator.writeEndObject();
+    }
+}
---
+++ mycore-restapi/src/main/java/org/mycore/restapi/v2/explore/model/MCRRestExploreResponseObject.java
@@ -0,0 +1,116 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.restapi.v2.explore.model;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.frontend.MCRFrontendUtil;
+import org.mycore.restapi.v2.explore.MCRInstantXMLAdapter;
+import org.mycore.restapi.v2.explore.MCRLinkListJSONSerializer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/**
+ * This object represents a single result object of a REST /explore response.
+ * 
+ * @author Robert Stephan
+ *
+ */
+@XmlType(
+        propOrder = { "id", "modified", "links", "payload" })
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlSeeAlso(ArrayList.class)
+public class MCRRestExploreResponseObject {
+    @XmlAttribute
+    private String id;
+
+    @XmlAttribute
+    @XmlJavaTypeAdapter(MCRInstantXMLAdapter.class)
+    private Instant modified;
+
+    @XmlElement(
+            name = "payload")
+    @JsonProperty("payloads")
+    private List<MCRRestExploreResponsePayload> payload = null;
+
+    @XmlElement(
+            name = "link")
+    @JsonProperty("links")
+    @JsonSerialize(
+            using = MCRLinkListJSONSerializer.class)
+    private List<MCRRestExploreResponseLink> links = new ArrayList<MCRRestExploreResponseLink>();
+
+    public MCRRestExploreResponseObject(String id, Instant modified) {
+        super();
+        this.id = id;
+        this.modified = modified;
+        updateLinks();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+        updateLinks();
+    }
+
+    public Instant getModified() {
+        return modified;
+    }
+
+    public void setLastModified(Instant modified) {
+        this.modified = modified;
+    }
+
+    public void addPayload(String key, Object value) {
+        if (payload == null) {
+            payload = new ArrayList<>();
+        }
+        payload.add(new MCRRestExploreResponsePayload(key, value));
+    }
+
+    public List<MCRRestExploreResponsePayload> getPayload() {
+        return payload;
+    }
+
+    private void updateLinks() {
+        links.clear();
+        links.add(new MCRRestExploreResponseLink("self", MCRFrontendUtil.getBaseURL() + "api/v2/objects/" + id));
+        String frontpageURL = MCRFrontendUtil.getBaseURL() + MCRConfiguration2.getString(
+                "MCR.RestAPI.V2.Links.FrontpageURL").orElse("receive/{id}").replace("{id}", id);
+        links.add(new MCRRestExploreResponseLink("html", frontpageURL));
+    }
+
+    public List<MCRRestExploreResponseLink> getLinks() {
+        return links;
+    }
+}
