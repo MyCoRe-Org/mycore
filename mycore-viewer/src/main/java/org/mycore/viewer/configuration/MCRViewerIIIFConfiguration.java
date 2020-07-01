10812b2c0d27682f15fe15527bfebb014e217829
eckstueck <michel.buechner@uni-jena.de>
2020-07-01 11:22:29 +0200

    MCR-2251 Fix IIIF API path for IIIF Viewer
    fixed IIIF Path

--- mycore-viewer/src/main/java/org/mycore/viewer/configuration/MCRViewerIIIFConfiguration.java
+++ mycore-viewer/src/main/java/org/mycore/viewer/configuration/MCRViewerIIIFConfiguration.java
@@ -33,10 +33,10 @@ public class MCRViewerIIIFConfiguration extends MCRViewerBaseConfiguration {
         final String derivate = getDerivate(request);
 
         setProperty("manifestURL", MCRFrontendUtil.getBaseURL()
-            + MCRConfiguration2.getString("MCR.Viewer.IIIF.URL.Presentation").orElse("api/iiif/v2/presentation/mets/")
+            + MCRConfiguration2.getString("MCR.Viewer.IIIF.URL.Presentation").orElse("api/iiif/presentation/v2/mets/")
             + derivate + "/manifest");
         setProperty("imageAPIURL", MCRFrontendUtil.getBaseURL()
-            + MCRConfiguration2.getString("MCR.Viewer.IIIF.URL.Image").orElse("api/iiif/v2/image/Iview/"));
+            + MCRConfiguration2.getString("MCR.Viewer.IIIF.URL.Image").orElse("api/iiif/image/v2/"));
         setProperty("filePath", getDerivate(request) + getFilePath(request));
 
         // script
