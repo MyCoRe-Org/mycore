b8f3364a7896aaf826d74ba951a1b7f0fc59f8ee
Thomas Scheffler <github@yagee.de>
2020-08-05 14:04:04 +0200

    MCR-2265 add support for multiple categories

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRDerivateDefaultClassEventHandler.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRDerivateDefaultClassEventHandler.java
@@ -19,35 +19,60 @@
 package org.mycore.datamodel.metadata;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.common.events.MCREvent;
 import org.mycore.common.events.MCREventHandlerBase;
+import org.mycore.datamodel.classifications2.MCRCategoryDAO;
 import org.mycore.datamodel.classifications2.MCRCategoryDAOFactory;
 import org.mycore.datamodel.classifications2.MCRCategoryID;
 
 public class MCRDerivateDefaultClassEventHandler extends MCREventHandlerBase {
-    private static final String DEFAULT_CATEGORY = MCRConfiguration2
-        .getString("MCR." + MCRDerivateDefaultClassEventHandler.class.getSimpleName() + ".DefaultCategory")
-        .orElse("content");
+    private static final List<String> DEFAULT_CATEGORIES = MCRConfiguration2
+        .getString("MCR." + MCRDerivateDefaultClassEventHandler.class.getSimpleName() + ".DefaultCategories")
+        .map(MCRConfiguration2::splitValue)
+        .orElseGet(() -> Stream.of("derivate_types:content"))
+        .collect(Collectors.toUnmodifiableList());
 
     private static final Logger LOGGER = LogManager.getLogger();
+
+    private static MCRMetaClassification asMetaClassification(MCRCategoryID categoryId) {
+        return new MCRMetaClassification("classification", 0, null,
+            categoryId.getRootID(), categoryId.getID());
+    }
 
     @Override
     protected void handleDerivateCreated(MCREvent evt, MCRDerivate der) {
         final ArrayList<MCRMetaClassification> classifications = der.getDerivate().getClassifications();
         if (!classifications.isEmpty()) {
+            //already defined at creation
             return;
         }
-        MCRCategoryID category = new MCRCategoryID("derivate_types", DEFAULT_CATEGORY);
-        if (!MCRCategoryDAOFactory.getInstance().exist(category)) {
-            LOGGER.error("Category does not exist: " + category);
+
+        final List<MCRCategoryID> categories = DEFAULT_CATEGORIES.stream()
+            .map(MCRCategoryID::fromString) //checks syntax
+            .collect(Collectors.toList());
+        final MCRCategoryDAO categoryDAO = MCRCategoryDAOFactory.getInstance();
+        final List<MCRCategoryID> missingCategories = categories.stream()
+            .filter(Predicate.not(categoryDAO::exist))
+            .collect(Collectors.toList());
+
+        if (!missingCategories.isEmpty()) {
+            LOGGER.error(() -> "Categories do not exist: " + missingCategories.stream()
+                .map(MCRCategoryID::toString)
+                .collect(Collectors.joining(", ")));
             return;
         }
-        LOGGER.warn(der.getId() + " has no classification, using " + DEFAULT_CATEGORY + " by default.");
-        classifications
-            .add(new MCRMetaClassification("classification", 0, null, category.getRootID(), category.getID()));
+        LOGGER.warn(() -> der.getId() + " has no classification, using "
+            + DEFAULT_CATEGORIES.stream().collect(Collectors.joining(", ")) + " by default.");
+        classifications.addAll(categories.stream()
+            .map(MCRDerivateDefaultClassEventHandler::asMetaClassification)
+            .collect(Collectors.toList()));
     }
 }
