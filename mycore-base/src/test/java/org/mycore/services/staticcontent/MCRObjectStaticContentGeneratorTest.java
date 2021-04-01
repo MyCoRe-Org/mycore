6c362e4794e5dc00e6fae1b3a64cf4aab13d11f8
Robert Stephan <robert.stephan@uni-rostock.de>
2021-04-01 17:42:41 +0200

    MCR-2391 use Path objects instead of strings for comparison

--- mycore-base/src/test/java/org/mycore/services/staticcontent/MCRObjectStaticContentGeneratorTest.java
+++ mycore-base/src/test/java/org/mycore/services/staticcontent/MCRObjectStaticContentGeneratorTest.java
@@ -36,15 +36,15 @@ public class MCRObjectStaticContentGeneratorTest extends MCRTestCase {
 
         MCRConfiguration2.set("MCR.Metadata.ObjectID.NumberPattern", "00000");
         MCRObjectID derivate = MCRObjectID.getInstance("mcr_derivate_00001");
-        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate).toString(), "/000/01");
+        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate), Paths.get("/000/01"));
 
         MCRConfiguration2.set("MCR.Metadata.ObjectID.NumberPattern", "000000");
         derivate = MCRObjectID.getInstance("mcr_derivate_000001");
-        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate).toString(), "/000/001");
+        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate), Paths.get("/000/001"));
 
         MCRConfiguration2.set("MCR.Metadata.ObjectID.NumberPattern", "0000000");
         derivate = MCRObjectID.getInstance("mcr_derivate_0000001");
-        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate).toString(), "/000/000/1");
+        Assert.assertEquals("Paths should match", generator.getSlotDirPath(derivate), Paths.get("/000/000/1"));
 
     }
 }
