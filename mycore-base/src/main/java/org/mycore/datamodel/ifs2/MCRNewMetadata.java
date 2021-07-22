81f6262cebfd120c607e29b98fecea25755a2195
OPNA2608 <christoph.neidahl@gmail.com>
2021-07-22 17:12:45 +0200

    WIP

---
+++ mycore-base/src/main/java/org/mycore/datamodel/ifs2/MCRNewMetadata.java
@@ -0,0 +1,116 @@
+package org.mycore.datamodel.ifs2;
+
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.common.MCRPersistenceException;
+import org.mycore.common.content.MCRContent;
+import org.mycore.datamodel.metadata.MCRObjectID;
+
+/**
+ * A generic class to interact with metadata stored in a {@link MCRNewMetadataStore}-based store.
+ * 
+ * A MCRNewMetadata instance represents an object that is (to be) stored in the linked store.
+ * It has an immutable set of information about its associated store, its name and its ID.
+ * @author bt1cn
+ *
+ */
+public class MCRNewMetadata {
+
+    protected static final Logger LOGGER = LogManager.getLogger(MCRNewMetadata.class);
+
+    private final MCRNewMetadataStore store;
+
+    private final String docType;
+
+    private final MCRObjectID id;
+
+    private String revision;
+
+    private MCRContent content;
+
+    private Date date;
+
+    public MCRNewMetadata(MCRNewMetadataStore store, int id) {
+        this.store = store;
+        docType = store.getDocType();
+        this.id = MCRObjectID.getInstance(MCRObjectID.formatID(store.getStoreID(), id));
+    }
+
+    public MCRNewMetadata(MCRNewMetadataStore store, int id, String revision) {
+        this(store, id);
+        this.revision = revision;
+    }
+
+    public void create(MCRContent content) throws MCRPersistenceException {
+        try {
+            store.createContent(this, content);
+        } catch (MCRPersistenceException e) {
+            throw new MCRPersistenceException(
+                "Failed to create " + id.toString() + " in revision " + revision + " in store!",
+                e);
+        }
+    }
+
+    public MCRContent read() throws MCRPersistenceException {
+        try {
+            return store.readContent(this);
+        } catch (MCRPersistenceException e) {
+            throw new MCRPersistenceException(
+                "Failed to read " + id.toString() + " in revision " + revision + " from store!",
+                e);
+        }
+    }
+
+    public void update(MCRContent content) throws MCRPersistenceException {
+        try {
+            store.updateContent(this, content);
+        } catch (MCRPersistenceException e) {
+            throw new MCRPersistenceException(
+                "Failed to update " + id.toString() + " in revision " + revision + " in store!",
+                e);
+        }
+    }
+
+    public void delete() throws MCRPersistenceException {
+        try {
+            store.deleteContent(this);
+        } catch (MCRPersistenceException e) {
+            throw new MCRPersistenceException(
+                "Failed to delete " + id.toString() + " in revision " + revision + " from store!", e);
+        }
+    }
+
+    public String getBase() {
+        return id.getBase();
+    }
+
+    public int getID() {
+        return id.getNumberAsInteger();
+    }
+
+    public MCRObjectID getFullID() {
+        return id;
+    }
+
+    public String getDocType() {
+        return docType;
+    }
+
+    public String getRevision() {
+        return revision;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    void setRevision(String revision) {
+        this.revision = revision;
+    }
+    
+    MCRContent getContent() {
+        return this.content;
+    }
+}
---
+++ mycore-base/src/main/java/org/mycore/datamodel/ifs2/MCRNewMetadataStore.java
@@ -0,0 +1,307 @@
+package org.mycore.datamodel.ifs2;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+import java.util.Optional;
+import java.util.StringTokenizer;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.common.MCRPersistenceException;
+import org.mycore.common.MCRUsageException;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.config.MCRConfigurationException;
+import org.mycore.common.content.MCRContent;
+import org.mycore.datamodel.common.MCRAbstractMetadataVersion;
+import org.mycore.datamodel.ifs2.MCRStore.MCRStoreConfig;
+
+public abstract class MCRNewMetadataStore {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    protected final String configPrefix;
+
+    protected final boolean forceXML;
+
+    protected final String forceDocType;
+
+    protected final MCRStoreConfig storeConfig;
+
+    protected Function<String, String> toNativePath;
+
+    protected boolean hasBaseDir = true;
+
+    protected boolean hasWorkDir = false;
+
+    protected boolean hasSlotLayout = true;
+
+    protected boolean canVerify = false;
+
+    protected final Path baseDir;
+
+    protected final Path workDir;
+
+    protected final int[] slots;
+
+    protected static boolean validateSlotLayout(String layout) {
+        // Only digits and - are valid characters
+        if (!Pattern.matches("[\\d-]+", layout)) {
+            LOGGER.error("Slot layout '{}' contains invalid characters", layout);
+            return false;
+        }
+
+        final StringTokenizer st = new StringTokenizer(layout, "-");
+        String layoutToken;
+        int tokenInt;
+        while (st.countTokens() > 1) {
+            layoutToken = st.nextToken();
+            // "--" or starting/ending with - makes no sense
+            if (layoutToken.length() == 0) {
+                LOGGER.error("Slot layout '{}' contains consecutive slot delimiters", layout);
+                return false;
+            }
+            tokenInt = Integer.parseInt(layoutToken);
+            // Slot length 0 makes no sense
+            if (tokenInt == 0) {
+                LOGGER.error("Slot layout '{}' contains slot of length 0", layout);
+            }
+        }
+        LOGGER.info("Slot layout '{}' is valid", layout);
+        return true;
+    }
+
+    protected static int[] readSlotLayout(String layout) throws MCRConfigurationException {
+        if (!validateSlotLayout(layout)) {
+            throw new MCRConfigurationException("Slot layout " + layout + " is invalid");
+        }
+
+        final StringTokenizer st = new StringTokenizer(layout, "-");
+        int[] slots = new int[st.countTokens()];
+        int i = 0;
+        while (st.countTokens() > 0) {
+            slots[i++] = Integer.parseInt(st.nextToken());
+        }
+        return slots;
+    }
+
+    protected static Path convertPath(String path) {
+        Path parsedPath = null;
+        try {
+            URI uri = new URI(path);
+            if (uri.getScheme() != null) {
+                parsedPath = Paths.get(uri);
+            }
+        } catch (URISyntaxException e) {
+            //not a uri, handle as relative path
+        }
+        if (parsedPath == null) {
+            parsedPath = Paths.get(path);
+        }
+        return parsedPath;
+    }
+
+    protected static String normalizeSeparators(String path) {
+        String separator = FileSystems.getDefault().getSeparator();
+        Function<String, String> toNativePath;
+        if (separator.equals("/")) {
+            toNativePath = s -> s;
+        } else {
+            toNativePath = s -> s.replace("/", separator);
+        }
+        return toNativePath.apply(path);
+    }
+
+    protected static Path getDirectory(String path) {
+        Path readPath = convertPath(normalizeSeparators(path));
+        try {
+            if (Files.exists(readPath)) {
+                BasicFileAttributes attrs = Files.readAttributes(readPath, BasicFileAttributes.class);
+                if (!attrs.isDirectory()) {
+                    throw new MCRConfigurationException("Path '" + readPath.toString() + "' is not a directory");
+                }
+
+                if (!Files.isReadable(readPath)) {
+                    throw new MCRConfigurationException("Directory '" + readPath.toString() + "' is not readable");
+                }
+            } else {
+                Files.createDirectories(readPath);
+            }
+        } catch (IOException e) {
+            throw new MCRConfigurationException("Failed to get directory", e);
+        }
+        return readPath;
+    }
+
+    protected MCRNewMetadataStore(String type) {
+        this(new MCRStoreDefaultConfig(type));
+    }
+
+    protected MCRNewMetadataStore(MCRStoreConfig storeConfig) {
+        this.storeConfig = storeConfig;
+        this.configPrefix = "MCR.IFS2.Store." + storeConfig.getID() + ".";
+
+        Optional<Boolean> shouldForceXML = MCRConfiguration2.getBoolean(configPrefix + "ForceXML");
+        if (shouldForceXML.isPresent()) {
+            forceXML = shouldForceXML.get();
+        } else {
+            forceXML = false;
+        }
+        if (forceXML) {
+            forceDocType = MCRConfiguration2.getStringOrThrow(configPrefix + "ForceDocType");
+        } else {
+            forceDocType = null;
+        }
+
+        if (hasBaseDir) {
+            baseDir = getDirectory(storeConfig.getBaseDir());
+        } else {
+            baseDir = null;
+        }
+
+        if (hasWorkDir) {
+            workDir = getDirectory(configPrefix + ".WorkDir");
+        } else {
+            workDir = null;
+        }
+
+        if (hasSlotLayout) {
+            this.slots = readSlotLayout(storeConfig.getSlotLayout());
+        } else {
+            this.slots = null;
+        }
+    }
+
+    public synchronized int getNextFreeID() {
+        return 0;
+    }
+
+    public MCRNewMetadata create(MCRContent content) throws MCRPersistenceException {
+        return create(getNextFreeID(), content);
+    }
+
+    public MCRNewMetadata create(int id, MCRContent content) throws MCRPersistenceException {
+        MCRNewMetadata metadata = new MCRNewMetadata(this, id);
+        metadata.create(content);
+        return metadata;
+    }
+
+    public MCRNewMetadata retrieve(int id) throws MCRPersistenceException {
+        MCRNewMetadata metadata = new MCRNewMetadata(this, id);
+        metadata.read();
+        return metadata;
+    }
+
+    public MCRNewMetadata update(int id, MCRContent content) throws MCRPersistenceException {
+        MCRNewMetadata metadata = new MCRNewMetadata(this, id);
+        metadata.update(content);
+        return metadata;
+    }
+
+    public void delete(int id) throws MCRPersistenceException {
+        new MCRNewMetadata(this, id).delete();
+    }
+
+    public String getStoreID() {
+        return storeConfig.getID();
+    }
+
+    public boolean getForcedXML() {
+        return forceXML;
+    }
+
+    public String getDocType() {
+        return forceDocType;
+    }
+
+    public abstract boolean exists(MCRNewMetadata metadata) throws MCRPersistenceException;
+
+    protected abstract void createContent(MCRNewMetadata metadata, MCRContent content)
+        throws MCRPersistenceException, MCRUsageException;
+
+    protected abstract MCRContent readContent(MCRNewMetadata metadata)
+        throws MCRPersistenceException, MCRUsageException;
+
+    protected abstract void updateContent(MCRNewMetadata metadata, MCRContent content)
+        throws MCRPersistenceException, MCRUsageException;
+
+    protected abstract void deleteContent(MCRNewMetadata metadata) throws MCRPersistenceException, MCRUsageException;
+
+    public abstract IntStream getIDs() throws MCRPersistenceException;
+
+    public MCRAbstractMetadataVersion<?> getVersion(int id, String revision)
+        throws MCRPersistenceException, MCRUsageException {
+        return getVersion(new MCRNewMetadata(this, id, revision));
+    }
+
+    public abstract MCRAbstractMetadataVersion<?> getVersion(MCRNewMetadata metadata)
+        throws MCRPersistenceException, MCRUsageException;
+
+    public MCRAbstractMetadataVersion<?> getVersionFirst(int id) throws MCRPersistenceException, MCRUsageException {
+        return getVersionFirst(new MCRNewMetadata(this, id));
+    }
+
+    public MCRAbstractMetadataVersion<?> getVersionFirst(MCRNewMetadata metadata)
+        throws MCRPersistenceException, MCRUsageException {
+        String firstRevision = getVersions(metadata)
+            .sequential()
+            .reduce((first, second) -> first)
+            .orElseThrow(
+                () -> new MCRUsageException("Failed to find first version of " + metadata.getFullID().toString()))
+            .getRevision();
+        metadata.setRevision(firstRevision);
+        return getVersion(metadata);
+    }
+
+    public MCRAbstractMetadataVersion<?> getVersionLast(MCRNewMetadata metadata)
+        throws MCRPersistenceException, MCRUsageException {
+        String firstRevision = getVersions(metadata)
+            .sequential()
+            .reduce((first, second) -> second)
+            .orElseThrow(
+                () -> new MCRUsageException("Failed to find last version of " + metadata.getFullID().toString()))
+            .getRevision();
+        metadata.setRevision(firstRevision);
+        return getVersion(metadata);
+    }
+
+    public Stream<MCRAbstractMetadataVersion<?>> getVersions(int id) throws MCRPersistenceException, MCRUsageException {
+        return getVersions(new MCRNewMetadata(this, id));
+    }
+
+    public abstract Stream<MCRAbstractMetadataVersion<?>> getVersions(MCRNewMetadata metadata)
+        throws MCRPersistenceException, MCRUsageException;
+
+    public void verify() throws MCRPersistenceException, MCRUsageException {
+        if (!canVerify) {
+            throw new MCRUsageException(
+                "Store class " + this.getClass().getCanonicalName() + " does not support verification");
+        } else {
+            doVerification();
+        }
+    }
+
+    protected abstract void doVerification() throws MCRPersistenceException;
+
+    public Date getModified(int id) throws MCRPersistenceException {
+        return getModified(new MCRNewMetadata(this, id));
+    }
+
+    public abstract Date getModified(MCRNewMetadata mcrNewMetadata) throws MCRPersistenceException;
+
+    public void setModified(int id, Date date) throws MCRPersistenceException {
+        setModified(new MCRNewMetadata(this, id), date);
+    }
+
+    public abstract void setModified(MCRNewMetadata metadata, Date date) throws MCRPersistenceException;
+}
