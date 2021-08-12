23a3a463baabd578ddd0b0b2c6563c04bd984ae9
Thomas Scheffler <github@yagee.de>
2021-08-12 10:53:12 +0200

    MCR-2439 update copyright and javadoc

--- mycore-restapi/src/main/java/org/mycore/restapi/MCRApiDraftFilter.java
+++ mycore-restapi/src/main/java/org/mycore/restapi/MCRApiDraftFilter.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.restapi;
 
 import java.io.IOException;
@@ -17,6 +35,16 @@ import org.apache.logging.log4j.LogManager;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.restapi.annotations.MCRApiDraft;
 
+/**
+ * Filter all methods and classes marked by {@link MCRApiDraft} and return {@link Response.Status#NOT_FOUND}.
+ *
+ * Use Property <code>MCR.RestApi.Draft.{{@link MCRApiDraft#value()}}=true</code> to enable a group of endpoints
+ * marked with the same <code>value</code>, e.g.<br>
+ * "<code>MCR.RestApi.Draft.Proposed=true</code>" to include all endpoints marked with
+ * <code>@MCRApiDraft("Proposed")</code>.
+ * @author Thomas Scheffler (yagee)
+ * @see MCRApiDraft
+ */
 @Priority(Priorities.AUTHORIZATION)
 public class MCRApiDraftFilter implements ContainerRequestFilter {
     @Context
--- mycore-restapi/src/main/java/org/mycore/restapi/annotations/MCRApiDraft.java
+++ mycore-restapi/src/main/java/org/mycore/restapi/annotations/MCRApiDraft.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.restapi.annotations;
 
 import java.lang.annotation.ElementType;
@@ -6,8 +24,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Used to mark REST methods that require a property activation
+ * Used to mark REST methods that are not stable for production.
+ *
+ * Those endpoints maybe changed or removed at a later date.
  * @author Thomas Scheffler (yagee)
+ * @see org.mycore.restapi.MCRApiDraftFilter
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
@@ -15,9 +36,9 @@ import java.lang.annotation.Target;
 public @interface MCRApiDraft {
 
     /**
-     * Used to construct the property name
+     * A simple value to mark a group of endpoints.
      *
-     * The returned value is prefixed by <code>MCR.RestApi.Draft.</code>
+     * Endpoints belong to the same group if this value is the same.
      */
     String value();
 }
