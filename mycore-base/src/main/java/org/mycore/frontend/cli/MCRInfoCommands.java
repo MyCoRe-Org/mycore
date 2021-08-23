c103f492ed42d8f3b555c1d12b9d96ab26ee63c6
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2021-08-23 13:05:59 +0200

    MCR-2463 Add CLI commands that show messages, properties and resources

---
+++ mycore-base/src/main/java/org/mycore/frontend/cli/MCRInfoCommands.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.frontend.cli;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.xml.MCRURIResolver;
+import org.mycore.frontend.cli.annotation.MCRCommand;
+import org.mycore.frontend.cli.annotation.MCRCommandGroup;
+import org.mycore.services.i18n.MCRTranslation;
+
+import java.util.Map;
+
+/**
+ * This class contains commands that show system components and resources.
+ *
+ * @author Torsten Krause
+ */
+
+@MCRCommandGroup(name = "Info Commands")
+public class MCRInfoCommands {
+
+    private static final Logger LOGGER = LogManager.getLogger(MCRInfoCommands.class);
+
+    @MCRCommand(
+            syntax = "show message {0} for {1}",
+            help = "Show message with key {0} for locale {1}",
+            order = 10
+    )
+    public static void showMessage(String key, String lang) {
+        String value = MCRTranslation.translate(key, MCRTranslation.getLocale(lang));
+        if (value == null || (value.startsWith("???") && value.endsWith("???"))) {
+            LOGGER.info("Found no message for key {}", key);
+        } else {
+            LOGGER.info("Found message for key {}: {}", key, value);
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show messages {0} for {1}",
+            help = "Show messages with key prefix {0} for locale {1}",
+            order = 20
+    )
+    public static void showMessages(String keyPrefix, String lang) {
+        Map<String, String> values = MCRTranslation.translatePrefix(keyPrefix, MCRTranslation.getLocale(lang));
+        if (values.isEmpty()) {
+            LOGGER.info("Found no messages for key prefix {}", keyPrefix);
+        } else {
+            values.forEach((key, value) -> {
+                LOGGER.info("Found message for key {}: {}", key, value);
+            });
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show all messages for {0}",
+            help = "Show all messages for locale {1}",
+            order = 30
+    )
+    public static void showMessages(String lang) {
+        Map<String, String> values = MCRTranslation.translatePrefix("", MCRTranslation.getLocale(lang));
+        if (!values.isEmpty()) {
+            LOGGER.info("Found no messages");
+        } else {
+            values.forEach((key, value) -> {
+                LOGGER.info("Found message for key {}: {}", key, value);
+            });
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show property {0}",
+            help = "Show configuration property with key {0}",
+            order = 40
+    )
+    public static void showProperty(String key) {
+        String value = MCRConfiguration2.getPropertiesMap().get(key);
+        if (value == null) {
+            LOGGER.info("Found no value for key {}", key);
+        } else {
+            LOGGER.info("Found value for key {}: {}", key, value);
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show properties {0}",
+            help = "Show configuration properties starting with key prefix {0}",
+            order = 50
+    )
+    public static void showProperties(String keyPrefix) {
+        Map<String, String> values = MCRConfiguration2.getSubPropertiesMap(keyPrefix);
+        if (values.isEmpty()) {
+            LOGGER.info("Found no values for key prefix {}", keyPrefix);
+        } else {
+            values.forEach((key, value) -> {
+                LOGGER.info("Found value for key {}: {}", keyPrefix + key, value);
+            });
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show all properties",
+            help = "Show all configuration properties",
+            order = 60
+    )
+    public static void showAllProperties() {
+        Map<String, String> values = MCRConfiguration2.getPropertiesMap();
+        if (values.isEmpty()) {
+            LOGGER.info("Found no values");
+        } else {
+            values.forEach((key, value) -> {
+                LOGGER.info("Found value for key {}: {}", key, value);
+            });
+        }
+    }
+
+    @MCRCommand(
+            syntax = "show resource {0}",
+            help = "Show resource with uri {0}",
+            order = 70
+    )
+    public static void showResource(String uri) {
+        try {
+            Element resource = MCRURIResolver.instance().resolve(uri);
+            String xmlText = new XMLOutputter(Format.getPrettyFormat()).outputString(resource);
+            LOGGER.info("Resolved resource for uri {}:\n{}", uri, xmlText);
+        } catch (Exception e) {
+            LOGGER.info("Failed to resolve resource for uri {}", uri);
+        }
+    }
+
+}
