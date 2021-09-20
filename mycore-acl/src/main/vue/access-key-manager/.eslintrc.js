2494e0d3b013b4ac0d4fee1279ccc4c6d700b1f6
Lennard Golsch <lennard.golsch@icloud.com>
2021-09-20 13:48:36 +0200

    disabled lint line break check

--- mycore-acl/src/main/vue/access-key-manager/.eslintrc.js
+++ mycore-acl/src/main/vue/access-key-manager/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    'linebreak-style': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
