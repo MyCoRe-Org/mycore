b9346c9b605a056e98a08d5ab78041b468683122
Lennard Golsch <lennard.golsch@icloud.com>
2021-09-28 18:10:46 +0200

    improved clean up

--- mycore-acl/src/main/java/org/mycore/mcr/acl/accesskey/MCRAccessKeyUtils.java
+++ mycore-acl/src/main/java/org/mycore/mcr/acl/accesskey/MCRAccessKeyUtils.java
@@ -20,6 +20,7 @@
 
 package org.mycore.mcr.acl.accesskey;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -116,34 +117,52 @@ public class MCRAccessKeyUtils {
     }
 
     /**
+     * Lists all users which own at least an access key user attribute in given range
+     *
+     * @param offset the offset
+     * @param limit the limit
+     * @return a list with all users which own at least an access key in given range
+     */
+    private static List<MCRUser> listUsersWithAccessKey(final int offset, final int limit) {
+        return MCRUserManager.listUsers(null, null, null, null, ACCESS_KEY_PREFIX + "*", offset, limit);
+    }
+
+    /**
      * Cleans all access key secret attributes of users if the corresponding key does not exist.
      */
     public static void cleanUpUserAttributes() {
-        final List<MCRUser> users = MCRUserManager.listUsers(null, null, null, null);
         final Set<MCRUserAttribute> validAttributes = new HashSet<>();
         final Set<MCRUserAttribute> deadAttributes = new HashSet<>();
-        for (MCRUser user : users) {
-            final List<MCRUserAttribute> attributes = user.getAttributes()
-                .stream()
-                .filter(attribute -> attribute.getName().startsWith(MCRAccessKeyUtils.ACCESS_KEY_PREFIX))
-                .filter(attribute -> !validAttributes.contains(attribute))
-                .collect(Collectors.toList());
-            for (MCRUserAttribute attribute : attributes) {
-                final String attributeName = attribute.getName();
-                final MCRObjectID objectId = MCRObjectID.getInstance(attributeName.substring(
-                    attributeName.indexOf("_") + 1));
-                if (deadAttributes.contains(attribute)) {
-                    MCRAccessKeyUtils.removeAccessKeySecret(user, objectId); //best solution?
-                } else {
-                    if (MCRAccessKeyManager.getAccessKeyWithSecret(objectId, attribute.getValue()) != null) {
-                        validAttributes.add(attribute);
+        int offset = 0;
+        final int limit = 1024;
+        List<MCRUser> users = new ArrayList<>();
+        do {
+            users = listUsersWithAccessKey(offset * limit, limit);
+            for (final MCRUser user : users) {
+                final List<MCRUserAttribute> attributes = user.getAttributes()
+                    .stream()
+                    .filter(attribute -> attribute.getName().startsWith(MCRAccessKeyUtils.ACCESS_KEY_PREFIX))
+                    .filter(attribute -> !validAttributes.contains(attribute))
+                    .collect(Collectors.toList());
+                for (MCRUserAttribute attribute : attributes) {
+                    final String attributeName = attribute.getName();
+                    final MCRObjectID objectId = MCRObjectID.getInstance(attributeName.substring(
+                        attributeName.indexOf("_") + 1));
+                    if (deadAttributes.contains(attribute)) {
+                        MCRAccessKeyUtils.removeAccessKeySecret(user, objectId);
                     } else {
-                        MCRAccessKeyUtils.removeAccessKeySecret(user, objectId); //best solution?
-                        deadAttributes.add(attribute);
+                        if (MCRAccessKeyManager.getAccessKeyWithSecret(objectId, attribute.getValue()) != null) {
+                            validAttributes.add(attribute);
+                        } else {
+                            MCRAccessKeyUtils.removeAccessKeySecret(user, objectId);
+                            deadAttributes.add(attribute);
+                        }
                     }
                 }
             }
+            offset++;
         }
+        while (users.size() > 0);
     }
 
     /**
