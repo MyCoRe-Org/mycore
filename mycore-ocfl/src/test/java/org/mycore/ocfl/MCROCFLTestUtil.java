3c8178c3a8a2b186c37933c130a223ca4b6be9da
Tobias Lenhardt <tobias.lenhardt@uni-leipzig.de>
2022-01-07 16:05:17 +0100

    MCR-2528 Codacy related file rename TestCase to TestUtil
    Signed-off-by: Tobias Lenhardt <tobias.lenhardt@uni-leipzig.de>

--- mycore-ocfl/src/test/java/org/mycore/ocfl/MCROCFLTestUtil.java
+++ mycore-ocfl/src/test/java/org/mycore/ocfl/MCROCFLTestUtil.java
@@ -32,7 +32,7 @@ import org.mycore.common.config.MCRDefaultConfigurationLoader;
 /**
  * @author Tobias Lenhardt [Hammer1279]
  */
-public class MCROCFLTestCase {
+public class MCROCFLTestUtil {
 
     @ClassRule
     public static TemporaryFolder junitFolder = new TemporaryFolder();
