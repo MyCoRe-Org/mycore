144e51772155637b413109cb9323782d4007d4ed
Sebastian Hofmann <7668803+sebhofmann@users.noreply.github.com>
2022-02-01 14:41:15 +0100

    MCR-2564 unnecessary initializations of MCROCFLRepositoryProvider (#1444)

--- mycore-ocfl/src/main/java/org/mycore/ocfl/MCROCFLRepositoryProvider.java
+++ mycore-ocfl/src/main/java/org/mycore/ocfl/MCROCFLRepositoryProvider.java
@@ -30,7 +30,7 @@ public abstract class MCROCFLRepositoryProvider {
     public static final String REPOSITORY_PROPERTY_PREFIX = "MCR.OCFL.Repository.";
 
     public static OcflRepository getRepository(String id){
-        return MCRConfiguration2.getInstanceOf(REPOSITORY_PROPERTY_PREFIX + id)
+        return MCRConfiguration2.getSingleInstanceOf(REPOSITORY_PROPERTY_PREFIX + id)
                 .map(MCROCFLRepositoryProvider.class::cast)
                 .get()
                 .getRepository();
