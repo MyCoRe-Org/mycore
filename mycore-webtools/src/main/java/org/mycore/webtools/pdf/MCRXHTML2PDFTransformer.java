b04850f2f4744a26f916a00da6cd3516144cab33
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-02-15 18:19:32 +0100

    MCR-2581 make mods csl pdf export more robust

--- mycore-webtools/src/main/java/org/mycore/webtools/pdf/MCRXHTML2PDFTransformer.java
+++ mycore-webtools/src/main/java/org/mycore/webtools/pdf/MCRXHTML2PDFTransformer.java
@@ -18,32 +18,71 @@
 
 package org.mycore.webtools.pdf;
 
-import com.itextpdf.html2pdf.ConverterProperties;
-import com.itextpdf.html2pdf.HtmlConverter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.Normalizer;
+
+import org.mycore.common.MCRClassTools;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.config.MCRConfigurationException;
 import org.mycore.common.content.MCRByteContent;
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.transformer.MCRContentTransformer;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.layout.font.FontProvider;
 
 /**
  * Takes XHTML as input and transforms it to a PDF file
  */
 public class MCRXHTML2PDFTransformer extends MCRContentTransformer {
 
+    private String id;
+
+    protected FontProvider fontProvider;
+
+    @Override
+    public void init(String id) {
+        this.id = id;
+        super.init(id);
+    }
+
     @Override
     public MCRContent transform(MCRContent source) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        try (InputStream xhtmlIS = source.getInputStream()) {
-            ConverterProperties converterProperties = new ConverterProperties();
-            HtmlConverter.convertToPdf(xhtmlIS, baos, converterProperties);
-        }
+        String s = source.asString();
+        s = Normalizer.normalize(s, Normalizer.Form.NFC); // required because some data is not normalized
+        s = s.replace(" ", ""); // required because itext cant handle these
+
+        ConverterProperties converterProperties = new ConverterProperties();
+        converterProperties.setFontProvider(lazyInitFontProvider());
+        HtmlConverter.convertToPdf(s, baos, converterProperties);
 
         return new MCRByteContent(baos.toByteArray());
     }
+
+    protected synchronized FontProvider lazyInitFontProvider() {
+        if (fontProvider == null) {
+            fontProvider = new FontProvider();
+            final ClassLoader cl = MCRClassTools.getClassLoader();
+            MCRConfiguration2.getString("MCR.ContentTransformer." + id + ".FontResources")
+                .stream()
+                .flatMap(MCRConfiguration2::splitValue)
+                .forEach((path) -> {
+                    try (InputStream is = cl.getResource(path).openStream()) {
+                        fontProvider.addFont(is.readAllBytes());
+                    } catch (IOException e) {
+                        throw new MCRConfigurationException("Error while loading configured fonts!", e);
+                    }
+                });
+        }
+
+        return fontProvider;
+    }
+
 
     @Override
     public String getFileExtension() throws Exception {
