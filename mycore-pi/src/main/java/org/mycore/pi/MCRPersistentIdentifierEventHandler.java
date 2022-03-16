c344e7e2cc7891596f7b845ab6eaae7ce194c930
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-03-16 15:57:51 +0100

    MCR-2600 run pi event handler as janitor

--- mycore-pi/src/main/java/org/mycore/pi/MCRPersistentIdentifierEventHandler.java
+++ mycore-pi/src/main/java/org/mycore/pi/MCRPersistentIdentifierEventHandler.java
@@ -25,11 +25,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mycore.common.MCRException;
 import org.mycore.common.events.MCREvent;
-import org.mycore.common.events.MCREventHandlerBase;
+import org.mycore.common.events.MCRJanitorEventHandlerBase;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.pi.exceptions.MCRPersistentIdentifierException;
 
-public class MCRPersistentIdentifierEventHandler extends MCREventHandlerBase {
+public class MCRPersistentIdentifierEventHandler extends MCRJanitorEventHandlerBase {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
