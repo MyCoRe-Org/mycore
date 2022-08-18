131a9f62858b0b7762d5d179b6128adf439c3be5
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-08-18 14:38:21 +0200

    MCR-2719 avoid SuppressWarnings

--- mycore-pi/src/main/java/org/mycore/pi/MCRPIServiceManager.java
+++ mycore-pi/src/main/java/org/mycore/pi/MCRPIServiceManager.java
@@ -39,18 +39,18 @@ public class MCRPIServiceManager {
                 .collect(Collectors.toList());
     }
 
-    public List<MCRPIService> getServiceList() {
+    public List<MCRPIService<MCRPersistentIdentifier>> getServiceList() {
         return getServiceIDList()
             .stream()
             .map(this::getRegistrationService)
             .collect(Collectors.toList());
     }
 
-    public List<MCRPIJobService> getAutoCreationList() {
+    public List<MCRPIJobService<MCRPersistentIdentifier>> getAutoCreationList() {
         return getServiceList()
             .stream()
             .filter(service -> service instanceof MCRPIJobService)
-            .map(MCRPIJobService.class::cast)
+            .map(s->(MCRPIJobService<MCRPersistentIdentifier>) s)
             .filter(service -> MCRConfiguration2
                 .getString(REGISTRATION_SERVICE_CONFIG_PREFIX + service.getServiceID() + "." +
                     CREATION_PREDICATE)
@@ -59,8 +59,7 @@ public class MCRPIServiceManager {
     }
 
     public <T extends MCRPersistentIdentifier> MCRPIService<T> getRegistrationService(String id) {
-        return (MCRPIService<T>) MCRConfiguration2
-            .getSingleInstanceOf(MCRPIServiceManager.REGISTRATION_SERVICE_CONFIG_PREFIX + id).get();
+        return MCRConfiguration2.<MCRPIService<T>>getSingleInstanceOf(MCRPIServiceManager.REGISTRATION_SERVICE_CONFIG_PREFIX + id).get();
     }
 
     private static class InstanceHolder {
