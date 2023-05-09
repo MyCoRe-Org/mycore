e3fcb1035a774e89cd4ebc609e51938633b40cc1
k.brandhorst <k.brandhorst@tu-braunschweig.de>
2023-05-09 10:39:43 +0200

    MCR-2771 Improve JS in orcidOAuth

--- mycore-orcid2/src/main/resources/META-INF/resources/modules/orcid2/js/orcid-auth.js
+++ mycore-orcid2/src/main/resources/META-INF/resources/modules/orcid2/js/orcid-auth.js
@@ -1,7 +1,7 @@
 async function orcidOAuth(scope) {
-    if(scope == null) {
-        console.warn('A scope needs to be specified');
-        return;
+    var resURI = 'rsc/orcid/auth';
+    if(scope) {
+        resURI += `?scope=${scope}`;
     }
     var width = 540;
     var height = 750;
@@ -9,8 +9,8 @@ async function orcidOAuth(scope) {
     var top = (window.innerHeight/2)-(height/2);
     const logout = await fetch('https://orcid.org/userStatus.json?logUserOut=true', {dataType: 'jsonp'});
     if(!logout.ok) {
-        console.warn('Could not logout user from ORCID')
+        console.warn('Could not logout user from ORCID');
         return;
     }
-    window.open(webApplicationBaseURL+'rsc/orcid/auth?scope='+scope, "_blank", 'toolbar=no, width='+width+', height='+height+', top='+top+', left='+left);
+    window.open(webApplicationBaseURL + resURI, '_blank', `toolbar=no, width=${width}, height=${height}, top=${top}, left=${left}`);
 }
