5e61719e57c912c4eb0391e684e0df7dea50200c
Robert Stephan <rsteph-de@users.noreply.github.com>
2023-06-07 14:59:55 +0200

    MCR-2886 new PI predicates to check other PIs (#1858)
    * MCR-2886 new PI predicates to check other PIs
    
    * MCR-2886 fix Javadoc
    
    * MCR-2886 update Javadoc

---
+++ mycore-pi/src/main/java/org/mycore/pi/condition/MCRPIOtherPIRegisteredPredicate.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.pi.condition;
+
+import org.mycore.datamodel.metadata.MCRBase;
+import org.mycore.pi.MCRPIManager;
+
+/**
+ * PI Predicate, that checks if an other PersistentIdentifier was registered within the PI component
+ * before the current PI will be created or registered.
+ * 
+ * Use the properties *.Service and *.Type 
+ * to specify the PI service and type of the PI which should be checked. 
+ * 
+ * sample configuration:
+ * MCR.PI.Service.RosDokURN.CreationPredicate=org.mycore.pi.condition.MCRPIAndPredicate
+ * MCR.PI.Service.RosDokURN.CreationPredicate.1=org.mycore.pi.condition.MCRPIOtherPIRegisteredPredicate
+ * MCR.PI.Service.RosDokURN.CreationPredicate.1.Service=MCRLocalID
+ * MCR.PI.Service.RosDokURN.CreationPredicate.1.Type=local_id
+ * ...
+ * 
+ * @author Robert Stephan
+ *
+ */
+public class MCRPIOtherPIRegisteredPredicate extends MCRPIPredicateBase
+    implements MCRPICreationPredicate, MCRPIObjectRegistrationPredicate {
+
+    final private String type;
+
+    final private String service;
+
+    public MCRPIOtherPIRegisteredPredicate(String propertyPrefix) {
+        super(propertyPrefix);
+
+        type = requireProperty("Type");
+        service = requireProperty("Service");
+    }
+
+    @Override
+    public boolean test(MCRBase mcrBase) {
+        return MCRPIManager.getInstance().isRegistered(mcrBase.getId(), "", type, service);
+    }
+}
