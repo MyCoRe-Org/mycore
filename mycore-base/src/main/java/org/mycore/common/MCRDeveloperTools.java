94075a6f1222c91ac0dbd55d681366b73edf9627
Robert Stephan <robert.stephan@uni-rostock.de>
2023-06-14 20:14:11 +0200

    MCR-2900 cleanup imports

--- mycore-base/src/main/java/org/mycore/common/MCRDeveloperTools.java
+++ mycore-base/src/main/java/org/mycore/common/MCRDeveloperTools.java
@@ -22,7 +22,6 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.InvalidAlgorithmParameterException;
 import java.util.Objects;
 import java.util.Optional;
 
