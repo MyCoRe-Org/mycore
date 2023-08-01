7318240b32657df6ec8f412869c2329006468fc1
Robert Stephan <robert.stephan@uni-rostock.de>
2023-08-01 19:24:56 +0200

    MCR-2938 Rewrite jackson.core.JsonFactory with GSON

--- mycore-base/src/main/java/org/mycore/frontend/jersey/MCRJWTUtil.java
+++ mycore-base/src/main/java/org/mycore/frontend/jersey/MCRJWTUtil.java
@@ -20,7 +20,6 @@ package org.mycore.frontend.jersey;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.security.NoSuchAlgorithmException;
@@ -29,6 +28,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
+import org.mycore.common.MCRJSONUtils;
 import org.mycore.common.MCRSession;
 import org.mycore.common.MCRUserInformation;
 import org.mycore.common.config.MCRConfiguration2;
@@ -39,8 +39,8 @@ import org.mycore.common.events.MCRStartupHandler;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import jakarta.servlet.ServletContext;
 import jakarta.ws.rs.core.Response;
@@ -54,7 +54,7 @@ public class MCRJWTUtil implements MCRStartupHandler.AutoExecutable {
 
     public static final String JWT_SESSION_ATTRIBUTE_PREFIX = "mcr:sa:";
 
-    private static final JsonFactory JSON_FACTORY = new JsonFactory();
+    private static Gson GSON = MCRJSONUtils.createGSON();
 
     private static final String ROLES_PROPERTY = "MCR.Rest.JWT.Roles";
 
@@ -102,53 +102,35 @@ public class MCRJWTUtil implements MCRStartupHandler.AutoExecutable {
     }
 
     public static Response getJWTLoginSuccessResponse(String jwt) throws IOException {
-        try (StringWriter sw = new StringWriter()) {
-            JsonGenerator jsonGenerator = JSON_FACTORY.createGenerator(sw);
-            jsonGenerator.writeStartObject();
-            jsonGenerator.writeBooleanField("login_success", true);
-            jsonGenerator.writeStringField("access_token", jwt);
-            jsonGenerator.writeStringField("token_type", "Bearer");
-            jsonGenerator.writeEndObject();
-            jsonGenerator.flush();
-            jsonGenerator.close();
-            return Response.status(Response.Status.OK)
-                .header("Authorization", "Bearer " + jwt)
-                .entity(sw.toString())
-                .build();
-        }
+        JsonObject response = new JsonObject();
+        response.addProperty("login_success", true);
+        response.addProperty("access_token", jwt);
+        response.addProperty("token_type", "Bearer");
+        return Response.status(Response.Status.OK)
+            .header("Authorization", "Bearer " + jwt)
+            .entity(GSON.toJson(response))
+            .build();
     }
 
     public static Response getJWTRenewSuccessResponse(String jwt) throws IOException {
-        try (StringWriter sw = new StringWriter()) {
-            JsonGenerator jsonGenerator = JSON_FACTORY.createGenerator(sw);
-            jsonGenerator.writeStartObject();
-            jsonGenerator.writeBooleanField("executed", true);
-            jsonGenerator.writeStringField("access_token", jwt);
-            jsonGenerator.writeStringField("token_type", "Bearer");
-            jsonGenerator.writeEndObject();
-            jsonGenerator.flush();
-            jsonGenerator.close();
-            return Response.status(Response.Status.OK)
-                .header("Authorization", "Bearer " + jwt)
-                .entity(sw.toString())
-                .build();
-        }
+        JsonObject response = new JsonObject();
+        response.addProperty("executed", true);
+        response.addProperty("access_token", jwt);
+        response.addProperty("token_type", "Bearer");
+        return Response.status(Response.Status.OK)
+            .header("Authorization", "Bearer " + jwt)
+            .entity(GSON.toJson(response))
+            .build();
     }
 
     public static Response getJWTLoginErrorResponse(String errorDescription) throws IOException {
-        try (StringWriter sw = new StringWriter()) {
-            JsonGenerator jsonGenerator = JSON_FACTORY.createGenerator(sw);
-            jsonGenerator.writeStartObject();
-            jsonGenerator.writeBooleanField("login_success", false);
-            jsonGenerator.writeStringField("error", "login_failed");
-            jsonGenerator.writeStringField("error_description", errorDescription);
-            jsonGenerator.writeEndObject();
-            jsonGenerator.flush();
-            jsonGenerator.close();
-            return Response.status(Response.Status.FORBIDDEN)
-                .entity(sw.toString())
-                .build();
-        }
+        JsonObject response = new JsonObject();
+        response.addProperty("login_success", false);
+        response.addProperty("error", "login_failed");
+        response.addProperty("error_description", errorDescription);
+        return Response.status(Response.Status.FORBIDDEN)
+            .entity(GSON.toJson(response))
+            .build();
     }
 
     @Override
