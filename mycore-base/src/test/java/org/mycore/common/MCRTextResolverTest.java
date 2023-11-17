7373505970e3ca476a354e8943e47eafe359c33c
Antoniafriedrich <antonia.friedrich@uni-jena.de>
2023-11-17 11:29:37 +0100

    MCR-2997 UnnecessaryBooleanAssertion

--- mycore-base/src/test/java/org/mycore/common/MCRTextResolverTest.java
+++ mycore-base/src/test/java/org/mycore/common/MCRTextResolverTest.java
@@ -21,6 +21,7 @@ package org.mycore.common;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Hashtable;
 
@@ -125,7 +126,7 @@ public class MCRTextResolverTest extends MCRTestCase {
         } catch (CircularDependencyExecption cde) {
             return;
         }
-        assertFalse("No circular dependency occurred", true);
+        fail("No circular dependency occurred");
     }
 
     public static class UppercaseTerm extends Term {
