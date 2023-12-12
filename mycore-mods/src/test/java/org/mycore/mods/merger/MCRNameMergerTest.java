d4cb6d9a90befe39975ebe0e91ec144fb9cadc85
k.brandhorst <k.brandhorst@tu-braunschweig.de>
2023-12-12 15:07:47 +0100

    MCR-3013 fixed (some) codacy issues

--- mycore-mods/src/test/java/org/mycore/mods/merger/MCRNameMergerTest.java
+++ mycore-mods/src/test/java/org/mycore/mods/merger/MCRNameMergerTest.java
@@ -156,9 +156,15 @@ public class MCRNameMergerTest extends MCRTestCase {
 
     @Test
     public void testRetainSame() throws JaxenException, IOException {
-        String a = "[mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='gnd']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='gnd']='2']]";
-        String b = "[mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='scopus']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='scopus']='2']]";
-        String e = "[mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='gnd']='1'][mods:nameIdentifier[@type='scopus']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='gnd']='2'][mods:nameIdentifier[@type='scopus']='2']]";
+        String a = "[mods:name[@type='personal'][mods:namePart='Thomas Müller']" 
+            + "[mods:nameIdentifier[@type='gnd']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller']"
+            + "[mods:nameIdentifier[@type='gnd']='2']]";
+        String b = "[mods:name[@type='personal'][mods:namePart='Thomas Müller']" 
+            + "[mods:nameIdentifier[@type='scopus']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller']" 
+            + "[mods:nameIdentifier[@type='scopus']='2']]";
+        String e = "[mods:name[@type='personal'][mods:namePart='Thomas Müller'][mods:nameIdentifier[@type='gnd']='1']" 
+            + "[mods:nameIdentifier[@type='scopus']='1']][mods:name[@type='personal'][mods:namePart='Thomas Müller']" 
+            + "[mods:nameIdentifier[@type='gnd']='2'][mods:nameIdentifier[@type='scopus']='2']]";
         MCRMergerTest.test(a, b, e);
     }
 
