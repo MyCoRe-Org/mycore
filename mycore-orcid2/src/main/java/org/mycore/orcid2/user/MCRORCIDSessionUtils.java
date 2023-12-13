b1edfc601de63ef5c406c8d6c6724f053b2cfe4e
Silvio Hermann <s.hermann@uni-jena.de>
2023-12-13 09:44:16 +0100

    MCR-3020 Get userid from MCRUserInformation and load user with MCRUserManager

--- mycore-orcid2/src/main/java/org/mycore/orcid2/user/MCRORCIDSessionUtils.java
+++ mycore-orcid2/src/main/java/org/mycore/orcid2/user/MCRORCIDSessionUtils.java
@@ -37,8 +37,9 @@ public class MCRORCIDSessionUtils {
      */
     public static MCRORCIDUser getCurrentUser() {
         final MCRSession session = MCRSessionMgr.getCurrentSession();
+        String userID = session.getUserInformation().getUserID();
         // refetch user because of rest issues
-        final MCRUser user = MCRUserManager.getUser(MCRUserManager.getCurrentUser().getUserID());
+        final MCRUser user = MCRUserManager.getUser(userID);
         MCRORCIDUser orcidUser = (MCRORCIDUser) session.get(KEY_ORCID_USER);
         if ((orcidUser == null) || !orcidUser.getUser().equals(user)) {
             orcidUser = new MCRORCIDUser(user);
