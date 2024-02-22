e3e3e557fab9e905a324c4d5869890557224f385
Mewel <Mewel@users.noreply.github.com>
2024-02-22 18:32:57 +0100

    MCR-3043 new text editor (#2068)
    * MCR-3043 add text editor to webtools
    
    * MCR-3043 rm service worker, use own Auth API
    
    * MCR-3043 prettify classifications
    
    * MCR-3043 use /try for access, improve ui
    
    * MCR-3043 usability improvements
    
    * fix prettyXML for classification in firefox
    * fix browser undo behaviour
    
    * MCR-3043 rm dist folder
    
    * MCR-3043 request changes
    
    * add TODO for allow header
    * rm npm-run-all2 as yarn dep
    * better documentation
    * use nextTick instead of setTimeout
    
    * MCR-3043 add npm-run-all2 again

---
+++ mycore-webtools/src/main/vue/texteditor/src/apis/ClassificationsContentHandler.ts
@@ -0,0 +1,69 @@
+import {BaseContentHandler} from "@/apis/BaseContentHandler";
+import {getAuthorizationHeader} from "@/apis/Auth";
+
+export class ClassificationsContentHandler extends BaseContentHandler {
+
+    constructor(webApplicationBaseURL: string) {
+        super(webApplicationBaseURL);
+    }
+
+    async load(classificationId: string): Promise<Content> {
+        const response = await fetch(`${this.mcrApplicationBaseURL}api/v2/classifications/${classificationId}`);
+        const xml = await response.text();
+        const prettifiedXml = this.prettifyXml(xml);
+        if (response.ok) {
+            return {
+                data: prettifiedXml,
+                type: "application/xml"
+            };
+        }
+        throw this.buildError(`Unable to load ${classificationId}.`, response);
+    }
+
+    async save(classificationId: string, content: Content): Promise<void> {
+        const authorizationHeader = await getAuthorizationHeader(this.mcrApplicationBaseURL);
+        const response = await fetch(`${this.mcrApplicationBaseURL}api/v2/classifications/${classificationId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/xml; charset=utf-8",
+                "Authorization": authorizationHeader
+            },
+            body: content.data
+        });
+        if (response.ok) {
+            return;
+        }
+        throw this.buildError(`Unable to save ${classificationId}.`, response);
+    }
+
+    async hasWriteAccess(classificationId: string): Promise<boolean> {
+        // TODO - rest API v2 does not provide support for checking write access
+        return true;
+    }
+
+    /**
+     * Prettifies classification xml. This is a workaround as long as the server does not returns a prettified xml by
+     * itself.
+     * TODO: remove if not necessary anymore
+     *
+     * https://stackoverflow.com/questions/376373/pretty-printing-xml-with-javascript
+     *
+     * @param xml xml to prettify
+     */
+    prettifyXml(xml: string) {
+        const tab = "  ";
+        let formatted = "";
+        let indent = "";
+        xml.split(/>\s*</).forEach(function (node) {
+            if (node.match(/^\/\w/)) indent = indent.substring(tab.length);
+            formatted += `${indent}<${node}>\r\n`;
+            if (node.match(/^<?\w[^>]*[^\/]$/)) indent += tab;
+        });
+        return formatted.substring(1, formatted.length - 3);
+    }
+
+    dirtyAfterSave(id: string): boolean {
+        return false;
+    }
+
+}
