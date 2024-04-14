b1756a43d1c5778ba38be80f6de7a34d6d6f5655
Robert Stephan <robert.stephan@uni-rostock.de>
2024-04-14 12:59:01 +0200

    MCR-3080 add license to class

--- mycore-solr/src/main/java/org/mycore/solr/iddetector/MCRSolrObjectIDDetector.java
+++ mycore-solr/src/main/java/org/mycore/solr/iddetector/MCRSolrObjectIDDetector.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mycore.solr.iddetector;
 
 import java.util.Optional;
@@ -16,6 +33,13 @@ import org.mycore.frontend.iddetector.MCRDefaultObjectIDDetector;
 import org.mycore.frontend.iddetector.MCRObjectIDDetector;
 import org.mycore.solr.MCRSolrClientFactory;
 
+/**
+ * implementation of an MCRObjectIDDetector
+ * 
+ * It uses Solr to retrieve the real id from the given input string
+ *  
+ * @author Robert Stephan
+ */
 public class MCRSolrObjectIDDetector extends MCRDefaultObjectIDDetector implements MCRObjectIDDetector {
 
     private static final Logger LOGGER = LogManager.getLogger();
