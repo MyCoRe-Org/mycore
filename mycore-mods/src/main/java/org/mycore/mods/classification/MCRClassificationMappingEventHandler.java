53a687dc347cc06a29bb87d5e338811e20dc8876
Eva Roddeck <eva.roddeck@uni-due.de>
2024-05-23 16:10:37 +0200

    MCR-3094 checkstyle

--- mycore-mods/src/main/java/org/mycore/mods/classification/MCRClassificationMappingEventHandler.java
+++ mycore-mods/src/main/java/org/mycore/mods/classification/MCRClassificationMappingEventHandler.java
@@ -17,7 +17,16 @@
  */
 package org.mycore.mods.classification;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
