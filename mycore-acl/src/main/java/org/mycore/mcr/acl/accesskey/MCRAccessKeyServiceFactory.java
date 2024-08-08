8916b9e54f5f5852daef059b579a864eeda0a0c6
Lennard Golsch <lennard.golsch@icloud.com>
2024-08-08 16:14:15 +0200

    added comments to factory (melt previous commit

--- mycore-acl/src/main/java/org/mycore/mcr/acl/accesskey/MCRAccessKeyServiceFactory.java
+++ mycore-acl/src/main/java/org/mycore/mcr/acl/accesskey/MCRAccessKeyServiceFactory.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.mcr.acl.accesskey;
 
 import org.mycore.common.config.MCRConfiguration2;
@@ -7,12 +25,20 @@ import org.mycore.mcr.acl.accesskey.persistence.MCRAccessKeyRepository;
 import org.mycore.mcr.acl.accesskey.persistence.MCRAccessKeyRepositoryImpl;
 import org.mycore.mcr.acl.accesskey.value.MCRAccessKeyValueProcessor;
 
+/**
+ * Provides default access key services.
+ */
 public class MCRAccessKeyServiceFactory {
 
     private static volatile MCRAccessKeyService service;
 
     private static final Object lock = new Object();
 
+    /**
+     * Returns single access key service instance.
+     *
+     * @return the instance
+     */
     public static MCRAccessKeyService getService() {
         if (service == null) {
             synchronized (lock) {
