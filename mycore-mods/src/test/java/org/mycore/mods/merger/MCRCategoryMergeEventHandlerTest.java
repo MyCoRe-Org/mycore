b3d072acbbb8ccb7555ac03fa415ec3268d4312c
Eva Roddeck <eva.roddeck@uni-due.de>
2024-09-25 11:02:14 +0200

    unused import

--- mycore-mods/src/test/java/org/mycore/mods/merger/MCRCategoryMergeEventHandlerTest.java
+++ mycore-mods/src/test/java/org/mycore/mods/merger/MCRCategoryMergeEventHandlerTest.java
@@ -2,7 +2,6 @@ package org.mycore.mods.merger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
