200291672a091c2fa4f8fb01ace2005bec33f976
Matthias Eichner <matthias.eichner@uni-jena.de>
2024-10-29 14:24:03 +0100

    MCR-3126 make ocfl compatible with changes in main

--- mycore-ocfl/src/test/java/org/mycore/ocfl/niofs/MCROCFLBasicFileAttributesTest.java
+++ mycore-ocfl/src/test/java/org/mycore/ocfl/niofs/MCROCFLBasicFileAttributesTest.java
@@ -14,7 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.Test;
-import org.mycore.common.MCRTransactionHelper;
+import org.mycore.common.MCRTransactionManager;
 import org.mycore.common.digest.MCRDigest;
 import org.mycore.datamodel.niofs.MCRFileAttributes;
 import org.mycore.datamodel.niofs.MCRPath;
@@ -33,14 +33,14 @@ public class MCROCFLBasicFileAttributesTest extends MCROCFLNioTestCase {
         assertNull("directory digest should be null", rootAttributes.digest());
 
         Thread.sleep(1);
-        MCRTransactionHelper.requireTransaction(MCROCFLFileSystemTransaction.class);
+        MCRTransactionManager.requireTransactions(MCROCFLFileSystemTransaction.class);
         Files.write(MCRPath.getPath(DERIVATE_1, "file1"), new byte[] { 1, 3, 3, 7 });
         MCRFileAttributes<?> rootAttributes2 = Files.readAttributes(root, MCRFileAttributes.class);
         assertEquals("should have the same creation time after writing a file", rootAttributes.creationTime(),
             rootAttributes2.creationTime());
         assertTrue("modified time should be later",
             rootAttributes2.lastModifiedTime().toInstant().isAfter(rootAttributes.lastModifiedTime().toInstant()));
-        MCRTransactionHelper.commitTransaction(MCROCFLFileSystemTransaction.class);
+        MCRTransactionManager.commitTransactions(MCROCFLFileSystemTransaction.class);
     }
 
     @Test
@@ -52,14 +52,14 @@ public class MCROCFLBasicFileAttributesTest extends MCROCFLNioTestCase {
         assertNotNull("'white.png' should have a digest", sourceAttributes.digest());
 
         MCRPath newFile = MCRPath.getPath(DERIVATE_1, "file1");
-        MCRTransactionHelper.beginTransaction();
+        MCRTransactionManager.beginTransactions();
         Files.write(newFile, new byte[] { 1, 3, 3, 7 });
         MCRFileAttributes<?> newFileAttributes = Files.readAttributes(newFile, MCRFileAttributes.class);
         assertFalse("'file1' should not be a directory", newFileAttributes.isDirectory());
         assertTrue("'file1' should not be a regular file", newFileAttributes.isRegularFile());
         MCRDigest newFileDigest = newFileAttributes.digest();
         assertNotNull("'file1' should have a digest", newFileDigest);
-        MCRTransactionHelper.commitTransaction();
+        MCRTransactionManager.commitTransactions();
 
         MCRFileAttributes<?> newFileAttributesAfterCommit = Files.readAttributes(newFile, MCRFileAttributes.class);
         MCRDigest afterCommitDigest = newFileAttributesAfterCommit.digest();
@@ -71,13 +71,13 @@ public class MCROCFLBasicFileAttributesTest extends MCROCFLNioTestCase {
     public void moveAndCheckAttributes() throws IOException {
         Path source = MCRPath.getPath(DERIVATE_1, "white.png");
         Path target = MCRPath.getPath(DERIVATE_2, "moved.png");
-        MCRTransactionHelper.beginTransaction();
+        MCRTransactionManager.beginTransactions();
         Files.move(source, target);
         BasicFileAttributes targetFileAttributes = Files.readAttributes(target, BasicFileAttributes.class);
         assertNotNull("'moved.png' should have basic file attributes", targetFileAttributes);
         assertTrue("'moved.png' should be a regular file", targetFileAttributes.isRegularFile());
         assertThrows(NoSuchFileException.class, () -> Files.readAttributes(source, BasicFileAttributes.class));
-        MCRTransactionHelper.commitTransaction();
+        MCRTransactionManager.commitTransactions();
     }
 
 }
