3c134aa92c9486a4401b1ab2b2424ee9b4b9664d
Matthias Eichner <matthias.eichner@uni-jena.de>
2024-10-30 16:50:43 +0100

    MCR-3126 fix license

--- mycore-ocfl/src/main/java/org/mycore/ocfl/util/MCROCFLVersionHelper.java
+++ mycore-ocfl/src/main/java/org/mycore/ocfl/util/MCROCFLVersionHelper.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ***  M y C o R e  ***
- * See http://www.mycore.de/ for details.
+ * See https://www.mycore.de/ for details.
  *
  * MyCoRe is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -54,7 +54,6 @@ public class MCROCFLVersionHelper {
      *   <li>"Updated" -> {@link MCROCFLMetadataVersion#UPDATED}</li>
      *   <li>"Deleted" -> {@link MCROCFLMetadataVersion#DELETED}</li>
      * </ul>
-     * </p>
      */
     public static final Map<String, Character> MESSAGE_TYPE_MAPPING = Map.ofEntries(
         Map.entry(MESSAGE_CREATED, MCROCFLMetadataVersion.CREATED),
