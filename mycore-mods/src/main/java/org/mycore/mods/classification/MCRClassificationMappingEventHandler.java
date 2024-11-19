ff600e9c9eaa8f40f1097541427febd322f4d5d1
Thomas Scheffler <github@yagee.de>
2024-11-19 15:22:52 +0100

    MCR-3299 throw exception on invalid MCRCategoryID

--- mycore-mods/src/main/java/org/mycore/mods/classification/MCRClassificationMappingEventHandler.java
+++ mycore-mods/src/main/java/org/mycore/mods/classification/MCRClassificationMappingEventHandler.java
@@ -96,7 +96,6 @@ public class MCRClassificationMappingEventHandler extends MCREventHandlerBase {
             final MCRCategoryDAO dao = MCRCategoryDAOFactory.getInstance();
             String label = labelOptional.get().getText();
             return Stream.of(label.split("\\s"))
-                .filter(MCRCategoryID::isValid)
                 .map(MCRCategoryID::fromString)
                 .filter(id -> !id.isRootID())
                 .filter(dao::exist)
