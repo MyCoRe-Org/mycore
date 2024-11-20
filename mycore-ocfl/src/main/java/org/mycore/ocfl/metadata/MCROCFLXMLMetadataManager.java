8fe9e47e29460ca9b53badf93ba3b01f857a43b9
Matthias Eichner <matthias.eichner@uni-jena.de>
2024-11-20 17:09:15 +0100

    MCR-3126 add s3 support
    -use own RepositoryBuilder
    -add S3RepositoryProvider
    -improve directory history method by using inventory API

--- mycore-ocfl/src/main/java/org/mycore/ocfl/metadata/MCROCFLXMLMetadataManager.java
+++ mycore-ocfl/src/main/java/org/mycore/ocfl/metadata/MCROCFLXMLMetadataManager.java
@@ -233,7 +233,7 @@ public class MCROCFLXMLMetadataManager implements MCRXMLMetadataManagerAdapter {
     protected InputStream getStoredContentStream(MCRObjectID mcrid, OcflObjectVersion storeObject) throws IOException {
         String path = buildFilePath(mcrid);
         OcflObjectVersionFile file = storeObject.getFile(path);
-        if(file == null) {
+        if (file == null) {
             throw new IOException("Couldn't find path '" + path + "' in '" + storeObject.getObjectId() + "'.");
         }
         return file.getStream();
@@ -335,9 +335,10 @@ public class MCROCFLXMLMetadataManager implements MCRXMLMetadataManagerAdapter {
         int maxDepth = Integer.MAX_VALUE;
 
         MCROCFLRepositoryProvider ocflRepoProvider = MCROCFLRepositoryProvider.getProvider(repositoryKey);
-        if(!(ocflRepoProvider instanceof MCROCFLLocalRepositoryProvider localRepositoryProvider)) {
-            throw new MCROCFLException("Cannot call getHighestStoredID() on non local repository."
-                + " Use MCRFileBaseCacheObjectIDGenerator instead!");
+        if (!(ocflRepoProvider instanceof MCROCFLLocalRepositoryProvider localRepositoryProvider)) {
+            throw new MCROCFLException("Cannot call getHighestStoredID() on non local repository. Set "
+                + "MCR.Metadata.ObjectID.Generator.Class=org.mycore.datamodel.common.MCRFileBaseCacheObjectIDGenerator "
+                + "instead!");
         }
 
         OcflExtensionConfig config = localRepositoryProvider.getExtensionConfig();
