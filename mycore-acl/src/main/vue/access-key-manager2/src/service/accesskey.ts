7ff4b68e9b97c42aed449dae4c3cac2436e5b233
Lennard Golsch <lennard.golsch@icloud.com>
2025-02-03 16:22:38 +0100

    fixes

--- mycore-acl/src/main/vue/access-key-manager2/src/service/accesskey.ts
+++ mycore-acl/src/main/vue/access-key-manager2/src/service/accesskey.ts
@@ -3,7 +3,6 @@ import {
   CreateAccessKeyDto,
   PartialUpdateAccessKeyDto,
 } from '@/dtos/accesskey';
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export interface AuthStrategy {
   getHeaders(): Record<string, string>;
@@ -28,27 +27,163 @@ export interface AccessKeyInformation {
   totalCount: number;
 }
 
-const extractResponse = (response: AxiosResponse): AccessKeyInformation => {
+const extractResponse = async (
+  response: Response
+): Promise<AccessKeyInformation> => {
+  const totalCount = response.headers.get('x-total-count');
   return {
-    accessKeys: response.data,
-    totalCount: parseInt(response.headers['x-total-count'], 10),
+    accessKeys: await response.json(),
+    totalCount: totalCount ? parseInt(totalCount, 10) : 0,
   };
 };
 
 const API_URL = 'api/v2/access-keys/';
 
-// TODO switch to fetch
+const request = async (
+  url: URL,
+  method: 'GET' | 'POST' | 'PATCH' | 'DELETE' | 'PUT',
+  headers: HeadersInit = {},
+  body?: string
+): Promise<Response> => {
+  const requestHeaders = new Headers(headers);
+  const config: RequestInit = {
+    method,
+    headers: requestHeaders,
+  };
+  if (body) {
+    config.body = body;
+  }
+  try {
+    const response = await fetch(url, config);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    return response;
+  } catch (error) {
+    console.error('Request failed', error);
+    throw error;
+  }
+};
+
+export class MCRHttpClient {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+  }
+
+  public async request(
+    url: string,
+    method: 'GET' | 'POST' | 'PATCH' | 'DELETE' | 'PUT',
+    params: Record<string, string | number | boolean> = {},
+    headers: HeadersInit = {},
+    body?: string
+  ): Promise<Response> {
+    const urlObj = new URL(
+      `${this.baseUrl}/${url.startsWith('/') ? url.slice(1) : url}`
+    );
+    if (params) {
+      Object.entries(params).forEach(([key, value]) => {
+        urlObj.searchParams.append(key, String(value));
+      });
+    }
+    return request(urlObj, method, headers, body);
+  }
+}
+
+export class MCRRestHttpClient extends MCRHttpClient {
+  private defaultHeaders: HeadersInit = {};
+
+  constructor(baseUrl: string, authStrategy?: AuthStrategy) {
+    super(baseUrl);
+    if (authStrategy) {
+      this.defaultHeaders = {
+        ...authStrategy.getHeaders(),
+      };
+    }
+  }
+
+  public async get(
+    url: string,
+    params: Record<string, string | number | boolean> = {},
+    responseType: 'json',
+    headers: HeadersInit = {}
+  ): Promise<Response> {
+    const requestHeaders = new Headers(
+      Object.assign({}, this.defaultHeaders, headers)
+    );
+    if (responseType === 'json') {
+      requestHeaders.set('Content-Accept', 'application/json');
+    }
+    return await super.request(url, 'GET', params, requestHeaders);
+  }
+
+  public async delete(
+    url: string,
+    params: Record<string, string | number | boolean> = {},
+    headers: HeadersInit = {}
+  ): Promise<void> {
+    const requestHeaders = new Headers(
+      Object.assign({}, this.defaultHeaders, headers)
+    );
+    await super.request(url, 'DELETE', params, requestHeaders);
+  }
+
+  public async post(
+    url: string,
+    body: string,
+    bodyType: 'json',
+    params: Record<string, string | number | boolean> = {},
+    headers: HeadersInit = {}
+  ): Promise<Response> {
+    return this.write(url, 'POST', params, body, bodyType, headers);
+  }
+
+  public async patch(
+    url: string,
+    body: string,
+    bodyType: 'json',
+    params: Record<string, string | number | boolean> = {},
+    headers: HeadersInit = {}
+  ): Promise<Response> {
+    return this.write(url, 'PATCH', params, body, bodyType, headers);
+  }
+
+  public async put(
+    url: string,
+    body: string,
+    bodyType: 'json',
+    params: Record<string, string | number | boolean> = {},
+    headers: HeadersInit = {}
+  ): Promise<Response> {
+    return this.write(url, 'PUT', params, body, bodyType, headers);
+  }
+
+  private async write(
+    url: string,
+    method: 'POST' | 'PUT' | 'PATCH',
+    params: Record<string, string | number | boolean> = {},
+    body: string,
+    bodyType: string,
+    headers: HeadersInit = {}
+  ): Promise<Response> {
+    const requestHeaders = new Headers(
+      Object.assign({}, this.defaultHeaders, headers)
+    );
+    if (bodyType === 'json') {
+      requestHeaders.set('Content-Type', 'application/json');
+    } else {
+      throw new Error(`Body type: ${bodyType} is not allowed`);
+    }
+    return await super.request(url, method, params, requestHeaders, body);
+  }
+}
+
 export class AccessKeyService {
-  private axiosInstance: AxiosInstance;
+  private client: MCRRestHttpClient;
 
   constructor(baseUrl: string, authStrategy: AuthStrategy) {
-    this.axiosInstance = axios.create({
-      baseURL: baseUrl,
-      timeout: 5000,
-    });
-    this.axiosInstance.defaults.headers.common = {
-      ...authStrategy.getHeaders(),
-    };
+    this.client = new MCRRestHttpClient(baseUrl, authStrategy);
   }
 
   public async getAccessKeys(
@@ -57,53 +192,66 @@ export class AccessKeyService {
     offset?: number,
     limit?: number
   ): Promise<AccessKeyInformation> {
-    return extractResponse(
-      await this.axiosInstance.get<AccessKeyDto[]>(API_URL, {
-        params: {
-          reference,
-          permissions:
-            permissions.length > 0 ? permissions.join(',') : undefined,
-          offset,
-          limit,
-        },
-      })
-    );
+    const params: Record<string, string | number> = {};
+    if (reference) {
+      params['reference'] = reference;
+    }
+    if (permissions.length > 0) {
+      params['permissions'] = permissions.join(',');
+    }
+    if (offset) {
+      params['offset'] = offset;
+    }
+    if (limit) {
+      params['limit'] = limit;
+    }
+    return extractResponse(await this.client.get(API_URL, params, 'json'));
   }
 
   public async getAccessKeysByReference(
     reference: string,
-    offset: number,
-    limit: number
+    offset?: number,
+    limit?: number
   ): Promise<AccessKeyInformation> {
-    return extractResponse(
-      await this.axiosInstance.get<AccessKeyDto[]>(API_URL, {
-        params: { reference, offset, limit },
-      })
-    );
+    const params: Record<string, string | number> = {};
+    if (reference) {
+      params['reference'] = reference;
+    }
+    if (offset) {
+      params['offset'] = offset;
+    }
+    if (limit) {
+      params['limit'] = limit;
+    }
+    return extractResponse(await this.client.get(API_URL, params, 'json'));
   }
 
   public async getAccessKey(id: string): Promise<AccessKeyDto> {
-    return (await this.axiosInstance.get<AccessKeyDto>(`${API_URL}${id}`)).data;
+    return (await this.client.get(`${API_URL}${id}`, {}, 'json')).json();
   }
 
   public async createAccessKey(accessKey: CreateAccessKeyDto): Promise<string> {
-    return (
-      (await this.axiosInstance.post(API_URL, accessKey, {
-        headers: { 'Content-Type': 'application/json' },
-      })) as AxiosResponse
-    ).headers.location
-      .split('/')
-      .pop() as string;
+    const response = await this.client.post(
+      API_URL,
+      JSON.stringify(accessKey),
+      'json'
+    );
+    const locationHeader = response.headers.get('Location');
+    return locationHeader?.split('/').pop() as string;
   }
 
   public async patchAccessKey(
     id: string,
     accessKey: PartialUpdateAccessKeyDto
   ): Promise<void> {
-    await this.axiosInstance.patch(`${API_URL}${id}`, accessKey);
+    await this.client.patch(
+      `${API_URL}${id}`,
+      JSON.stringify(accessKey),
+      'json'
+    );
   }
 
   public async deleteAccessKey(id: string): Promise<void> {
-    await this.axiosInstance.delete(`${API_URL}${id}`);
+    this.client.delete(`${API_URL}${id}`);
   }
 }
