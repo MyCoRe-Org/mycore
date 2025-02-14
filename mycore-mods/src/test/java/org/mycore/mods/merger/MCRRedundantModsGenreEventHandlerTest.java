9837719a96e36b108c8bfa953e7208a3ff00c688
Eva Roddeck <eva.roddeck@uni-due.de>
2025-02-14 13:23:07 +0100

    MCR-3198 add license info to all new classes

--- mycore-mods/src/test/java/org/mycore/mods/merger/MCRRedundantModsGenreEventHandlerTest.java
+++ mycore-mods/src/test/java/org/mycore/mods/merger/MCRRedundantModsGenreEventHandlerTest.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See https://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.mods.merger;
 
 import org.apache.logging.log4j.LogManager;
@@ -38,7 +56,6 @@ public class MCRRedundantModsGenreEventHandlerTest extends MCRJPATestCase {
     }
 
     private Element loadMods(String fileName) throws Exception {
-
         MCRObject object = new MCRObject();
         Document modsDocument = loadXml(fileName);
 
@@ -52,7 +69,6 @@ public class MCRRedundantModsGenreEventHandlerTest extends MCRJPATestCase {
         LOGGER.info(new MCRJDOMContent(modsWrapper.getMODS()).asString());
 
         return modsWrapper.getMODS();
-
     }
 
     private static Document loadXml(String fileName) throws JDOMException, IOException {
