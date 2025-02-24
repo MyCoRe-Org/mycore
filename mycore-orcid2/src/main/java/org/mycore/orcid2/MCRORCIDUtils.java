f35f53a085800e783891705244209b22fb4dd25e
Lennard Golsch <lennard.golsch@icloud.com>
2025-02-24 12:32:14 +0100

    MCR-3341 fixed XPath in MCRORCIDUtils#getNameIdentifiers

--- mycore-orcid2/src/main/java/org/mycore/orcid2/MCRORCIDUtils.java
+++ mycore-orcid2/src/main/java/org/mycore/orcid2/MCRORCIDUtils.java
@@ -75,7 +75,7 @@ public class MCRORCIDUtils {
 
     /**
      * Checks if MCRObjects' state is ready to publish.
-     * 
+     *
      * @param object the MCRObject
      * @return true if state is ready to publish or there is not state
      */
@@ -85,7 +85,7 @@ public class MCRORCIDUtils {
 
     /**
      * Checks if state is ready to publish.
-     * 
+     *
      * @param state the state
      * @return true if state is ready to publish
      */
@@ -98,7 +98,7 @@ public class MCRORCIDUtils {
 
     /**
      * Checks if MCRObject has empty MODS.
-     * 
+     *
      * @param object the MCRObject
      * @return true if MCRObject's MODS has children
      */
@@ -109,7 +109,7 @@ public class MCRORCIDUtils {
 
     /**
      * Filters MCRObject.
-     * 
+     *
      * @param object the MCRObject
      * @return filtered MCRObject
      * @throws MCRORCIDException if filtering fails
@@ -126,7 +126,7 @@ public class MCRORCIDUtils {
 
     /**
      * Compares String with auth client's client id.
-     * 
+     *
      * @param sourcePath source path
      * @return true if source path equals client id
      */
@@ -136,7 +136,7 @@ public class MCRORCIDUtils {
 
     /**
      * Builds a modsCollection Element and adds given elements.
-     * 
+     *
      * @param elements List of elements
      * @return Element containing elements
      */
@@ -148,7 +148,7 @@ public class MCRORCIDUtils {
 
     /**
      * Extracts all ORCID name identifiers of MCRObject.
-     * 
+     *
      * @param object the MCRObject
      * @return Set of ORCID MCRIdentifier
      */
@@ -159,7 +159,7 @@ public class MCRORCIDUtils {
 
     /**
      * Lists mods:name Elements.
-     * 
+     *
      * @param wrapper the MCRMODSWrapper
      * @return List of name elements
      */
@@ -169,7 +169,7 @@ public class MCRORCIDUtils {
 
     /**
      * Returns mods:nameIdentifer.
-     * 
+     *
      * @param nameElement the mods:name Element
      * @return Set of MCRIdentifier
      */
@@ -181,12 +181,12 @@ public class MCRORCIDUtils {
 
     /**
      * Returns mods:nameIdentifer.
-     * 
+     *
      * @param wrapper the MCRMODSWrapper
      * @return Set of MCRIdentifier
      */
     public static Set<MCRIdentifier> getNameIdentifiers(MCRMODSWrapper wrapper) {
-        return wrapper.getElements("mods:nameIdentifier").stream().map(e -> getIdentfierFromElement(e))
+        return wrapper.getElements("mods:name/mods:nameIdentifier").stream().map(e -> getIdentfierFromElement(e))
             .collect(Collectors.toSet());
     }
 
