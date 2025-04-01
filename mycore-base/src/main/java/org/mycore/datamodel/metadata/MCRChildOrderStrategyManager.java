59f6049a4092fecbf89b0b793a24aae341f92232
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2025-04-01 17:05:06 +0200

    MCR-3375 adapt to recent changes in mycore

--- mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRChildOrderStrategyManager.java
+++ mycore-base/src/main/java/org/mycore/datamodel/metadata/MCRChildOrderStrategyManager.java
@@ -2,12 +2,16 @@ package org.mycore.datamodel.metadata;
 
 import org.mycore.common.config.MCRConfiguration2;
 
-public class MCRChildOrderStrategyManager {
+public final class MCRChildOrderStrategyManager {
 
     private static volatile MCRChildOrderStrategy childOrderStrategy;
 
-    public static MCRChildOrderStrategy getChildOrderStrategy() {
-        if (childOrderStrategy == null) {
+
+  private MCRChildOrderStrategyManager() {
+  }
+
+  public static MCRChildOrderStrategy getChildOrderStrategy() {
+    if (childOrderStrategy == null) {
             synchronized (MCRChildOrderStrategyManager.class) {
                 if (childOrderStrategy == null) {
                     childOrderStrategy = MCRConfiguration2.getSingleInstanceOfOrThrow(MCRChildOrderStrategy.class,
