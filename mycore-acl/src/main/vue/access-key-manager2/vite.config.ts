a2abaccd5fa6672f98d688d27c4ca793ff1dd356
Lennard Golsch <lennard.golsch@icloud.com>
2025-04-01 14:44:33 +0200

    fixed structure

--- mycore-acl/src/main/vue/access-key-manager2/vite.config.ts
+++ mycore-acl/src/main/vue/access-key-manager2/vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-// @ts-expect-error See https://github.com/gxmari007/vite-plugin-eslint/issues/79
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
 import externalGlobals from 'rollup-plugin-external-globals';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default {
   plugins: [
     vue(),
     eslint(),
@@ -23,4 +20,4 @@ export default defineConfig({
     outDir: '../../../../target/classes/META-INF/resources/access-key-manager',
   },
   base: './',
-});
+};
