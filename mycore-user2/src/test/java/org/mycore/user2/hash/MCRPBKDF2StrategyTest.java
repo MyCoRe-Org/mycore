6690f8087ac549631a4a025f8c5c2c54cd744fcf
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2025-04-02 14:08:49 +0200

    MCR-3385 migrate password check manager and strategy tests to JUnit 5

--- mycore-user2/src/test/java/org/mycore/user2/hash/MCRPBKDF2StrategyTest.java
+++ mycore-user2/src/test/java/org/mycore/user2/hash/MCRPBKDF2StrategyTest.java
@@ -18,16 +18,17 @@
 
 package org.mycore.user2.hash;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.SecureRandom;
 
-import org.junit.Test;
-import org.mycore.common.MCRTestCase;
+import org.junit.jupiter.api.Test;
+import org.mycore.test.MyCoReTest;
 
-public class MCRPBKDF2StrategyTest extends MCRTestCase {
+@MyCoReTest
+public class MCRPBKDF2StrategyTest {
 
     public static final String TYPE = "type";
 
