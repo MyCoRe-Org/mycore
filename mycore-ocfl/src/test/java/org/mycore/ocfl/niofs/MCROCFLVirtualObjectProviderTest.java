3292ea8efd38e8a2ef2eb632e8a44e700ca26553
Matthias Eichner <thegreatmewel@gmail.com>
2025-04-14 13:14:41 +0200

    MCR-3126 use junit5

--- mycore-ocfl/src/test/java/org/mycore/ocfl/niofs/MCROCFLVirtualObjectProviderTest.java
+++ mycore-ocfl/src/test/java/org/mycore/ocfl/niofs/MCROCFLVirtualObjectProviderTest.java
@@ -1,76 +1,89 @@
 package org.mycore.ocfl.niofs;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.FileSystemException;
 import java.nio.file.Files;
 
-import org.junit.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mycore.common.MCRTransactionManager;
 import org.mycore.datamodel.niofs.MCRPath;
+import org.mycore.ocfl.MCROCFLTestCaseHelper;
+import org.mycore.ocfl.repository.MCROCFLRepository;
+import org.mycore.ocfl.test.PermutedParam;
+import org.mycore.ocfl.test.MCRPermutationExtension;
+import org.mycore.ocfl.test.MCROCFLSetupExtension;
+import org.mycore.test.MyCoReTest;
 
-public class MCROCFLVirtualObjectProviderTest extends MCROCFLNioTestCase {
+@MyCoReTest
+@ExtendWith({ MCRPermutationExtension.class, MCROCFLSetupExtension.class })
+public class MCROCFLVirtualObjectProviderTest {
 
-    public MCROCFLVirtualObjectProviderTest(boolean remote, boolean purge) {
-        super(remote, purge);
-    }
+    protected MCROCFLRepository repository;
 
-    @Test
+    @PermutedParam
+    private boolean remote;
+
+    @PermutedParam
+    private boolean purge;
+
+    @TestTemplate
     public void exists() throws FileSystemException {
         MCROCFLVirtualObjectProvider virtualObjectProvider = MCROCFLFileSystemProvider.get().virtualObjectProvider();
         MCROCFLFileSystem fs = MCROCFLFileSystemProvider.getMCROCFLFileSystem();
 
         // check default
-        assertTrue(virtualObjectProvider.exists(DERIVATE_1));
-        assertFalse(virtualObjectProvider.exists(DERIVATE_2));
+        assertTrue(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_1));
+        assertFalse(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
 
         // check after read map is filled
-        assertFalse(Files.exists(MCRPath.getPath(DERIVATE_2, "/")));
-        assertFalse(virtualObjectProvider.exists(DERIVATE_2));
+        assertFalse(Files.exists(MCRPath.getPath(MCROCFLTestCaseHelper.DERIVATE_2, "/")));
+        assertFalse(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
 
         // check after write map is filled
         MCRTransactionManager.beginTransactions();
-        assertFalse(Files.exists(MCRPath.getPath(DERIVATE_2, "/")));
-        assertFalse(virtualObjectProvider.exists(DERIVATE_2));
-        fs.createRoot(DERIVATE_2);
-        assertTrue(virtualObjectProvider.exists(DERIVATE_2));
+        assertFalse(Files.exists(MCRPath.getPath(MCROCFLTestCaseHelper.DERIVATE_2, "/")));
+        assertFalse(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
+        fs.createRoot(MCROCFLTestCaseHelper.DERIVATE_2);
+        assertTrue(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
         MCRTransactionManager.commitTransactions();
-        assertTrue(virtualObjectProvider.exists(DERIVATE_2));
+        assertTrue(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
 
         // check after rm
         MCRTransactionManager.beginTransactions();
-        fs.removeRoot(DERIVATE_2);
-        assertTrue(virtualObjectProvider.exists(DERIVATE_2));
+        fs.removeRoot(MCROCFLTestCaseHelper.DERIVATE_2);
+        assertTrue(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
         MCRTransactionManager.commitTransactions();
 
-        if (isPurge()) {
-            assertFalse(virtualObjectProvider.exists(DERIVATE_2));
+        if (purge) {
+            assertFalse(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
         } else {
-            assertTrue(virtualObjectProvider.exists(DERIVATE_2));
+            assertTrue(virtualObjectProvider.exists(MCROCFLTestCaseHelper.DERIVATE_2));
         }
     }
 
-    @Test
+    @TestTemplate
     public void isDeleted() throws FileSystemException {
         MCROCFLVirtualObjectProvider virtualObjectProvider = MCROCFLFileSystemProvider.get().virtualObjectProvider();
         MCROCFLFileSystem fs = MCROCFLFileSystemProvider.getMCROCFLFileSystem();
 
         // check default
-        assertFalse(virtualObjectProvider.isDeleted(DERIVATE_1));
-        assertFalse(virtualObjectProvider.isDeleted(DERIVATE_2));
+        assertFalse(virtualObjectProvider.isDeleted(MCROCFLTestCaseHelper.DERIVATE_1));
+        assertFalse(virtualObjectProvider.isDeleted(MCROCFLTestCaseHelper.DERIVATE_2));
 
         // delete
         MCRTransactionManager.beginTransactions();
-        fs.removeRoot(DERIVATE_1);
-        assertFalse(virtualObjectProvider.isDeleted(DERIVATE_1));
+        fs.removeRoot(MCROCFLTestCaseHelper.DERIVATE_1);
+        assertFalse(virtualObjectProvider.isDeleted(MCROCFLTestCaseHelper.DERIVATE_1));
         MCRTransactionManager.commitTransactions();
 
         // check after commit
-        if (isPurge()) {
-            assertFalse(virtualObjectProvider.isDeleted(DERIVATE_1));
+        if (purge) {
+            assertFalse(virtualObjectProvider.isDeleted(MCROCFLTestCaseHelper.DERIVATE_1));
         } else {
-            assertTrue(virtualObjectProvider.isDeleted(DERIVATE_1));
+            assertTrue(virtualObjectProvider.isDeleted(MCROCFLTestCaseHelper.DERIVATE_1));
         }
     }
 
