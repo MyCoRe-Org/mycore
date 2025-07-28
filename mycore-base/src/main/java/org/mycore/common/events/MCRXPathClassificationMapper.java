2adcdcf5c2840f145aa9dae329e6fa3df6bd8001
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2025-07-28 18:06:29 +0200

    MCR-3479 extract common base class for XPath classification mappers

--- mycore-base/src/main/java/org/mycore/common/events/MCRXPathClassificationMapper.java
+++ mycore-base/src/main/java/org/mycore/common/events/MCRXPathClassificationMapper.java
@@ -17,29 +17,24 @@
  */
 package org.mycore.common.events;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
+import org.jdom2.Parent;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.common.config.annotation.MCRConfigurationProxy;
 import org.mycore.common.config.annotation.MCRProperty;
 import org.mycore.common.events.MCRClassificationMappingEventHandler.Mapper;
-import org.mycore.common.xml.MCRXPathEvaluator;
 import org.mycore.datamodel.classifications2.MCRCategoryDAO;
 import org.mycore.datamodel.classifications2.MCRCategoryID;
 
 /**
- * A {@link MCRXMappingClassificationMapper} is a {@link Mapper} that looks for mapping
- * information for a given list of classifications, by checking if XPaths configured for categories of such
- * classifications match the metadata document.
+ * A {@link MCRXMappingClassificationMapper} is a {@link Mapper} that looks for mapping information
+ * for a given list of classifications, by checking if XPaths configured for categories of such classifications
+ * match the metadata document.
  * <p>
  * For each classification, the set of classification categories containing a <code>x-mapping-xpath</code> label is
  * obtained. For all such classification categories, the corresponding XPath is evaluated and if the metadata document
@@ -50,18 +45,18 @@ import org.mycore.datamodel.classifications2.MCRCategoryID;
  * <pre><code>
  * &lt;category ID=&quot;article&quot;&gt;
  * &nbsp;&lt;label xml:lang=&quot;en&quot; text=&quot;Article&quot; /&gt;
- * &nbsp;&lt;label xml:lang=&quot;x-mapping-xpath&quot; text=&quot;mods:genre[text()='article']&quot; /&gt;
- * &nbsp;&lt;label xml:lang=&quot;x-mapping-xpathfb&quot; text=&quot;mods:genre[text()='other']&quot; /&gt;
+ * &nbsp;&lt;label xml:lang=&quot;x-mapping-xpath&quot; text=&quot;genre[text()='article']&quot; /&gt;
+ * &nbsp;&lt;label xml:lang=&quot;x-mapping-xpathfb&quot; text=&quot;genre[text()='other']&quot; /&gt;
  * &lt;/category&gt;
  * </code></pre>
  * If <code>foo_bar</code> is in the list of classifications, the classification category
- * <code>foo_bar:article</code> will be provided, if the XPath <code>mods:genre[text()='article']</code> is matching
- * (or if no other classification category has a matching XPath and <code>mods:genre[text()='other']</code> is
+ * <code>foo_bar:article</code> will be provided, if the XPath <code>genre[text()='article']</code> is matching
+ * (or if no other classification category has a matching XPath and <code>genre[text()='other']</code> is
  * matching).
  * <p>
  * The following configuration options are available:
  * <ul>
- * <li> The property suffix {@link MCRXPathClassificationMapper#CLASSIFICATION_IDS_KEY} can be used to
+ * <li> The property suffix {@link MCRXPathClassificationMapperBase#CLASSIFICATION_IDS_KEY} can be used to
  * specify the comma separated list of classifications IDs.
  * </ul>
  * Example:
@@ -71,73 +66,26 @@ import org.mycore.datamodel.classifications2.MCRCategoryID;
  * </code></pre>
  */
 @MCRConfigurationProxy(proxyClass = MCRXPathClassificationMapper.Factory.class)
-public class MCRXPathClassificationMapper implements Mapper {
-
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    private static final String LABEL_LANG_XPATH_MAPPING = "x-mapping-xpath";
-
-    private static final String LABEL_LANG_XPATH_MAPPING_FALLBACK = "x-mapping-xpathfb";
-
-    public static final String CLASSIFICATION_IDS_KEY = "ClassificationIds";
-
-    private final List<String> classificationsIds;
+public class MCRXPathClassificationMapper
+    extends MCRXPathClassificationMapperBase<Document, MCRCategoryID>
+    implements Mapper {
 
     public MCRXPathClassificationMapper(String... classificationsIds) {
         this(Arrays.asList(Objects.requireNonNull(classificationsIds, "Classification IDs must not be null")));
     }
 
     public MCRXPathClassificationMapper(List<String> classificationsIds) {
-        this.classificationsIds = new ArrayList<>(Objects
-                .requireNonNull(classificationsIds, "Classification IDs must not be null"));
-        this.classificationsIds.forEach(filter -> Objects
-                .requireNonNull(filter, "Classification ID must not be null"));
+        super(classificationsIds);
     }
 
     @Override
-    public List<MCRCategoryID> findMappings(MCRCategoryDAO dao, Document metadataDocument) {
-        return classificationsIds.stream()
-                .flatMap(xPathClassificationId -> findMappings(dao, metadataDocument, xPathClassificationId))
-                .peek(XPathMapping::logInfo)
-                .map(XPathMapping::toCategoryId)
-                .toList();
+    protected Parent toParent(MCRCategoryDAO dao, Document metadataDocument) {
+        return metadataDocument;
     }
 
-    private Stream<XPathMapping> findMappings(MCRCategoryDAO dao, Document metadataDocument, String classificationId) {
-        List<XPathMapping> mappings = findMappings(dao, metadataDocument, classificationId, LABEL_LANG_XPATH_MAPPING);
-        if (!mappings.isEmpty()) {
-            return mappings.stream();
-        }
-        return findMappings(dao, metadataDocument, classificationId, LABEL_LANG_XPATH_MAPPING_FALLBACK).stream();
-    }
-
-    private List<XPathMapping> findMappings(MCRCategoryDAO dao, Document metadataDocument,
-                                            String xPathClassificationId, String xLanguage) {
-        List<XPathMapping> mappings = new ArrayList<>();
-        dao.getCategoriesByClassAndLang(xPathClassificationId, xLanguage).forEach(category -> {
-            assert category.getLabel(xLanguage).isPresent();
-            String xPath = category.getLabel(xLanguage).get().getText();
-            xPath = MCRClassificationMappingUtil.replacePattern(xPath);
-            MCRXPathEvaluator evaluator = new MCRXPathEvaluator(new HashMap<>(), metadataDocument);
-            if (evaluator.test(xPath)) {
-                mappings.add(new XPathMapping(xLanguage, category.getId()));
-            }
-        });
-        return mappings;
-    }
-
-    private record XPathMapping(String xLanguage, MCRCategoryID targetCategoryId) {
-
-        private void logInfo() {
-            if (LOGGER.isInfoEnabled()) {
-                LOGGER.info("found x-path-mapping to {} for {}", targetCategoryId.toString(), xLanguage);
-            }
-        }
-
-        private MCRCategoryID toCategoryId() {
-            return targetCategoryId;
-        }
-
+    @Override
+    protected MCRCategoryID toMappedValue(XPathMapping mapping) {
+        return mapping.targetCategoryId();
     }
 
     public static class Factory implements Supplier<MCRXPathClassificationMapper> {
--- mycore-mods/src/main/java/org/mycore/mods/classification/MCRMODSXPathClassificationMapper.java
+++ mycore-mods/src/main/java/org/mycore/mods/classification/MCRMODSXPathClassificationMapper.java
@@ -17,23 +17,17 @@
  */
 package org.mycore.mods.classification;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jdom2.Element;
+import org.jdom2.Parent;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.common.config.annotation.MCRConfigurationProxy;
 import org.mycore.common.config.annotation.MCRProperty;
-import org.mycore.common.events.MCRClassificationMappingUtil;
-import org.mycore.common.xml.MCRXPathEvaluator;
+import org.mycore.common.events.MCRXPathClassificationMapperBase;
 import org.mycore.datamodel.classifications2.MCRCategoryDAO;
 import org.mycore.datamodel.classifications2.MCRCategoryID;
 import org.mycore.mods.MCRMODSWrapper;
@@ -41,9 +35,9 @@ import org.mycore.mods.classification.MCRMODSClassificationMappingEventHandler.M
 import org.mycore.mods.classification.MCRMODSClassificationMappingEventHandler.Mapping;
 
 /**
- * A {@link MCRMODSXMappingClassificationMapper} is a {@link Mapper} that looks for mapping
- * information for a given list of classifications, by checking if XPaths configured for categories of such
- * classifications match the MODS document.
+ * A {@link MCRMODSXMappingClassificationMapper} is a {@link Mapper} that looks for mapping information
+ * for a given list of classifications, by checking if XPaths configured for categories of such classifications
+ * match the MODS document.
  * <p>
  * For each classification, the set of classification categories containing a <code>x-mapping-xpath</code> label is
  * obtained. For all such classification categories, the corresponding XPath is evaluated and if the MODS document
@@ -65,7 +59,7 @@ import org.mycore.mods.classification.MCRMODSClassificationMappingEventHandler.M
  * <p>
  * The following configuration options are available:
  * <ul>
- * <li> The property suffix {@link MCRMODSXPathClassificationMapper#CLASSIFICATION_IDS_KEY} can be used to
+ * <li> The property suffix {@link MCRXPathClassificationMapperBase#CLASSIFICATION_IDS_KEY} can be used to
  * specify the comma separated list of classifications IDs.
  * </ul>
  * Example:
@@ -75,78 +69,28 @@ import org.mycore.mods.classification.MCRMODSClassificationMappingEventHandler.M
  * </code></pre>
  */
 @MCRConfigurationProxy(proxyClass = MCRMODSXPathClassificationMapper.Factory.class)
-public class MCRMODSXPathClassificationMapper implements Mapper {
-
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    private static final String LABEL_LANG_XPATH_MAPPING = "x-mapping-xpath";
-
-    private static final String LABEL_LANG_XPATH_MAPPING_FALLBACK = "x-mapping-xpathfb";
-
-    public static final String CLASSIFICATION_IDS_KEY = "ClassificationIds";
-
-    private final List<String> classificationsIds;
+public class MCRMODSXPathClassificationMapper
+    extends MCRXPathClassificationMapperBase<MCRMODSWrapper, Mapping>
+    implements Mapper {
 
     public MCRMODSXPathClassificationMapper(String... classificationsIds) {
         this(Arrays.asList(Objects.requireNonNull(classificationsIds, "Classification IDs must not be null")));
     }
 
     public MCRMODSXPathClassificationMapper(List<String> classificationsIds) {
-        this.classificationsIds = new ArrayList<>(Objects
-                .requireNonNull(classificationsIds, "Classification IDs must not be null"));
-        this.classificationsIds.forEach(filter -> Objects
-                .requireNonNull(filter, "Classification ID must not be null"));
+        super(classificationsIds);
     }
 
     @Override
-    public List<Mapping> findMappings(MCRCategoryDAO dao, MCRMODSWrapper wrapper) {
-        Element modsElement = wrapper.getMODS();
-        return classificationsIds.stream()
-                .flatMap(xPathClassificationId -> findMappings(dao, modsElement, xPathClassificationId))
-                .peek(XPathMapping::logInfo)
-                .map(XPathMapping::toMapping)
-                .toList();
+    protected Parent toParent(MCRCategoryDAO dao, MCRMODSWrapper modsWrapper) {
+        return modsWrapper.getMODS();
     }
 
-    private Stream<XPathMapping> findMappings(MCRCategoryDAO dao, Element modsElement, String classificationId) {
-        List<XPathMapping> mappings = findMappings(dao, modsElement, classificationId, LABEL_LANG_XPATH_MAPPING);
-        if (!mappings.isEmpty()) {
-            return mappings.stream();
-        }
-        return findMappings(dao, modsElement, classificationId, LABEL_LANG_XPATH_MAPPING_FALLBACK).stream();
-    }
-
-    private List<XPathMapping> findMappings(MCRCategoryDAO dao, Element modsElement,
-                                            String xPathClassificationId, String xLanguage) {
-        List<XPathMapping> mappings = new ArrayList<>();
-        dao.getCategoriesByClassAndLang(xPathClassificationId, xLanguage).forEach(category -> {
-            assert category.getLabel(xLanguage).isPresent();
-            String xPath = category.getLabel(xLanguage).get().getText();
-            xPath = MCRClassificationMappingUtil.replacePattern(xPath);
-            MCRXPathEvaluator evaluator = new MCRXPathEvaluator(new HashMap<>(), modsElement);
-            if (evaluator.test(xPath)) {
-                mappings.add(new XPathMapping(xLanguage, category.getId()));
-            }
-        });
-        return mappings;
-    }
-
-    private record XPathMapping(String xLanguage, MCRCategoryID targetCategoryId) {
-
-        private void logInfo() {
-            if (LOGGER.isInfoEnabled()) {
-                LOGGER.info("found x-path-mapping to {} for {}", targetCategoryId.toString(), xLanguage);
-            }
-        }
-
-        private Mapping toMapping() {
-            return new Mapping(getGenerator(), targetCategoryId);
-        }
-
-        private String getGenerator() {
-            return String.format(Locale.ROOT, "xpathmapping2%s", targetCategoryId.getRootID());
-        }
-
+    @Override
+    protected Mapping toMappedValue(XPathMapping mapping) {
+        MCRCategoryID targetCategoryId = mapping.targetCategoryId();
+        String generator = String.format(Locale.ROOT, "xpathmapping2%s", targetCategoryId.getRootID());
+        return new Mapping(generator, targetCategoryId);
     }
 
     public static class Factory implements Supplier<MCRMODSXPathClassificationMapper> {
