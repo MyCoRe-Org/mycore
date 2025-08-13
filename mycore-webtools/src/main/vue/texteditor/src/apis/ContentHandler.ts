03630dc7c26db05ff74c2ebe64ab276ff9329885
Matthias Eichner <matthias.eichner@uni-jena.de>
2025-08-13 18:14:46 +0200

    MCR-3489 text editor locking

--- mycore-webtools/src/main/vue/texteditor/src/apis/ContentHandler.ts
+++ mycore-webtools/src/main/vue/texteditor/src/apis/ContentHandler.ts
@@ -1,19 +1,25 @@
 interface ContentHandler {
 
-  load(id: string): Promise<Content>;
+  load(path: string): Promise<Content>;
 
-  save(id: string, content: Content): Promise<void>;
+  save(path: string, content: Content): Promise<void>;
 
-  hasWriteAccess(id: string): Promise<boolean>
+  hasWriteAccess(path: string): Promise<boolean>
 
   /**
    * Determines if the content is changed by the server after a save. This happens for example if a mycore object
    * is saved. Their modifyDate will be updated by the server. If a object is dirty after a save the content should
    * be reloaded or updated in the ui.
    *
-   * @param id the id to check
+   * @param path the path to check
    */
-  dirtyAfterSave(id: string): boolean
+  dirtyAfterSave(path: string): boolean
+
+  supportsLocking(path: string): boolean
+
+  lock(path: string): Promise<LockResult>
+
+  unlock(path: string): Promise<LockResult>
 
 }
 
@@ -22,5 +28,11 @@ interface Content {
   data: string;
 
   type: string;
+
+}
+
+interface LockResult {
+
+  status: "locked" | "unlocked" | "not_owner";
 
 }
