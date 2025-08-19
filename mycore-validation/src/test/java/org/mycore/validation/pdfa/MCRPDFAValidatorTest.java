77ded0e9ff23841c922ee08291aa8cfb9e22fb41
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2025-08-19 15:29:24 +0200

    MCR-3407 migrate tests in mycore-validation to JUnit 5

--- mycore-validation/src/test/java/org/mycore/validation/pdfa/MCRPDFAValidatorTest.java
+++ mycore-validation/src/test/java/org/mycore/validation/pdfa/MCRPDFAValidatorTest.java
@@ -18,20 +18,22 @@
 
 package org.mycore.validation.pdfa;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MCRPDFAValidatorTest {
 
     @Test
     public void isCompliant() throws Exception {
         MCRPDFAValidator validator = new MCRPDFAValidator();
-        Assert.assertFalse("shouldn't be a valid pdf/a document", isCompliant(validator, "/noPdfA.pdf"));
-        Assert.assertTrue("should be a valid pdf/a document", isCompliant(validator, "/pdfA-1b.pdf"));
-        Assert.assertTrue("should be a valid pdf/a document", isCompliant(validator, "/pdfA-2b.pdf"));
-        Assert.assertTrue("should be a valid pdf/a document", isCompliant(validator, "/pdfA-3b.pdf"));
+        assertFalse(isCompliant(validator, "/noPdfA.pdf"), "shouldn't be a valid pdf/a document");
+        assertTrue(isCompliant(validator, "/pdfA-1b.pdf"), "should be a valid pdf/a document");
+        assertTrue(isCompliant(validator, "/pdfA-2b.pdf"), "should be a valid pdf/a document");
+        assertTrue(isCompliant(validator, "/pdfA-3b.pdf"), "should be a valid pdf/a document");
     }
 
     private static boolean isCompliant(MCRPDFAValidator validator, String resourceName)
