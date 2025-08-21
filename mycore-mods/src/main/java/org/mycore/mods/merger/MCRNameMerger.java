35666416200f39a0154ee3e1013112ece57eece7
Eva Roddeck <eva.roddeck@uni-due.de>
2025-08-21 12:18:25 +0200

    MCR-3441 revert changes to MCRNameMerger, will be implemented later

--- mycore-mods/src/main/java/org/mycore/mods/merger/MCRNameMerger.java
+++ mycore-mods/src/main/java/org/mycore/mods/merger/MCRNameMerger.java
@@ -168,23 +168,6 @@ public class MCRNameMerger extends MCRMerger {
         return text.trim();
     }
 
-    /**
-     * Checks if this merger has an alternativeName-element that is
-     * {@link MCRNameMerger#isProbablySameAs(MCRMerger) probably the same as} the other given merger.
-     * @param other the other merger
-     * @return returns true if the other merger is also a {@link MCRNameMerger} and if this merger has
-     * an alternative name that matches the other
-     */
-    public boolean hasAlternativeNameSameAs(MCRMerger other) {
-        if (!(other instanceof MCRNameMerger)) {
-            return false;
-        }
-        return this.element.getChildren(ALTERNATIVE_NAME, MCRConstants.MODS_NAMESPACE)
-            .stream()
-            .map(MCRMergerFactory::buildFrom)
-            .anyMatch(altMerger -> altMerger.isProbablySameAs(other));
-    }
-
     @Override
     public boolean isProbablySameAs(MCRMerger e) {
         if (!(e instanceof MCRNameMerger other)) {
@@ -195,20 +178,16 @@ public class MCRNameMerger extends MCRMerger {
             return false;
         } else if (this.allNames.equals(other.allNames)) {
             return true;
-        } else if (Objects.equals(familyName, other.familyName)) {
-            if (initials.isEmpty() && other.initials.isEmpty()) {
-                return true; // same family name, no given name, no initals, then assumed same
-            } else if (!haveAtLeastOneCommon(this.initials, other.initials)) {
-                return false;
-            } else if (this.givenNames.isEmpty() || other.givenNames.isEmpty()) {
-                return true;
-            } else {
-                return haveAtLeastOneCommon(this.givenNames, other.givenNames);
-            }
+        } else if (!Objects.equals(familyName, other.familyName)) {
+            return false;
+        } else if (initials.isEmpty() && other.initials.isEmpty()) {
+            return true; // same family name, no given name, no initals, then assumed same
+        } else if (!haveAtLeastOneCommon(this.initials, other.initials)) {
+            return false;
+        } else if (this.givenNames.isEmpty() || other.givenNames.isEmpty()) {
+            return true;
         } else {
-            // compound name with same given names assumes same
-            return this.givenNames.equals(other.givenNames) &&
-                (this.familyName.contains(other.familyName) || other.familyName.contains(this.familyName));
+            return haveAtLeastOneCommon(this.givenNames, other.givenNames);
         }
     }
 
@@ -272,49 +251,6 @@ public class MCRNameMerger extends MCRMerger {
         }
 
         return sb.toString();
-    }
-
-    /**
-     * Merges the contents of the element wrapped by the other merger into a new alternativeName element
-     * in the element wrapped by this merger. Should only be called if this.isProbablySameAs(other).
-     * The alternative name is only added if the two names are not exactly the same and if the
-     * alternative name doesn't yet exist in the element wrapped by this merger.
-     * Only the family name and given names are merged into the alternativeName element.
-     * @param e the other merger
-     */
-    public void mergeAsAlternativeName(MCRMerger e) {
-        if (!(e instanceof MCRNameMerger other)) {
-            return;
-        }
-        if (this.allNames.equals(other.allNames)) {
-            return;
-        }
-        if (this.hasAlternativeNameSameAs(e)) {
-            return;
-        }
-        Element alternativeName = new Element(ALTERNATIVE_NAME, MCRConstants.MODS_NAMESPACE);
-
-        other.element.getChildren(NAME_PART, MCRConstants.MODS_NAMESPACE)
-            .stream()
-            .filter(namePart -> "given".equals(namePart.getAttributeValue(TYPE)))
-            .forEach(namePart -> {
-                Element altGivenName = new Element(NAME_PART, MCRConstants.MODS_NAMESPACE)
-                    .setAttribute(TYPE, "given");
-                altGivenName.addContent(namePart.getText());
-                alternativeName.addContent(altGivenName);
-            });
-
-        Element altFamilyName = new Element(NAME_PART, MCRConstants.MODS_NAMESPACE)
-            .setAttribute(TYPE, "family");
-        Element familyName = other.element.getChildren(NAME_PART, MCRConstants.MODS_NAMESPACE).stream()
-            .filter(namePart -> "family".equals(namePart.getAttributeValue(TYPE)))
-            .findFirst()
-            .orElse(null);
-        altFamilyName.addContent(familyName != null ? familyName.getText() : null);
-        alternativeName.addContent(altFamilyName);
-
-        this.element.addContent(alternativeName);
-
     }
 
     @Override
