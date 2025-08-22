8356404c659b8ca6f66c1b7c02c9015bf7ffb7de
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2025-08-22 15:31:20 +0200

    MCR-3398 migrate tests in mycore-mets to JUnit 5, except integration tests

--- mycore-mets/src/test/java/org/mycore/mets/model/converter/MCRJSONSimpleModelConverterTest.java
+++ mycore-mets/src/test/java/org/mycore/mets/model/converter/MCRJSONSimpleModelConverterTest.java
@@ -18,12 +18,13 @@
 
 package org.mycore.mets.model.converter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mycore.mets.model.simple.MCRMetsLink;
 import org.mycore.mets.model.simple.MCRMetsSection;
 import org.mycore.mets.model.simple.MCRMetsSimpleModel;
@@ -32,7 +33,7 @@ public class MCRJSONSimpleModelConverterTest {
 
     private String json;
 
-    @Before
+    @BeforeEach
     public void buildJson() throws IOException {
         json = MCRMetsTestUtil.readJsonFile("text-json-1.json");
     }
@@ -45,21 +46,24 @@ public class MCRJSONSimpleModelConverterTest {
         MCRMetsSection s1RootSection = metsSimpleModel.getRootSection();
         MCRMetsSection s2RootSection = compareSimpleModel.getRootSection();
 
-        Assert.assertEquals("labels of root must be the same", s1RootSection.getLabel(), s2RootSection.getLabel());
-        Assert.assertEquals("types of root must be the same", s1RootSection.getType(), s2RootSection.getType());
+        assertEquals(s1RootSection.getLabel(), s2RootSection.getLabel(),
+            "labels of root must be the same");
+        assertEquals(s1RootSection.getType(), s2RootSection.getType(),
+            "types of root must be the same");
 
-        Assert.assertEquals("page count must be the same", metsSimpleModel.getMetsPageList().size(),
-            compareSimpleModel.getMetsPageList().size());
+        assertEquals(metsSimpleModel.getMetsPageList().size(), compareSimpleModel.getMetsPageList().size(),
+            "page count must be the same");
 
         List<MCRMetsLink> s1SectionPageLinkList = metsSimpleModel.getSectionPageLinkList();
         List<MCRMetsLink> s2SectionPageLinkList = compareSimpleModel.getSectionPageLinkList();
 
         for (int n = 0; n < 3; n++) {
-            Assert.assertEquals("from of " + n + " link must be the same",
-                s1SectionPageLinkList.get(n).getFrom().getLabel(), s2SectionPageLinkList.get(n).getFrom().getLabel());
-            Assert.assertEquals("to of " + n + " link must be the same",
-                s1SectionPageLinkList.get(n).getTo().getOrderLabel(),
-                s2SectionPageLinkList.get(n).getTo().getOrderLabel());
+            assertEquals(s1SectionPageLinkList.get(n).getFrom().getLabel(),
+                s2SectionPageLinkList.get(n).getFrom().getLabel(),
+                "from of " + n + " link must be the same");
+            assertEquals(s1SectionPageLinkList.get(n).getTo().getOrderLabel(),
+                s2SectionPageLinkList.get(n).getTo().getOrderLabel(),
+                "to of " + n + " link must be the same");
         }
     }
 }
