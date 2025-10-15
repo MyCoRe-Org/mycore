b4f05484e98f784761e8b257facdb04873e0c656
Matthias Eichner <matthias.eichner@uni-jena.de>
2025-10-15 14:37:23 +0200

    add dev mode url calc again

--- mycore-webtools/src/main/vue/processing/src/App.vue
+++ mycore-webtools/src/main/vue/processing/src/App.vue
@@ -21,8 +21,12 @@ let registry = ref(new Registry());
 connect();
 
 function getSocketURL() {
+  if (import.meta.env.DEV) {
+    // in dev mode connect to a local mir
+    return "ws://localhost:8291/mir/ws/mycore-webtools/processing";
+  }
   const wsProtocol = location.protocol === "https:" ? "wss://" : "ws://";
-  const { pathname } = new URL(mycore.webApplicationBaseURL);
+  const {pathname} = new URL(mycore.webApplicationBaseURL);
   const basePath = pathname === "/" ? "" : pathname.replace(/\/$/, "");
   return `${wsProtocol}${location.host}${basePath}/ws/mycore-webtools/processing`;
 }
