1f8ab5b12a431f54dd54877272ef34cf5c984b8c
Sebastian Hofmann <7668803+sebhofmann@users.noreply.github.com>
2025-10-16 10:40:48 +0200

    MCR-3530 MCRTikaManager getMapper is not optional (#2702)
    * MCR-3530 MCRTikaManager getMapper is not optional
    
    * MCR-3530 fix MCRTikaManager javadoc

--- mycore-solr/src/main/java/org/mycore/solr/index/file/tika/MCRTikaManager.java
+++ mycore-solr/src/main/java/org/mycore/solr/index/file/tika/MCRTikaManager.java
@@ -55,7 +55,7 @@ public final class MCRTikaManager {
     }
 
     /**
-     * Returns the Tika mapper for the given key. If no mapper is defined for the key, the default mapper is returned.
+     * Returns the Tika mapper for the given key. If no mapper is defined for the key, an empty Optional is returned.
      *
      * @param key The key for which the mapper should be returned
      * @return The Tika mapper for the given key
@@ -68,7 +68,9 @@ public final class MCRTikaManager {
                 if (instance == null) {
                     instance = MCRConfiguration2.getInstanceOf(MCRTikaMapper.class,
                         SOLR_CONFIG_PREFIX + "Tika.Mapper." + key + ".Class").orElse(null);
-                    mapperCache.put(key, instance);
+                    if (instance != null) {
+                        mapperCache.put(key, instance);
+                    }
                 }
             }
         }
