9e6a38e337f2fe76cb8785bd4a23d4728a469f80
Silvio Hermann <s.hermann@uni-jena.de>
2025-11-24 13:26:03 +0100

    MCR-3588 Added MCRCSLXMLOutputFormat

---
+++ mycore-csl/src/main/java/org/mycore/csl/MCRCSLXMLOutputFormat.java
@@ -0,0 +1,35 @@
+package org.mycore.csl;
+
+import de.undercouch.citeproc.csl.internal.SBibliography;
+import de.undercouch.citeproc.csl.internal.format.HtmlFormat;
+import de.undercouch.citeproc.output.Bibliography;
+import de.undercouch.citeproc.output.SecondFieldAlign;
+
+/**
+ * Class to enable XML output in the {@link de.undercouch.citeproc.CSL} processor.
+ * */
+public class MCRCSLXMLOutputFormat extends HtmlFormat {
+
+    @Override
+    public Bibliography makeBibliography(String[] entries, SBibliography bibliographyElement) {
+        SecondFieldAlign sfa = bibliographyElement.getSecondFieldAlign();
+        return new Bibliography(entries, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<div class=\"csl-bib-body\">\n",
+            "</div>",
+            null, null, null, null, null, null, sfa);
+    }
+
+    @Override
+    protected String escape(String str) {
+        return str
+            .replaceAll("&", "&amp;")
+            .replaceAll("<", "&lt;")
+            .replaceAll(">", "&gt;")
+            .replaceAll("\"", "&quot;")
+            .replaceAll("'", "&apos;");
+    }
+
+    @Override
+    public String getName() {
+        return "xml";
+    }
+}
