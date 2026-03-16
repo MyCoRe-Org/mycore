0be5b0d414b636274e3aed55b046c12a1193f3dd
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2026-03-16 15:32:15 +0100

    MCR-3650 refactor MCRConfigurableInstanceHelper into multiple classes

---
+++ mycore-base/src/main/java/org/mycore/common/config/instantiator/source/MCRSource.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See https://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.common.config.instantiator.source;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import org.mycore.common.config.MCRInstanceConfiguration;
+import org.mycore.common.config.instantiator.target.MCRTarget;
+import org.mycore.common.config.instantiator.target.MCRTargetType;
+
+/**
+ * Common abstraction for components of a class (i.e., a {@link Field} or a {@link Method}) that can,
+ * in principle, be used for injection. It provides an abstraction for obtaining a value based on present annotations.
+ */
+public interface MCRSource {
+
+    MCRSourceType type();
+
+    Class<? extends Annotation> annotationClass();
+
+    int order();
+
+    Set<MCRTargetType> allowedTargetTypes();
+
+    Class<?> valueClass();
+
+    Object get(MCRInstanceConfiguration configuration, MCRTarget target);
+
+}
