806e2fec7f72b30d7e89f4998899541a866b89e2
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2026-03-18 17:57:56 +0100

    MCR-3645 fixes from review
    - fix indentation

--- mycore-solr/src/main/java/org/mycore/solr/MCRSolrDefaultPropertyConstants.java
+++ mycore-solr/src/main/java/org/mycore/solr/MCRSolrDefaultPropertyConstants.java
@@ -20,18 +20,18 @@ package org.mycore.solr;
 
 public class MCRSolrDefaultPropertyConstants {
 
-  public static final String USE_JETTY_HTTP_CLIENT = "MCR.Solr.Default.UseJettyHttpClient";
+    public static final String USE_JETTY_HTTP_CLIENT = "MCR.Solr.Default.UseJettyHttpClient";
 
-  public static final String CLIENT_IDLE_TIMEOUT = "MCR.Solr.Default.Client.IdleTimeout";
-  public static final String CLIENT_IDLE_TIMEOUT_UNIT = "MCR.Solr.Default.Client.IdleTimeout.Unit";
+    public static final String CLIENT_IDLE_TIMEOUT = "MCR.Solr.Default.Client.IdleTimeout";
+    public static final String CLIENT_IDLE_TIMEOUT_UNIT = "MCR.Solr.Default.Client.IdleTimeout.Unit";
 
-  public static final String CLIENT_CONNECTION_TIMEOUT = "MCR.Solr.Default.Client.ConnectionTimeout";
-  public static final String CLIENT_CONNECTION_TIMEOUT_UNIT = "MCR.Solr.Default.Client.ConnectionTimeout.Unit";
+    public static final String CLIENT_CONNECTION_TIMEOUT = "MCR.Solr.Default.Client.ConnectionTimeout";
+    public static final String CLIENT_CONNECTION_TIMEOUT_UNIT = "MCR.Solr.Default.Client.ConnectionTimeout.Unit";
 
-  public static final String CLIENT_REQUEST_TIMEOUT = "MCR.Solr.Default.Client.RequestTimeout";
-  public static final String CLIENT_REQUEST_TIMEOUT_UNIT = "MCR.Solr.Default.Client.RequestTimeout.Unit";
+    public static final String CLIENT_REQUEST_TIMEOUT = "MCR.Solr.Default.Client.RequestTimeout";
+    public static final String CLIENT_REQUEST_TIMEOUT_UNIT = "MCR.Solr.Default.Client.RequestTimeout.Unit";
 
-  public static final String DEFAULT_SHARD_COUNT = "MCR.Solr.Default.ShardCount";
-  public static final String DEFAULT_REPLICA_COUNT = "MCR.Solr.Default.ReplicaCount";
+    public static final String DEFAULT_SHARD_COUNT = "MCR.Solr.Default.ShardCount";
+    public static final String DEFAULT_REPLICA_COUNT = "MCR.Solr.Default.ReplicaCount";
 
 }
