703ac6555861a21fcdda77112a69b95b154e5564
Lennard Golsch <lennard.golsch@icloud.com>
2026-05-18 17:59:33 +0200

    refactor: rename MCRXslStyleResolver to MCRXSLStyleResolver

--- mycore-base/src/main/java/org/mycore/common/xsl/uriresolver/MCRXSLStyleResolver.java
+++ mycore-base/src/main/java/org/mycore/common/xsl/uriresolver/MCRXSLStyleResolver.java
@@ -46,7 +46,7 @@ import org.mycore.common.xsl.MCRXSLResourceHelper;
  * xslStyle:&lt;stylesheet&gt;&lt;,&lt;stylesheet&gt;&gt;&lt;?param1=value1&amp;param2=value2&gt;&lt;#flavor&gt;:&lt;anyMyCoReURI&gt;
  * </pre>
  */
-public class MCRXslStyleResolver implements URIResolver {
+public class MCRXSLStyleResolver implements URIResolver {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -58,7 +58,7 @@ public class MCRXslStyleResolver implements URIResolver {
 
     private final Map<String, Flavor> flavors;
 
-    public MCRXslStyleResolver() {
+    public MCRXSLStyleResolver() {
         Class<? extends TransformerFactory> defaultFactoryClass = MCRConfiguration2
             .<TransformerFactory>getClass("MCR.LayoutService.TransformerFactoryClass")
             .orElseGet(TransformerFactory.newInstance()::getClass);
@@ -91,29 +91,6 @@ public class MCRXslStyleResolver implements URIResolver {
             .collect(Collectors.toSet());
     }
 
-    /**
-     * Resolves the target URI and transforms its content using the specified XSL stylesheets.
-     * <p>Multiple stylesheets are applied in order. Optional query parameters are passed to the
-     * transformer. An optional flavor selects the transformer factory and XSL folder to use;
-     * if omitted, the default flavor is used. The flavor can also be passed as the
-     * {@code xslStyleFlavor} query parameter.
-     * <p>URI Syntax:
-     * <pre>
-     *   &lt;scheme&gt;:{stylesheet}[,{stylesheet}...][?param=value...][#{flavor}]:{anyMCRUri}
-     * </pre>
-     * <p>Example request:
-     * <pre>
-     *   xslStyle:my-stylesheet?lang=de:mcrobject:mcr_document_00000001
-     *   xslStyle:sheet1,sheet2?lang=de#xslt3:mcrobject:mcr_document_00000001
-     * </pre>
-     * <p>Example response: the transformed XML content produced by the stylesheets.
-     *
-     * @param href the URI in the syntax above to resolve
-     * @param base the base URI of the calling stylesheet, passed through to the delegated resolver
-     * @return a {@link Source} wrapping the transformed content
-     * @throws TransformerException if the target URI cannot be resolved or the transformation fails
-     * @throws MCRUsageException if the target URI is missing or an unknown flavor is specified
-     */
     @Override
     public Source resolve(String href, String base) throws TransformerException {
         String help = href.substring(href.indexOf(':') + 1);
