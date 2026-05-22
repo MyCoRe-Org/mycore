1700edd7c6af65419d19db8ee347d8f570ff6b47
Torsten Krause <toKrause@users.noreply.github.com>
2026-05-22 15:03:46 +0200

    MCR-3669 do not remove empty names when non-empty suffix is used (#2923)

--- mycore-base/src/main/java/org/mycore/common/config/instantiator/MCRInstanceName.java
+++ mycore-base/src/main/java/org/mycore/common/config/instantiator/MCRInstanceName.java
@@ -19,7 +19,6 @@
 package org.mycore.common.config.instantiator;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -107,11 +106,11 @@ public final class MCRInstanceName {
 
     public enum Suffix {
 
-        NONE(null, Collections.emptyList()),
+        NONE(null, List.of("")),
 
-        UPPER_CASE("Class", Arrays.asList("Class", "class")),
+        UPPER_CASE("Class", List.of("Class", "class")),
 
-        LOWER_CASE("class", Arrays.asList("Class", "class"));
+        LOWER_CASE("class", List.of("Class", "class"));
 
         private static final Suffix[] REPRESENTED_VALUES = Arrays.stream(values())
             .filter(suffix -> suffix.representation != null)
