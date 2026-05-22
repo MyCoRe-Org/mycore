333f83ea2ed3ee4e054ac5ff174ccf66ba9be29f
erodde <155449327+erodde@users.noreply.github.com>
2026-05-22 17:31:56 +0200

    MCR-3578 expand MCRNameMerger for modsperson (#2774)
    * MCR-3441 expand MCRNameMerger
    
    * MCR-3441 fixed comment
    
    * MCR-3441 checkstyle
    
    * MCR-3441 MCRNameMerger new extending class
    
    * MCR-3441 MCRNameMerger fix unit test
    
    * MCR-3441 MCRNameMerger remove additional methods from MCRNameMerger
    
    * MCR-3441 MCRNameMerger: fix pmd

---
+++ mycore-mods/src/test/java/org/mycore/mods/merger/MCRModspersonNameMergerTest.java
@@ -0,0 +1,145 @@
+package org.mycore.mods.merger;
+
+import org.jaxen.JaxenException;
+import org.jdom2.Element;
+import org.junit.jupiter.api.Test;
+import org.mycore.common.MCRConstants;
+import org.mycore.common.xml.MCRNodeBuilder;
+import org.mycore.test.MyCoReTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@MyCoReTest
+public class MCRModspersonNameMergerTest {
+
+    @Test
+    public void testIsProbablySameAs() throws Exception {
+        MCRNameMerger a = buildNameEntry("[mods:namePart='Thomas Müller']");
+        MCRNameMerger b = buildNameEntry("[mods:namePart='thomas Mueller']");
+        assertTrue(a.isProbablySameAs(b));
+
+        MCRNameMerger c = buildNameEntry("[mods:namePart='Muller, T.']");
+        assertTrue(a.isProbablySameAs(c));
+
+        MCRNameMerger d = buildNameEntry("[mods:namePart='Mueller, T']");
+        assertTrue(a.isProbablySameAs(d));
+
+        MCRNameMerger e = buildNameEntry("[mods:namePart='Müller, Egon']");
+        assertFalse(a.isProbablySameAs(e));
+
+        MCRNameMerger f = buildNameEntry("[mods:namePart='Thorsten Mueller']");
+        assertTrue(c.isProbablySameAs(f));
+        assertFalse(a.isProbablySameAs(f));
+
+        MCRNameMerger g = buildNameEntry("[mods:namePart='Thorsten Egon Mueller']");
+        assertTrue(e.isProbablySameAs(g));
+        assertTrue(f.isProbablySameAs(g));
+
+        MCRNameMerger h = buildNameEntry(
+            "[mods:namePart[@type='given']='Thomas'][mods:namePart[@type='family']='Müller']");
+        assertTrue(h.isProbablySameAs(a));
+        assertTrue(h.isProbablySameAs(d));
+
+        MCRNameMerger i = buildNameEntry("[mods:namePart[@type='given']='T.'][mods:namePart[@type='family']='Müller']"
+            + "[mods:namePart[@type='termsOfAddress']='Jun.']");
+        assertTrue(i.isProbablySameAs(h));
+        assertTrue(i.isProbablySameAs(a));
+        assertTrue(i.isProbablySameAs(d));
+
+        MCRNameMerger j = buildNameEntry("[mods:namePart='Thorsten Müller-Doppelname']");
+        assertFalse(j.isProbablySameAs(a));
+        assertFalse(j.isProbablySameAs(g));
+        assertFalse(j.isProbablySameAs(i));
+        assertTrue(j.isProbablySameAs(f));
+
+        MCRNameMerger k = buildNameEntry("[mods:namePart='Thomas Müller-Doppelname']");
+        assertTrue(k.isProbablySameAs(a));
+        assertTrue(k.isProbablySameAs(b));
+        assertTrue(k.isProbablySameAs(h));
+        assertFalse(k.isProbablySameAs(c));
+        assertFalse(k.isProbablySameAs(d));
+        assertFalse(k.isProbablySameAs(i));
+
+        assertThrows(NullPointerException.class, () -> new MCRModspersonNameMerger().setElement(null));
+    }
+
+    @Test
+    public void testHasAlternativeNameSameAs() throws JaxenException {
+        Element modsNameElement = new MCRNodeBuilder()
+            .buildElement("mods:name[@type='personal'][mods:namePart[@type='given']='Thomas']"
+                + "[mods:namePart[@type='family']='Müller']", null, null);
+
+        Element altNameElement = buildAlternativeNameElement("Thomas", "Meyer");
+        modsNameElement.addContent(altNameElement);
+
+        MCRModspersonNameMerger a = new MCRModspersonNameMerger();
+        a.setElement(modsNameElement);
+        MCRModspersonNameMerger b = buildNameEntry(
+            "[mods:namePart[@type='given']='Thomas'][mods:namePart[@type='family']='Meyer']");
+        MCRModspersonNameMerger c = buildNameEntry(
+            "[mods:namePart[@type='given']='T'][mods:namePart[@type='family']='Meyer']");
+        MCRModspersonNameMerger d = buildNameEntry(
+            "[mods:namePart[@type='given']='Thomas'][mods:namePart[@type='family']='Mayer']");
+
+        assertTrue(a.hasAlternativeNameSameAs(b));
+        assertTrue(a.hasAlternativeNameSameAs(c));
+        assertFalse(a.hasAlternativeNameSameAs(d));
+        assertFalse(b.hasAlternativeNameSameAs(c));
+    }
+
+    @Test
+    public void testMergeAsAlternativeName() throws JaxenException {
+        MCRModspersonNameMerger a = buildNameEntry(
+            "[mods:namePart[@type='given']='Thomas'][mods:namePart[@type='family']='Müller']");
+        MCRModspersonNameMerger b = buildNameEntry(
+            "[mods:namePart[@type='given']='Thomas'][mods:namePart[@type='family']='Meyer']");
+
+        a.mergeAsAlternativeName(b);
+
+        Element mergedModsName = a.element;
+
+        List<Element> nameParts = mergedModsName.getChildren("namePart", MCRConstants.MODS_NAMESPACE);
+        assertEquals(2, nameParts.size());
+
+        assertEquals("Thomas", nameParts.getFirst().getText());
+        assertEquals("Müller", nameParts.get(1).getText());
+
+        List<Element> alternativeNames = mergedModsName.getChildren("alternativeName", MCRConstants.MODS_NAMESPACE);
+        assertEquals(1, alternativeNames.size());
+        Element alternativeName = alternativeNames.getFirst();
+        nameParts = alternativeName.getChildren("namePart", MCRConstants.MODS_NAMESPACE);
+        assertEquals(2, nameParts.size());
+
+        assertEquals("Thomas", nameParts.getFirst().getText());
+        assertEquals("Meyer", nameParts.get(1).getText());
+    }
+
+    private MCRModspersonNameMerger buildNameEntry(String predicates) throws JaxenException {
+        Element modsName = new MCRNodeBuilder().buildElement("mods:name[@type='personal']" + predicates, null, null);
+        MCRModspersonNameMerger ne = new MCRModspersonNameMerger();
+        ne.setElement(modsName);
+        return ne;
+    }
+
+    private Element buildAlternativeNameElement(String givenName, String familyName) {
+        Element altNameElement = new Element("alternativeName", MCRConstants.MODS_NAMESPACE);
+
+        Element altFamilyNameElement = new Element("namePart", MCRConstants.MODS_NAMESPACE);
+        altFamilyNameElement.setAttribute("type", "family");
+        altFamilyNameElement.setText(familyName);
+
+        Element altGivenNameElement = new Element("namePart", MCRConstants.MODS_NAMESPACE);
+        altGivenNameElement.setAttribute("type", "given");
+        altGivenNameElement.setText(givenName);
+
+        altNameElement.setContent(Arrays.asList(altFamilyNameElement, altGivenNameElement));
+
+        return altNameElement;
+    }
+}
