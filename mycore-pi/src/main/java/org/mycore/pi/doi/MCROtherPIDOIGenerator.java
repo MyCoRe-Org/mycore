67351a48bb731d2d86c56262c9c7a90d80c190b3
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2026-06-10 12:40:21 +0200

    MCR-3722 improve support for entangled PIs

---
+++ mycore-pi/src/main/java/org/mycore/pi/doi/MCROtherPIDOIGenerator.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See https://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.pi.doi;
+
+import java.util.Optional;
+
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.config.annotation.MCRPostConstruction;
+import org.mycore.common.config.annotation.MCRProperty;
+import org.mycore.datamodel.metadata.MCRBase;
+import org.mycore.pi.MCRPIGenerator;
+import org.mycore.pi.MCRPersistentIdentifier;
+import org.mycore.pi.util.MCROtherPIValueExtractor;
+
+public class MCROtherPIDOIGenerator extends MCRPIGenerator<MCRDigitalObjectIdentifier> {
+
+    private final MCRDOIParser mcrdoiParser;
+
+    private MCROtherPIValueExtractor extractor;
+
+    private String prefix = MCRConfiguration2.getStringOrThrow("MCR.DOI.Prefix");
+
+    @MCRProperty(name = "Type")
+    public String type;
+
+    @MCRProperty(name = "Service")
+    public String service;
+
+    @MCRProperty(name = "Pattern")
+    public String pattern;
+
+    public MCROtherPIDOIGenerator() {
+        super();
+        mcrdoiParser = new MCRDOIParser();
+    }
+
+    @MCRPostConstruction
+    public void createExtractor() {
+        extractor = new MCROtherPIValueExtractor(type, service, pattern);
+    }
+
+    @Override
+    public MCRDigitalObjectIdentifier generate(MCRBase mcrObject, String additional) {
+        String suffix = extractor.extractValue(mcrObject.getId());
+        Optional<MCRDigitalObjectIdentifier> parse = mcrdoiParser.parse(prefix + "/" + suffix);
+        MCRPersistentIdentifier doi = parse.get();
+        return (MCRDigitalObjectIdentifier) doi;
+    }
+
+}
