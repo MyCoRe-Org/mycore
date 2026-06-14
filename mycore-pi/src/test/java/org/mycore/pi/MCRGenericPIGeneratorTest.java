dfb177b85b37ab55bdc594a5b3f2fb57dbe9feb8
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2026-06-14 19:55:53 +0200

    MCR-3760 ! externalize date formatting from generic PI generator

--- mycore-pi/src/test/java/org/mycore/pi/MCRGenericPIGeneratorTest.java
+++ mycore-pi/src/test/java/org/mycore/pi/MCRGenericPIGeneratorTest.java
@@ -22,10 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.jdom2.Element;
@@ -34,6 +31,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mycore.common.MCRTestConfiguration;
 import org.mycore.common.MCRTestProperty;
 import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.date.MCRMockDateFormatter;
+import org.mycore.common.date.MCRSimpleDateFormatter;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.pi.doi.MCRDigitalObjectIdentifier;
@@ -53,8 +52,14 @@ public class MCRGenericPIGeneratorTest {
 
     public static final String DATE_FORMAT = "ddMMyyyy";
 
-    public static final String GENERAL_PATTERN =
-        "urn:nbn:de:gbv:xyz:$CurrentDate-$1-$2-$ObjectType-$ObjectProject-$ObjectNumber-$Count-";
+    public static final String GENERAL_PATTERN = "urn:nbn:de:gbv:xyz:"
+        + "$CurrentDate-$1-$2-$ObjectType-$ObjectProject-$ObjectNumber-$Count-";
+
+    public static final String MOCK_DATE = "2025-11-05";
+
+    public static final String MY_PROJECT_MAPPING = "MY";
+
+    public static final String TEST_TYPE_MAPPING = "TEST";
 
     public static final String XPATH_1 = "/mycoreobject/metadata/test1/test2/text()";
 
@@ -80,19 +85,16 @@ public class MCRGenericPIGeneratorTest {
         MCRGenericPIGenerator generator = new MCRGenericPIGenerator(
             new MCRMockCounter(42),
             GENERAL_PATTERN,
-            DATE_FORMAT,
-            Map.of("my", "MY"),
-            Map.of("test", "TEST"),
+            new MCRMockDateFormatter(MOCK_DATE),
+            Map.of("my", MY_PROJECT_MAPPING),
+            Map.of("test", TEST_TYPE_MAPPING),
             3,
             MCRDNBURN.TYPE,
             List.of(XPATH_1, XPATH_2));
 
         String pi = generator.generate(object, "").asString();
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT, Locale.ROOT);
-        String currenDate = dateFormatter.format(new Date());
-
-        assertEquals("urn:nbn:de:gbv:xyz:" + currenDate + "-result1-result2-TEST-MY-00000123-042-",
+        assertEquals("urn:nbn:de:gbv:xyz:" + MOCK_DATE + "-result1-result2-TEST-MY-00000123-042-",
             pi.substring(0, pi.length() - 1));
 
     }
@@ -107,7 +109,7 @@ public class MCRGenericPIGeneratorTest {
         MCRGenericPIGenerator generator = new MCRGenericPIGenerator(
             new MCRMockCounter(42),
             "10.1234/$ObjectType-$Count",
-            DATE_FORMAT,
+            new MCRSimpleDateFormatter(DATE_FORMAT),
             Map.of(),
             Map.of(),
             -1,
@@ -136,11 +138,12 @@ public class MCRGenericPIGeneratorTest {
     @MCRTestConfiguration(properties = {
         @MCRTestProperty(key = "Test.Class", classNameOf = MCRGenericPIGenerator.class),
         @MCRTestProperty(key = "Test.GeneralPattern", string = GENERAL_PATTERN),
-        @MCRTestProperty(key = "Test.DateFormat", string = DATE_FORMAT),
-        @MCRTestProperty(key = "Test.ObjectProjectMapping.my", string = "MY"),
-        @MCRTestProperty(key = "Test.ObjectTypeMapping.test", string = "TEST"),
+        @MCRTestProperty(key = "Test.DateFormatter.Class", classNameOf = MCRMockDateFormatter.class),
+        @MCRTestProperty(key = "Test.DateFormatter.Value", string = MOCK_DATE),
+        @MCRTestProperty(key = "Test.ObjectProjectMapping.my", string = MY_PROJECT_MAPPING),
+        @MCRTestProperty(key = "Test.ObjectTypeMapping.test", string = TEST_TYPE_MAPPING),
         @MCRTestProperty(key = "Test.CountPrecision", string = "3"),
-        @MCRTestProperty(key = "Test.Type", string = "dnbUrn"),
+        @MCRTestProperty(key = "Test.Type", string = MCRDNBURN.TYPE),
         @MCRTestProperty(key = "Test.XPath.1", string = XPATH_1),
         @MCRTestProperty(key = "Test.XPath.2", string = XPATH_2),
     })
@@ -164,11 +167,41 @@ public class MCRGenericPIGeneratorTest {
 
         String pi = generator.generate(object, "").asString();
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT, Locale.ROOT);
-        String currenDate = dateFormatter.format(new Date());
-
-        assertEquals("urn:nbn:de:gbv:xyz:" + currenDate + "-result1-result2-TEST-MY-00000123-000-",
+        assertEquals("urn:nbn:de:gbv:xyz:" + MOCK_DATE + "-result1-result2-TEST-MY-00000123-000-",
             pi.substring(0, pi.length() - 1));
+
+    }
+
+    @Test
+    @MCRTestConfiguration(properties = {
+        @MCRTestProperty(key = "Test.Class", classNameOf = MCRGenericPIGenerator.class),
+        @MCRTestProperty(key = "Test.GeneralPattern", string = "10.1234/$ObjectDate-$ObjectNumber"),
+        @MCRTestProperty(key = "Test.DateFormat", string = DATE_FORMAT),
+        @MCRTestProperty(key = "Test.DateFormatter.Class", classNameOf = MCRMockDateFormatter.class),
+        @MCRTestProperty(key = "Test.DateFormatter.Value", string = MOCK_DATE),
+        @MCRTestProperty(key = "Test.Type", string = MCRDigitalObjectIdentifier.TYPE)
+    })
+    public void configurationDateFormatterPrecedence() throws MCRPersistentIdentifierException {
+
+        MCRObject object = new MCRObject();
+        object.setSchema("http://www.w3.org/2001/XMLSchema");
+        object.setId(MCRObjectID.getInstance("my_test_00000123"));
+
+        Element testElement = new Element("test1");
+        testElement.setAttribute("class", "MCRMetaXML");
+        testElement.addContent(new Element("test2").setText("result1"));
+        testElement.addContent(new Element("test3").setText("result2"));
+
+        Element metadata = new Element("metadata");
+        metadata.addContent(testElement);
+
+        object.getMetadata().setFromDOM(metadata);
+
+        MCRPIGenerator<?> generator = MCRConfiguration2.getInstanceOfOrThrow(MCRPIGenerator.class, "Test");
+
+        String pi = generator.generate(object, "").asString();
+
+        assertEquals("10.1234/" + MOCK_DATE + "-00000123", pi);
 
     }
 
